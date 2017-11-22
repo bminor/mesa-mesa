acidburn0zzz/chromiumos-mesa

CHROMIUM: i965: Disable hardware contexts for gen6
They don't seem to work, and cause regular GPU hangs, so let's disable
them.

BUG=chromium:288818
TEST=by hand: (along with the kernel patch) run multiple flash videos with hardware decode, no GPU hang happens

Signed-off-by: Dominik Behr <dbehr@chromium.org>
Signed-off-by: Prince Agyeman <prince.agyeman@intel.com>
Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
Signed-off-by: James Ausmus <james.ausmus@intel.com>
Signed-off-by: Tomasz Figa <tfiga@chromium.org>

src/mesa/drivers/dri/i965/brw_context.c
@@ -958,7 +958,7 @@ brwCreateContext(gl_api api,
 
    intel_batchbuffer_init(brw);
 
-   if (brw->gen >= 6) {
+   if (brw->gen >= 7) {
       /* Create a new hardware context.  Using a hardware context means that
        * our GPU state will be saved/restored on context switch, allowing us
        * to assume that the GPU is in the same state we left it in.

bminor/mesa-mesa

(applied manually from src/third_party/media-libs/mesa/files)
BUG=b:33533853
TEST=No CTS regressions on Cyan and Reef.
Change-Id: I374fd27f113c3399362f2ca40bdbbaf7574f5fae
Reviewed-on: https://chromium-review.googlesource.com/558125
Reviewed-by: Chad Versace <chadversary@chromium.org>
(cherry picked from commit f12a531c67e865e427f1f21c960e6c4a30ff45c8)
BUG=b:69553386
TEST=No regressions on Eve in `cts-tradefed run cts -m CtsDeqpTestCases`.
Change-Id: I0277438a011a9161f49d6bcbb57747d04a8e832d
Reviewed-on: https://chromium-review.googlesource.com/780788
Tested-by: Chad Versace <chadversary@chromium.org>
Commit-Queue: Chad Versace <chadversary@chromium.org>
Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

src/mesa/drivers/dri/i965/brw_context.c
@@ -942,7 +942,7 @@ brwCreateContext(gl_api api,
 
    intel_batchbuffer_init(brw);
 
-   if (devinfo->gen >= 6) {
+   if (devinfo->gen >= 7) {
       /* Create a new hardware context.  Using a hardware context means that
        * our GPU state will be saved/restored on context switch, allowing us
        * to assume that the GPU is in the same state we left it in.