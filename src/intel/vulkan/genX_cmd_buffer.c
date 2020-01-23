23f23f23cbcfbd7f1e9dfbf81424ee0cbb5e058b
Jason Ekstrand <jason@jlekstrand.net>
2020-01-22 22:37:10 -0600

    anv: Improve BTI change cache flushing
    This commit makes two changes:
    
     1. We set pending_pipe_bits instead of emitting PIPE_CONTROL directly
        for the flush at the end of cmd_buffer_begin_subpass.
    
     2. Because BLORP ops such as vkCmdClearAttachments may come in the
        middle of a render pass, we have to also flag the need for a cache
        flush after the blorp op.
    
    Fixes: 185630c6bc97 "anv/blorp: Do the gen11 BTI flush"
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/merge_requests/3547>
    (cherry picked from commit c70a786c77370bbc47f71a9f529d50116fd511da)
    
    Conflicts:
    	src/intel/vulkan/genX_cmd_buffer.c

--- src/intel/vulkan/genX_cmd_buffer.c
+++ src/intel/vulkan/genX_cmd_buffer.c
@@ -4548,6 +4548,20 @@ cmd_buffer_begin_subpass(struct anv_cmd_buffer *cmd_buffer,
    }
 
    cmd_buffer_emit_depth_stencil(cmd_buffer);
+
+#if GEN_GEN >= 11
+   /* The PIPE_CONTROL command description says:
+    *
+    *    "Whenever a Binding Table Index (BTI) used by a Render Taget Message
+    *     points to a different RENDER_SURFACE_STATE, SW must issue a Render
+    *     Target Cache Flush by enabling this bit. When render target flush
+    *     is set due to new association of BTI, PS Scoreboard Stall bit must
+    *     be set in this packet."
+    */
+   cmd_buffer->state.pending_pipe_bits |=
+      ANV_PIPE_RENDER_TARGET_CACHE_FLUSH_BIT |
+      ANV_PIPE_STALL_AT_SCOREBOARD_BIT;
+#endif
 }
 
 static enum blorp_filter
