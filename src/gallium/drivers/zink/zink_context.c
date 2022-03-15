4855a4d445a9d879c423f3fb439e08757bb9c668
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2022-03-15 18:24:28 -0400

    zink: flag sample locations for re-set on batch flush
    this needs to be re-set any time the cmdbuf changes
    
    cc: mesa-stable
    
    Tested-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Dave Airlie <airlied@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/15397>
    (cherry picked from commit efa724133f6e454b209837713ad4218ade6eb7d0)

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -2459,6 +2459,7 @@ flush_batch(struct zink_context *ctx, bool sync)
       ctx->oom_stall = false;
       ctx->dd->bindless_bound = false;
       ctx->di.bindless_refs_dirty = true;
+      ctx->sample_locations_changed = ctx->gfx_pipeline_state.sample_locations_enabled;
    }
 }
 
