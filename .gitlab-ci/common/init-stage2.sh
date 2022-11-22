joshuaswarren/mesa-1

CI: convert to use the new S3 server instead of the legacy minio
We don't need to login anymore, but we can't use plain minio commands
now. `ci-fairy` got a helper as `s3cp` to keep an almost identical
API.

Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19076>

.gitlab-ci/common/init-stage2.sh
@@ -150,8 +150,7 @@ cleanup
 # upload artifacts
 if [ -n "$MINIO_RESULTS_UPLOAD" ]; then
   tar --zstd -cf results.tar.zst results/;
-  ci-fairy minio login --token-file "${CI_JOB_JWT_FILE}";
-  ci-fairy minio cp results.tar.zst minio://"$MINIO_RESULTS_UPLOAD"/results.tar.zst;
+  ci-fairy s3cp --token-file "${CI_JOB_JWT_FILE}" results.tar.zst https://"$MINIO_RESULTS_UPLOAD"/results.tar.zst;
 fi
 
 # We still need to echo the hwci: mesa message, as some scripts rely on it, such

bminor/mesa-mesa

Solved Conflicts:
	.gitlab-ci.yml
	.gitlab-ci/common/init-stage2.sh
	.gitlab-ci/container/lava_build.sh
	.gitlab-ci/lava/lava-submit.sh
	.gitlab-ci/piglit/piglit-traces.sh
	.gitlab-ci/prepare-artifacts.sh
	.gitlab-ci/test/gitlab-ci.yml
	src/broadcom/ci/traces-broadcom.yml
	src/freedreno/ci/gitlab-ci.yml
	src/gallium/drivers/crocus/ci/traces-crocus.yml
	src/gallium/drivers/virgl/ci/traces-virgl-iris.yml
(cherry picked from commit 67cee534a88c95a8eb6839f7bcf28a5e6dac8fbf)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19928>

.gitlab-ci/common/init-stage2.sh
@@ -71,8 +71,7 @@ fi
 MINIO=$(cat /proc/cmdline | tr ' ' '\n' | grep minio_results | cut -d '=' -f 2 || true)
 if [ -n "$MINIO" ]; then
   tar -czf results.tar.gz results/;
-  ci-fairy minio login "$CI_JOB_JWT";
-  ci-fairy minio cp results.tar.gz minio://"$MINIO"/results.tar.gz;
+  ci-fairy s3cp --token-file "${CI_JOB_JWT_FILE}" results.tar.gz https://"$MINIO_RESULTS_UPLOAD"/results.tar.gz;
 fi
 
 echo "hwci: mesa: $RESULT"