b4eed47828104bcf321be37071a39725e0bd045b
Emma Anholt <emma@anholt.net>
2022-11-22 14:34:58 -0800

    glx: clean up a litttle remains of DRI2 support.
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19942>

--- src/glx/glxclient.h
+++ src/glx/glxclient.h
@@ -136,7 +136,6 @@ struct __GLXDRIdrawableRec
 ** dependent methods.
 */
 extern __GLXDRIdisplay *driswCreateDisplay(Display * dpy, enum glx_driver glx_driver);
-extern __GLXDRIdisplay *dri2CreateDisplay(Display * dpy);
 extern __GLXDRIdisplay *dri3_create_display(Display * dpy);
 extern __GLXDRIdisplay *driwindowsCreateDisplay(Display * dpy);
 
