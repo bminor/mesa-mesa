78c325d34641456cf36d901d1b2767ee4833d2f0
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2023-02-13 09:18:28 +0200

    anv/hasvk: handle a SAMPLED_READ/STORAGE_READ access flags
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21277>
    (cherry picked from commit eb5d7056e04460be3b1152234ea7c8c21104fe04)

--- src/intel/vulkan_hasvk/anv_private.h
+++ src/intel/vulkan_hasvk/anv_private.h
@@ -2206,6 +2206,7 @@ anv_pipe_invalidate_bits_for_access_flags(struct anv_device *device,
       case VK_ACCESS_2_SHADER_READ_BIT:
       case VK_ACCESS_2_INPUT_ATTACHMENT_READ_BIT:
       case VK_ACCESS_2_TRANSFER_READ_BIT:
+      case VK_ACCESS_2_SHADER_SAMPLED_READ_BIT:
          /* Transitioning a buffer to be read through the sampler, so
           * invalidate the texture cache, we don't want any stale data.
           */
@@ -2249,6 +2250,7 @@ anv_pipe_invalidate_bits_for_access_flags(struct anv_device *device,
           */
          pipe_bits |= ANV_PIPE_TILE_CACHE_FLUSH_BIT;
          break;
+      case VK_ACCESS_2_SHADER_STORAGE_READ_BIT:
       default:
          break; /* Nothing to do */
       }
