ddf8ae7b1aa69650d2a37f62d63a1ff6eb9cadf3
Konstantin Seurer <konstantin.seurer@gmail.com>
2023-02-14 10:55:44 +0100

    radv: Make accel struct meta state initialization thread safe
    Fixes: 0d5570b ("radv: Always compile accel structure shaders on demand.")
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21268>
    (cherry picked from commit 2ef5acedc259994c3dcb09feb2f173d636a1e125)

--- src/amd/vulkan/radv_acceleration_structure.c
+++ src/amd/vulkan/radv_acceleration_structure.c
@@ -438,23 +438,24 @@ radix_sort_fill_buffer(VkCommandBuffer commandBuffer,
 static VkResult
 radv_device_init_accel_struct_build_state(struct radv_device *device)
 {
-   VkResult result;
+   VkResult result = VK_SUCCESS;
+   mtx_lock(&device->meta_state.mtx);
 
    if (device->meta_state.accel_struct_build.radix_sort)
-      return VK_SUCCESS;
+      goto exit;
 
    result = create_build_pipeline_spv(device, leaf_spv, sizeof(leaf_spv), sizeof(struct leaf_args),
                                       &device->meta_state.accel_struct_build.leaf_pipeline,
                                       &device->meta_state.accel_struct_build.leaf_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result = create_build_pipeline_spv(device, lbvh_main_spv, sizeof(lbvh_main_spv),
                                       sizeof(struct lbvh_main_args),
                                       &device->meta_state.accel_struct_build.lbvh_main_pipeline,
                                       &device->meta_state.accel_struct_build.lbvh_main_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result =
       create_build_pipeline_spv(device, lbvh_generate_ir_spv, sizeof(lbvh_generate_ir_spv),
@@ -462,20 +463,20 @@ radv_device_init_accel_struct_build_state(struct radv_device *device)
                                 &device->meta_state.accel_struct_build.lbvh_generate_ir_pipeline,
                                 &device->meta_state.accel_struct_build.lbvh_generate_ir_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result = create_build_pipeline_spv(device, ploc_spv, sizeof(ploc_spv), sizeof(struct ploc_args),
                                       &device->meta_state.accel_struct_build.ploc_pipeline,
                                       &device->meta_state.accel_struct_build.ploc_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result = create_build_pipeline_spv(device, convert_leaf_spv, sizeof(convert_leaf_spv),
                                       sizeof(struct convert_leaf_args),
                                       &device->meta_state.accel_struct_build.convert_leaf_pipeline,
                                       &device->meta_state.accel_struct_build.convert_leaf_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result =
       create_build_pipeline_spv(device, convert_internal_spv, sizeof(convert_internal_spv),
@@ -483,14 +484,14 @@ radv_device_init_accel_struct_build_state(struct radv_device *device)
                                 &device->meta_state.accel_struct_build.convert_internal_pipeline,
                                 &device->meta_state.accel_struct_build.convert_internal_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    result =
       create_build_pipeline_spv(device, morton_spv, sizeof(morton_spv), sizeof(struct morton_args),
                                 &device->meta_state.accel_struct_build.morton_pipeline,
                                 &device->meta_state.accel_struct_build.morton_p_layout);
    if (result != VK_SUCCESS)
-      return result;
+      goto exit;
 
    device->meta_state.accel_struct_build.radix_sort =
       radv_create_radix_sort_u64(radv_device_to_handle(device), &device->meta_state.alloc,
@@ -502,15 +503,23 @@ radv_device_init_accel_struct_build_state(struct radv_device *device)
    radix_sort_info->key_bits = 24;
    radix_sort_info->fill_buffer = radix_sort_fill_buffer;
 
+exit:
+   mtx_unlock(&device->meta_state.mtx);
    return result;
 }
 
 static VkResult
 radv_device_init_accel_struct_copy_state(struct radv_device *device)
 {
-   return create_build_pipeline_spv(device, copy_spv, sizeof(copy_spv), sizeof(struct copy_args),
-                                    &device->meta_state.accel_struct_build.copy_pipeline,
-                                    &device->meta_state.accel_struct_build.copy_p_layout);
+   mtx_lock(&device->meta_state.mtx);
+
+   VkResult result =
+      create_build_pipeline_spv(device, copy_spv, sizeof(copy_spv), sizeof(struct copy_args),
+                                &device->meta_state.accel_struct_build.copy_pipeline,
+                                &device->meta_state.accel_struct_build.copy_p_layout);
+
+   mtx_unlock(&device->meta_state.mtx);
+   return result;
 }
 
 struct bvh_state {
