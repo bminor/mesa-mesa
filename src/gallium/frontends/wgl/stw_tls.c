1542ce3803528613cb886b09317b55332496118d
Jose Fonseca <jfonseca@vmware.com>
2023-04-26 10:10:45 +0100

    wgl: Fix unintentional assignment on assert.
    Spotted by Nanley Chery.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/4427
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Reviewed-by: Charmaine Lee <charmainel@vmware.com>
    Reviewed-by: Neha Bhende <bhenden@vmware.com>
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/22706>
    (cherry picked from commit 9105e771afece456835adf911735be0e8d27dc87)

--- src/gallium/frontends/wgl/stw_tls.c
+++ src/gallium/frontends/wgl/stw_tls.c
@@ -298,7 +298,7 @@ stw_tls_get_data(void)
    }
 
    assert(data);
-   assert(data->dwThreadId = GetCurrentThreadId());
+   assert(data->dwThreadId == GetCurrentThreadId());
    assert(data->next == NULL);
 
    return data;
