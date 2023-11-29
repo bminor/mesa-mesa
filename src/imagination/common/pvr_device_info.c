ea28791d403272ba4c25073e8a26263dbe7a3dd9
Frank Binns <frank.binns@imgtec.com>
2023-11-29 23:36:45 +0000

    pvr: add device info for BXE-4-32 (36.50.54.182)
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>
    Reviewed-by: Ashish Chauhan <ashish.chauhan@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37790>

--- src/imagination/common/pvr_device_info.c
+++ src/imagination/common/pvr_device_info.c
@@ -33,14 +33,16 @@
 
 #include "device_info/gx6250.h"
 #include "device_info/axe-1-16m.h"
+#include "device_info/bxe-4-32.h"
 #include "device_info/bxs-4-64.h"
 
 #include "util/u_string.h"
 
 static const struct pvr_device_info *device_infos[] = {
-   &pvr_device_info_33_15_11_3,
-   &pvr_device_info_36_53_104_796,
    &pvr_device_info_4_40_2_51,
+   &pvr_device_info_33_15_11_3,
+   &pvr_device_info_36_50_54_182,
+   &pvr_device_info_36_53_104_796,
 };
 
 /**
@@ -62,6 +64,7 @@ int pvr_device_info_init(struct pvr_device_info *info, uint64_t bvnc)
    switch (bvnc) {
       CASE_PACKED_BVNC_DEVICE_INFO(4, 40, 2, 51);
       CASE_PACKED_BVNC_DEVICE_INFO(33, 15, 11, 3);
+      CASE_PACKED_BVNC_DEVICE_INFO(36, 50, 54, 182);
       CASE_PACKED_BVNC_DEVICE_INFO(36, 53, 104, 796);
    }
 
