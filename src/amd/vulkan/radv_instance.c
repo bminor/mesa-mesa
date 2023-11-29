065c7d1f8c44dfe481f20b5638b80ae4d0940da7
Friedrich Vock <friedrich.vock@gmx.de>
2023-11-29 01:20:54 +0100

    radv,vtn,driconf: Add and use radv_rt_ssbo_non_uniform workaround for Crysis 2/3 Remastered
    Crysis 2 and 3 Remastered's RT shaders non-uniformly index into SSBO
    descriptor arrays without specifying the NonUniformEXT qualifier on the
    relevant access chains/load ops. This leads to artifacts around objects.
    
    To add insult to injury, the game fails to provide a meaningful
    applicationName/engineName in the Vulkan part of the DX11-Vulkan interop
    solution used for RT. Both of these fields are set to "nvpro-sample"
    (perhaps the code has been copied from NVIDIA's sample applications).
    Therefore, fall back to executable name matching.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/9883
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26391>
    (cherry picked from commit f1817ab7e03f188164c7d6850149781066418d49)

--- src/amd/vulkan/radv_instance.c
+++ src/amd/vulkan/radv_instance.c
@@ -153,6 +153,7 @@ static const driOptionDescription radv_dri_options[] = {
       DRI_CONF_RADV_FLUSH_BEFORE_TIMESTAMP_WRITE(false)
       DRI_CONF_RADV_RT_WAVE64(false)
       DRI_CONF_DUAL_COLOR_BLEND_BY_LOCATION(false)
+      DRI_CONF_RADV_SSBO_NON_UNIFORM(false)
       DRI_CONF_RADV_APP_LAYER()
    DRI_CONF_SECTION_END
 };
@@ -202,6 +203,8 @@ radv_init_dri_options(struct radv_instance *instance)
    instance->enable_unified_heap_on_apu = driQueryOptionb(&instance->dri_options, "radv_enable_unified_heap_on_apu");
 
    instance->tex_non_uniform = driQueryOptionb(&instance->dri_options, "radv_tex_non_uniform");
+
+   instance->ssbo_non_uniform = driQueryOptionb(&instance->dri_options, "radv_ssbo_non_uniform");
 
    instance->app_layer = driQueryOptionstr(&instance->dri_options, "radv_app_layer");
 
