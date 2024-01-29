3f4fa5b839dbb18e23a4a7acd492004a328e53fb
Eric Engestrom <eric@igalia.com>
2024-01-29 12:42:18 +0000

    tree-wide: use __normal_user() everywhere instead of writing the check manually
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27346>
    (cherry picked from commit 92c24191d4ee9379626c5fef9f6c234f64eeeee1)

--- src/util/disk_cache_os.c
+++ src/util/disk_cache_os.c
@@ -33,6 +33,7 @@
 
 #include "util/compress.h"
 #include "util/crc32.h"
+#include "util/u_debug.h"
 #include "util/disk_cache.h"
 #include "util/disk_cache_os.h"
 
@@ -940,7 +941,7 @@ disk_cache_enabled()
       return false;
 
    /* If running as a users other than the real user disable cache */
-   if (geteuid() != getuid())
+   if (!__normal_user())
       return false;
 
    /* At user request, disable shader cache entirely. */
