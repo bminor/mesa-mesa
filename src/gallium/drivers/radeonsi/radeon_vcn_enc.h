ac45a57bfcae873875a7e65ee152f2a9555f10e7
David Rosca <nowrep@gmail.com>
2024-02-09 13:13:41 +0100

    radeonsi/vcn: Don't reinitialize encode session on bitrate/fps change
    When bitrate or fps change is detected, only update rate control
    parameters instead of completely reinitializing encode session.
    
    This fixes an issue where if application changed bitrate or fps often,
    the output bitrate would significantly overshoot the target bitrate in some
    cases. In other cases, the output bitrate would be extremely low instead.
    
    Cc: mesa-stable
    
    Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/27548>
    (cherry picked from commit 8d44a115080645c210e055fa410fb18accf510aa)

--- src/gallium/drivers/radeonsi/radeon_vcn_enc.h
+++ src/gallium/drivers/radeonsi/radeon_vcn_enc.h
@@ -258,6 +258,7 @@ struct radeon_encoder {
 
    bool emulation_prevention;
    bool need_feedback;
+   bool need_rate_control;
    unsigned dpb_size;
    rvcn_enc_picture_info_t dpb_info[RENCODE_MAX_NUM_RECONSTRUCTED_PICTURES];
    unsigned max_ltr_idx;
