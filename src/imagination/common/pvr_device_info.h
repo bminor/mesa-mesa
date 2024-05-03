7a32dc673ba6544000f0a9ed0aa36f792ba0cfce
Simon Perretta <simon.perretta@imgtec.com>
2024-05-03 21:23:53 +0100

    pvr: add device info and functions for calculating available temps
    Signed-off-by: Simon Perretta <simon.perretta@imgtec.com>
    Acked-by: Frank Binns <frank.binns@imgtec.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32258>

--- src/imagination/common/pvr_device_info.h
+++ src/imagination/common/pvr_device_info.h
@@ -292,6 +292,7 @@ struct pvr_device_features {
    bool has_tpu_dm_global_registers : 1;
    bool has_tpu_extended_integer_lookup : 1;
    bool has_tpu_image_state_v2 : 1;
+   bool has_unified_store_depth : 1;
    bool has_usc_f16sop_u8 : 1;
    bool has_usc_min_output_registers_per_pix : 1;
    bool has_usc_pixel_partition_mask : 1;
@@ -320,6 +321,7 @@ struct pvr_device_features {
    uint32_t slc_cache_line_size_bits;
    uint32_t tile_size_x;
    uint32_t tile_size_y;
+   uint32_t unified_store_depth;
    uint32_t usc_min_output_registers_per_pix;
    uint32_t usc_slots;
    uint32_t uvs_banks;
