54db037efb754fff333c48cb19537f3ec411ee9c
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2024-07-19 19:44:44 +0300

    nir/divergence: add missing load_constant_base_ptr
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Cc: mesa-stable
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30712>
    (cherry picked from commit 2158fe2ae29d20b397b5c10bdd36a9279fc86c0b)

--- src/compiler/nir/nir_divergence_analysis.c
+++ src/compiler/nir/nir_divergence_analysis.c
@@ -265,6 +265,7 @@ visit_intrinsic(nir_intrinsic_instr *instr, struct divergence_state *state)
    case nir_intrinsic_preamble_start_ir3:
    case nir_intrinsic_optimization_barrier_sgpr_amd:
    case nir_intrinsic_load_printf_buffer_address:
+   case nir_intrinsic_load_constant_base_ptr:
       is_divergent = false;
       break;
 
