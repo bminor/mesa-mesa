39fd67b1aadcefed0a6a8794390ecb95b1b56b10
Timothy Arceri <tarceri@itsqueeze.com>
2024-07-27 18:27:03 +1000

    glsl: fix glsl to nir support for lower precision builtins
    When we switch to the full nir based glsl linker in an upcomming merge
    request this is required for existing tests from 8fcf8e7fd401 to
    continue to pass, this is because they never exercised glsl to nir so
    the missing support went unnoticed.
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11456
    Fixes: 8fcf8e7fd401 ("glsl: lower builtins to mediump that ignore precision of certain parameters ")
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30395>
    (cherry picked from commit 140ca7e5d72323a4685b5043dbac272931b055bb)

--- src/compiler/glsl/glsl_to_nir.cpp
+++ src/compiler/glsl/glsl_to_nir.cpp
@@ -1756,13 +1756,27 @@ nir_visitor::visit(ir_expression *ir)
    case ir_binop_interpolate_at_sample: {
       ir_dereference *deref = ir->operands[0]->as_dereference();
       ir_swizzle *swizzle = NULL;
+      ir_expression *precision_op = NULL;
       if (!deref) {
-         /* the api does not allow a swizzle here, but the varying packing code
-          * may have pushed one into here.
-          */
-         swizzle = ir->operands[0]->as_swizzle();
-         assert(swizzle);
-         deref = swizzle->val->as_dereference();
+         precision_op = ir->operands[0]->as_expression();
+         if (precision_op) {
+            /* For some builtins precision is lowered to mediump for certain
+             * parameters that ignore precision. For example for Interpolation
+             * and Bitfield functions.
+             */
+            assert(precision_op->operation == ir_unop_f2fmp);
+            deref = precision_op->operands[0]->as_dereference();
+         }
+
+         if (!deref) {
+            /* the api does not allow a swizzle here, but the varying packing code
+             * may have pushed one into here.
+             */
+            swizzle = ir->operands[0]->as_swizzle();
+            assert(swizzle);
+            deref = swizzle->val->as_dereference();
+         }
+
          assert(deref);
       }
 
@@ -1802,6 +1816,10 @@ nir_visitor::visit(ir_expression *ir)
 
          result = nir_swizzle(&b, result, swiz,
                               swizzle->type->vector_elements);
+      }
+
+      if (precision_op) {
+         result = nir_build_alu(&b, nir_op_f2fmp, result, NULL, NULL, NULL);
       }
 
       return;
