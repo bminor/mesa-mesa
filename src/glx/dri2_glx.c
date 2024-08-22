upcliujie/mesa

glx/dri2: strdup driver name
this is freed by the caller

cc: mesa-stable

src/glx/dri2_glx.c
@@ -660,7 +660,7 @@ static const struct glx_context_vtable dri2_context_vtable = {
 static char *
 dri2_get_driver_name(struct glx_screen *glx_screen)
 {
-    return glx_screen->driverName;
+    return strdup(glx_screen->driverName);
 }
 
 static const struct glx_screen_vtable dri2_screen_vtable = {

bminor/mesa-mesa

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30619>
(cherry picked from commit 046728f47a0557a23d8603700a6f748ae880dc7d)

src/glx/dri2_glx.c
@@ -989,7 +989,7 @@ dri2_get_driver_name(struct glx_screen *glx_screen)
 {
     struct dri2_screen *psc = (struct dri2_screen *)glx_screen;
 
-    return psc->driverName;
+    return strdup(psc->driverName);
 }
 
 static const struct glx_screen_vtable dri2_screen_vtable = {