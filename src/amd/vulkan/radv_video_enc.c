80c9ca152b7329c8aaf9b8a0cda23530d319712b
Dave Airlie <airlied@redhat.com>
2024-09-10 05:18:17 +1000

    radv: Fix radeon_enc_code_ue with values over 2^16
    This ports this fixes from radeonsi:
    
    138ba42a8705 ("radeonsi/vcn: Fix radeon_enc_code_ue with values over 2^16")
    
    Cc: mesa-stable
    Reviewed-by: Lynne <dev@lynne.ee>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31135>
    (cherry picked from commit e89f4a48fd1296aec98cdce95834277cb0834205)

--- src/amd/vulkan/radv_video_enc.c
+++ src/amd/vulkan/radv_video_enc.c
@@ -375,7 +375,7 @@ radv_enc_flush_headers(struct radv_cmd_buffer *cmd_buffer)
 static void
 radv_enc_code_ue(struct radv_cmd_buffer *cmd_buffer, unsigned int value)
 {
-   int x = -1;
+   unsigned int x = 0;
    unsigned int ue_code = value + 1;
    value += 1;
 
@@ -383,9 +383,9 @@ radv_enc_code_ue(struct radv_cmd_buffer *cmd_buffer, unsigned int value)
       value = (value >> 1);
       x += 1;
    }
-
-   unsigned int ue_length = (x << 1) + 1;
-   radv_enc_code_fixed_bits(cmd_buffer, ue_code, ue_length);
+   if (x > 1)
+     radv_enc_code_fixed_bits(cmd_buffer, 0, x - 1);
+   radv_enc_code_fixed_bits(cmd_buffer, ue_code, x);
 }
 
 static void
