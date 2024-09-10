dead2b7e6238aa6c5bbbccdb002619daf8056fc8
Daniel Stone <daniels@collabora.com>
2024-09-10 22:03:10 +0100

    ci/lava: Fix colour definitions
    All the foreground colours pass 1 to ANSI SGR, which sets bold. The
    other arguments are either a colour from 30-37 (passed directly), or
    38;5;nnn, where nnn is an extended RGB colour. It looks like most of the
    definitions were cargo-culted from FG_RED, which correctly sets an
    extended colour, because the arguments there were being parsed as
    setting blinking, followed by 197 which was ignored as unknown.
    
    Fix them to just set the original definition.
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31602>

--- .gitlab-ci/lava/utils/console_format.py
+++ .gitlab-ci/lava/utils/console_format.py
@@ -1,8 +1,8 @@
 CONSOLE_LOG = {
-    "FG_GREEN": "\x1b[1;32;5;197m",
+    "FG_GREEN": "\x1b[1;32m",
     "FG_RED": "\x1b[1;38;5;197m",
-    "FG_YELLOW": "\x1b[1;33;5;197m",
-    "FG_MAGENTA": "\x1b[1;35;5;197m",
+    "FG_YELLOW": "\x1b[1;33m",
+    "FG_MAGENTA": "\x1b[1;35m",
     "RESET": "\x1b[0m",
     "UNDERLINED": "\x1b[3m",
     "BOLD": "\x1b[1m",
