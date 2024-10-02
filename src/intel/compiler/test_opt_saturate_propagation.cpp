cc5a6a5ae8c4a3831698aa46f29dca81e450564f
Ian Romanick <ian.d.romanick@intel.com>
2024-10-02 10:59:15 -0700

    brw/sat: Convert tests to use load_reg
    This is in prepartion for a commit that removes the non-defs version of
    the pass.
    
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31497>

--- src/intel/compiler/test_opt_saturate_propagation.cpp
+++ src/intel/compiler/test_opt_saturate_propagation.cpp
@@ -18,12 +18,12 @@ TEST_F(saturate_propagation_test, basic)
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.ADD(dst0, src0, src1);
+   bld.ADD(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1));
    bld.MOV(dst1, dst0)->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.ADD(dst0, src0, src1)->saturate = true;
+   exp.ADD(dst0, exp.LOAD_REG(src0), exp.LOAD_REG(src1))->saturate = true;
    exp.MOV(dst1, dst0);
 
    EXPECT_SHADERS_MATCH(bld, exp);
@@ -36,8 +36,8 @@ TEST_F(saturate_propagation_test, other_non_saturated_use)
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst2 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.ADD(dst0, src0, src1);
    bld.MOV(dst1, dst0)->saturate = true;
@@ -52,8 +52,8 @@ TEST_F(saturate_propagation_test, predicated_instruction)
 
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.ADD(dst0, src0, src1)->predicate = BRW_PREDICATE_NORMAL;
    bld.MOV(dst1, dst0)->saturate = true;
@@ -66,7 +66,7 @@ TEST_F(saturate_propagation_test, neg_mov_sat)
    brw_builder bld = make_shader(MESA_SHADER_FRAGMENT, 16);
 
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
    brw_reg dst0 = bld.RNDU(src0);
    dst0.negate = true;
    set_saturate(true, bld.MOV(dst1, dst0));
@@ -84,12 +84,12 @@ TEST_F(saturate_propagation_test, add_neg_mov_sat)
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.ADD(dst0, src0, src1);
+   bld.ADD(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1));
    bld.MOV(dst1, negate(dst0))->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.ADD(dst0, negate(src0), negate(src1))->saturate = true;
+   exp.ADD(dst0, negate(exp.LOAD_REG(src0)), negate(exp.LOAD_REG(src1)))->saturate = true;
    exp.MOV(dst1, dst0);
 
    EXPECT_SHADERS_MATCH(bld, exp);
@@ -104,12 +104,12 @@ TEST_F(saturate_propagation_test, add_imm_float_neg_mov_sat)
    brw_reg dst1 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.ADD(dst0, src0, brw_imm_f(1.0f));
+   bld.ADD(dst0, bld.LOAD_REG(src0), brw_imm_f(1.0f));
    bld.MOV(dst1, negate(dst0))->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.ADD(dst0, negate(src0), brw_imm_f(-1.0f))->saturate = true;
+   exp.ADD(dst0, negate(exp.LOAD_REG(src0)), brw_imm_f(-1.0f))->saturate = true;
    exp.MOV(dst1, dst0);
 
    EXPECT_SHADERS_MATCH(bld, exp);
@@ -125,12 +125,12 @@ TEST_F(saturate_propagation_test, mul_neg_mov_sat)
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.MUL(dst0, src0, src1);
+   bld.MUL(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1));
    bld.MOV(dst1, negate(dst0))->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.MUL(dst0, negate(src0), src1)->saturate = true;
+   exp.MUL(dst0, negate(exp.LOAD_REG(src0)), exp.LOAD_REG(src1))->saturate = true;
    exp.MOV(dst1, dst0);
 
    EXPECT_SHADERS_MATCH(bld, exp);
@@ -147,12 +147,12 @@ TEST_F(saturate_propagation_test, mad_neg_mov_sat)
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src2 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.MAD(dst0, src0, src1, src2);
+   bld.MAD(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1), bld.LOAD_REG(src2));
    bld.MOV(dst1, negate(dst0))->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.MAD(dst0, negate(src0), negate(src1), src2)->saturate = true;
+   exp.MAD(dst0, negate(exp.LOAD_REG(src0)), negate(exp.LOAD_REG(src1)), exp.LOAD_REG(src2))->saturate = true;
    exp.MOV(dst1, dst0);
 
    EXPECT_SHADERS_MATCH(bld, exp);
@@ -172,7 +172,8 @@ TEST_F(saturate_propagation_test, mad_imm_float_neg_mov_sat)
     */
 
    {
-      brw_inst *mad = bld.MAD(dst0, src2, src2, src2);
+      brw_reg def2 = bld.LOAD_REG(src2);
+      brw_inst *mad = bld.MAD(dst0, def2, def2, def2);
       mad->src[0] = brw_imm_f(1.0f);
       mad->src[1] = brw_imm_f(-2.0f);
       bld.MOV(dst1, negate(dst0))->saturate = true;
@@ -181,7 +182,8 @@ TEST_F(saturate_propagation_test, mad_imm_float_neg_mov_sat)
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
    {
-      brw_inst *mad = exp.MAD(dst0, src2, src2, src2);
+      brw_reg def2 = exp.LOAD_REG(src2);
+      brw_inst *mad = exp.MAD(dst0, def2, def2, def2);
       mad->saturate = true;
       mad->src[0] = brw_imm_f(-1.0f);
       mad->src[1] = brw_imm_f(2.0f);
@@ -198,8 +200,8 @@ TEST_F(saturate_propagation_test, mul_mov_sat_neg_mov_sat)
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst2 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.MUL(dst0, src0, src1);
    bld.MOV(dst1, dst0)->saturate = true;
@@ -214,8 +216,8 @@ TEST_F(saturate_propagation_test, mul_neg_mov_sat_neg_mov_sat)
 
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst2 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    brw_reg dst0 = bld.MUL(src0, src1);
    bld.MOV(dst1, negate(dst0))->saturate = true;
@@ -229,8 +231,8 @@ TEST_F(saturate_propagation_test, abs_mov_sat)
    brw_builder bld = make_shader(MESA_SHADER_FRAGMENT, 16);
 
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    brw_reg dst0 = bld.ADD(src0, src1);
    bld.MOV(dst1, brw_abs(dst0))->saturate = true;
@@ -249,13 +251,13 @@ TEST_F(saturate_propagation_test, producer_saturates)
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.ADD(dst0, src0, src1)->saturate = true;
+   bld.ADD(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1))->saturate = true;
    bld.MOV(dst1, dst0)->saturate = true;
    bld.MOV(dst2, dst0);
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.ADD(dst0, src0, src1)->saturate = true;
+   exp.ADD(dst0, exp.LOAD_REG(src0), exp.LOAD_REG(src1))->saturate = true;
    exp.MOV(dst1, dst0);
    exp.MOV(dst2, dst0);
 
@@ -273,13 +275,13 @@ TEST_F(saturate_propagation_test, intervening_saturating_copy)
    brw_reg src0 = vgrf(bld, exp, BRW_TYPE_F);
    brw_reg src1 = vgrf(bld, exp, BRW_TYPE_F);
 
-   bld.ADD(dst0, src0, src1);
+   bld.ADD(dst0, bld.LOAD_REG(src0), bld.LOAD_REG(src1));
    bld.MOV(dst1, dst0)->saturate = true;
    bld.MOV(dst2, dst0)->saturate = true;
 
    EXPECT_PROGRESS(brw_opt_saturate_propagation, bld);
 
-   exp.ADD(dst0, src0, src1)->saturate = true;
+   exp.ADD(dst0, exp.LOAD_REG(src0), exp.LOAD_REG(src1))->saturate = true;
    exp.MOV(dst1, dst0);
    exp.MOV(dst2, dst0);
 
@@ -292,9 +294,9 @@ TEST_F(saturate_propagation_test, intervening_dest_write)
 
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F, 4);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src2 = bld.vgrf(BRW_TYPE_F, 2);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src2 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F, 2));
 
    brw_reg tex_srcs[TEX_LOGICAL_NUM_SRCS] = {};
    tex_srcs[TEX_LOGICAL_SRC_COORDINATE] = src2;
@@ -318,8 +320,8 @@ TEST_F(saturate_propagation_test, mul_neg_mov_sat_mov_sat)
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst2 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.MUL(dst0, src0, src1);
    bld.MOV(dst1, negate(dst0))->saturate = true;
@@ -334,8 +336,8 @@ TEST_F(saturate_propagation_test, smaller_exec_size_consumer)
 
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld            .ADD(src0, src1);
    bld.group(8, 0).MOV(dst1, dst0)->saturate = true;
@@ -349,8 +351,8 @@ TEST_F(saturate_propagation_test, larger_exec_size_consumer)
 
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.group(8, 0).ADD(dst0, src0, src1);
    bld            .MOV(dst1, dst0)->saturate = true;
@@ -365,8 +367,8 @@ TEST_F(saturate_propagation_test, offset_source_barrier)
    brw_reg dst0 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst1 = bld.vgrf(BRW_TYPE_F);
    brw_reg dst2 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src0 = bld.vgrf(BRW_TYPE_F);
-   brw_reg src1 = bld.vgrf(BRW_TYPE_F);
+   brw_reg src0 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
+   brw_reg src1 = bld.LOAD_REG(bld.vgrf(BRW_TYPE_F));
 
    bld.group(16, 0).ADD(dst0, src0, src1);
    bld.group(1, 0) .ADD(dst1, component(dst0, 8), brw_imm_f(1.0f));
