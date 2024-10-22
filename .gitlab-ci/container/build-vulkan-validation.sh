930b4009e10a2cee63637145b067a5e81958db0f
Daniel Stone <daniels@collabora.com>
2024-10-22 10:55:59 +0100

    ci/vvl: Use appropriate level of parallelism
    The whack-a-mole continues ...
    
    Signed-off-by: Daniel Stone <daniels@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32927>

--- .gitlab-ci/container/build-vulkan-validation.sh
+++ .gitlab-ci/container/build-vulkan-validation.sh
@@ -17,7 +17,7 @@ pushd Vulkan-ValidationLayers
 sed -i scripts/known_good.json -e 's/SPIRV_SKIP_EXECUTABLES=OFF/SPIRV_SKIP_EXECUTABLES=ON/'
 python3 scripts/update_deps.py --dir external --config release --generator Ninja --optional tests
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_TESTS=OFF -DBUILD_WERROR=OFF -C external/helper.cmake -S . -B build
-ninja -C build
+ninja -C build -j"${FDO_CI_CONCURRENT:-4}"
 cmake --install build --strip
 popd
 rm -rf Vulkan-ValidationLayers
