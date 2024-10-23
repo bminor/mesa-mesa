bminor/mesa-mesa

commit bb5eace0dc60b176bbc258f389856ec1b4cb7410

aco/live_var_analysis: check for isPrecolored flag rather than isFixed
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31387>

src/amd/compiler/aco_live_var_analysis.cpp
@@ -179,7 +179,7 @@ process_live_temps_per_block(live_ctx& ctx, Block* block)
          if (!definition.isTemp()) {
             continue;
          }
-         if (definition.isFixed() && definition.physReg() == vcc)
+         if (definition.isPrecolored() && definition.physReg() == vcc)
             ctx.program->needs_vcc = true;
 
          const Temp temp = definition.getTemp();
@@ -252,7 +252,7 @@ process_live_temps_per_block(live_ctx& ctx, Block* block)
             continue;
 
          const Temp temp = operand.getTemp();
-         if (operand.isFixed() && ctx.program->progress < CompilationProgress::after_ra) {
+         if (operand.isPrecolored()) {
             assert(!operand.isLateKill());
             ctx.program->needs_vcc |= operand.physReg() == vcc;
 
@@ -266,13 +266,12 @@ process_live_temps_per_block(live_ctx& ctx, Block* block)
                             }))
                operand.setClobbered(true);
 
-            /* Check if this temp is fixed to a different register as well.
+            /* Check if another precolored operand uses the same temporary.
              * This assumes that operands of one instruction are not precolored twice to
              * the same register. In this case, register pressure might be overestimated.
              */
             for (unsigned j = i + 1; !operand.isCopyKill() && j < insn->operands.size(); ++j) {
-               if (insn->operands[j].isTemp() && insn->operands[j].getTemp() == temp &&
-                   insn->operands[j].isFixed()) {
+               if (insn->operands[j].isPrecolored() && insn->operands[j].getTemp() == temp) {
                   operand_demand += temp;
                   insn->operands[j].setCopyKill(true);
                }

commit d5581b112452398e3e56ae0e9ab8f585b6374020

aco/live_var_analysis: check isFixed() for definitions in order to set needs_vcc
In rare cases, it could happen that during post-RA validation,
live-var-analysis sets needs_vcc = false after if was true
before register allocation.

Fixes: bb5eace0dc60b176bbc258f389856ec1b4cb7410 ('aco/live_var_analysis: check for isPrecolored flag rather than isFixed')
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31791>

src/amd/compiler/aco_live_var_analysis.cpp
@@ -179,7 +179,7 @@ process_live_temps_per_block(live_ctx& ctx, Block* block)
          if (!definition.isTemp()) {
             continue;
          }
-         if (definition.isPrecolored() && definition.physReg() == vcc)
+         if (definition.isFixed() && definition.physReg() == vcc)
             ctx.program->needs_vcc = true;
 
          const Temp temp = definition.getTemp();
@@ -311,6 +311,7 @@ process_live_temps_per_block(live_ctx& ctx, Block* block)
          continue;
       }
       Definition& definition = insn->definitions[0];
+      ctx.program->needs_vcc |= definition.isFixed() && definition.physReg() == vcc;
       const size_t n = live.erase(definition.tempId());
       if (n && (definition.isKill() || ctx.handled_once > block->index)) {
          Block::edge_vec& preds =