alexvorxx/zink-xlib-termux

commit df3ba95a2498447b037d0d8efbdf605be25ff8ff

util/vma: Add function to get max continuous free size
Useful to find out what's the biggest allocation we could make.

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28276>

src/util/vma.c
@@ -352,6 +352,16 @@ util_vma_heap_free(struct util_vma_heap *heap,
    util_vma_heap_validate(heap);
 }
 
+uint64_t
+util_vma_heap_get_max_free_continuous_size(struct util_vma_heap *heap)
+{
+   if (list_is_empty(&heap->holes))
+      return 0;
+
+   struct util_vma_hole *top_hole = list_first_entry(&heap->holes, struct util_vma_hole, link);
+   return top_hole->size;
+}
+
 void
 util_vma_heap_print(struct util_vma_heap *heap, FILE *fp,
                     const char *tab, uint64_t total_size)

src/util/vma.h
@@ -66,6 +66,8 @@ bool util_vma_heap_alloc_addr(struct util_vma_heap *heap,
 void util_vma_heap_free(struct util_vma_heap *heap,
                         uint64_t offset, uint64_t size);
 
+uint64_t util_vma_heap_get_max_free_continuous_size(struct util_vma_heap *heap);
+
 void util_vma_heap_print(struct util_vma_heap *heap, FILE *fp,
                          const char *tab, uint64_t total_size);
 

bminor/mesa-mesa

commit 3209a97c5c391aee3bbd3aab25853d9c4a5daff9

util/vma: Fix util_vma_heap_get_max_free_continuous_size calculation
It was based on misunderstanding of how holes are sorted, they are
sorted by address and not by size.

Fixes: df3ba95a2498447b037d0d8efbdf605be25ff8ff
("util/vma: Add function to get max continuous free size")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31722>

src/util/vma.c
@@ -355,11 +355,12 @@ util_vma_heap_free(struct util_vma_heap *heap,
 uint64_t
 util_vma_heap_get_max_free_continuous_size(struct util_vma_heap *heap)
 {
-   if (list_is_empty(&heap->holes))
-      return 0;
+   uint64_t max_size = 0;
+   util_vma_foreach_hole_safe(hole, heap) {
+      max_size = MAX2(max_size, hole->size);
+   }
 
-   struct util_vma_hole *top_hole = list_first_entry(&heap->holes, struct util_vma_hole, link);
-   return top_hole->size;
+   return max_size;
 }
 
 void