aerisarn/mesa-uwp

commit e277b1318253a4776babf587c76c7bac534a6de9

freedreno: Stop exposing MSAA image load/store on desktop GL.
GLES doesn't support it, and blob VK doesn't support it.  We could
theoretically lower it, but don't bother since it's not required.  Fixes
various piglit image load/store tests.

Suggested-by: Ilia Mirkin <imirkin@alum.mit.edu>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/13852>

src/gallium/drivers/freedreno/a5xx/fd5_screen.c
@@ -90,6 +90,11 @@ fd5_screen_is_format_supported(struct pipe_screen *pscreen,
                          PIPE_BIND_COMPUTE_RESOURCE);
    }
 
+   if (usage & PIPE_BIND_SHADER_IMAGE) {
+      if (sample_count > 1)
+         return false;
+   }
+
    /* For ARB_framebuffer_no_attachments: */
    if ((usage & PIPE_BIND_RENDER_TARGET) && (format == PIPE_FORMAT_NONE)) {
       retval |= usage & PIPE_BIND_RENDER_TARGET;

src/gallium/drivers/freedreno/a6xx/fd6_screen.c
@@ -89,6 +89,11 @@ fd6_screen_is_format_supported(struct pipe_screen *pscreen,
       retval |= usage & (PIPE_BIND_SAMPLER_VIEW | PIPE_BIND_SHADER_IMAGE);
    }
 
+   if (usage & PIPE_BIND_SHADER_IMAGE) {
+      if (sample_count > 1)
+         return false;
+   }
+
    if ((usage &
         (PIPE_BIND_RENDER_TARGET | PIPE_BIND_DISPLAY_TARGET |
          PIPE_BIND_SCANOUT | PIPE_BIND_SHARED | PIPE_BIND_COMPUTE_RESOURCE)) &&

bminor/mesa-mesa

commit f8e7c0e2a2f0e65837abc513f4d1ce30f67a9449

freedreno/a6xx: Stop exposing MSAA image load/store harder
Fixes KHR-GL46.multi_bind.dispatch_bind_image_textures which decides
max_image_samples==1 means that MSAA image load/store is supported.
Switch the condition to > 0, which matches what zink does.

Fixes: e277b1318253 ("freedreno: Stop exposing MSAA image load/store on desktop GL.")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31857>

src/gallium/drivers/freedreno/a5xx/fd5_screen.c
@@ -73,7 +73,7 @@ fd5_screen_is_format_supported(struct pipe_screen *pscreen,
    }
 
    if (usage & PIPE_BIND_SHADER_IMAGE) {
-      if (sample_count > 1)
+      if (sample_count > 0)
          return false;
    }
 

src/gallium/drivers/freedreno/a6xx/fd6_screen.cc
@@ -74,7 +74,7 @@ fd6_screen_is_format_supported(struct pipe_screen *pscreen,
    }
 
    if (usage & PIPE_BIND_SHADER_IMAGE) {
-      if (sample_count > 1)
+      if (sample_count > 0)
          return false;
 
       /* So, this only matters for image writes but 'usage' doesn't