alexvorxx/Mesa-VirGL

commit b8bfbbdf66a95d5163ae2f1f0c3bdb5a6ee64455

panvk: check against texfeat_bit
This is the same as the previous commit, but for PanVK instead.

Fixes: 213e895da06 ("panvk: Allow compressed formats")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31419>

src/panfrost/vulkan/panvk_physical_device.c
@@ -931,7 +931,8 @@ panvk_DestroyDevice(VkDevice _device, const VkAllocationCallbacks *pAllocator)
 
 static bool
 format_is_supported(struct panvk_physical_device *physical_device,
-                    const struct panfrost_format fmt)
+                    const struct panfrost_format fmt,
+                    enum pipe_format pfmt)
 {
    /* If the format ID is zero, it's not supported. */
    if (!fmt.hw)
@@ -939,14 +940,13 @@ format_is_supported(struct panvk_physical_device *physical_device,
 
    /* Compressed formats (ID < 32) are optional. We need to check against
     * the supported formats reported by the GPU. */
-   unsigned idx = MALI_EXTRACT_INDEX(fmt.hw);
-   if (MALI_EXTRACT_TYPE(idx) == MALI_FORMAT_COMPRESSED) {
+   if (util_format_is_compressed(pfmt)) {
       uint32_t supported_compr_fmts =
          panfrost_query_compressed_formats(&physical_device->kmod.props);
 
-      assert(idx < 32);
+      assert(fmt.texfeat_bit < 32);
 
-      if (!(BITFIELD_BIT(idx) & supported_compr_fmts))
+      if (!(BITFIELD_BIT(fmt.texfeat_bit) & supported_compr_fmts))
          return false;
    }
 
@@ -977,7 +977,7 @@ get_format_properties(struct panvk_physical_device *physical_device,
 
    const struct panfrost_format fmt = physical_device->formats.all[pfmt];
 
-   if (!format_is_supported(physical_device, fmt))
+   if (!format_is_supported(physical_device, fmt, pfmt))
       goto end;
 
    /* 3byte formats are not supported by the buffer <-> image copy helpers. */

bminor/mesa-mesa

commit 19fdfd64294f88078993781a500c21f4d02dbbe7

panvk: drop needless assert
The value can't be larger than 31 here anyway, due to the bitfield
width. So the assert is completely needless.

CID: 1633082
Fixes: b8bfbbdf66a ("panvk: check against texfeat_bit")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31767>

src/panfrost/vulkan/panvk_physical_device.c
@@ -963,8 +963,6 @@ format_is_supported(struct panvk_physical_device *physical_device,
       uint32_t supported_compr_fmts =
          panfrost_query_compressed_formats(&physical_device->kmod.props);
 
-      assert(fmt.texfeat_bit < 32);
-
       if (!(BITFIELD_BIT(fmt.texfeat_bit) & supported_compr_fmts))
          return false;
    }