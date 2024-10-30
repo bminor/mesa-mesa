be26012f1d9be3a3602505807dee21a8f069f478
Ian Romanick <ian.d.romanick@intel.com>
2024-10-30 09:56:48 -0700

    brw/opt: Always do copy prop, DCE, and register coalesce after lower_regioning
    shader-db:
    
    Lunar Lake
    total instructions in shared programs: 18100289 -> 18083853 (-0.09%)
    instructions in affected programs: 790048 -> 773612 (-2.08%)
    helped: 3058 / HURT: 1
    
    total cycles in shared programs: 921691992 -> 921293816 (-0.04%)
    cycles in affected programs: 37210762 -> 36812586 (-1.07%)
    helped: 2329 / HURT: 624
    
    LOST:   27
    GAINED: 26
    
    Meteor Lake, DG2, Tiger Lake, and Ice Lake had similar results. (Meteor Lake shown)
    total instructions in shared programs: 19825635 -> 19821391 (-0.02%)
    instructions in affected programs: 138675 -> 134431 (-3.06%)
    helped: 877 / HURT: 0
    
    total cycles in shared programs: 907900598 -> 907885713 (<.01%)
    cycles in affected programs: 7127161 -> 7112276 (-0.21%)
    helped: 318 / HURT: 242
    
    total spills in shared programs: 5790 -> 5758 (-0.55%)
    spills in affected programs: 660 -> 628 (-4.85%)
    helped: 8 / HURT: 0
    
    total fills in shared programs: 6744 -> 6712 (-0.47%)
    fills in affected programs: 708 -> 676 (-4.52%)
    helped: 8 / HURT: 0
    
    LOST:   10
    GAINED: 0
    
    Skylake
    total instructions in shared programs: 18722197 -> 18637637 (-0.45%)
    instructions in affected programs: 2757553 -> 2672993 (-3.07%)
    helped: 12290 / HURT: 1
    
    total cycles in shared programs: 859716039 -> 859432560 (-0.03%)
    cycles in affected programs: 113731837 -> 113448358 (-0.25%)
    helped: 9555 / HURT: 2422
    
    LOST:   265
    GAINED: 714
    
    fossil-db:
    
    Lunar Lake, Meteor Lake, and DG2 had similar results. (Lunar Lake shown)
    Totals:
    Instrs: 142000618 -> 141928331 (-0.05%); split: -0.05%, +0.00%
    Subgroup size: 10995136 -> 10995072 (-0.00%)
    Cycle count: 21994723230 -> 21990481140 (-0.02%); split: -0.08%, +0.06%
    Spill count: 69911 -> 69754 (-0.22%); split: -0.23%, +0.00%
    Fill count: 128723 -> 128559 (-0.13%); split: -0.15%, +0.02%
    Scratch Memory Size: 5936128 -> 5934080 (-0.03%)
    Max live registers: 48006880 -> 48020936 (+0.03%); split: -0.01%, +0.04%
    
    Totals from 17450 (3.16% of 551410) affected shaders:
    Instrs: 14984149 -> 14911862 (-0.48%); split: -0.48%, +0.00%
    Subgroup size: 365744 -> 365680 (-0.02%)
    Cycle count: 2585095128 -> 2580853038 (-0.16%); split: -0.71%, +0.54%
    Spill count: 20893 -> 20736 (-0.75%); split: -0.76%, +0.00%
    Fill count: 44181 -> 44017 (-0.37%); split: -0.44%, +0.07%
    Scratch Memory Size: 995328 -> 993280 (-0.21%)
    Max live registers: 2378069 -> 2392125 (+0.59%); split: -0.20%, +0.79%
    
    Tiger Lake, Ice Lake, and Skylake had similar results. (Tiger Lake shown)
    Totals:
    Instrs: 150719758 -> 150676269 (-0.03%); split: -0.04%, +0.01%
    Subgroup size: 7764560 -> 7764632 (+0.00%)
    Cycle count: 15526689814 -> 15525687740 (-0.01%); split: -0.03%, +0.02%
    Spill count: 60120 -> 59472 (-1.08%); split: -1.17%, +0.10%
    Fill count: 105973 -> 104675 (-1.22%); split: -1.40%, +0.17%
    Scratch Memory Size: 2396160 -> 2381824 (-0.60%); split: -0.73%, +0.13%
    Max live registers: 31782879 -> 31788857 (+0.02%); split: -0.01%, +0.03%
    Max dispatch width: 5569200 -> 5569344 (+0.00%); split: +0.00%, -0.00%
    
    Totals from 10089 (1.60% of 632405) affected shaders:
    Instrs: 6389866 -> 6346377 (-0.68%); split: -0.87%, +0.19%
    Subgroup size: 102912 -> 102984 (+0.07%)
    Cycle count: 681310278 -> 680308204 (-0.15%); split: -0.65%, +0.51%
    Spill count: 19571 -> 18923 (-3.31%); split: -3.61%, +0.30%
    Fill count: 38229 -> 36931 (-3.40%); split: -3.88%, +0.48%
    Scratch Memory Size: 808960 -> 794624 (-1.77%); split: -2.15%, +0.38%
    Max live registers: 677473 -> 683451 (+0.88%); split: -0.45%, +1.33%
    Max dispatch width: 88672 -> 88816 (+0.16%); split: +0.27%, -0.11%
    
    Reviewed-by: Kenneth Graunke <kenneth@whitecape.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32041>

--- src/intel/compiler/brw_fs_opt.cpp
+++ src/intel/compiler/brw_fs_opt.cpp
@@ -156,18 +156,20 @@ brw_fs_optimize(fs_visitor &s)
    progress = false;
    OPT(brw_fs_lower_derivatives);
    OPT(brw_fs_lower_regioning);
-   if (progress) {
-      /* Try both copy propagation passes.  The defs one will likely not be
-       * able to handle everything at this point.
-       */
-      const bool cp1 = OPT(brw_fs_opt_copy_propagation_defs);
-      const bool cp2 = OPT(brw_fs_opt_copy_propagation);
-      if (cp1 || cp2) {
-         OPT(brw_fs_opt_combine_constants);
-      }
-      OPT(brw_fs_opt_dead_code_eliminate);
+
+   /* Try both copy propagation passes.  The defs one will likely not be
+    * able to handle everything at this point.
+    */
+   const bool cp1 = OPT(brw_fs_opt_copy_propagation_defs);
+   const bool cp2 = OPT(brw_fs_opt_copy_propagation);
+   if (cp1 || cp2)
+      OPT(brw_fs_opt_combine_constants);
+
+   OPT(brw_fs_opt_dead_code_eliminate);
+   OPT(brw_fs_opt_register_coalesce);
+
+   if (progress)
       OPT(brw_fs_lower_simd_width);
-   }
 
    OPT(brw_fs_lower_sends_overlapping_payload);
 
