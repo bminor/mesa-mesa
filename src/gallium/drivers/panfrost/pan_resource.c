eac8f1d4602cb1e44793b959c5680c92c9854be7
Erik Faye-Lund <erik.faye-lund@collabora.com>
2024-11-04 16:40:06 +0100

    Revert "panfrost: Disable CRC by default"
    There's been a bunch of CRC fixes applied recently. Let's see if this
    allows us to default to this as enabled instead.
    
    This reverts commit fc30fe5bc5e744dd1cdb7a69cc8a5c382e940fbe.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31965>

--- src/gallium/drivers/panfrost/pan_resource.c
+++ src/gallium/drivers/panfrost/pan_resource.c
@@ -526,10 +526,6 @@ static bool
 panfrost_should_checksum(const struct panfrost_device *dev,
                          const struct panfrost_resource *pres)
 {
-   /* Checksumming is disabled by default due to fundamental unsoundness */
-   if (!(dev->debug & PAN_DBG_CRC))
-      return false;
-
    /* When checksumming is enabled, the tile data must fit in the
     * size of the writeback buffer, so don't checksum formats
     * that use too much space. */
@@ -540,7 +536,8 @@ panfrost_should_checksum(const struct panfrost_device *dev,
                               util_format_get_blocksize(pres->base.format);
 
    return pres->base.bind & PIPE_BIND_RENDER_TARGET && panfrost_is_2d(pres) &&
-          bytes_per_pixel <= bytes_per_pixel_max && pres->base.last_level == 0;
+          bytes_per_pixel <= bytes_per_pixel_max &&
+          pres->base.last_level == 0 && !(dev->debug & PAN_DBG_NO_CRC);
 }
 
 static void
