7d6e923a4fc83d520039828365a539d06084a89e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2024-11-17 18:17:41 -0400

    vtn: gather workgroup size in libraries
    we have multiple kernels. that's fine, gather sizes for each.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Karol Herbst <kherbst@redhat.com>
    Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32382>

--- src/compiler/spirv/vtn_cfg.c
+++ src/compiler/spirv/vtn_cfg.c
@@ -240,6 +240,29 @@ function_decoration_cb(struct vtn_builder *b, struct vtn_value *val, int member,
    }
 }
 
+/*
+ * Usually, execution modes are per-shader and handled elsewhere. However, with
+ * create_library we will have modes per-nir_function. We can't represent all
+ * SPIR-V execution modes in nir_function, so this is lossy for multi-entrypoint
+ * SPIR-V. However, we do have workgroup_size in nir_function so we gather that
+ * here. If other execution modes are needed in the multi-entrypoint case, both
+ * nir_function and this callback will need to be extended suitably.
+ */
+static void
+function_execution_mode_cb(struct vtn_builder *b, struct vtn_value *func,
+                           const struct vtn_decoration *mode, void *data)
+{
+   nir_function *nir_func = data;
+
+   if (mode->exec_mode == SpvExecutionModeLocalSize) {
+      vtn_assert(b->shader->info.stage == MESA_SHADER_KERNEL);
+
+      nir_func->workgroup_size[0] = mode->operands[0];
+      nir_func->workgroup_size[1] = mode->operands[1];
+      nir_func->workgroup_size[2] = mode->operands[2];
+   }
+}
+
 bool
 vtn_cfg_handle_prepass_instruction(struct vtn_builder *b, SpvOp opcode,
                                    const uint32_t *w, unsigned count)
@@ -267,6 +290,12 @@ vtn_cfg_handle_prepass_instruction(struct vtn_builder *b, SpvOp opcode,
 
       nir_function *func =
          nir_function_create(b->shader, ralloc_strdup(b->shader, val->name));
+
+      /* Execution modes are gathered per-function with create_library (here)
+       * but per shader with !create_library (elsewhere).
+       */
+      if (b->options->create_library)
+         vtn_foreach_execution_mode(b, val, function_execution_mode_cb, func);
 
       unsigned num_params = 0;
       for (unsigned i = 0; i < func_type->length; i++)
