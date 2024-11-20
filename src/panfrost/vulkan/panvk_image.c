aerisarn/mesa-uwp

commit d970fe2e9d6a8e9997a0ce212146d62013b3b455

panfrost: Add a Vulkan driver for Midgard/Bifrost GPUs
Based on turnip.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Kristian H. Kristensen <hoegsberg@google.com>
Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
Acked-by: Jason Ekstrand <jason@jlekstrand.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/11139>

src/panfrost/vulkan/panvk_image.c
@@ -1,0 +1,438 @@
+/*
+ * Copyright © 2021 Collabora Ltd.
+ *
+ * Derived from tu_image.c which is:
+ * Copyright © 2016 Red Hat.
+ * Copyright © 2016 Bas Nieuwenhuizen
+ * Copyright © 2015 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include "panvk_private.h"
+#include "panfrost-quirks.h"
+
+#include "util/debug.h"
+#include "util/u_atomic.h"
+#include "vk_format.h"
+#include "vk_object.h"
+#include "vk_util.h"
+#include "drm-uapi/drm_fourcc.h"
+
+unsigned
+panvk_image_get_plane_size(const struct panvk_image *image, unsigned plane)
+{
+   assert(!plane);
+   return image->pimage.layout.data_size;
+}
+
+unsigned
+panvk_image_get_total_size(const struct panvk_image *image)
+{
+   assert(util_format_get_num_planes(image->pimage.layout.format) == 1);
+   return image->pimage.layout.data_size;
+}
+
+static enum mali_texture_dimension
+panvk_image_type_to_mali_tex_dim(VkImageType type)
+{
+   switch (type) {
+   case VK_IMAGE_TYPE_1D: return MALI_TEXTURE_DIMENSION_1D;
+   case VK_IMAGE_TYPE_2D: return MALI_TEXTURE_DIMENSION_2D;
+   case VK_IMAGE_TYPE_3D: return MALI_TEXTURE_DIMENSION_3D;
+   default: unreachable("Invalid image type");
+   }
+}
+
+static VkResult
+panvk_image_create(VkDevice _device,
+                   const VkImageCreateInfo *pCreateInfo,
+                   const VkAllocationCallbacks *alloc,
+                   VkImage *pImage,
+                   uint64_t modifier,
+                   const VkSubresourceLayout *plane_layouts)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   const struct panfrost_device *pdev = &device->physical_device->pdev;
+   struct panvk_image *image = NULL;
+   assert(pCreateInfo->sType == VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO);
+
+   assert(pCreateInfo->mipLevels > 0);
+   assert(pCreateInfo->arrayLayers > 0);
+   assert(pCreateInfo->samples > 0);
+   assert(pCreateInfo->extent.width > 0);
+   assert(pCreateInfo->extent.height > 0);
+   assert(pCreateInfo->extent.depth > 0);
+
+   image = vk_object_zalloc(&device->vk, alloc, sizeof(*image),
+                            VK_OBJECT_TYPE_IMAGE);
+   if (!image)
+      return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   image->type = pCreateInfo->imageType;
+
+   image->vk_format = pCreateInfo->format;
+   image->tiling = pCreateInfo->tiling;
+   image->usage = pCreateInfo->usage;
+   image->flags = pCreateInfo->flags;
+   image->extent = pCreateInfo->extent;
+   pan_image_layout_init(pdev, &image->pimage.layout, modifier,
+                         vk_format_to_pipe_format(pCreateInfo->format),
+                         panvk_image_type_to_mali_tex_dim(pCreateInfo->imageType),
+                         pCreateInfo->extent.width, pCreateInfo->extent.height,
+                         pCreateInfo->extent.depth, pCreateInfo->arrayLayers,
+                         pCreateInfo->samples, pCreateInfo->mipLevels,
+                         PAN_IMAGE_CRC_NONE, NULL);
+
+   image->exclusive = pCreateInfo->sharingMode == VK_SHARING_MODE_EXCLUSIVE;
+   if (pCreateInfo->sharingMode == VK_SHARING_MODE_CONCURRENT) {
+      for (uint32_t i = 0; i < pCreateInfo->queueFamilyIndexCount; ++i) {
+         if (pCreateInfo->pQueueFamilyIndices[i] == VK_QUEUE_FAMILY_EXTERNAL)
+            image->queue_family_mask |= (1u << PANVK_MAX_QUEUE_FAMILIES) - 1u;
+         else
+            image->queue_family_mask |= 1u << pCreateInfo->pQueueFamilyIndices[i];
+       }
+   }
+
+   if (vk_find_struct_const(pCreateInfo->pNext, EXTERNAL_MEMORY_IMAGE_CREATE_INFO))
+      image->shareable = true;
+
+   *pImage = panvk_image_to_handle(image);
+   return VK_SUCCESS;
+}
+
+static uint64_t
+panvk_image_select_mod(VkDevice _device,
+                       const VkImageCreateInfo *pCreateInfo,
+                       const VkSubresourceLayout **plane_layouts)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   const struct panfrost_device *pdev = &device->physical_device->pdev;
+   enum pipe_format fmt = vk_format_to_pipe_format(pCreateInfo->format);
+   bool noafbc = !(device->physical_device->instance->debug_flags & PANVK_DEBUG_AFBC);
+   bool linear = device->physical_device->instance->debug_flags & PANVK_DEBUG_LINEAR;
+
+   *plane_layouts = NULL;
+
+   if (pCreateInfo->tiling == VK_IMAGE_TILING_LINEAR)
+      return DRM_FORMAT_MOD_LINEAR;
+
+   if (pCreateInfo->tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
+      const VkImageDrmFormatModifierListCreateInfoEXT *mod_info =
+         vk_find_struct_const(pCreateInfo->pNext,
+                              IMAGE_DRM_FORMAT_MODIFIER_LIST_CREATE_INFO_EXT);
+      const VkImageDrmFormatModifierExplicitCreateInfoEXT *drm_explicit_info =
+         vk_find_struct_const(pCreateInfo->pNext,
+                              IMAGE_DRM_FORMAT_MODIFIER_EXPLICIT_CREATE_INFO_EXT);
+
+      assert(mod_info || drm_explicit_info);
+
+      uint64_t modifier;
+
+      if (mod_info) {
+         modifier = DRM_FORMAT_MOD_LINEAR;
+         for (unsigned i = 0; i < mod_info->drmFormatModifierCount; i++) {
+            if (drm_is_afbc(mod_info->pDrmFormatModifiers[i]) && !noafbc) {
+               modifier = mod_info->pDrmFormatModifiers[i];
+               break;
+            }
+         }
+      } else {
+         modifier = drm_explicit_info->drmFormatModifier;
+         assert(modifier == DRM_FORMAT_MOD_LINEAR ||
+                modifier == DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED ||
+                (drm_is_afbc(modifier) && !noafbc));
+         *plane_layouts = drm_explicit_info->pPlaneLayouts;
+      }
+
+      return modifier;
+   }
+
+   const struct wsi_image_create_info *wsi_info =
+      vk_find_struct_const(pCreateInfo->pNext, WSI_IMAGE_CREATE_INFO_MESA);
+   if (wsi_info && wsi_info->scanout)
+      return DRM_FORMAT_MOD_LINEAR;
+
+   assert(pCreateInfo->tiling == VK_IMAGE_TILING_OPTIMAL);
+
+   if (linear)
+      return DRM_FORMAT_MOD_LINEAR;
+
+   /* Image store don't work on AFBC images */
+   if (pCreateInfo->usage & VK_IMAGE_USAGE_STORAGE_BIT)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   /* AFBC does not support layered multisampling */
+   if (pCreateInfo->samples > 1)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   if (pdev->quirks & MIDGARD_NO_AFBC)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   /* Only a small selection of formats are AFBC'able */
+   if (!panfrost_format_supports_afbc(pdev, fmt))
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   /* 3D AFBC is only supported on Bifrost v7+. It's supposed to
+    * be supported on Midgard but it doesn't seem to work.
+    */
+   if (pCreateInfo->imageType == VK_IMAGE_TYPE_3D && pdev->arch < 7)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   /* For one tile, AFBC is a loss compared to u-interleaved */
+   if (pCreateInfo->extent.width <= 16 && pCreateInfo->extent.height <= 16)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   if (noafbc)
+      return DRM_FORMAT_MOD_ARM_16X16_BLOCK_U_INTERLEAVED;
+
+   uint64_t afbc_type = AFBC_FORMAT_MOD_BLOCK_SIZE_16x16 |
+                        AFBC_FORMAT_MOD_SPARSE;
+
+   if (panfrost_afbc_can_ytr(fmt))
+      afbc_type |= AFBC_FORMAT_MOD_YTR;
+
+   return DRM_FORMAT_MOD_ARM_AFBC(afbc_type);
+}
+
+VkResult
+panvk_CreateImage(VkDevice device,
+                  const VkImageCreateInfo *pCreateInfo,
+                  const VkAllocationCallbacks *pAllocator,
+                  VkImage *pImage)
+{
+   const VkSubresourceLayout *plane_layouts;
+   uint64_t modifier = panvk_image_select_mod(device, pCreateInfo, &plane_layouts);
+
+   return panvk_image_create(device, pCreateInfo, pAllocator, pImage, modifier, plane_layouts);
+}
+
+void
+panvk_DestroyImage(VkDevice _device,
+                   VkImage _image,
+                   const VkAllocationCallbacks *pAllocator)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   VK_FROM_HANDLE(panvk_image, image, _image);
+
+   if (!image)
+      return;
+
+   vk_object_free(&device->vk, pAllocator, image);
+}
+
+static unsigned
+panvk_plane_index(VkFormat format, VkImageAspectFlags aspect_mask)
+{
+   switch (aspect_mask) {
+   default:
+      return 0;
+   case VK_IMAGE_ASPECT_PLANE_1_BIT:
+      return 1;
+   case VK_IMAGE_ASPECT_PLANE_2_BIT:
+      return 2;
+   case VK_IMAGE_ASPECT_STENCIL_BIT:
+      return format == VK_FORMAT_D32_SFLOAT_S8_UINT;
+   }
+}
+
+void
+panvk_GetImageSubresourceLayout(VkDevice _device,
+                                VkImage _image,
+                                const VkImageSubresource *pSubresource,
+                                VkSubresourceLayout *pLayout)
+{
+   VK_FROM_HANDLE(panvk_image, image, _image);
+
+   unsigned plane = panvk_plane_index(image->vk_format, pSubresource->aspectMask);
+   assert(plane < PANVK_MAX_PLANES);
+
+   const struct pan_image_slice_layout *slice_layout =
+      &image->pimage.layout.slices[pSubresource->mipLevel];
+
+   pLayout->offset = slice_layout->offset +
+                     (pSubresource->arrayLayer *
+                      image->pimage.layout.array_stride);
+   pLayout->size = slice_layout->size;
+   pLayout->rowPitch = slice_layout->line_stride;
+   pLayout->arrayPitch = image->pimage.layout.array_stride;
+   pLayout->depthPitch = slice_layout->surface_stride;
+}
+
+static enum mali_texture_dimension
+panvk_view_type_to_mali_tex_dim(VkImageViewType type)
+{
+   switch (type) {
+   case VK_IMAGE_VIEW_TYPE_1D:
+   case VK_IMAGE_VIEW_TYPE_1D_ARRAY:
+      return MALI_TEXTURE_DIMENSION_1D;
+   case VK_IMAGE_VIEW_TYPE_2D:
+   case VK_IMAGE_VIEW_TYPE_2D_ARRAY:
+      return MALI_TEXTURE_DIMENSION_2D;
+   case VK_IMAGE_VIEW_TYPE_3D:
+      return MALI_TEXTURE_DIMENSION_3D;
+   case VK_IMAGE_VIEW_TYPE_CUBE:
+   case VK_IMAGE_VIEW_TYPE_CUBE_ARRAY:
+      return MALI_TEXTURE_DIMENSION_CUBE;
+   default:
+      unreachable("Invalid view type");
+   }
+}
+
+static void
+panvk_convert_swizzle(const VkComponentMapping *in,
+                      unsigned char *out)
+{
+   const VkComponentSwizzle *comp = &in->r;
+   for (unsigned i = 0; i < 4; i++) {
+      switch (comp[i]) {
+      case VK_COMPONENT_SWIZZLE_IDENTITY:
+         out[i] = PIPE_SWIZZLE_X + i;
+         break;
+      case VK_COMPONENT_SWIZZLE_ZERO:
+         out[i] = PIPE_SWIZZLE_0;
+         break;
+      case VK_COMPONENT_SWIZZLE_ONE:
+         out[i] = PIPE_SWIZZLE_1;
+         break;
+      case VK_COMPONENT_SWIZZLE_R:
+         out[i] = PIPE_SWIZZLE_X;
+         break;
+      case VK_COMPONENT_SWIZZLE_G:
+         out[i] = PIPE_SWIZZLE_Y;
+         break;
+      case VK_COMPONENT_SWIZZLE_B:
+         out[i] = PIPE_SWIZZLE_Z;
+         break;
+      case VK_COMPONENT_SWIZZLE_A:
+         out[i] = PIPE_SWIZZLE_W;
+         break;
+      default:
+         unreachable("Invalid swizzle");
+      }
+   }
+}
+
+
+
+VkResult
+panvk_CreateImageView(VkDevice _device,
+                      const VkImageViewCreateInfo *pCreateInfo,
+                      const VkAllocationCallbacks *pAllocator,
+                      VkImageView *pView)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   VK_FROM_HANDLE(panvk_image, image, pCreateInfo->image);
+   struct panvk_image_view *view;
+
+   view = vk_object_zalloc(&device->vk, pAllocator, sizeof(*view),
+                          VK_OBJECT_TYPE_IMAGE_VIEW);
+   if (view == NULL)
+      return vk_error(device->instance, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   view->pview.format = vk_format_to_pipe_format(pCreateInfo->format);
+
+   if (pCreateInfo->subresourceRange.aspectMask == VK_IMAGE_ASPECT_DEPTH_BIT)
+      view->pview.format = util_format_get_depth_only(view->pview.format);
+   else if (pCreateInfo->subresourceRange.aspectMask == VK_IMAGE_ASPECT_STENCIL_BIT)
+      view->pview.format = util_format_stencil_only(view->pview.format);
+
+   view->pview.dim = panvk_view_type_to_mali_tex_dim(pCreateInfo->viewType);
+   view->pview.first_level = pCreateInfo->subresourceRange.baseMipLevel;
+   view->pview.last_level = pCreateInfo->subresourceRange.baseMipLevel +
+                            pCreateInfo->subresourceRange.levelCount - 1;
+   view->pview.first_layer = pCreateInfo->subresourceRange.baseArrayLayer;
+   view->pview.last_layer = pCreateInfo->subresourceRange.baseArrayLayer +
+                            pCreateInfo->subresourceRange.layerCount - 1;
+   panvk_convert_swizzle(&pCreateInfo->components, view->pview.swizzle);
+   view->pview.image = &image->pimage;
+   view->pview.nr_samples = image->pimage.layout.nr_samples;
+   view->vk_format = pCreateInfo->format;
+
+   struct panfrost_device *pdev = &device->physical_device->pdev;
+   unsigned bo_size =
+      panfrost_estimate_texture_payload_size(pdev, &view->pview);
+
+   unsigned surf_descs_offset = 0;
+   if (!pan_is_bifrost(pdev)) {
+      bo_size += MALI_MIDGARD_TEXTURE_LENGTH;
+      surf_descs_offset = MALI_MIDGARD_TEXTURE_LENGTH;
+   }
+
+   view->bo = panfrost_bo_create(pdev, bo_size, 0, "Texture descriptor");
+
+   struct panfrost_ptr surf_descs = {
+      .cpu = view->bo->ptr.cpu + surf_descs_offset,
+      .gpu = view->bo->ptr.gpu + surf_descs_offset,
+   };
+   void *tex_desc = pan_is_bifrost(pdev) ?
+                    &view->bifrost.tex_desc : view->bo->ptr.cpu;
+
+   panfrost_new_texture(pdev, &view->pview, tex_desc, &surf_descs);
+
+   *pView = panvk_image_view_to_handle(view);
+   return VK_SUCCESS;
+}
+
+void
+panvk_DestroyImageView(VkDevice _device,
+                       VkImageView _view,
+                       const VkAllocationCallbacks *pAllocator)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   VK_FROM_HANDLE(panvk_image_view, view, _view);
+
+   if (!view)
+      return;
+
+   panfrost_bo_unreference(view->bo);
+   vk_object_free(&device->vk, pAllocator, view);
+}
+
+VkResult
+panvk_CreateBufferView(VkDevice _device,
+                       const VkBufferViewCreateInfo *pCreateInfo,
+                       const VkAllocationCallbacks *pAllocator,
+                       VkBufferView *pView)
+{
+   panvk_stub();
+   return VK_SUCCESS;
+}
+
+void
+panvk_DestroyBufferView(VkDevice _device,
+                        VkBufferView bufferView,
+                        const VkAllocationCallbacks *pAllocator)
+{
+   panvk_stub();
+}
+
+VkResult
+panvk_GetImageDrmFormatModifierPropertiesEXT(VkDevice device,
+                                             VkImage _image,
+                                             VkImageDrmFormatModifierPropertiesEXT *pProperties)
+{
+   VK_FROM_HANDLE(panvk_image, image, _image);
+
+   assert(pProperties->sType == VK_STRUCTURE_TYPE_IMAGE_DRM_FORMAT_MODIFIER_PROPERTIES_EXT);
+
+   pProperties->drmFormatModifier = image->pimage.layout.modifier;
+   return VK_SUCCESS;
+}

meson.build
@@ -278,6 +278,7 @@ endif
 with_intel_vk = _vulkan_drivers.contains('intel')
 with_amd_vk = _vulkan_drivers.contains('amd')
 with_freedreno_vk = _vulkan_drivers.contains('freedreno')
+with_panfrost_vk = _vulkan_drivers.contains('panfrost')
 with_swrast_vk = _vulkan_drivers.contains('swrast')
 with_virtio_vk = _vulkan_drivers.contains('virtio-experimental')
 with_freedreno_kgsl = get_option('freedreno-kgsl')

src/meson.build
@@ -86,7 +86,7 @@ endif
 if with_gallium_freedreno or with_freedreno_vk
   subdir('freedreno')
 endif
-if with_gallium_panfrost or with_gallium_lima
+if with_gallium_panfrost or with_gallium_lima or with_panfrost_vk
   subdir('panfrost')
 endif
 if with_gallium_virgl or with_virtio_vk

src/panfrost/meson.build
@@ -66,3 +66,7 @@ bifrost_compiler = executable(
   ],
   build_by_default : with_tools.contains('panfrost')
 )
+
+if with_panfrost_vk
+  subdir('vulkan')
+endif

src/panfrost/vulkan/meson.build
@@ -1,0 +1,122 @@
+# Copyright © 2021 Collabora Ltd.
+#
+# Derived from the freedreno driver which is:
+# Copyright © 2017 Intel Corporation
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+panvk_entrypoints = custom_target(
+  'panvk_entrypoints.[ch]',
+  input : [vk_entrypoints_gen, vk_api_xml],
+  output : ['panvk_entrypoints.h', 'panvk_entrypoints.c'],
+  command : [
+    prog_python, '@INPUT0@', '--xml', '@INPUT1@', '--proto', '--weak',
+    '--out-h', '@OUTPUT0@', '--out-c', '@OUTPUT1@', '--prefix', 'panvk',
+  ],
+)
+
+libpanvk_files = files(
+  'panvk_cmd_buffer.c',
+  'panvk_cs.c',
+  'panvk_device.c',
+  'panvk_descriptor_set.c',
+  'panvk_formats.c',
+  'panvk_image.c',
+  'panvk_meta.c',
+  'panvk_pass.c',
+  'panvk_pipeline.c',
+  'panvk_pipeline_cache.c',
+  'panvk_private.h',
+  'panvk_query.c',
+  'panvk_shader.c',
+  'panvk_sync.c',
+  'panvk_util.c',
+  'panvk_varyings.c',
+  'panvk_wsi.c',
+  'panvk_wsi_display.c',
+)
+
+panvk_deps = []
+panvk_flags = []
+
+if system_has_kms_drm
+  panvk_flags += '-DVK_USE_PLATFORM_DISPLAY_KHR'
+  libpanvk_files += files('panvk_wsi_display.c')
+endif
+
+if with_platform_wayland
+  panvk_deps += [dep_wayland_client, dep_wl_protocols]
+  panvk_flags += '-DVK_USE_PLATFORM_WAYLAND_KHR'
+  libpanvk_files += files('panvk_wsi_wayland.c')
+  libpanvk_files += [wayland_drm_client_protocol_h, wayland_drm_protocol_c]
+endif
+
+libvulkan_panfrost = shared_library(
+  'vulkan_panfrost',
+  [libpanvk_files, panvk_entrypoints],
+  include_directories : [
+    inc_include,
+    inc_src,
+    inc_compiler,
+    inc_gallium, # XXX: pipe/p_format.h
+    inc_gallium_aux, # XXX: renderonly
+    inc_vulkan_wsi,
+    inc_panfrost,
+  ],
+  link_with : [
+    libvulkan_wsi,
+    libpanfrost_shared,
+    libpanfrost_midgard,
+    libpanfrost_bifrost,
+    libpanfrost_decode,
+    libpanfrost_lib,
+    libpanfrost_util,
+  ],
+  dependencies : [
+    dep_dl,
+    dep_elf,
+    dep_libdrm,
+    dep_m,
+    dep_thread,
+    dep_valgrind,
+    idep_nir,
+    panvk_deps,
+    idep_vulkan_util,
+    idep_mesautil,
+  ],
+  c_args : [no_override_init_args, panvk_flags],
+  link_args : [ld_args_bsymbolic, ld_args_gc_sections],
+  install : true,
+)
+
+panfrost_icd = custom_target(
+  'panfrost_icd',
+  input : [vk_icd_gen, vk_api_xml],
+  output : 'panfrost_icd.@0@.json'.format(host_machine.cpu()),
+  command : [
+    prog_python, '@INPUT0@',
+    '--api-version', '1.1', '--xml', '@INPUT1@',
+    '--lib-path', join_paths(get_option('prefix'), get_option('libdir'),
+                             'libvulkan_panfrost.so'),
+    '--out', '@OUTPUT@',
+  ],
+  build_by_default : true,
+  install_dir : with_vulkan_icd_dir,
+  install : true,
+)

src/panfrost/vulkan/panvk_cs.h
@@ -1,0 +1,164 @@
+/*
+ * Copyright (C) 2021 Collabora Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef PANVK_CS_H
+#define PANVK_CS_H
+
+#include <vulkan/vulkan.h>
+
+#include "compiler/shader_enums.h"
+#include "panfrost-job.h"
+#include "pan_cs.h"
+
+struct pan_blend_state;
+struct pan_shader_info;
+struct panfrost_ptr;
+struct pan_pool;
+
+union panvk_sysval_data;
+struct panvk_framebuffer;
+struct panvk_cmd_state;
+struct panvk_compute_dim;
+struct panvk_device;
+struct panvk_batch;
+struct panvk_varyings_info;
+struct panvk_attrib_buf;
+struct panvk_attribs_info;
+struct panvk_pipeline;
+struct panvk_draw_info;
+struct panvk_descriptor_state;
+struct panvk_subpass;
+struct panvk_clear_value;
+
+void
+panvk_emit_varyings(const struct panvk_device *dev,
+                    const struct panvk_varyings_info *varyings,
+                    gl_shader_stage stage,
+                    void *descs);
+
+void
+panvk_emit_varying_bufs(const struct panvk_device *dev,
+                        const struct panvk_varyings_info *varyings,
+                        void *descs);
+
+void
+panvk_emit_attrib_bufs(const struct panvk_device *dev,
+                       const struct panvk_attribs_info *info,
+                       const struct panvk_attrib_buf *bufs,
+                       unsigned buf_count,
+                       const struct panvk_draw_info *draw,
+                       void *descs);
+
+void
+panvk_emit_attribs(const struct panvk_device *dev,
+                   const struct panvk_attribs_info *attribs,
+                   const struct panvk_attrib_buf *bufs,
+                   unsigned buf_count,
+                   void *descs);
+
+void
+panvk_emit_ubos(const struct panvk_pipeline *pipeline,
+                const struct panvk_descriptor_state *state,
+                void *descs);
+
+void
+panvk_emit_vertex_job(const struct panvk_device *dev,
+                      const struct panvk_pipeline *pipeline,
+                      const struct panvk_draw_info *draw,
+                      void *job);
+
+void
+panvk_emit_tiler_job(const struct panvk_device *dev,
+                     const struct panvk_pipeline *pipeline,
+                     const struct panvk_draw_info *draw,
+                     void *job);
+
+void
+panvk_emit_fragment_job(const struct panvk_device *dev,
+                        const struct panvk_framebuffer *fb,
+                        mali_ptr fbdesc,
+                        void *job);
+
+void
+panvk_emit_viewport(const VkViewport *viewport, const VkRect2D *scissor,
+                    void *vpd);
+
+void
+panvk_emit_blend(const struct panvk_device *dev,
+                 const struct panvk_pipeline *pipeline,
+                 unsigned rt, void *bd);
+
+void
+panvk_emit_blend_constant(const struct panvk_device *dev,
+                          const struct panvk_pipeline *pipeline,
+                          unsigned rt, const float *constants, void *bd);
+
+void
+panvk_emit_dyn_fs_rsd(const struct panvk_device *dev,
+                      const struct panvk_pipeline *pipeline,
+                      const struct panvk_cmd_state *state,
+                      void *rsd);
+
+void
+panvk_emit_base_fs_rsd(const struct panvk_device *dev,
+                       const struct panvk_pipeline *pipeline,
+                       void *rsd);
+
+void
+panvk_emit_non_fs_rsd(const struct panvk_device *dev,
+                      const struct pan_shader_info *shader_info,
+                      mali_ptr shader_ptr,
+                      void *rsd);
+
+void
+panvk_emit_bifrost_tiler_context(const struct panvk_device *dev,
+                                 unsigned width, unsigned height,
+                                 const struct panfrost_ptr *descs);
+
+unsigned
+panvk_emit_fb(const struct panvk_device *dev,
+              const struct panvk_batch *batch,
+              const struct panvk_subpass *subpass,
+              const struct panvk_pipeline *pipeline,
+              const struct panvk_framebuffer *fb,
+              const struct panvk_clear_value *clears,
+              const struct pan_tls_info *tlsinfo,
+              const struct pan_tiler_context *tilerctx,
+              void *desc);
+
+void
+panvk_emit_tls(const struct panvk_device *dev,
+               const struct panvk_pipeline *pipeline,
+               const struct pan_compute_dim *wg_count,
+               struct pan_pool *tls_pool,
+               void *desc);
+
+void
+panvk_sysval_upload_viewport_scale(const VkViewport *viewport,
+                                   union panvk_sysval_data *data);
+
+void
+panvk_sysval_upload_viewport_offset(const VkViewport *viewport,
+                                    union panvk_sysval_data *data);
+
+#endif

bminor/mesa-mesa

commit f77fe432c11d6ca0c22e98e8a9350b27eb61f98d

panvk: support binding swapchain memory
This is required by VK_KHR_swapchain, so let's support it.

Fixes a crash in this CTS test-case:
dEQP-VK.wsi.wayland.swapchain.simulate_oom.image_swapchain_create_info

Fixes: d970fe2e9d6 ("panfrost: Add a Vulkan driver for Midgard/Bifrost GPUs")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32216>

src/panfrost/vulkan/panvk_image.c
@@ -393,19 +393,34 @@ VKAPI_ATTR VkResult VKAPI_CALL
 panvk_BindImageMemory2(VkDevice device, uint32_t bindInfoCount,
                        const VkBindImageMemoryInfo *pBindInfos)
 {
+   const VkBindImageMemorySwapchainInfoKHR *swapchain_info =
+      vk_find_struct_const(pBindInfos->pNext, BIND_IMAGE_MEMORY_SWAPCHAIN_INFO_KHR);
+
    for (uint32_t i = 0; i < bindInfoCount; ++i) {
       VK_FROM_HANDLE(panvk_image, image, pBindInfos[i].image);
-      VK_FROM_HANDLE(panvk_device_memory, mem, pBindInfos[i].memory);
       struct pan_kmod_bo *old_bo = image->bo;
 
-      assert(mem);
-      image->bo = pan_kmod_bo_get(mem->bo);
-      image->pimage.data.base = mem->addr.dev;
-      image->pimage.data.offset = pBindInfos[i].memoryOffset;
+      if (swapchain_info && swapchain_info->swapchain != VK_NULL_HANDLE) {
+         VkImage wsi_vk_image = wsi_common_get_image(swapchain_info->swapchain,
+                                                   swapchain_info->imageIndex);
+         VK_FROM_HANDLE(panvk_image, wsi_image, wsi_vk_image);
+
+         image->bo = pan_kmod_bo_get(wsi_image->bo);
+         image->pimage.data.base = wsi_image->pimage.data.base;
+         image->pimage.data.offset = wsi_image->pimage.data.offset;
+      } else {
+         VK_FROM_HANDLE(panvk_device_memory, mem, pBindInfos[i].memory);
+         assert(mem);
+         image->bo = pan_kmod_bo_get(mem->bo);
+         image->pimage.data.base = mem->addr.dev;
+         image->pimage.data.offset = pBindInfos[i].memoryOffset;
+      }
+
       /* Reset the AFBC headers */
       if (drm_is_afbc(image->pimage.layout.modifier)) {
          /* Transient CPU mapping */
-         void *base = pan_kmod_bo_mmap(mem->bo, 0, pan_kmod_bo_size(mem->bo),
+         void *base = pan_kmod_bo_mmap(image->bo, 0,
+                                       pan_kmod_bo_size(image->bo),
                                        PROT_WRITE, MAP_SHARED, NULL);
 
          assert(base != MAP_FAILED);
@@ -422,7 +437,7 @@ panvk_BindImageMemory2(VkDevice device, uint32_t bindInfoCount,
             }
          }
 
-         ASSERTED int ret = os_munmap(base, pan_kmod_bo_size(mem->bo));
+         ASSERTED int ret = os_munmap(base, pan_kmod_bo_size(image->bo));
          assert(!ret);
       }
 