bminor/mesa-mesa

commit d1934e44fc8e5fccc31a0ffcc5357461466f3d81

panvk: Implement occlusion queries for JM
Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.cim>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31843>

src/panfrost/vulkan/panvk_vX_query_pool.c
@@ -1,0 +1,221 @@
+/*
+ * Copyright © 2024 Collabora Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "vk_log.h"
+
+#include "pan_props.h"
+#include "panvk_device.h"
+#include "panvk_entrypoints.h"
+#include "panvk_query_pool.h"
+
+#define PANVK_QUERY_TIMEOUT 2000000000ull
+
+VKAPI_ATTR VkResult VKAPI_CALL
+panvk_per_arch(CreateQueryPool)(VkDevice _device,
+                                const VkQueryPoolCreateInfo *pCreateInfo,
+                                const VkAllocationCallbacks *pAllocator,
+                                VkQueryPool *pQueryPool)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+
+   struct panvk_query_pool *pool;
+
+   pool =
+      vk_query_pool_create(&device->vk, pCreateInfo, pAllocator, sizeof(*pool));
+   if (!pool)
+      return vk_error(device, VK_ERROR_OUT_OF_HOST_MEMORY);
+
+   uint32_t reports_per_query;
+   switch (pCreateInfo->queryType) {
+   case VK_QUERY_TYPE_OCCLUSION: {
+      /* The counter is per core on Bifrost */
+#if PAN_ARCH < 9
+      const struct panvk_physical_device *phys_dev =
+         to_panvk_physical_device(device->vk.physical);
+
+      panfrost_query_core_count(&phys_dev->kmod.props, &reports_per_query);
+#else
+      reports_per_query = 1;
+#endif
+      break;
+   }
+   default:
+      unreachable("Unsupported query type");
+   }
+
+   pool->reports_per_query = reports_per_query;
+   pool->query_stride = reports_per_query * sizeof(struct panvk_query_report);
+
+   if (pool->vk.query_count > 0) {
+      struct panvk_pool_alloc_info alloc_info = {
+         .size = pool->reports_per_query * sizeof(struct panvk_query_report) *
+                 pool->vk.query_count,
+         .alignment = sizeof(struct panvk_query_report),
+      };
+      pool->mem = panvk_pool_alloc_mem(&device->mempools.rw, alloc_info);
+      if (!pool->mem.bo) {
+         vk_query_pool_destroy(&device->vk, pAllocator, &pool->vk);
+         return vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
+      }
+
+      struct panvk_pool_alloc_info syncobjs_alloc_info = {
+         .size =
+            sizeof(struct panvk_query_available_obj) * pool->vk.query_count,
+         .alignment = 64,
+      };
+      pool->available_mem =
+         panvk_pool_alloc_mem(&device->mempools.rw_nc, syncobjs_alloc_info);
+      if (!pool->available_mem.bo) {
+         panvk_pool_free_mem(&pool->mem);
+         vk_query_pool_destroy(&device->vk, pAllocator, &pool->vk);
+         return vk_error(device, VK_ERROR_OUT_OF_DEVICE_MEMORY);
+      }
+
+      memset(panvk_priv_mem_host_addr(pool->available_mem), 0,
+             sizeof(struct panvk_query_available_obj));
+   }
+
+   *pQueryPool = panvk_query_pool_to_handle(pool);
+
+   return VK_SUCCESS;
+}
+
+VKAPI_ATTR void VKAPI_CALL
+panvk_per_arch(DestroyQueryPool)(VkDevice _device, VkQueryPool queryPool,
+                                 const VkAllocationCallbacks *pAllocator)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   VK_FROM_HANDLE(panvk_query_pool, pool, queryPool);
+
+   if (!pool)
+      return;
+
+   panvk_pool_free_mem(&pool->mem);
+   panvk_pool_free_mem(&pool->available_mem);
+   vk_query_pool_destroy(&device->vk, pAllocator, &pool->vk);
+}
+
+VKAPI_ATTR void VKAPI_CALL
+panvk_per_arch(ResetQueryPool)(VkDevice device, VkQueryPool queryPool,
+                               uint32_t firstQuery, uint32_t queryCount)
+{
+   VK_FROM_HANDLE(panvk_query_pool, pool, queryPool);
+
+   struct panvk_query_available_obj *available =
+      panvk_query_available_host_addr(pool, firstQuery);
+   memset(available, 0, queryCount * sizeof(*available));
+
+   struct panvk_query_report *reports =
+      panvk_query_report_host_addr(pool, firstQuery);
+   memset(reports, 0, queryCount * pool->reports_per_query * sizeof(*reports));
+}
+
+static bool
+panvk_query_is_available(struct panvk_query_pool *pool, uint32_t query)
+{
+   struct panvk_query_available_obj *available =
+      panvk_query_available_host_addr(pool, query);
+   return p_atomic_read(&available->value) != 0;
+}
+
+static VkResult
+panvk_query_wait_for_available(struct panvk_device *dev,
+                               struct panvk_query_pool *pool, uint32_t query)
+{
+   uint64_t abs_timeout_ns = os_time_get_absolute_timeout(PANVK_QUERY_TIMEOUT);
+
+   while (os_time_get_nano() < abs_timeout_ns) {
+      if (panvk_query_is_available(pool, query))
+         return VK_SUCCESS;
+
+      VkResult status = vk_device_check_status(&dev->vk);
+      if (status != VK_SUCCESS)
+         return status;
+   }
+
+   return vk_device_set_lost(&dev->vk, "query timeout");
+}
+
+static void
+cpu_write_query_result(void *dst, uint32_t idx, VkQueryResultFlags flags,
+                       uint64_t result)
+{
+   if (flags & VK_QUERY_RESULT_64_BIT) {
+      uint64_t *dst64 = dst;
+      dst64[idx] = result;
+   } else {
+      uint32_t *dst32 = dst;
+      dst32[idx] = result;
+   }
+}
+
+static void
+cpu_write_occlusion_query_result(void *dst, uint32_t idx,
+                                 VkQueryResultFlags flags,
+                                 const struct panvk_query_report *src,
+                                 unsigned core_count)
+{
+   uint64_t result = 0;
+
+   for (uint32_t core_idx = 0; core_idx < core_count; core_idx++)
+      result += src[core_idx].value;
+
+   cpu_write_query_result(dst, idx, flags, result);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+panvk_per_arch(GetQueryPoolResults)(VkDevice _device, VkQueryPool queryPool,
+                                    uint32_t firstQuery, uint32_t queryCount,
+                                    size_t dataSize, void *pData,
+                                    VkDeviceSize stride,
+                                    VkQueryResultFlags flags)
+{
+   VK_FROM_HANDLE(panvk_device, device, _device);
+   VK_FROM_HANDLE(panvk_query_pool, pool, queryPool);
+
+   if (vk_device_is_lost(&device->vk))
+      return VK_ERROR_DEVICE_LOST;
+
+   VkResult status = VK_SUCCESS;
+   for (uint32_t i = 0; i < queryCount; i++) {
+      const uint32_t query = firstQuery + i;
+
+      bool available = panvk_query_is_available(pool, query);
+
+      if (!available && (flags & VK_QUERY_RESULT_WAIT_BIT)) {
+         status = panvk_query_wait_for_available(device, pool, query);
+         if (status != VK_SUCCESS)
+            return status;
+
+         available = true;
+      }
+
+      bool write_results = available || (flags & VK_QUERY_RESULT_PARTIAL_BIT);
+
+      const struct panvk_query_report *src =
+         panvk_query_report_host_addr(pool, query);
+      assert(i * stride < dataSize);
+      void *dst = (char *)pData + i * stride;
+
+      switch (pool->vk.query_type) {
+      case VK_QUERY_TYPE_OCCLUSION: {
+         if (write_results)
+            cpu_write_occlusion_query_result(dst, 0, flags, src,
+                                             pool->reports_per_query);
+         break;
+      }
+      default:
+         unreachable("Unsupported query type");
+      }
+
+      if (!write_results)
+         status = VK_NOT_READY;
+
+      if (flags & VK_QUERY_RESULT_WITH_AVAILABILITY_BIT)
+         cpu_write_query_result(dst, 1, flags, available);
+   }
+
+   return status;
+}

src/panfrost/vulkan/meson.build
@@ -27,7 +27,6 @@ libpanvk_files = files(
   'panvk_mempool.c',
   'panvk_physical_device.c',
   'panvk_priv_bo.c',
-  'panvk_query.c',
   'panvk_wsi.c',
 )
 libpanvk_files += [sha1_h]
@@ -53,6 +52,7 @@ jm_files = [
   'jm/panvk_vX_cmd_dispatch.c',
   'jm/panvk_vX_cmd_draw.c',
   'jm/panvk_vX_cmd_event.c',
+  'jm/panvk_vX_cmd_query.c',
   'jm/panvk_vX_event.c',
   'jm/panvk_vX_queue.c',
 ]
@@ -80,6 +80,7 @@ common_per_arch_files = [
   'panvk_vX_descriptor_set.c',
   'panvk_vX_descriptor_set_layout.c',
   'panvk_vX_device.c',
+  'panvk_vX_query_pool.c',
   'panvk_vX_image_view.c',
   'panvk_vX_nir_lower_descriptors.c',
   'panvk_vX_sampler.c',

src/panfrost/vulkan/panvk_meta.h
@@ -16,6 +16,8 @@ enum panvk_meta_object_key_type {
    PANVK_META_OBJECT_KEY_BLEND_SHADER = VK_META_OBJECT_KEY_DRIVER_OFFSET,
    PANVK_META_OBJECT_KEY_COPY_DESC_SHADER,
    PANVK_META_OBJECT_KEY_FB_PRELOAD_SHADER,
+   PANVK_META_OBJECT_KEY_CLEAR_QUERY_POOL_PIPELINE,
+   PANVK_META_OBJECT_KEY_COPY_QUERY_POOL_RESULTS_OQ_PIPELINE,
 };
 
 static inline VkFormat

src/panfrost/vulkan/panvk_query.c
@@ -1,91 +1,0 @@
-/*
- * Copyright © 2021 Collabora Ltd.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- */
-
-#include "panvk_entrypoints.h"
-#include "panvk_macros.h"
-
-VKAPI_ATTR VkResult VKAPI_CALL
-panvk_CreateQueryPool(VkDevice _device,
-                      const VkQueryPoolCreateInfo *pCreateInfo,
-                      const VkAllocationCallbacks *pAllocator,
-                      VkQueryPool *pQueryPool)
-{
-   panvk_stub();
-   return VK_SUCCESS;
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_DestroyQueryPool(VkDevice _device, VkQueryPool _pool,
-                       const VkAllocationCallbacks *pAllocator)
-{
-   panvk_stub();
-}
-
-VKAPI_ATTR VkResult VKAPI_CALL
-panvk_GetQueryPoolResults(VkDevice _device, VkQueryPool queryPool,
-                          uint32_t firstQuery, uint32_t queryCount,
-                          size_t dataSize, void *pData, VkDeviceSize stride,
-                          VkQueryResultFlags flags)
-{
-   panvk_stub();
-   return VK_SUCCESS;
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_CmdCopyQueryPoolResults(VkCommandBuffer commandBuffer,
-                              VkQueryPool queryPool, uint32_t firstQuery,
-                              uint32_t queryCount, VkBuffer dstBuffer,
-                              VkDeviceSize dstOffset, VkDeviceSize stride,
-                              VkQueryResultFlags flags)
-{
-   panvk_stub();
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_CmdResetQueryPool(VkCommandBuffer commandBuffer, VkQueryPool queryPool,
-                        uint32_t firstQuery, uint32_t queryCount)
-{
-   panvk_stub();
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_CmdBeginQuery(VkCommandBuffer commandBuffer, VkQueryPool queryPool,
-                    uint32_t query, VkQueryControlFlags flags)
-{
-   panvk_stub();
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_CmdEndQuery(VkCommandBuffer commandBuffer, VkQueryPool queryPool,
-                  uint32_t query)
-{
-   panvk_stub();
-}
-
-VKAPI_ATTR void VKAPI_CALL
-panvk_CmdWriteTimestamp2(VkCommandBuffer commandBuffer,
-                         VkPipelineStageFlags2 stage, VkQueryPool queryPool,
-                         uint32_t query)
-{
-   panvk_stub();
-}

src/panfrost/vulkan/panvk_query_pool.h
@@ -1,0 +1,76 @@
+/*
+ * Copyright © 2024 Collabora Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef PANVK_QUERY_POOL_H
+#define PANVK_QUERY_POOL_H
+
+#ifndef PAN_ARCH
+#error "PAN_ARCH must be defined"
+#endif
+
+#include <stdint.h>
+
+#include "panvk_mempool.h"
+#include "vk_query_pool.h"
+
+struct panvk_query_report {
+   uint64_t value;
+};
+
+struct panvk_query_available_obj {
+   uint32_t value;
+};
+
+static_assert(sizeof(struct panvk_query_report) == 8,
+              "panvk_query_report size is expected to be 8");
+
+struct panvk_query_pool {
+   struct vk_query_pool vk;
+
+   uint32_t query_stride;
+   uint32_t reports_per_query;
+
+   struct panvk_priv_mem mem;
+   struct panvk_priv_mem available_mem;
+};
+
+VK_DEFINE_NONDISP_HANDLE_CASTS(panvk_query_pool, vk.base, VkQueryPool,
+                               VK_OBJECT_TYPE_QUERY_POOL)
+
+static uint64_t
+panvk_query_available_dev_addr(struct panvk_query_pool *pool, uint32_t query)
+{
+   assert(query < pool->vk.query_count);
+   return panvk_priv_mem_dev_addr(pool->available_mem) + query * sizeof(struct panvk_query_available_obj);
+}
+
+static struct panvk_query_available_obj *
+panvk_query_available_host_addr(struct panvk_query_pool *pool, uint32_t query)
+{
+   assert(query < pool->vk.query_count);
+   return (struct panvk_query_available_obj *)panvk_priv_mem_host_addr(pool->available_mem) + query;
+}
+
+static uint64_t
+panvk_query_offset(struct panvk_query_pool *pool, uint32_t query)
+{
+   assert(query < pool->vk.query_count);
+   return query * pool->query_stride;
+}
+
+static uint64_t
+panvk_query_report_dev_addr(struct panvk_query_pool *pool, uint32_t query)
+{
+   return panvk_priv_mem_dev_addr(pool->mem) + panvk_query_offset(pool, query);
+}
+
+static struct panvk_query_report *
+panvk_query_report_host_addr(struct panvk_query_pool *pool, uint32_t query)
+{
+   return (void *)((char *)panvk_priv_mem_host_addr(pool->mem) +
+                   panvk_query_offset(pool, query));
+}
+
+#endif

commit 2c6bc9615d441c30826357a83231b6cc45a21cb6

panvk: correct signedness of timestamps
These functions returns signed values, so we shouldn't use an unsigned
variable to store one of them in.

Fixes: d1934e44fc8 ("panvk: Implement occlusion queries for JM")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
CID: 1635021
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32380>

src/panfrost/vulkan/panvk_vX_query_pool.c
@@ -129,7 +129,7 @@ static VkResult
 panvk_query_wait_for_available(struct panvk_device *dev,
                                struct panvk_query_pool *pool, uint32_t query)
 {
-   uint64_t abs_timeout_ns = os_time_get_absolute_timeout(PANVK_QUERY_TIMEOUT);
+   int64_t abs_timeout_ns = os_time_get_absolute_timeout(PANVK_QUERY_TIMEOUT);
 
    while (os_time_get_nano() < abs_timeout_ns) {
       if (panvk_query_is_available(pool, query))