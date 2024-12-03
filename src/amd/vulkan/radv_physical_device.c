bminor/mesa-mesa

commit 8300378bf367609242fc6509e9a34325bcc075d1

radv: advertise VK_EXT_device_generated_commands on GFX8+
GFX6-7 can't really support it and it's not worth the effort anyways.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31383>

src/amd/vulkan/radv_physical_device.c
@@ -644,6 +644,7 @@ radv_physical_device_get_supported_extensions(const struct radv_physical_device 
       .EXT_descriptor_indexing = true,
       .EXT_device_address_binding_report = true,
       .EXT_device_fault = pdev->info.has_gpuvm_fault_query,
+      .EXT_device_generated_commands = pdev->info.gfx_level >= GFX8,
       .EXT_discard_rectangles = true,
 #ifdef VK_USE_PLATFORM_DISPLAY_KHR
       .EXT_display_control = true,
@@ -1277,6 +1278,10 @@ radv_physical_device_get_features(const struct radv_physical_device *pdev, struc
 
       /* VK_EXT_depth_clamp_control */
       .depthClampControl = true,
+
+      /* VK_EXT_device_generated_commands */
+      .deviceGeneratedCommands = true,
+      .dynamicGeneratedPipelineLayout = true,
    };
 }
 
@@ -1965,6 +1970,22 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
 
    /* VK_KHR_compute_shader_derivatives */
    p->meshAndTaskShaderDerivatives = radv_taskmesh_enabled(pdev);
+
+   /* VK_EXT_device_generated_commands */
+   p->maxIndirectPipelineCount = 4096;
+   p->maxIndirectShaderObjectCount = 4096;
+   p->maxIndirectSequenceCount = 1048576;
+   p->maxIndirectCommandsTokenCount = 128;
+   p->maxIndirectCommandsTokenOffset = 2047;
+   p->maxIndirectCommandsIndirectStride = 2048;
+   p->supportedIndirectCommandsInputModes =
+      VK_INDIRECT_COMMANDS_INPUT_MODE_VULKAN_INDEX_BUFFER_EXT | VK_INDIRECT_COMMANDS_INPUT_MODE_DXGI_INDEX_BUFFER_EXT;
+   p->supportedIndirectCommandsShaderStages =
+      RADV_GRAPHICS_STAGE_BITS | VK_SHADER_STAGE_COMPUTE_BIT | RADV_RT_STAGE_BITS;
+   p->supportedIndirectCommandsShaderStagesPipelineBinding = VK_SHADER_STAGE_COMPUTE_BIT;
+   p->supportedIndirectCommandsShaderStagesShaderBinding = 0;
+   p->deviceGeneratedCommandsTransformFeedback = true;
+   p->deviceGeneratedCommandsMultiDrawIndirectCount = true;
 }
 
 static VkResult

commit b961537a17b9d9d5a73833621a4f76273acbf49e

radv: fix reporting mesh/task/rt as supported dgc indirect stages
Fixes: 8300378bf36 ("radv: advertise VK_EXT_device_generated_commands on GFX8+")

Acked-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32457>

src/amd/vulkan/radv_physical_device.c
@@ -1395,6 +1395,10 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
       device_type = VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU;
    }
 
+   VkShaderStageFlags taskmesh_stages =
+      radv_taskmesh_enabled(pdev) ? VK_SHADER_STAGE_MESH_BIT_EXT | VK_SHADER_STAGE_TASK_BIT_EXT : 0;
+   VkShaderStageFlags rt_stages = radv_enable_rt(pdev, true) ? RADV_RT_STAGE_BITS : 0;
+
    pdev->vk.properties = (struct vk_properties){
 #ifdef ANDROID_STRICT
       .apiVersion = RADV_API_VERSION,
@@ -2052,7 +2056,7 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
    p->supportedIndirectCommandsInputModes =
       VK_INDIRECT_COMMANDS_INPUT_MODE_VULKAN_INDEX_BUFFER_EXT | VK_INDIRECT_COMMANDS_INPUT_MODE_DXGI_INDEX_BUFFER_EXT;
    p->supportedIndirectCommandsShaderStages =
-      RADV_GRAPHICS_STAGE_BITS | VK_SHADER_STAGE_COMPUTE_BIT | RADV_RT_STAGE_BITS;
+      VK_SHADER_STAGE_ALL_GRAPHICS | VK_SHADER_STAGE_COMPUTE_BIT | taskmesh_stages | rt_stages;
    p->supportedIndirectCommandsShaderStagesPipelineBinding = VK_SHADER_STAGE_COMPUTE_BIT;
    p->supportedIndirectCommandsShaderStagesShaderBinding = 0;
    p->deviceGeneratedCommandsTransformFeedback = true;