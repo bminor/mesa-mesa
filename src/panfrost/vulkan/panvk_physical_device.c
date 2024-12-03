bminor/mesa-mesa

commit 1fc454673ac5d446aa620f75edf47ac4a8aa94f1

panvk: Implement VK_EXT_sampler_filter_minmax for v10
This is supported by the hardware since v10.

Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32215>

src/panfrost/vulkan/panvk_physical_device.c
@@ -184,6 +184,8 @@ static void
 get_device_extensions(const struct panvk_physical_device *device,
                       struct vk_device_extension_table *ext)
 {
+   const unsigned arch = pan_arch(device->kmod.props.gpu_prod_id);
+
    *ext = (struct vk_device_extension_table){
       .KHR_16bit_storage = true,
       .KHR_bind_memory2 = true,
@@ -236,6 +238,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .EXT_pipeline_creation_feedback = true,
       .EXT_private_data = true,
       .EXT_queue_family_foreign = true,
+      .EXT_sampler_filter_minmax = arch >= 10,
       .EXT_shader_module_identifier = true,
       .EXT_tooling_info = true,
       .EXT_vertex_attribute_divisor = true,
@@ -315,7 +318,7 @@ get_features(const struct panvk_physical_device *device,
       .descriptorBindingVariableDescriptorCount = false,
       .runtimeDescriptorArray = false,
 
-      .samplerFilterMinmax = false,
+      .samplerFilterMinmax = arch >= 10,
       .scalarBlockLayout = false,
       .imagelessFramebuffer = false,
       .uniformBufferStandardLayout = false,
@@ -712,9 +715,8 @@ get_device_properties(const struct panvk_instance *instance,
       .maxDescriptorSetUpdateAfterBindSampledImages = 0,
       .maxDescriptorSetUpdateAfterBindStorageImages = 0,
       .maxDescriptorSetUpdateAfterBindInputAttachments = 0,
-      /* XXX: VK_EXT_sampler_filter_minmax */
-      .filterMinmaxSingleComponentFormats = false,
-      .filterMinmaxImageComponentMapping = false,
+      .filterMinmaxSingleComponentFormats = arch >= 10,
+      .filterMinmaxImageComponentMapping = arch >= 10,
       .maxTimelineSemaphoreValueDifference = INT64_MAX,
       .framebufferIntegerColorSampleCounts = sample_counts,
 

src/panfrost/vulkan/panvk_vX_sampler.c
@@ -140,6 +140,22 @@ panvk_per_arch(CreateSampler)(VkDevice _device,
          cfg.maximum_anisotropy = pCreateInfo->maxAnisotropy;
          cfg.lod_algorithm = MALI_LOD_ALGORITHM_ANISOTROPIC;
       }
+
+#if PAN_ARCH >= 10
+      switch (sampler->vk.reduction_mode) {
+      case VK_SAMPLER_REDUCTION_MODE_WEIGHTED_AVERAGE:
+         cfg.reduction_mode = MALI_REDUCTION_MODE_AVERAGE;
+         break;
+      case VK_SAMPLER_REDUCTION_MODE_MIN:
+         cfg.reduction_mode = MALI_REDUCTION_MODE_MINIMUM;
+         break;
+      case VK_SAMPLER_REDUCTION_MODE_MAX:
+         cfg.reduction_mode = MALI_REDUCTION_MODE_MAXIMUM;
+         break;
+      default:
+         unreachable("Invalid reduction mode");
+      }
+#endif
    }
 
    *pSampler = panvk_sampler_to_handle(sampler);

commit 7b8f5b08814bd728677a14eb1a0f97e459f3b14e

panvk: report minmax-support for sampled formats
We also need to report minmax as part of the format-features.

This fixes the following CTS tests for me:
- dEQP-VK.api.info.format_properties.r8_unorm
- dEQP-VK.api.info.format_properties.r8_snorm
- dEQP-VK.api.info.format_properties.r16_sfloat
- dEQP-VK.api.info.format_properties.r32_sfloat
- dEQP-VK.api.info.format_properties.d16_unorm

Fixes: 1fc454673ac ("panvk: Implement VK_EXT_sampler_filter_minmax for v10")
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32445>

src/panfrost/vulkan/panvk_physical_device.c
@@ -1091,6 +1091,7 @@ get_format_properties(struct panvk_physical_device *physical_device,
 {
    VkFormatFeatureFlags tex = 0, buffer = 0;
    enum pipe_format pfmt = vk_format_to_pipe_format(format);
+   unsigned arch = pan_arch(physical_device->kmod.props.gpu_prod_id);
 
    if (pfmt == PIPE_FORMAT_NONE)
       goto end;
@@ -1116,6 +1117,9 @@ get_format_properties(struct panvk_physical_device *physical_device,
              VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT |
              VK_FORMAT_FEATURE_COSITED_CHROMA_SAMPLES_BIT |
              VK_FORMAT_FEATURE_MIDPOINT_CHROMA_SAMPLES_BIT;
+
+      if (arch >= 10)
+         tex |= VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_MINMAX_BIT;
 
       /* Integer formats only support nearest filtering */
       if (!util_format_is_scaled(pfmt) && !util_format_is_pure_integer(pfmt))