aerisarn/mesa-uwp

commit d0342e28b32d7aa4b25cf045ac9933348ec053a9

nir: Add helper to create passthrough GS shader
Based on nir_create_passthrough_tcs and d3d12_make_passthrough_gs, this
creates a passthrough geometry shader that can be used by drivers that
needs to emulate some graphics features in the geometry shader.

Reviewed-by: Rob Clark <robclark@freedesktop.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/19987>

src/compiler/nir/nir.h
@@ -4991,6 +4991,10 @@ nir_shader * nir_create_passthrough_tcs_impl(const nir_shader_compiler_options *
                                              uint8_t patch_vertices);
 nir_shader * nir_create_passthrough_tcs(const nir_shader_compiler_options *options,
                                         const nir_shader *vs, uint8_t patch_vertices);
+nir_shader * nir_create_passthrough_gs(const nir_shader_compiler_options *options,
+                                       const nir_shader *prev_stage,
+                                       enum shader_prim primitive_type,
+                                       unsigned vertices);
 
 bool nir_lower_fragcolor(nir_shader *shader, unsigned max_cbufs);
 bool nir_lower_fragcoord_wtrans(nir_shader *shader);

src/compiler/nir/nir_passthrough_gs.c
@@ -1,0 +1,108 @@
+/*
+ * Copyright © 2022 Collabora Ltc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "nir.h"
+#include "nir_builder.h"
+
+/*
+ * A helper to create a passthrough GS shader for drivers that needs to lower
+ * some rendering tasks to the GS.
+ */
+
+nir_shader *
+nir_create_passthrough_gs(const nir_shader_compiler_options *options,
+                          const nir_shader *prev_stage,
+                          enum shader_prim primitive_type,
+                          unsigned vertices)
+{
+   nir_builder b = nir_builder_init_simple_shader(MESA_SHADER_GEOMETRY,
+                                                  options,
+                                                  "gs passthrough");
+
+   nir_shader *nir = b.shader;
+   nir->info.gs.input_primitive = primitive_type;
+   nir->info.gs.output_primitive = primitive_type;
+   nir->info.gs.vertices_in = vertices;
+   nir->info.gs.vertices_out = vertices;
+   nir->info.gs.invocations = 1;
+   nir->info.gs.active_stream_mask = 1;
+
+   nir_variable *in_vars[VARYING_SLOT_MAX];
+   nir_variable *out_vars[VARYING_SLOT_MAX];
+   unsigned num_vars = 0;
+
+   /* Create input/output variables. */
+   nir_foreach_shader_out_variable(var, prev_stage) {
+      assert(!var->data.patch);
+
+      char name[100];
+      if (var->name)
+         snprintf(name, sizeof(name), "in_%s", var->name);
+      else
+         snprintf(name, sizeof(name), "in_%d", var->data.driver_location);
+
+      nir_variable *in = nir_variable_create(nir, nir_var_shader_in,
+                                             glsl_array_type(var->type,
+                                                             vertices,
+                                                             false),
+                                             name);
+      in->data.location = var->data.location;
+      in->data.location_frac = var->data.location_frac;
+      in->data.driver_location = var->data.driver_location;
+      in->data.interpolation = var->data.interpolation;
+      in->data.compact = var->data.compact;
+
+      if (var->name)
+         snprintf(name, sizeof(name), "out_%s", var->name);
+      else
+         snprintf(name, sizeof(name), "out_%d", var->data.driver_location);
+
+      nir_variable *out = nir_variable_create(nir, nir_var_shader_out,
+                                              var->type, name);
+      out->data.location = var->data.location;
+      out->data.location_frac = var->data.location_frac;
+      out->data.driver_location = var->data.driver_location;
+      out->data.interpolation = var->data.interpolation;
+      out->data.compact = var->data.compact;
+
+      in_vars[num_vars] = in;
+      out_vars[num_vars++] = out;
+   }
+
+   for (unsigned i = 0; i < vertices; ++i) {
+      /* Copy inputs to outputs. */
+      for (unsigned j = 0; j < num_vars; ++j) {
+         /* no need to use copy_var to save a lower pass */
+         nir_ssa_def *value = nir_load_array_var_imm(&b, in_vars[j], i);
+         nir_store_var(&b, out_vars[j], value,
+                       (1u << value->num_components) - 1);
+      }
+      nir_emit_vertex(&b, 0);
+   }
+
+   nir_end_primitive(&b, 0);
+   nir_shader_gather_info(nir, nir_shader_get_entrypoint(nir));
+   nir_validate_shader(nir, "in nir_create_passthrough_gs");
+
+   return nir;
+}

src/compiler/nir/meson.build
@@ -262,6 +262,7 @@ files_libnir = files(
   'nir_opt_undef.c',
   'nir_opt_uniform_atomics.c',
   'nir_opt_vectorize.c',
+  'nir_passthrough_gs.c',
   'nir_passthrough_tcs.c',
   'nir_phi_builder.c',
   'nir_phi_builder.h',

bminor/mesa-mesa

commit 2b9738ce6d318bb3cffe06b8d7c137ac5b8d5a6d

nir,zink,asahi: support passing through gl_PrimitiveID
When this pass is used with Zink, gl_PrimitiveID needs to be passed
through, however this is unnecessary for other divers.

Analogous to previous commit

Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Fixes: d0342e28b32 ("nir: Add helper to create passthrough GS shader")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32397>

src/compiler/nir/nir.h
@@ -6126,7 +6126,8 @@ nir_shader *nir_create_passthrough_gs(const nir_shader_compiler_options *options
                                       enum mesa_prim primitive_type,
                                       enum mesa_prim output_primitive_type,
                                       bool emulate_edgeflags,
-                                      bool force_line_strip_out);
+                                      bool force_line_strip_out,
+                                      bool passthrough_prim_id);
 
 bool nir_lower_fragcolor(nir_shader *shader, unsigned max_cbufs);
 bool nir_lower_fragcoord_wtrans(nir_shader *shader);

src/compiler/nir/nir_passthrough_gs.c
@@ -123,7 +123,8 @@ nir_create_passthrough_gs(const nir_shader_compiler_options *options,
                           enum mesa_prim primitive_type,
                           enum mesa_prim output_primitive_type,
                           bool emulate_edgeflags,
-                          bool force_line_strip_out)
+                          bool force_line_strip_out,
+                          bool passthrough_prim_id)
 {
    unsigned int vertices_out = vertices_for_prim(primitive_type);
    emulate_edgeflags = emulate_edgeflags && (prev_stage->info.outputs_written & VARYING_BIT_EDGE);
@@ -158,6 +159,8 @@ nir_create_passthrough_gs(const nir_shader_compiler_options *options,
    /* Create input/output variables. */
    nir_foreach_shader_out_variable(var, prev_stage) {
       assert(!var->data.patch);
+      assert(var->data.location != VARYING_SLOT_PRIMITIVE_ID &&
+            "not a VS output");
 
       /* input vars can't be created for those */
       if (var->data.location == VARYING_SLOT_LAYER ||
@@ -200,6 +203,24 @@ nir_create_passthrough_gs(const nir_shader_compiler_options *options,
       out_vars[num_outputs++] = out;
    }
 
+   if (passthrough_prim_id) {
+      /* When a geometry shader is not used, a fragment shader may read primitive
+       * ID and get an implicit value without the vertex shader writing an ID. This
+       * case needs to work even when we inject a GS internally.
+       *
+       * However, if a geometry shader precedes a fragment shader that reads
+       * primitive ID, Vulkan requires that the geometry shader write primitive ID.
+       * To handle this case correctly, we must write primitive ID, copying the
+       * fixed-function gl_PrimitiveIDIn input which matches what the fragment
+       * shader will expect.
+       */
+      in_vars[num_inputs++] = nir_create_variable_with_location(
+            nir, nir_var_shader_in, VARYING_SLOT_PRIMITIVE_ID, glsl_int_type());
+
+      out_vars[num_outputs++] = nir_create_variable_with_location(
+            nir, nir_var_shader_out, VARYING_SLOT_PRIMITIVE_ID, glsl_int_type());
+   }
+
    unsigned int start_vert = 0;
    unsigned int end_vert = vertices_out;
    unsigned int vert_step = 1;
@@ -240,7 +261,12 @@ nir_create_passthrough_gs(const nir_shader_compiler_options *options,
             uint64_t mask = BITFIELD64_BIT(in_vars[j]->data.location);
             index = nir_bcsel(&b, nir_ieq_imm(&b, nir_iand_imm(&b, flat_interp_mask_def, mask), 0), nir_imm_int(&b, idx), pv_vert_index);
          }
-         nir_deref_instr *value = nir_build_deref_array(&b, nir_build_deref_var(&b, in_vars[j]), index);
+
+         /* gl_PrimitiveIDIn is not arrayed, all other inputs are */
+         nir_deref_instr *value = nir_build_deref_var(&b, in_vars[j]);
+         if (in_vars[j]->data.location != VARYING_SLOT_PRIMITIVE_ID)
+            value = nir_build_deref_array(&b, value, index);
+
          copy_vars(&b, nir_build_deref_var(&b, out_vars[oj]), value);
          ++oj;
       }

src/gallium/drivers/asahi/agx_state.c
@@ -4341,7 +4341,7 @@ agx_get_passthrough_gs(struct agx_context *ctx,
 
    nir_shader *gs = nir_create_passthrough_gs(
       &agx_nir_options, prev, mode, rast_prim(mode, poly_mode), edgeflags,
-      false /* force line strip out */);
+      false /* force line strip out */, false);
 
    ralloc_free(prev);
 

src/gallium/drivers/zink/zink_program.c
@@ -2584,7 +2584,8 @@ zink_set_primitive_emulation_keys(struct zink_context *ctx)
                   prim,
                   ctx->gfx_pipeline_state.rast_prim,
                   lower_edge_flags,
-                  lower_line_stipple || lower_quad_prim);
+                  lower_line_stipple || lower_quad_prim,
+                  true);
             }
             zink_lower_system_values_to_inlined_uniforms(nir);
 