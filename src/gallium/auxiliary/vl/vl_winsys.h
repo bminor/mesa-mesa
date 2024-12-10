alexvorxx/Mesa-VirGL

commit 2548471ca1b33b76303b4967cdf103cee6c26744

vl/vl_win32_screen_create_from_d3d12_device: Allow winsys to be injected
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30996>

src/gallium/auxiliary/vl/vl_winsys.h
@@ -102,7 +102,7 @@ vl_dri3_screen_create(void *display, int screen) { return NULL; };
 
 #ifdef _WIN32
 struct vl_screen *vl_win32_screen_create(LUID *adapter);
-struct vl_screen *vl_win32_screen_create_from_d3d12_device(IUnknown* d3d12_device);
+struct vl_screen *vl_win32_screen_create_from_d3d12_device(IUnknown* d3d12_device, struct sw_winsys* winsys);
 #else
 /* Always enable the DRM vl winsys */
 struct vl_screen *

src/gallium/auxiliary/vl/vl_winsys_win32.c
@@ -88,15 +88,11 @@ release_pipe:
 }
 
 struct vl_screen *
-vl_win32_screen_create_from_d3d12_device(IUnknown* d3d12_device)
+vl_win32_screen_create_from_d3d12_device(IUnknown* d3d12_device, struct sw_winsys* winsys)
 {
    struct vl_win32_screen *vscreen = CALLOC_STRUCT(vl_win32_screen);
    if (!vscreen)
       return NULL;
-
-   struct sw_winsys* winsys = gdi_create_sw_winsys(gdi_sw_acquire_hdc_by_value, gdi_sw_release_hdc_by_value);
-   if (!winsys)
-      goto release_pipe;
 
    vscreen->base.pscreen = d3d12_create_dxcore_screen_from_d3d12_device(winsys, d3d12_device, &vscreen->adapter_luid);
 
@@ -111,8 +107,6 @@ vl_win32_screen_create_from_d3d12_device(IUnknown* d3d12_device)
    return &vscreen->base;
 
 release_pipe:
-   if(winsys)
-      winsys->destroy(winsys);
    vl_win32_screen_destroy(&vscreen->base);
    return NULL;
 }

bminor/mesa-mesa

commit ce1aad45055f0437fe852386b10be2bfd1ee3c48

vl/vl_winsys: Add missing include for function declaration
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12150
Fixes: 2548471ca1b ("vl/vl_win32_screen_create_from_d3d12_device: Allow winsys to be injected")
Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32559>

src/gallium/auxiliary/vl/vl_winsys.h
@@ -46,6 +46,7 @@ extern "C" {
 #endif
 #include "pipe/p_defines.h"
 #include "util/format/u_formats.h"
+#include "frontend/sw_winsys.h"
 
 struct pipe_screen;
 struct pipe_surface;