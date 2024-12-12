9014c7e74f9e757a8f854183a97e769b5c4b8d28
David Rosca <david.rosca@amd.com>
2024-12-12 10:01:50 +0100

    gallium/vl: Add plane order for Y8_400 format
    Also add assert for unhandled format and remove assert for number of
    components.
    
    Cc: mesa-stable
    Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
    (cherry picked from commit 8a20e634ce536cf028c725d647ab7c1e0b4cd0ed)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32730>

--- src/gallium/auxiliary/vl/vl_video_buffer.c
+++ src/gallium/auxiliary/vl/vl_video_buffer.c
@@ -92,9 +92,11 @@ vl_video_buffer_plane_order(enum pipe_format format)
    case PIPE_FORMAT_UYVY:
    case PIPE_FORMAT_P010:
    case PIPE_FORMAT_P016:
+   case PIPE_FORMAT_Y8_400_UNORM:
       return const_resource_plane_order_YUV;
 
    default:
+      assert(0);
       return NULL;
    }
 }
@@ -323,7 +325,6 @@ vl_video_buffer_sampler_view_components(struct pipe_video_buffer *buffer)
             goto error;
       }
    }
-   assert(component == VL_NUM_COMPONENTS);
 
    return buf->sampler_view_components;
 
