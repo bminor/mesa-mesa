57dc5bb31fed3896dd3769fc0b3e317054e84d48
Erik Faye-Lund <erik.faye-lund@collabora.com>
2024-12-13 13:09:15 +0100

    asahi: use vk_descriptor_type_is_dynamic
    No need to open-code this one now that we have a generic helper.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32627>

--- src/asahi/vulkan/hk_descriptor_set.c
+++ src/asahi/vulkan/hk_descriptor_set.c
@@ -316,9 +316,7 @@ hk_UpdateDescriptorSets(VkDevice device, uint32_t descriptorWriteCount,
          }
       }
 
-      switch (src_binding_layout->type) {
-      case VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC:
-      case VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC: {
+      if (vk_descriptor_type_is_dynamic(src_binding_layout->type)) {
          const uint32_t dst_dyn_start =
             dst_binding_layout->dynamic_buffer_index + copy->dstArrayElement;
          const uint32_t src_dyn_start =
@@ -326,10 +324,6 @@ hk_UpdateDescriptorSets(VkDevice device, uint32_t descriptorWriteCount,
          typed_memcpy(&dst->dynamic_buffers[dst_dyn_start],
                       &src->dynamic_buffers[src_dyn_start],
                       copy->descriptorCount);
-         break;
-      }
-      default:
-         break;
       }
    }
 }
