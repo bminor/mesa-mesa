66512cd897fe6c4eeff250699a0785d694a7a3d7
Lucas Stach <l.stach@pengutronix.de>
2024-12-16 17:35:17 +0100

    etnaviv: Update headers from rnndb
    Update to rnndb commit 1d174e311be6, documenting the TX
    descriptor enable bit.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Christian Gmeiner <cgmeiner@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32662>

--- src/gallium/drivers/etnaviv/etnaviv_context.c
+++ src/gallium/drivers/etnaviv/etnaviv_context.c
@@ -493,7 +493,8 @@ etna_reset_gpu_state(struct etna_context *ctx)
       etna_set_state(stream, VIVS_PE_HALTI4_UNK014C0, 0x00000000);
    }
    if (screen->info->halti >= 5) { /* Only on HALTI5+ */
-      etna_set_state(stream, VIVS_NTE_DESCRIPTOR_UNK14C40, 0x00000001);
+      etna_set_state(stream, VIVS_NTE_DESCRIPTOR_CONTROL,
+                     VIVS_NTE_DESCRIPTOR_CONTROL_ENABLE);
       etna_set_state(stream, VIVS_FE_HALTI5_UNK007D8, 0x00000002);
       etna_set_state(stream, VIVS_PS_SAMPLER_BASE, 0x00000000);
       etna_set_state(stream, VIVS_VS_SAMPLER_BASE, 0x00000020);
