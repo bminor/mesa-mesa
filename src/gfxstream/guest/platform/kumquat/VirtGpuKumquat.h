f7a9991b893955b34d3344aad20302e63b345378
Gurchetan Singh <gurchetansingh@google.com>
2024-12-16 13:09:54 -0800

    mesa: add option to enable virtgpu_kumquat FFI for gfxstream
    Will be used by Cuttlefish CI/CD, but also commonly used by
    gfxstream developers.
    
    meson setup gfxstream-build -Dvulkan-drivers="gfxstream" -Dgallium-drivers="" -Dvirtgpu_kumquat=true -Dopengl=false -Drust_std=2021
    
    In distros, virtgpu_kumquat is not expected to be packaged since
    it is a CI/CD tool.
    
    Reviewed-by: Aaron Ruby <aruby@qnx.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35210>

--- src/gfxstream/guest/platform/kumquat/VirtGpuKumquat.h
+++ src/gfxstream/guest/platform/kumquat/VirtGpuKumquat.h
@@ -8,12 +8,7 @@
 #include "GfxStreamVulkanMapper.h"
 #include "VirtGpu.h"
 
-// Blueprint and Meson builds place things differently
-#if defined(ANDROID)
 #include "virtgpu_kumquat_ffi.h"
-#else
-#include "virtgpu_kumquat/virtgpu_kumquat_ffi.h"
-#endif
 
 class VirtGpuKumquatResource : public std::enable_shared_from_this<VirtGpuKumquatResource>,
                                public VirtGpuResource {
