bminor/mesa-mesa

commit a6e03ce4282327fd102720e9b9e9b8abd92b8520

panvk: advertise version 1.1 support
We know we have a broken Vulkan driver, so it's debatable whether it's
a broken Vulkan 1.0 or broken 1.1. Advertising 1.1 lets us run more
tests, and this patch does this. We also bump the instance version id
to 1.4, which seems appropriate since the overall Vulkan infrastructure
within Mesa is at that level.

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32464>

src/panfrost/vulkan/panvk_physical_device.h
@@ -69,8 +69,9 @@ panvk_get_vk_version(unsigned arch)
    const uint32_t version_override = vk_get_version_override();
    if (version_override)
       return version_override;
-
-   return VK_MAKE_API_VERSION(0, 1, 0, VK_HEADER_VERSION);
+   if (arch < 10)
+      return VK_MAKE_API_VERSION(0, 1, 0, VK_HEADER_VERSION);
+   return VK_MAKE_API_VERSION(0, 1, 1, VK_HEADER_VERSION);
 }
 
 VkResult panvk_physical_device_init(struct panvk_physical_device *device,

src/panfrost/vulkan/meson.build
@@ -187,7 +187,7 @@ panfrost_icd = custom_target(
   output : 'panfrost_icd.@0@.json'.format(host_machine.cpu()),
   command : [
     prog_python, '@INPUT0@',
-    '--api-version', '1.0', '--xml', '@INPUT1@',
+    '--api-version', '1.4', '--xml', '@INPUT1@',
     '--lib-path', join_paths(get_option('prefix'), get_option('libdir'),
                              'libvulkan_panfrost.so'),
     '--out', '@OUTPUT@',
@@ -205,7 +205,7 @@ _dev_icd = custom_target(
   output : _dev_icdname,
   command : [
     prog_python, '@INPUT0@',
-    '--api-version', '1.0', '--xml', '@INPUT1@',
+    '--api-version', '1.4', '--xml', '@INPUT1@',
     '--lib-path', meson.current_build_dir() / 'libvulkan_panfrost.so',
     '--out', '@OUTPUT@',
   ],

src/panfrost/vulkan/panvk_instance.c
@@ -47,7 +47,7 @@ panvk_EnumerateInstanceVersion(uint32_t *pApiVersion)
 {
    uint32_t version_override = vk_get_version_override();
    *pApiVersion = version_override ? version_override :
-      VK_MAKE_VERSION(1, 0, VK_HEADER_VERSION);
+      VK_MAKE_API_VERSION(0, 1, 4, VK_HEADER_VERSION);
 
    return VK_SUCCESS;
 }

commit 42bfbe1a946585bd07c99c9a10d3e74fa27364a3

panvk: back out of vk 1.1 support
Supporting Vulkan 1.1 was premature, as we don't support subgroup
operations, which are actually required (even if it's not listed in
section "46.1. Feature Requirements"):

Here's the relevant phrasing from the Vulkan 1.1 spec:

> subgroupSupportedOperations will have the VK_SUBGROUP_FEATURE_BASIC_BIT
> bit set if any of the physical device’s queues support
> VK_QUEUE_GRAPHICS_BIT or VK_QUEUE_COMPUTE_BIT.

So, it seems we have to support at least the basic feature set before we
can expose Vulkan 1.1. So let's back out of that for now.

Fixes: a6e03ce4282 ("panvk: advertise version 1.1 support")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32654>

src/panfrost/vulkan/panvk_physical_device.c
@@ -433,7 +433,7 @@ get_device_properties(const struct panvk_instance *instance,
    assert(arch > 8 || device->kmod.props.max_threads_per_wg <= 1024);
 
    *properties = (struct vk_properties){
-      .apiVersion = panvk_get_vk_version(arch),
+      .apiVersion = panvk_get_vk_version(),
       .driverVersion = vk_get_driver_version(),
       .vendorID = ARM_VENDOR_ID,
 

src/panfrost/vulkan/panvk_physical_device.h
@@ -64,14 +64,12 @@ to_panvk_physical_device(struct vk_physical_device *phys_dev)
 }
 
 static inline uint32_t
-panvk_get_vk_version(unsigned arch)
+panvk_get_vk_version()
 {
    const uint32_t version_override = vk_get_version_override();
    if (version_override)
       return version_override;
-   if (arch < 10)
-      return VK_MAKE_API_VERSION(0, 1, 0, VK_HEADER_VERSION);
-   return VK_MAKE_API_VERSION(0, 1, 1, VK_HEADER_VERSION);
+   return VK_MAKE_API_VERSION(0, 1, 0, VK_HEADER_VERSION);
 }
 
 VkResult panvk_physical_device_init(struct panvk_physical_device *device,