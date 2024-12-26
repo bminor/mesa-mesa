bminor/mesa-mesa

commit 9b7ea720c939a2d341aa558a126b32fbb3af61e7

radeonsi: use nir->info instead of sel->info.base
sel->info is out of date after shader variant optimizations. We need to
stop using it.

Reviewed-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32713>

src/gallium/drivers/radeonsi/si_shader_llvm.c
@@ -548,13 +548,13 @@ static bool si_llvm_translate_nir(struct si_shader_context *ctx, struct si_shade
    const struct si_shader_info *info = &sel->info;
 
    ctx->shader = shader;
-   ctx->stage = shader->is_gs_copy_shader ? MESA_SHADER_VERTEX : sel->stage;
+   ctx->stage = shader->is_gs_copy_shader ? MESA_SHADER_VERTEX : nir->info.stage;
 
-   ctx->num_const_buffers = info->base.num_ubos;
-   ctx->num_shader_buffers = info->base.num_ssbos;
+   ctx->num_const_buffers = nir->info.num_ubos;
+   ctx->num_shader_buffers = nir->info.num_ssbos;
 
-   ctx->num_samplers = BITSET_LAST_BIT(info->base.textures_used);
-   ctx->num_images = info->base.num_images;
+   ctx->num_samplers = BITSET_LAST_BIT(nir->info.textures_used);
+   ctx->num_images = nir->info.num_images;
 
    ctx->abi.intrinsic_load = si_llvm_load_intrinsic;
    ctx->abi.load_sampler_desc = si_llvm_load_sampler_desc;
@@ -710,7 +710,7 @@ static bool si_llvm_translate_nir(struct si_shader_context *ctx, struct si_shade
              * the wave size is a multiple of the number of patch vertices.
              */
             if (!shader->key.ge.opt.same_patch_vertices ||
-                ctx->ac.wave_size % sel->info.base.tess.tcs_vertices_out != 0)
+                ctx->ac.wave_size % nir->info.tess.tcs_vertices_out != 0)
                ac_build_s_barrier(&ctx->ac, ctx->stage);
          }
       } else if (ctx->stage == MESA_SHADER_GEOMETRY) {
@@ -810,11 +810,12 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *
 {
    struct si_shader_selector *sel = shader->selector;
    struct si_shader_context ctx;
-   enum ac_float_mode float_mode = nir->info.stage == MESA_SHADER_KERNEL ? AC_FLOAT_MODE_DEFAULT : AC_FLOAT_MODE_DEFAULT_OPENGL;
+   enum ac_float_mode float_mode = nir->info.stage == MESA_SHADER_KERNEL ?
+                                       AC_FLOAT_MODE_DEFAULT : AC_FLOAT_MODE_DEFAULT_OPENGL;
    bool exports_color_null = false;
    bool exports_mrtz = false;
 
-   if (sel->stage == MESA_SHADER_FRAGMENT) {
+   if (nir->info.stage == MESA_SHADER_FRAGMENT) {
       exports_color_null = sel->info.colors_written;
       exports_mrtz = shader->ps.writes_z || shader->ps.writes_stencil ||
                      shader->ps.writes_samplemask ||
@@ -834,7 +835,7 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *
 
    /* For merged shader stage. */
    if (shader->is_monolithic && sscreen->info.gfx_level >= GFX9 &&
-       (sel->stage == MESA_SHADER_TESS_CTRL || sel->stage == MESA_SHADER_GEOMETRY)) {
+       (nir->info.stage == MESA_SHADER_TESS_CTRL || nir->info.stage == MESA_SHADER_GEOMETRY)) {
       /* LS or ES shader. */
       struct si_shader prev_shader = {};
 
@@ -853,7 +854,7 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *
 
       /* Reset the shader context. */
       ctx.shader = shader;
-      ctx.stage = sel->stage;
+      ctx.stage = nir->info.stage;
 
       bool same_thread_count = shader->key.ge.opt.same_patch_vertices;
       si_build_wrapper_function(&ctx, parts, same_thread_count);
@@ -866,7 +867,7 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *
 
    /* Compile to bytecode. */
    if (!si_compile_llvm(sscreen, &shader->binary, &shader->config, compiler, &ctx.ac, debug,
-                        sel->stage, si_get_shader_name(shader),
+                        nir->info.stage, si_get_shader_name(shader),
                         si_should_optimize_less(compiler, shader->selector))) {
       si_llvm_dispose(&ctx);
       fprintf(stderr, "LLVM failed to compile shader\n");

src/gallium/drivers/radeonsi/si_compute.c
@@ -103,7 +103,7 @@ static void si_create_compute_state_async(void *job, void *gdata, int thread_ind
    assert(program->ir_type == PIPE_SHADER_IR_NIR);
    si_nir_scan_shader(sscreen, sel->nir, &sel->info);
 
-   if (!sel->info.base.use_aco_amd && !*compiler)
+   if (!sel->nir->info.use_aco_amd && !*compiler)
       *compiler = si_create_llvm_compiler(sscreen);
 
    si_get_active_slot_masks(sscreen, &sel->info, &sel->active_const_and_shader_buffers,
@@ -117,11 +117,11 @@ static void si_create_compute_state_async(void *job, void *gdata, int thread_ind
     */
    unsigned user_sgprs = SI_NUM_RESOURCE_SGPRS + (sel->info.uses_grid_size ? 3 : 0) +
                          (sel->info.uses_variable_block_size ? 1 : 0) +
-                         sel->info.base.cs.user_data_components_amd;
+                         sel->nir->info.cs.user_data_components_amd;
 
    /* Fast path for compute shaders - some descriptors passed via user SGPRs. */
    /* Shader buffers in user SGPRs. */
-   for (unsigned i = 0; i < MIN2(3, sel->info.base.num_ssbos) && user_sgprs <= 12; i++) {
+   for (unsigned i = 0; i < MIN2(3, sel->nir->info.num_ssbos) && user_sgprs <= 12; i++) {
       user_sgprs = align(user_sgprs, 4);
       if (i == 0)
          sel->cs_shaderbufs_sgpr_index = user_sgprs;
@@ -130,16 +130,16 @@ static void si_create_compute_state_async(void *job, void *gdata, int thread_ind
    }
 
    /* Images in user SGPRs. */
-   unsigned non_fmask_images = u_bit_consecutive(0, sel->info.base.num_images);
+   unsigned non_fmask_images = u_bit_consecutive(0, sel->nir->info.num_images);
 
    /* Remove images with FMASK from the bitmask.  We only care about the first
     * 3 anyway, so we can take msaa_images[0] and ignore the rest.
     */
    if (sscreen->info.gfx_level < GFX11)
-      non_fmask_images &= ~sel->info.base.msaa_images[0];
+      non_fmask_images &= ~sel->nir->info.msaa_images[0];
 
    for (unsigned i = 0; i < 3 && non_fmask_images & (1 << i); i++) {
-      unsigned num_sgprs = BITSET_TEST(sel->info.base.image_buffers, i) ? 4 : 8;
+      unsigned num_sgprs = BITSET_TEST(sel->nir->info.image_buffers, i) ? 4 : 8;
 
       if (align(user_sgprs, num_sgprs) + num_sgprs > 16)
          break;

src/gallium/drivers/radeonsi/si_nir_lower_abi.c
@@ -38,9 +38,9 @@ static nir_def *build_attr_ring_desc(nir_builder *b, struct si_shader *shader,
    struct si_shader_selector *sel = shader->selector;
 
    nir_def *attr_address =
-      sel->stage == MESA_SHADER_VERTEX && sel->info.base.vs.blit_sgprs_amd ?
+      b->shader->info.stage == MESA_SHADER_VERTEX && b->shader->info.vs.blit_sgprs_amd ?
       ac_nir_load_arg_at_offset(b, &args->ac, args->vs_blit_inputs,
-                                sel->info.base.vs.blit_sgprs_amd - 1) :
+                                b->shader->info.vs.blit_sgprs_amd - 1) :
       ac_nir_load_arg(b, &args->ac, args->gs_attr_address);
 
    unsigned stride = 16 * si_shader_num_alloc_param_exports(shader);
@@ -178,7 +178,7 @@ static void build_gsvs_ring_desc(nir_builder *b, struct lower_abi_state *s)
 
    if (s->shader->is_gs_copy_shader) {
       s->gsvs_ring[0] = si_nir_load_internal_binding(b, s->args, SI_RING_GSVS, 4);
-   } else if (sel->stage == MESA_SHADER_GEOMETRY && !key->ge.as_ngg) {
+   } else if (b->shader->info.stage == MESA_SHADER_GEOMETRY && !key->ge.as_ngg) {
       nir_def *base_addr = si_nir_load_internal_binding(b, s->args, SI_RING_GSVS, 2);
       base_addr = nir_pack_64_2x32(b, base_addr);
 
@@ -196,7 +196,7 @@ static void build_gsvs_ring_desc(nir_builder *b, struct lower_abi_state *s)
          if (!num_components)
             continue;
 
-         unsigned stride = 4 * num_components * sel->info.base.gs.vertices_out;
+         unsigned stride = 4 * num_components * b->shader->info.gs.vertices_out;
          /* Limit on the stride field for <= GFX7. */
          assert(stride < (1 << 14));
 
@@ -240,12 +240,13 @@ static void preload_reusable_variables(nir_builder *b, struct lower_abi_state *s
 
    b->cursor = nir_before_impl(b->impl);
 
-   if (sel->screen->info.gfx_level <= GFX8 && sel->stage <= MESA_SHADER_GEOMETRY &&
-       (key->ge.as_es || sel->stage == MESA_SHADER_GEOMETRY)) {
+   if (sel->screen->info.gfx_level <= GFX8 && b->shader->info.stage <= MESA_SHADER_GEOMETRY &&
+       (key->ge.as_es || b->shader->info.stage == MESA_SHADER_GEOMETRY)) {
       s->esgs_ring = build_esgs_ring_desc(b, sel->screen->info.gfx_level, s->args);
    }
 
-   if (sel->stage == MESA_SHADER_TESS_CTRL || sel->stage == MESA_SHADER_TESS_EVAL)
+   if (b->shader->info.stage == MESA_SHADER_TESS_CTRL ||
+       b->shader->info.stage == MESA_SHADER_TESS_EVAL)
       s->tess_offchip_ring = build_tess_ring_desc(b, sel->screen, s->args);
 
    build_gsvs_ring_desc(b, s);
@@ -286,7 +287,7 @@ static bool lower_intrinsic(nir_builder *b, nir_instr *instr, struct lower_abi_s
    struct si_shader_args *args = s->args;
    struct si_shader_selector *sel = shader->selector;
    union si_shader_key *key = &shader->key;
-   gl_shader_stage stage = sel->stage;
+   gl_shader_stage stage = b->shader->info.stage;
 
    b->cursor = nir_before_instr(instr);
 
@@ -305,7 +306,7 @@ static bool lower_intrinsic(nir_builder *b, nir_instr *instr, struct lower_abi_s
       break;
    }
    case nir_intrinsic_load_workgroup_size: {
-      assert(sel->info.base.workgroup_size_variable && sel->info.uses_variable_block_size);
+      assert(b->shader->info.workgroup_size_variable && sel->info.uses_variable_block_size);
 
       nir_def *block_size = ac_nir_load_arg(b, &args->ac, args->block_size);
       nir_def *comp[] = {
@@ -373,7 +374,7 @@ static bool lower_intrinsic(nir_builder *b, nir_instr *instr, struct lower_abi_s
       if (stage == MESA_SHADER_TESS_CTRL) {
          const unsigned num_hs_out = util_last_bit64(sel->info.tcs_outputs_written_for_tes);
          const unsigned out_vtx_size = num_hs_out * 16;
-         const unsigned out_vtx_per_patch = sel->info.base.tess.tcs_vertices_out;
+         const unsigned out_vtx_per_patch = b->shader->info.tess.tcs_vertices_out;
          per_vtx_out_patch_size = nir_imm_int(b, out_vtx_size * out_vtx_per_patch);
       } else {
          nir_def *num_hs_out = ac_nir_unpack_arg(b, &args->ac, args->tcs_offchip_layout, 23, 6);
@@ -681,7 +682,7 @@ static bool lower_intrinsic(nir_builder *b, nir_instr *instr, struct lower_abi_s
       if (output_prim == MESA_PRIM_POINTS || output_prim == MESA_PRIM_LINES ||
           output_prim == SI_PRIM_RECTANGLE_LIST) {
          replacement = nir_imm_int(b, 0);
-      } else if (shader->selector->stage == MESA_SHADER_VERTEX) {
+      } else if (stage == MESA_SHADER_VERTEX) {
          if (sel->screen->info.gfx_level >= GFX12) {
             replacement = nir_iand_imm(b, ac_nir_load_arg(b, &args->ac, args->ac.gs_vtx_offset[0]),
                                        ac_get_all_edge_flag_bits(sel->screen->info.gfx_level));
@@ -716,7 +717,7 @@ static bool lower_intrinsic(nir_builder *b, nir_instr *instr, struct lower_abi_s
       break;
    case nir_intrinsic_load_tess_rel_patch_id_amd:
       /* LLVM need to replace patch id arg, so have to be done in LLVM backend. */
-      if (!sel->info.base.use_aco_amd)
+      if (!b->shader->info.use_aco_amd)
          return false;
 
       if (stage == MESA_SHADER_TESS_CTRL) {

src/gallium/drivers/radeonsi/si_nir_lower_resource.c
@@ -67,10 +67,10 @@ static nir_def *load_ubo_desc(nir_builder *b, nir_def *index,
 
    nir_def *addr = ac_nir_load_arg(b, &s->args->ac, s->args->const_and_shader_buffers);
 
-   if (sel->info.base.num_ubos == 1 && sel->info.base.num_ssbos == 0)
+   if (b->shader->info.num_ubos == 1 && b->shader->info.num_ssbos == 0)
       return load_ubo_desc_fast_path(b, addr, sel);
 
-   index = clamp_index(b, index, sel->info.base.num_ubos);
+   index = clamp_index(b, index, b->shader->info.num_ubos);
    index = nir_iadd_imm(b, index, SI_NUM_SHADER_BUFFERS);
 
    nir_def *offset = nir_ishl_imm(b, index, 4);
@@ -90,7 +90,7 @@ static nir_def *load_ssbo_desc(nir_builder *b, nir_src *index,
    }
 
    nir_def *addr = ac_nir_load_arg(b, &s->args->ac, s->args->const_and_shader_buffers);
-   nir_def *slot = clamp_index(b, index->ssa, sel->info.base.num_ssbos);
+   nir_def *slot = clamp_index(b, index->ssa, b->shader->info.num_ssbos);
    slot = nir_isub_imm(b, SI_NUM_SHADER_BUFFERS - 1, slot);
 
    nir_def *offset = nir_ishl_imm(b, slot, 4);
@@ -219,7 +219,7 @@ static nir_def *load_deref_image_desc(nir_builder *b, nir_deref_instr *deref,
 {
    unsigned const_index;
    nir_def *dynamic_index;
-   nir_def *index = deref_to_index(b, deref, s->shader->selector->info.base.num_images,
+   nir_def *index = deref_to_index(b, deref, b->shader->info.num_images,
                                        &dynamic_index, &const_index);
 
    nir_def *desc;

src/gallium/drivers/radeonsi/si_nir_lower_vs_inputs.c
@@ -131,7 +131,7 @@ load_vs_input_from_blit_sgpr(nir_builder *b, unsigned input_index,
       /* Color or texture coordinates: */
       assert(input_index == 1);
 
-      unsigned vs_blit_property = s->shader->selector->info.base.vs.blit_sgprs_amd;
+      unsigned vs_blit_property = b->shader->info.vs.blit_sgprs_amd;
       if (vs_blit_property == SI_VS_BLIT_SGPRS_POS_COLOR + has_attribute_ring_address) {
          for (int i = 0; i < 4; i++)
             out[i] = ac_nir_load_arg_at_offset(b, &s->args->ac, s->args->vs_blit_inputs, 3 + i);
@@ -571,7 +571,7 @@ lower_vs_input_instr(nir_builder *b, nir_intrinsic_instr *intrin, void *state)
    unsigned num_components = intrin->def.num_components;
 
    nir_def *comp[4];
-   if (s->shader->selector->info.base.vs.blit_sgprs_amd)
+   if (b->shader->info.vs.blit_sgprs_amd)
       load_vs_input_from_blit_sgpr(b, input_index, s, comp);
    else
       load_vs_input_from_vertex_buffer(b, input_index, s, intrin->def.bit_size, comp);
@@ -598,7 +598,7 @@ si_nir_lower_vs_inputs(nir_shader *nir, struct si_shader *shader, struct si_shad
       .args = args,
    };
 
-   if (!sel->info.base.vs.blit_sgprs_amd)
+   if (!nir->info.vs.blit_sgprs_amd)
       get_vertex_index_for_all_inputs(nir, &state);
 
    return nir_shader_intrinsics_pass(nir, lower_vs_input_instr,

src/gallium/drivers/radeonsi/si_shader_aco.c
@@ -155,7 +155,7 @@ si_aco_compile_shader(struct si_shader *shader,
    const struct si_shader_selector *sel = shader->selector;
 
    struct aco_compiler_options options = {0};
-   si_fill_aco_options(sel->screen, sel->stage, &options, debug);
+   si_fill_aco_options(sel->screen, nir->info.stage, &options, debug);
 
    struct aco_shader_info info = {0};
    si_fill_aco_shader_info(shader, &info, args);
@@ -169,8 +169,7 @@ si_aco_compile_shader(struct si_shader *shader,
 
    /* For merged shader stage. */
    if (shader->is_monolithic && sel->screen->info.gfx_level >= GFX9 &&
-       (sel->stage == MESA_SHADER_TESS_CTRL || sel->stage == MESA_SHADER_GEOMETRY)) {
-
+       (nir->info.stage == MESA_SHADER_TESS_CTRL || nir->info.stage == MESA_SHADER_GEOMETRY)) {
       shaders[num_shaders++] =
          si_get_prev_stage_nir_shader(shader, &prev_shader, &prev_args, &free_nir);
 

src/gallium/drivers/radeonsi/si_shader_info.c
@@ -548,7 +548,7 @@ void si_nir_scan_shader(struct si_screen *sscreen, struct nir_shader *nir,
 
    /* Get options from shader profiles. */
    for (unsigned i = 0; i < ARRAY_SIZE(si_shader_profiles); i++) {
-      if (_mesa_printed_blake3_equal(info->base.source_blake3, si_shader_profiles[i].blake3)) {
+      if (_mesa_printed_blake3_equal(nir->info.source_blake3, si_shader_profiles[i].blake3)) {
          info->options = si_shader_profiles[i].options;
          break;
       }
@@ -681,7 +681,7 @@ void si_nir_scan_shader(struct si_screen *sscreen, struct nir_shader *nir,
                                    BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_SAMPLE_MASK_IN) ||
                                    BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_HELPER_INVOCATION));
 
-      info->uses_vmem_load_other |= info->base.fs.uses_fbfetch_output;
+      info->uses_vmem_load_other |= nir->info.fs.uses_fbfetch_output;
 
       /* Add both front and back color inputs. */
       unsigned num_inputs_with_colors = info->num_inputs;
@@ -710,7 +710,7 @@ void si_nir_scan_shader(struct si_screen *sscreen, struct nir_shader *nir,
 
    if (nir->info.stage == MESA_SHADER_VERTEX) {
       info->num_vs_inputs =
-         nir->info.stage == MESA_SHADER_VERTEX && !info->base.vs.blit_sgprs_amd ? info->num_inputs : 0;
+         nir->info.stage == MESA_SHADER_VERTEX && !nir->info.vs.blit_sgprs_amd ? info->num_inputs : 0;
       unsigned num_vbos_in_sgprs = si_num_vbos_in_user_sgprs_inline(sscreen->info.gfx_level);
       info->num_vbos_in_user_sgprs = MIN2(info->num_vs_inputs, num_vbos_in_sgprs);
    }
@@ -731,23 +731,23 @@ void si_nir_scan_shader(struct si_screen *sscreen, struct nir_shader *nir,
          assert(((info->esgs_vertex_stride / 4) & C_028AAC_ITEMSIZE) == 0);
       }
 
-      info->tcs_inputs_via_temp = info->base.tess.tcs_same_invocation_inputs_read;
-      info->tcs_inputs_via_lds = info->base.tess.tcs_cross_invocation_inputs_read |
-                                 (info->base.tess.tcs_same_invocation_inputs_read &
-                                  info->base.inputs_read_indirectly);
+      info->tcs_inputs_via_temp = nir->info.tess.tcs_same_invocation_inputs_read;
+      info->tcs_inputs_via_lds = nir->info.tess.tcs_cross_invocation_inputs_read |
+                                 (nir->info.tess.tcs_same_invocation_inputs_read &
+                                  nir->info.inputs_read_indirectly);
    }
 
    if (nir->info.stage == MESA_SHADER_GEOMETRY) {
       info->gsvs_vertex_size = info->num_outputs * 16;
-      info->max_gsvs_emit_size = info->gsvs_vertex_size * info->base.gs.vertices_out;
+      info->max_gsvs_emit_size = info->gsvs_vertex_size * nir->info.gs.vertices_out;
       info->gs_input_verts_per_prim =
-         mesa_vertices_per_prim(info->base.gs.input_primitive);
+         mesa_vertices_per_prim(nir->info.gs.input_primitive);
    }
 
    info->clipdist_mask = info->writes_clipvertex ? SI_USER_CLIP_PLANE_MASK :
-                         u_bit_consecutive(0, info->base.clip_distance_array_size);
-   info->culldist_mask = u_bit_consecutive(0, info->base.cull_distance_array_size) <<
-                         info->base.clip_distance_array_size;
+                         u_bit_consecutive(0, nir->info.clip_distance_array_size);
+   info->culldist_mask = u_bit_consecutive(0, nir->info.cull_distance_array_size) <<
+                         nir->info.clip_distance_array_size;
 
    if (nir->info.stage == MESA_SHADER_FRAGMENT) {
       for (unsigned i = 0; i < info->num_inputs; i++) {

commit a0579f75fb5aa6926f4acfdee3fa91f2666df559

radeonsi: fix a TCS regression
This change caused the regression:
@@ -853,7 +853,7 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *

       /* Reset the shader context. */
       ctx.shader = shader;
-      ctx.stage = sel->stage;
+      ctx.stage = nir->info.stage;

       bool same_thread_count = shader->key.ge.opt.same_patch_vertices;
       si_build_wrapper_function(&ctx, parts, same_thread_count);

because "nir" contains the previous shader (LS), not the current shader (HS).
Fix it by using prev_nir for the previous shader, so that we can keep using
"nir".

Fixes: 9b7ea720c93 - radeonsi: use nir->info instead of sel->info.base

Reviewed-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32780>

src/gallium/drivers/radeonsi/si_shader_llvm.c
@@ -840,12 +840,12 @@ bool si_llvm_compile_shader(struct si_screen *sscreen, struct ac_llvm_compiler *
       struct si_shader prev_shader = {};
 
       bool free_nir;
-      nir = si_get_prev_stage_nir_shader(shader, &prev_shader, ctx.args, &free_nir);
+      nir_shader *prev_nir = si_get_prev_stage_nir_shader(shader, &prev_shader, ctx.args, &free_nir);
 
       struct ac_llvm_pointer parts[2];
       parts[1] = ctx.main_fn;
 
-      if (!si_llvm_translate_nir(&ctx, &prev_shader, nir, free_nir)) {
+      if (!si_llvm_translate_nir(&ctx, &prev_shader, prev_nir, free_nir)) {
          si_llvm_dispose(&ctx);
          return false;
       }