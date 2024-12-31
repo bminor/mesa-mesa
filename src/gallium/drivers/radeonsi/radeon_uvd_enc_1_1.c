186980de2615dc6070ae98958909e8762750aab5
David Rosca <david.rosca@amd.com>
2024-12-31 16:36:47 +0100

    radeonsi/uvd_enc: Support min/max QP and max frame size
    Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
    Acked-by: Leo Liu <leo.liu@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32829>

--- src/gallium/drivers/radeonsi/radeon_uvd_enc_1_1.c
+++ src/gallium/drivers/radeonsi/radeon_uvd_enc_1_1.c
@@ -771,9 +771,9 @@ static void radeon_uvd_enc_rc_per_pic(struct radeon_uvd_encoder *enc,
 {
    struct pipe_h265_enc_picture_desc *pic = (struct pipe_h265_enc_picture_desc *)picture;
    enc->enc_pic.rc_per_pic.qp = pic->rc[0].quant_i_frames;
-   enc->enc_pic.rc_per_pic.min_qp_app = 0;
-   enc->enc_pic.rc_per_pic.max_qp_app = 51;
-   enc->enc_pic.rc_per_pic.max_au_size = 0;
+   enc->enc_pic.rc_per_pic.min_qp_app = pic->rc[0].min_qp;
+   enc->enc_pic.rc_per_pic.max_qp_app = pic->rc[0].max_qp ? pic->rc[0].max_qp : 51;
+   enc->enc_pic.rc_per_pic.max_au_size = pic->rc[0].max_au_size;
    enc->enc_pic.rc_per_pic.enabled_filler_data = pic->rc[0].fill_data_enable;
    enc->enc_pic.rc_per_pic.skip_frame_enable = false;
    enc->enc_pic.rc_per_pic.enforce_hrd = pic->rc[0].enforce_hrd;
