cd01162e8151d474c16242c6416b3c00facc0dab
Georg Lehmann <dadschoorse@gmail.com>
2025-01-02 12:27:11 +0100

    aco/insert_exec: reset top exec for p_discard_if
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12363
    Fixes: 31f62a6123d ("aco/insert_exec: don't always reset top exec")
    (cherry picked from commit 272ff275fa27a2536e8699c7de100c8e266ae2b0)
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32852>

--- src/amd/compiler/aco_insert_exec_mask.cpp
+++ src/amd/compiler/aco_insert_exec_mask.cpp
@@ -467,6 +467,7 @@ process_instructions(exec_ctx& ctx, Block* block, std::vector<aco_ptr<Instructio
             exit_cond = andn2->definitions[1].getTemp();
          }
 
+         info.exec.back().op = Operand(exec, bld.lm);
          instr->opcode = aco_opcode::p_exit_early_if;
          instr->operands[0] = bld.scc(exit_cond);
          assert(!ctx.handle_wqm || (info.exec[0].type & mask_type_wqm) == 0);
