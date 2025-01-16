f504ed9e73f169cfe3bea9c5e3ded7faaedf43c3
Julia Zhang <julia.zhang@amd.com>
2025-01-16 12:29:52 +0800

    radv: emit device memory report for device memory events
    Emit device memory report when radv create memory or free memory.
    
    Signed-off-by: Julia Zhang <julia.zhang@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33088>

--- src/amd/vulkan/radv_device_memory.c
+++ src/amd/vulkan/radv_device_memory.c
@@ -17,6 +17,33 @@
 #include "radv_rmv.h"
 
 #include "vk_log.h"
+#include "vk_debug_utils.h"
+
+static void
+radv_device_memory_emit_report(struct radv_device *device,
+                               struct radv_device_memory *mem,
+                               bool is_alloc,
+                               VkResult result)
+{
+   if (likely(!device->vk.memory_reports))
+      return;
+
+   VkDeviceMemoryReportEventTypeEXT type;
+   if (result != VK_SUCCESS) {
+      type = VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_ALLOCATION_FAILED_EXT;
+   } else if (is_alloc) {
+      type = mem->import_handle_type
+             ? VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_IMPORT_EXT
+             : VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_ALLOCATE_EXT;
+   } else {
+      type = mem->import_handle_type
+             ? VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_UNIMPORT_EXT
+             : VK_DEVICE_MEMORY_REPORT_EVENT_TYPE_FREE_EXT;
+   }
+
+   vk_emit_device_memory_report(&device->vk, type, mem->bo->obj_id, mem->bo->size,
+                                (uintptr_t)(mem), mem->heap_index);
+}
 
 void
 radv_free_memory(struct radv_device *device, const VkAllocationCallbacks *pAllocator, struct radv_device_memory *mem)
@@ -280,10 +307,13 @@ radv_alloc_memory(struct radv_device *device, const VkMemoryAllocateInfo *pAlloc
    *pMem = radv_device_memory_to_handle(mem);
    radv_rmv_log_heap_create(device, *pMem, is_internal, flags_info ? flags_info->flags : 0);
 
+   radv_device_memory_emit_report(device, mem, /* is_alloc */ true, result);
+
    return VK_SUCCESS;
 
 fail:
    radv_free_memory(device, pAllocator, mem);
+   radv_device_memory_emit_report(device, mem, /* is_alloc */ true, result);
 
    return result;
 }
@@ -301,6 +331,9 @@ radv_FreeMemory(VkDevice _device, VkDeviceMemory _mem, const VkAllocationCallbac
 {
    VK_FROM_HANDLE(radv_device, device, _device);
    VK_FROM_HANDLE(radv_device_memory, mem, _mem);
+
+   if (mem)
+      radv_device_memory_emit_report(device, mem, /* is_alloc */ false, VK_SUCCESS);
 
    radv_free_memory(device, pAllocator, mem);
 }
