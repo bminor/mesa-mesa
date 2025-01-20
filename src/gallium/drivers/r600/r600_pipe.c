390854e52358284048c65f14ef86ac0658de3508
Qiang Yu <yuq825@gmail.com>
2025-01-20 21:41:47 +0800

    r600: init shader and compute caps
    Acked-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Acked-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33176>

--- src/gallium/drivers/r600/r600_pipe.c
+++ src/gallium/drivers/r600/r600_pipe.c
@@ -328,6 +328,112 @@ static int r600_get_shader_param(struct pipe_screen* pscreen,
 	return 0;
 }
 
+static void r600_init_shader_caps(struct r600_screen *rscreen)
+{
+	for (unsigned i = 0; i <= PIPE_SHADER_COMPUTE; i++) {
+		struct pipe_shader_caps *caps =
+			(struct pipe_shader_caps *)&rscreen->b.b.shader_caps[i];
+
+		switch (i) {
+		case PIPE_SHADER_TESS_CTRL:
+		case PIPE_SHADER_TESS_EVAL:
+		case PIPE_SHADER_COMPUTE:
+			if (rscreen->b.family < CHIP_CEDAR)
+				continue;
+			break;
+		default:
+			break;
+		}
+
+		caps->max_instructions =
+		caps->max_alu_instructions =
+		caps->max_tex_instructions =
+		caps->max_tex_indirections = 16384;
+		caps->max_control_flow_depth = 32;
+		caps->max_inputs = i == PIPE_SHADER_VERTEX ? 16 : 32;
+		caps->max_outputs = i == PIPE_SHADER_FRAGMENT ? 8 : 32;
+		caps->max_temps = 256; /* Max native temporaries. */
+
+		caps->max_const_buffer0_size = i == PIPE_SHADER_COMPUTE ?
+			MIN2(rscreen->b.b.compute_caps.max_mem_alloc_size, INT_MAX) :
+			R600_MAX_CONST_BUFFER_SIZE;
+
+		caps->max_const_buffers = R600_MAX_USER_CONST_BUFFERS;
+		caps->cont_supported = true;
+		caps->tgsi_sqrt_supported = true;
+		caps->indirect_temp_addr = true;
+		caps->indirect_const_addr = true;
+		caps->integers = true;
+		caps->tgsi_any_inout_decl_range = true;
+		caps->max_texture_samplers =
+		caps->max_sampler_views = 16;
+
+		caps->supported_irs = 1 << PIPE_SHADER_IR_NIR;
+		if (i == PIPE_SHADER_COMPUTE)
+			caps->supported_irs |= 1 << PIPE_SHADER_IR_NATIVE;
+
+		caps->max_shader_buffers =
+		caps->max_shader_images =
+			rscreen->b.family >= CHIP_CEDAR &&
+			(i == PIPE_SHADER_FRAGMENT || i == PIPE_SHADER_COMPUTE) ? 8 : 0;
+
+		caps->max_hw_atomic_counters =
+			rscreen->b.family >= CHIP_CEDAR && rscreen->has_atomics ? 8 : 0;
+
+		/* having to allocate the atomics out amongst shaders stages is messy,
+		   so give compute 8 buffers and all the others one */
+		caps->max_hw_atomic_counter_buffers =
+			rscreen->b.family >= CHIP_CEDAR && rscreen->has_atomics ?
+			EG_MAX_ATOMIC_BUFFERS : 0;
+	}
+}
+
+static void r600_init_compute_caps(struct r600_screen *screen)
+{
+	struct r600_common_screen *rscreen = &screen->b;
+
+	struct pipe_compute_caps *caps =
+		(struct pipe_compute_caps *)&rscreen->b.compute_caps;
+
+	snprintf(caps->ir_target, sizeof(caps->ir_target), "%s-r600--",
+		 r600_get_llvm_processor_name(rscreen->family));
+
+	caps->grid_dimension = 3;
+
+	caps->max_grid_size[0] =
+	caps->max_grid_size[1] =
+	caps->max_grid_size[2] = 65535;
+
+	caps->max_block_size[0] =
+	caps->max_block_size[1] =
+	caps->max_block_size[2] = rscreen->gfx_level >= EVERGREEN ? 1024 : 256;
+
+	caps->max_block_size_clover[0] =
+	caps->max_block_size_clover[1] =
+	caps->max_block_size_clover[2] = 256;
+
+	caps->max_threads_per_block = rscreen->gfx_level >= EVERGREEN ? 1024 : 256;
+	caps->max_threads_per_block_clover = 256;
+	caps->address_bits = 32;
+	caps->max_mem_alloc_size = (rscreen->info.max_heap_size_kb / 4) * 1024ull;
+
+	/* In OpenCL, the MAX_MEM_ALLOC_SIZE must be at least
+	 * 1/4 of the MAX_GLOBAL_SIZE.  Since the
+	 * MAX_MEM_ALLOC_SIZE is fixed for older kernels,
+	 * make sure we never report more than
+	 * 4 * MAX_MEM_ALLOC_SIZE.
+	 */
+	caps->max_global_size = MIN2(4 * caps->max_mem_alloc_size,
+				     rscreen->info.max_heap_size_kb * 1024ull);
+
+	/* Value reported by the closed source driver. */
+	caps->max_local_size = 32768;
+	caps->max_input_size = 1024;
+	caps->max_clock_frequency = rscreen->info.max_gpu_freq_mhz;
+	caps->max_compute_units = rscreen->info.num_cu;
+	caps->subgroup_sizes = r600_wavefront_size(rscreen->family);
+}
+
 static void r600_init_screen_caps(struct r600_screen *rscreen)
 {
 	struct pipe_caps *caps = (struct pipe_caps *)&rscreen->b.b.caps;
@@ -625,6 +731,8 @@ struct pipe_screen *r600_screen_create(struct radeon_winsys *ws,
 
 	rscreen->has_msaa = true;
 
+	r600_init_compute_caps(rscreen);
+	r600_init_shader_caps(rscreen);
 	r600_init_screen_caps(rscreen);
 
 	/* MSAA support. */
