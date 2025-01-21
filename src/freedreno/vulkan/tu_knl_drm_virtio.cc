bminor/mesa-mesa

commit 4a5b234d5e955db722968c52efd60676abf30fef

tu: Plumb through raytracing fuse
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28447>

src/freedreno/vulkan/tu_knl_drm_virtio.cc
@@ -299,6 +299,19 @@ virtio_device_get_suspend_count(struct tu_device *dev, uint64_t *suspend_count)
    return ret;
 }
 
+
+static bool
+tu_drm_get_raytracing(struct vdrm_device *vdrm)
+{
+   uint64_t value;
+   int ret = tu_drm_get_param(vdrm, MSM_PARAM_RAYTRACING, &value);
+   if (ret)
+      return false;
+
+   return value;
+}
+
+
 static VkResult
 virtio_device_check_status(struct tu_device *device)
 {
@@ -1213,6 +1226,9 @@ tu_knl_drm_virtio_load(struct tu_instance *instance,
    device->has_cached_coherent_memory = caps.u.msm.has_cached_coherent;
 
    device->submitqueue_priority_count = caps.u.msm.priorities;
+
+   /* TODO add a cap for this */
+   device->has_raytracing = tu_drm_get_raytracing(vdrm);
 
    device->syncobj_type = vk_drm_syncobj_get_type(fd);
    /* we don't support DRM_CAP_SYNCOBJ_TIMELINE, but drm-shim does */

src/freedreno/vulkan/tu_device.h
@@ -118,6 +118,7 @@ struct tu_physical_device
    int32_t reserved_set_idx;
 
    bool has_set_iova;
+   bool has_raytracing;
    uint64_t va_start;
    uint64_t va_size;
 

src/freedreno/vulkan/tu_knl_drm_msm.cc
@@ -77,6 +77,17 @@ tu_drm_get_gmem_base(const struct tu_physical_device *dev, uint64_t *base)
    return tu_drm_get_param(dev->local_fd, MSM_PARAM_GMEM_BASE, base);
 }
 
+static bool
+tu_drm_get_raytracing(const struct tu_physical_device *dev)
+{
+   uint64_t value;
+   int ret = tu_drm_get_param(dev->local_fd, MSM_PARAM_RAYTRACING, &value);
+   if (ret)
+      return false;
+
+   return value;
+}
+
 static int
 tu_drm_get_va_prop(const struct tu_physical_device *dev,
                    uint64_t *va_start, uint64_t *va_size)
@@ -1039,6 +1050,7 @@ tu_knl_drm_msm_load(struct tu_instance *instance,
 
    device->has_set_iova = !tu_drm_get_va_prop(device, &device->va_start,
                                               &device->va_size);
+   device->has_raytracing = tu_drm_get_raytracing(device);
 
    device->has_preemption = tu_drm_has_preemption(device);
 

src/freedreno/vulkan/tu_knl_kgsl.cc
@@ -1382,6 +1382,17 @@ static const struct tu_knl kgsl_knl_funcs = {
       .queue_wait_fence = kgsl_queue_wait_fence,
 };
 
+static bool
+tu_kgsl_get_raytracing(int fd)
+{
+   uint32_t value;
+   int ret = get_kgsl_prop(fd, KGSL_PROP_IS_RAYTRACING_ENABLED, &value, sizeof(value));
+   if (ret)
+      return false;
+
+   return value;
+}
+
 VkResult
 tu_knl_kgsl_load(struct tu_instance *instance, int fd)
 {
@@ -1457,6 +1468,8 @@ tu_knl_kgsl_load(struct tu_instance *instance, int fd)
    device->dev_id.chip_id = info.chip_id;
    device->gmem_size = debug_get_num_option("TU_GMEM", info.gmem_sizebytes);
    device->gmem_base = gmem_iova;
+
+   device->has_raytracing = tu_kgsl_get_raytracing(fd);
 
    device->submitqueue_priority_count = 1;
    

commit 27284d14fd486a977da3d254073922d0e2ce43a6

tu: Fix raytracing query with vdrm
Need to query the cap _before_ we close the connection.

Fixes: 4a5b234d5e95 ("tu: Plumb through raytracing fuse")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33139>

src/freedreno/vulkan/tu_knl_drm_virtio.cc
@@ -1159,6 +1159,9 @@ tu_knl_drm_virtio_load(struct tu_instance *instance,
    uint32_t bank_swizzle_levels = tu_drm_get_ubwc_swizzle(vdrm);
    enum fdl_macrotile_mode macrotile_mode = tu_drm_get_macrotile_mode(vdrm);
 
+   /* TODO add a cap for this */
+   bool has_raytracing = tu_drm_get_raytracing(vdrm);
+
    vdrm_device_close(vdrm);
 
    mesa_logd("wire_format_version: %u", caps.wire_format_version);
@@ -1227,8 +1230,7 @@ tu_knl_drm_virtio_load(struct tu_instance *instance,
 
    device->submitqueue_priority_count = caps.u.msm.priorities;
 
-   /* TODO add a cap for this */
-   device->has_raytracing = tu_drm_get_raytracing(vdrm);
+   device->has_raytracing = has_raytracing;
 
    device->syncobj_type = vk_drm_syncobj_get_type(fd);
    /* we don't support DRM_CAP_SYNCOBJ_TIMELINE, but drm-shim does */