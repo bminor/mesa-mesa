bminor/mesa-mesa

commit a565f2994fec38e20cce46589698dd5487e3ea61

amd: move all uses of libdrm_amdgpu to ac_linux_drm
This is required to implement virtio native-context.

In a virtualized environment, most of the functions provided
by libdrm_amdgpu will be implemented using virtio.
This allows to implement efficient virtualization, by
forwarding the kernel API to the host, instead of the GL/VK calls.

Similarly, the raw 'fd' or 'gem_handle' arguments are replaced
by opaque types. This allows to encapsulate all the needed
state in the handle, and use unmodified API between baremetal
and virtualized contexts.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21658>

src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
@@ -68,7 +68,7 @@ static bool do_winsys_init(struct amdgpu_winsys *aws,
    return true;
 
 fail:
-   amdgpu_device_deinitialize(aws->dev);
+   ac_drm_device_deinitialize(aws->dev);
    aws->dev = NULL;
    return false;
 }
@@ -76,7 +76,7 @@ fail:
 static void do_winsys_deinit(struct amdgpu_winsys *aws)
 {
    if (aws->reserve_vmid)
-      ac_drm_vm_unreserve_vmid(aws->fd, 0);
+      ac_drm_vm_unreserve_vmid(aws->dev, 0);
 
    for (unsigned i = 0; i < ARRAY_SIZE(aws->queues); i++) {
       for (unsigned j = 0; j < ARRAY_SIZE(aws->queues[i].fences); j++)
@@ -102,8 +102,8 @@ static void do_winsys_deinit(struct amdgpu_winsys *aws)
    simple_mtx_destroy(&aws->bo_export_table_lock);
 
    ac_addrlib_destroy(aws->addrlib);
-   amdgpu_device_deinitialize(aws->dev);
-   drmSyncobjDestroy(aws->fd, aws->vm_timeline_syncobj);
+   ac_drm_device_deinitialize(aws->dev);
+   ac_drm_cs_destroy_syncobj(aws->fd, aws->vm_timeline_syncobj);
    simple_mtx_destroy(&aws->bo_fence_lock);
 
    FREE(aws);
@@ -189,7 +189,7 @@ static uint64_t amdgpu_query_value(struct radeon_winsys *rws,
    case RADEON_NUM_MAPPED_BUFFERS:
       return aws->num_mapped_buffers;
    case RADEON_TIMESTAMP:
-      ac_drm_query_info(aws->fd, AMDGPU_INFO_TIMESTAMP, 8, &retval);
+      ac_drm_query_info(aws->dev, AMDGPU_INFO_TIMESTAMP, 8, &retval);
       return retval;
    case RADEON_NUM_GFX_IBS:
       return aws->num_gfx_IBs;
@@ -200,32 +200,32 @@ static uint64_t amdgpu_query_value(struct radeon_winsys *rws,
    case RADEON_GFX_IB_SIZE_COUNTER:
       return aws->gfx_ib_size_counter;
    case RADEON_NUM_BYTES_MOVED:
-      ac_drm_query_info(aws->fd, AMDGPU_INFO_NUM_BYTES_MOVED, 8, &retval);
+      ac_drm_query_info(aws->dev, AMDGPU_INFO_NUM_BYTES_MOVED, 8, &retval);
       return retval;
    case RADEON_NUM_EVICTIONS:
-      ac_drm_query_info(aws->fd, AMDGPU_INFO_NUM_EVICTIONS, 8, &retval);
+      ac_drm_query_info(aws->dev, AMDGPU_INFO_NUM_EVICTIONS, 8, &retval);
       return retval;
    case RADEON_NUM_VRAM_CPU_PAGE_FAULTS:
-      ac_drm_query_info(aws->fd, AMDGPU_INFO_NUM_VRAM_CPU_PAGE_FAULTS, 8, &retval);
+      ac_drm_query_info(aws->dev, AMDGPU_INFO_NUM_VRAM_CPU_PAGE_FAULTS, 8, &retval);
       return retval;
    case RADEON_VRAM_USAGE:
-      ac_drm_query_heap_info(aws->fd, AMDGPU_GEM_DOMAIN_VRAM, 0, &heap);
+      ac_drm_query_heap_info(aws->dev, AMDGPU_GEM_DOMAIN_VRAM, 0, &heap);
       return heap.heap_usage;
    case RADEON_VRAM_VIS_USAGE:
-      ac_drm_query_heap_info(aws->fd, AMDGPU_GEM_DOMAIN_VRAM,
+      ac_drm_query_heap_info(aws->dev, AMDGPU_GEM_DOMAIN_VRAM,
                              AMDGPU_GEM_CREATE_CPU_ACCESS_REQUIRED, &heap);
       return heap.heap_usage;
    case RADEON_GTT_USAGE:
-      ac_drm_query_heap_info(aws->fd, AMDGPU_GEM_DOMAIN_GTT, 0, &heap);
+      ac_drm_query_heap_info(aws->dev, AMDGPU_GEM_DOMAIN_GTT, 0, &heap);
       return heap.heap_usage;
    case RADEON_GPU_TEMPERATURE:
-      ac_drm_query_sensor_info(aws->fd, AMDGPU_INFO_SENSOR_GPU_TEMP, 4, &retval);
+      ac_drm_query_sensor_info(aws->dev, AMDGPU_INFO_SENSOR_GPU_TEMP, 4, &retval);
       return retval;
    case RADEON_CURRENT_SCLK:
-      ac_drm_query_sensor_info(aws->fd, AMDGPU_INFO_SENSOR_GFX_SCLK, 4, &retval);
+      ac_drm_query_sensor_info(aws->dev, AMDGPU_INFO_SENSOR_GFX_SCLK, 4, &retval);
       return retval;
    case RADEON_CURRENT_MCLK:
-      ac_drm_query_sensor_info(aws->fd, AMDGPU_INFO_SENSOR_GFX_MCLK, 4, &retval);
+      ac_drm_query_sensor_info(aws->dev, AMDGPU_INFO_SENSOR_GFX_MCLK, 4, &retval);
       return retval;
    case RADEON_CS_THREAD_TIME:
       return util_queue_get_thread_time_nano(&aws->cs_queue, 0);
@@ -239,7 +239,7 @@ static bool amdgpu_read_registers(struct radeon_winsys *rws,
 {
    struct amdgpu_winsys *aws = amdgpu_winsys(rws);
 
-   return ac_drm_read_mm_registers(aws->fd, reg_offset / 4, num_registers,
+   return ac_drm_read_mm_registers(aws->dev, reg_offset / 4, num_registers,
                                    0xffffffff, 0, out) == 0;
 }
 
@@ -337,7 +337,7 @@ amdgpu_cs_set_pstate(struct radeon_cmdbuf *rcs, enum radeon_ctx_pstate pstate)
       return false;
 
    uint32_t amdgpu_pstate = radeon_to_amdgpu_pstate(pstate);
-   return ac_drm_cs_ctx_stable_pstate(cs->aws->fd, cs->ctx->ctx_handle,
+   return ac_drm_cs_ctx_stable_pstate(cs->aws->dev, cs->ctx->ctx_handle,
       AMDGPU_CTX_OP_SET_STABLE_PSTATE, amdgpu_pstate, NULL) == 0;
 }
 
@@ -377,7 +377,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
 {
    struct amdgpu_screen_winsys *sws;
    struct amdgpu_winsys *aws;
-   amdgpu_device_handle dev;
+   ac_drm_device *dev;
    uint32_t drm_major, drm_minor;
    int r;
 
@@ -395,7 +395,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
 
    /* Initialize the amdgpu device. This should always return the same pointer
     * for the same fd. */
-   r = amdgpu_device_initialize(fd, &drm_major, &drm_minor, &dev);
+   r = ac_drm_device_initialize(fd, &drm_major, &drm_minor, &dev);
    if (r) {
       fprintf(stderr, "amdgpu: amdgpu_device_initialize failed.\n");
       goto fail;
@@ -410,7 +410,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
        * This function is returning an existing winsys instance, which
        * has its own device handle.
        */
-      amdgpu_device_deinitialize(dev);
+      ac_drm_device_deinitialize((void*)dev);
 
       simple_mtx_lock(&aws->sws_list_lock);
       for (sws_iter = aws->sws_list; sws_iter; sws_iter = sws_iter->next) {
@@ -437,12 +437,13 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
          goto fail;
 
       aws->dev = dev;
+
       /* The device fd might be different from the one we passed because of
        * libdrm_amdgpu device dedup logic. This can happen if radv is initialized
        * first.
        * Get the correct fd or the buffer sharing will not work (see #3424).
        */
-      aws->fd = amdgpu_device_get_fd(dev);
+      aws->fd = ac_drm_device_get_fd(dev);
       if (!are_file_descriptions_equal(aws->fd, fd)) {
          sws->kms_handles = _mesa_hash_table_create(NULL, kms_handle_hash,
                                                    kms_handle_equals);
@@ -454,7 +455,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
       aws->info.drm_major = drm_major;
       aws->info.drm_minor = drm_minor;
 
-      if (amdgpu_cs_create_syncobj(dev, &aws->vm_timeline_syncobj))
+      if (ac_drm_cs_create_syncobj(aws->fd, &aws->vm_timeline_syncobj))
          goto fail_alloc;
       simple_mtx_init(&aws->vm_ioctl_lock, mtx_plain);
 
@@ -515,7 +516,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
       _mesa_hash_table_insert(dev_tab, dev, aws);
 
       if (aws->reserve_vmid) {
-         r = ac_drm_vm_reserve_vmid(aws->fd, 0);
+         r = ac_drm_vm_reserve_vmid(aws->dev, 0);
          if (r) {
             amdgpu_winsys_destroy_locked(&sws->base, true);
             simple_mtx_unlock(&dev_tab_mutex);

src/amd/common/ac_gpu_info.c
@@ -239,17 +239,13 @@ typedef struct _drmDevice {
 enum amdgpu_sw_info {
    amdgpu_sw_info_address32_hi = 0,
 };
-typedef struct amdgpu_device *amdgpu_device_handle;
-typedef struct amdgpu_bo *amdgpu_bo_handle;
 struct amdgpu_bo_alloc_request {
    uint64_t alloc_size;
    uint64_t phys_alignment;
    uint32_t preferred_heap;
    uint64_t flags;
 };
-struct amdgpu_heap_info {
-   uint64_t heap_size;
-};
+
 struct amdgpu_gpu_info {
    uint32_t asic_id;
    uint32_t chip_external_rev;
@@ -283,25 +279,6 @@ static void drmFreeDevice(drmDevicePtr *device)
 static int drmGetDevice2(int fd, uint32_t flags, drmDevicePtr *device)
 {
    return -ENODEV;
-}
-static int amdgpu_bo_alloc(amdgpu_device_handle dev,
-   struct amdgpu_bo_alloc_request *alloc_buffer,
-   amdgpu_bo_handle *buf_handle)
-{
-   return -EINVAL;
-}
-static int amdgpu_bo_free(amdgpu_bo_handle buf_handle)
-{
-   return -EINVAL;
-}
-static int amdgpu_query_sw_info(amdgpu_device_handle dev, enum amdgpu_sw_info info,
-   void *value)
-{
-   return -EINVAL;
-}
-static const char *amdgpu_get_marketing_name(amdgpu_device_handle dev)
-{
-   return NULL;
 }
 static intptr_t readlink(const char *path, char *buf, size_t bufsiz)
 {
@@ -346,11 +323,11 @@ static uint64_t fix_vram_size(uint64_t size)
 }
 
 static bool
-has_tmz_support(amdgpu_device_handle dev, struct radeon_info *info, uint32_t ids_flags)
+has_tmz_support(ac_drm_device *dev, struct radeon_info *info, uint32_t ids_flags)
 {
    struct amdgpu_bo_alloc_request request = {0};
    int r;
-   amdgpu_bo_handle bo;
+   ac_drm_bo bo;
 
    if (ids_flags & AMDGPU_IDS_FLAGS_TMZ)
       return true;
@@ -370,10 +347,10 @@ has_tmz_support(amdgpu_device_handle dev, struct radeon_info *info, uint32_t ids
    request.phys_alignment = 1024;
    request.preferred_heap = AMDGPU_GEM_DOMAIN_VRAM;
    request.flags = AMDGPU_GEM_CREATE_ENCRYPTED;
-   r = amdgpu_bo_alloc(dev, &request, &bo);
+   r = ac_drm_bo_alloc(dev, &request, &bo);
    if (r)
       return false;
-   amdgpu_bo_free(bo);
+   ac_drm_bo_free(dev, bo);
    return true;
 }
 
@@ -573,7 +550,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    uint32_t vidip_fw_version = 0, vidip_fw_feature = 0;
    uint32_t num_instances = 0;
    int r, i, j;
-   amdgpu_device_handle dev = dev_p;
+   ac_drm_device *dev = dev_p;
 
    STATIC_ASSERT(AMDGPU_HW_IP_GFX == AMD_IP_GFX);
    STATIC_ASSERT(AMDGPU_HW_IP_COMPUTE == AMD_IP_COMPUTE);
@@ -611,13 +588,13 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    }
 
    /* Query hardware and driver information. */
-   r = ac_drm_query_gpu_info(fd, &amdinfo);
+   r = ac_drm_query_gpu_info(dev, &amdinfo);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_gpu_info failed.\n");
       return false;
    }
 
-   r = ac_drm_query_info(fd, AMDGPU_INFO_DEV_INFO, sizeof(device_info), &device_info);
+   r = ac_drm_query_info(dev, AMDGPU_INFO_DEV_INFO, sizeof(device_info), &device_info);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_info(dev_info) failed.\n");
       return false;
@@ -626,7 +603,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    for (unsigned ip_type = 0; ip_type < AMD_NUM_IP_TYPES; ip_type++) {
       struct drm_amdgpu_info_hw_ip ip_info = {0};
 
-      r = ac_drm_query_hw_ip_info(fd, ip_type, 0, &ip_info);
+      r = ac_drm_query_hw_ip_info(dev, ip_type, 0, &ip_info);
       if (r || !ip_info.available_rings)
          continue;
 
@@ -655,7 +632,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       info->ip[ip_type].num_queues = util_bitcount(ip_info.available_rings);
 
       /* query ip count */
-      r = ac_drm_query_hw_ip_count(fd, ip_type, &num_instances);
+      r = ac_drm_query_hw_ip_count(dev, ip_type, &num_instances);
       if (!r)
          info->ip[ip_type].num_instances = num_instances;
 
@@ -686,21 +663,21 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       return false;
    }
 
-   r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_GFX_ME, 0, 0, &info->me_fw_version,
+   r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_GFX_ME, 0, 0, &info->me_fw_version,
                                      &info->me_fw_feature);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(me) failed.\n");
       return false;
    }
 
-   r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_GFX_MEC, 0, 0, &info->mec_fw_version,
+   r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_GFX_MEC, 0, 0, &info->mec_fw_version,
                                      &info->mec_fw_feature);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(mec) failed.\n");
       return false;
    }
 
-   r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_GFX_PFP, 0, 0, &info->pfp_fw_version,
+   r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_GFX_PFP, 0, 0, &info->pfp_fw_version,
                                      &info->pfp_fw_feature);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(pfp) failed.\n");
@@ -708,7 +685,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    }
 
    if (info->ip[AMD_IP_VCN_DEC].num_queues || info->ip[AMD_IP_VCN_UNIFIED].num_queues) {
-      r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_VCN, 0, 0, &vidip_fw_version, &vidip_fw_feature);
+      r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_VCN, 0, 0, &vidip_fw_version, &vidip_fw_feature);
       if (r) {
          fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(vcn) failed.\n");
          return false;
@@ -719,7 +696,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       }
    } else {
       if (info->ip[AMD_IP_VCE].num_queues) {
-         r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_VCE, 0, 0, &vidip_fw_version, &vidip_fw_feature);
+         r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_VCE, 0, 0, &vidip_fw_version, &vidip_fw_feature);
          if (r) {
             fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(vce) failed.\n");
             return false;
@@ -728,7 +705,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       }
 
       if (info->ip[AMD_IP_UVD].num_queues) {
-         r = ac_drm_query_firmware_version(fd, AMDGPU_INFO_FW_UVD, 0, 0, &vidip_fw_version, &vidip_fw_feature);
+         r = ac_drm_query_firmware_version(dev, AMDGPU_INFO_FW_UVD, 0, 0, &vidip_fw_version, &vidip_fw_feature);
          if (r) {
             fprintf(stderr, "amdgpu: ac_drm_query_firmware_version(uvd) failed.\n");
             return false;
@@ -737,7 +714,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       }
    }
 
-   r = amdgpu_query_sw_info(dev, amdgpu_sw_info_address32_hi, &info->address32_hi);
+   r = ac_drm_query_sw_info(dev, amdgpu_sw_info_address32_hi, &info->address32_hi);
    if (r) {
       fprintf(stderr, "amdgpu: amdgpu_query_sw_info(address32_hi) failed.\n");
       return false;
@@ -745,7 +722,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
 
    struct drm_amdgpu_memory_info meminfo = {0};
 
-   r = ac_drm_query_info(fd, AMDGPU_INFO_MEMORY, sizeof(meminfo), &meminfo);
+   r = ac_drm_query_info(dev, AMDGPU_INFO_MEMORY, sizeof(meminfo), &meminfo);
    if (r) {
       fprintf(stderr, "amdgpu: ac_drm_query_info(memory) failed.\n");
       return false;
@@ -757,9 +734,9 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    info->vram_vis_size_kb = DIV_ROUND_UP(meminfo.cpu_accessible_vram.total_heap_size, 1024);
 
    if (info->drm_minor >= 41) {
-      ac_drm_query_video_caps_info(fd, AMDGPU_INFO_VIDEO_CAPS_DECODE,
+      ac_drm_query_video_caps_info(dev, AMDGPU_INFO_VIDEO_CAPS_DECODE,
                                    sizeof(info->dec_caps), &(info->dec_caps));
-      ac_drm_query_video_caps_info(fd, AMDGPU_INFO_VIDEO_CAPS_ENCODE,
+      ac_drm_query_video_caps_info(dev, AMDGPU_INFO_VIDEO_CAPS_ENCODE,
                                    sizeof(info->enc_caps), &(info->enc_caps));
    }
 
@@ -894,7 +871,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
       info->family_id = device_info.family;
       info->chip_external_rev = device_info.external_rev;
       info->chip_rev = device_info.chip_rev;
-      info->marketing_name = amdgpu_get_marketing_name(dev);
+      info->marketing_name = ac_drm_get_marketing_name(dev);
       info->is_pro_graphics = info->marketing_name && (strstr(info->marketing_name, "Pro") ||
                                                        strstr(info->marketing_name, "PRO") ||
                                                        strstr(info->marketing_name, "Frontier"));
@@ -1615,7 +1592,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
     * It can be calculated according to the kernel source code as:
     * (ring->max_dw - emit_frame_size) / emit_ib_size
     */
-   r = ac_drm_query_info(fd, AMDGPU_INFO_MAX_IBS,
+   r = ac_drm_query_info(dev, AMDGPU_INFO_MAX_IBS,
                          sizeof(info->max_submitted_ibs), info->max_submitted_ibs);
    if (r) {
       /* When the number of IBs can't be queried from the kernel, we choose a
@@ -1679,7 +1656,7 @@ bool ac_query_gpu_info(int fd, void *dev_p, struct radeon_info *info,
    if (info->gfx_level >= GFX11 && debug_get_bool_option("AMD_USERQ", false)) {
       struct drm_amdgpu_info_uq_fw_areas fw_info;
 
-      r = ac_drm_query_uq_fw_area_info(fd, AMDGPU_HW_IP_GFX, 0, &fw_info);
+      r = ac_drm_query_uq_fw_area_info(dev, AMDGPU_HW_IP_GFX, 0, &fw_info);
       if (r) {
          fprintf(stderr, "amdgpu: amdgpu_query_uq_fw_area_info() failed.\n");
          return false;

src/amd/common/ac_linux_drm.h
@@ -28,32 +28,58 @@ extern "C" {
 #define TAILV                                                                                      \
    {                                                                                               \
    }
+#define TAILPTR                                                                                    \
+   {                                                                                               \
+      return NULL;                                                                                 \
+   }
+typedef void* amdgpu_va_handle;
 #else
 #define PROC
 #define TAIL
 #define TAILV
+#define TAILPTR
 #endif
 
-PROC int ac_drm_bo_set_metadata(int device_fd, uint32_t bo_handle,
+struct ac_drm_device;
+typedef struct ac_drm_device ac_drm_device;
+
+typedef union ac_drm_bo {
+#ifdef _WIN32
+   void *abo;
+#else
+   amdgpu_bo_handle abo;
+#endif
+} ac_drm_bo;
+
+struct ac_drm_bo_import_result {
+   ac_drm_bo bo;
+   uint64_t alloc_size;
+};
+
+PROC int ac_drm_device_initialize(int fd, uint32_t *major_version, uint32_t *minor_version,
+                                  ac_drm_device **dev) TAIL;
+PROC void ac_drm_device_deinitialize(ac_drm_device *dev) TAILV;
+PROC int ac_drm_device_get_fd(ac_drm_device *dev) TAIL;
+PROC int ac_drm_bo_set_metadata(ac_drm_device *dev, uint32_t bo_handle,
                                 struct amdgpu_bo_metadata *info) TAIL;
-PROC int ac_drm_bo_query_info(int device_fd, uint32_t bo_handle, struct amdgpu_bo_info *info) TAIL;
-PROC int ac_drm_bo_wait_for_idle(int device_fd, uint32_t bo_handle, uint64_t timeout_ns,
+PROC int ac_drm_bo_query_info(ac_drm_device *dev, uint32_t bo_handle, struct amdgpu_bo_info *info) TAIL;
+PROC int ac_drm_bo_wait_for_idle(ac_drm_device *dev, ac_drm_bo bo, uint64_t timeout_ns,
                                  bool *busy) TAIL;
-PROC int ac_drm_bo_va_op(int device_fd, uint32_t bo_handle, uint64_t offset, uint64_t size,
+PROC int ac_drm_bo_va_op(ac_drm_device *dev, uint32_t bo_handle, uint64_t offset, uint64_t size,
                          uint64_t addr, uint64_t flags, uint32_t ops) TAIL;
-PROC int ac_drm_bo_va_op_raw(int device_fd, uint32_t bo_handle, uint64_t offset, uint64_t size,
+PROC int ac_drm_bo_va_op_raw(ac_drm_device *dev, uint32_t bo_handle, uint64_t offset, uint64_t size,
                              uint64_t addr, uint64_t flags, uint32_t ops) TAIL;
-PROC int ac_drm_bo_va_op_raw2(int device_fd, uint32_t bo_handle, uint64_t offset, uint64_t size,
+PROC int ac_drm_bo_va_op_raw2(ac_drm_device *dev, uint32_t bo_handle, uint64_t offset, uint64_t size,
                               uint64_t addr, uint64_t flags, uint32_t ops,
                               uint32_t vm_timeline_syncobj_out, uint64_t vm_timeline_point,
                               uint64_t input_fence_syncobj_handles,
                               uint32_t num_syncobj_handles) TAIL;
-PROC int ac_drm_cs_ctx_create2(int device_fd, uint32_t priority, uint32_t *ctx_handle) TAIL;
-PROC int ac_drm_cs_ctx_free(int device_fd, uint32_t ctx_handle) TAIL;
-PROC int ac_drm_cs_ctx_stable_pstate(int device_fd, uint32_t ctx_handle, uint32_t op,
+PROC int ac_drm_cs_ctx_create2(ac_drm_device *dev, uint32_t priority, uint32_t *ctx_id) TAIL;
+PROC int ac_drm_cs_ctx_free(ac_drm_device *dev, uint32_t ctx_id) TAIL;
+PROC int ac_drm_cs_ctx_stable_pstate(ac_drm_device *dev, uint32_t ctx_id, uint32_t op,
                                      uint32_t flags, uint32_t *out_flags) TAIL;
-PROC int ac_drm_cs_query_reset_state2(int device_fd, uint32_t ctx_handle, uint64_t *flags) TAIL;
-PROC int ac_drm_cs_query_fence_status(int device_fd, uint32_t ctx_handle, uint32_t ip_type,
+PROC int ac_drm_cs_query_reset_state2(ac_drm_device *dev, uint32_t ctx_id, uint64_t *flags) TAIL;
+PROC int ac_drm_cs_query_fence_status(ac_drm_device *dev, uint32_t ctx_id, uint32_t ip_type,
                                       uint32_t ip_instance, uint32_t ring, uint64_t fence_seq_no,
                                       uint64_t timeout_ns, uint64_t flags, uint32_t *expired) TAIL;
 PROC int ac_drm_cs_create_syncobj2(int device_fd, uint32_t flags, uint32_t *handle) TAIL;
@@ -72,37 +98,60 @@ PROC int ac_drm_cs_syncobj_export_sync_file2(int device_fd, uint32_t syncobj, ui
                                              uint32_t flags, int *sync_file_fd) TAIL;
 PROC int ac_drm_cs_syncobj_transfer(int device_fd, uint32_t dst_handle, uint64_t dst_point,
                                     uint32_t src_handle, uint64_t src_point, uint32_t flags) TAIL;
-PROC int ac_drm_cs_submit_raw2(int device_fd, uint32_t ctx_handle, uint32_t bo_list_handle,
+PROC int ac_drm_cs_submit_raw2(ac_drm_device *dev, uint32_t ctx_id, uint32_t bo_list_handle,
                                int num_chunks, struct drm_amdgpu_cs_chunk *chunks,
                                uint64_t *seq_no) TAIL;
 PROC void ac_drm_cs_chunk_fence_info_to_data(uint32_t bo_handle, uint64_t offset,
                                              struct drm_amdgpu_cs_chunk_data *data) TAILV;
-PROC int ac_drm_query_info(int device_fd, unsigned info_id, unsigned size, void *value) TAIL;
-PROC int ac_drm_read_mm_registers(int device_fd, unsigned dword_offset, unsigned count,
+PROC int ac_drm_cs_syncobj_timeline_wait(int device_fd, uint32_t *handles, uint64_t *points,
+                                         unsigned num_handles, int64_t timeout_nsec, unsigned flags,
+                                         uint32_t *first_signaled) TAIL;
+PROC int ac_drm_query_info(ac_drm_device *dev, unsigned info_id, unsigned size, void *value) TAIL;
+PROC int ac_drm_read_mm_registers(ac_drm_device *dev, unsigned dword_offset, unsigned count,
                                   uint32_t instance, uint32_t flags, uint32_t *values) TAIL;
-PROC int ac_drm_query_hw_ip_count(int device_fd, unsigned type, uint32_t *count) TAIL;
-PROC int ac_drm_query_hw_ip_info(int device_fd, unsigned type, unsigned ip_instance,
+PROC int ac_drm_query_hw_ip_count(ac_drm_device *dev, unsigned type, uint32_t *count) TAIL;
+PROC int ac_drm_query_hw_ip_info(ac_drm_device *dev, unsigned type, unsigned ip_instance,
                                  struct drm_amdgpu_info_hw_ip *info) TAIL;
-PROC int ac_drm_query_firmware_version(int device_fd, unsigned fw_type, unsigned ip_instance,
+PROC int ac_drm_query_firmware_version(ac_drm_device *dev, unsigned fw_type, unsigned ip_instance,
                                        unsigned index, uint32_t *version, uint32_t *feature) TAIL;
-PROC int ac_drm_query_uq_fw_area_info(int device_fd, unsigned type, unsigned ip_instance,
+PROC int ac_drm_query_uq_fw_area_info(ac_drm_device *dev, unsigned type, unsigned ip_instance,
                                       struct drm_amdgpu_info_uq_fw_areas *info) TAIL;
-PROC int ac_drm_query_gpu_info(int device_fd, struct amdgpu_gpu_info *info) TAIL;
-PROC int ac_drm_query_heap_info(int device_fd, uint32_t heap, uint32_t flags,
+PROC int ac_drm_query_gpu_info(ac_drm_device *dev, struct amdgpu_gpu_info *info) TAIL;
+PROC int ac_drm_query_heap_info(ac_drm_device *dev, uint32_t heap, uint32_t flags,
                                 struct amdgpu_heap_info *info) TAIL;
-PROC int ac_drm_query_sensor_info(int device_fd, unsigned sensor_type, unsigned size,
+PROC int ac_drm_query_sensor_info(ac_drm_device *dev, unsigned sensor_type, unsigned size,
                                   void *value) TAIL;
-PROC int ac_drm_query_video_caps_info(int device_fd, unsigned cap_type, unsigned size,
+PROC int ac_drm_query_video_caps_info(ac_drm_device *dev, unsigned cap_type, unsigned size,
                                       void *value) TAIL;
-PROC int ac_drm_vm_reserve_vmid(int device_fd, uint32_t flags) TAIL;
-PROC int ac_drm_vm_unreserve_vmid(int device_fd, uint32_t flags) TAIL;
-PROC int ac_drm_create_userqueue(int device_fd, uint32_t ip_type, uint32_t doorbell_handle,
+PROC int ac_drm_query_gpuvm_fault_info(ac_drm_device *dev, unsigned size, void *value) TAIL;
+PROC int ac_drm_vm_reserve_vmid(ac_drm_device *dev, uint32_t flags) TAIL;
+PROC int ac_drm_vm_unreserve_vmid(ac_drm_device *dev, uint32_t flags) TAIL;
+PROC const char *ac_drm_get_marketing_name(ac_drm_device *device) TAILPTR;
+PROC int ac_drm_query_sw_info(ac_drm_device *dev,
+                              enum amdgpu_sw_info info, void *value) TAIL;
+PROC int ac_drm_bo_alloc(ac_drm_device *dev, struct amdgpu_bo_alloc_request *alloc_buffer,
+                         ac_drm_bo *bo) TAIL;
+PROC int ac_drm_bo_export(ac_drm_device *dev, ac_drm_bo bo,
+                          enum amdgpu_bo_handle_type type, uint32_t *shared_handle) TAIL;
+PROC int ac_drm_bo_import(ac_drm_device *dev, enum amdgpu_bo_handle_type type,
+                          uint32_t shared_handle, struct ac_drm_bo_import_result *output) TAIL;
+PROC int ac_drm_create_bo_from_user_mem(ac_drm_device *dev, void *cpu,
+                                        uint64_t size, ac_drm_bo *bo) TAIL;
+PROC int ac_drm_bo_free(ac_drm_device *dev, ac_drm_bo bo) TAIL;
+PROC int ac_drm_bo_cpu_map(ac_drm_device *dev, ac_drm_bo bo, void **cpu) TAIL;
+PROC int ac_drm_bo_cpu_unmap(ac_drm_device *dev, ac_drm_bo bo) TAIL;
+PROC int ac_drm_va_range_alloc(ac_drm_device *dev, enum amdgpu_gpu_va_range va_range_type,
+                               uint64_t size, uint64_t va_base_alignment, uint64_t va_base_required,
+                               uint64_t *va_base_allocated, amdgpu_va_handle *va_range_handle,
+                               uint64_t flags) TAIL;
+PROC int ac_drm_va_range_free(amdgpu_va_handle va_range_handle) TAIL;
+PROC int ac_drm_create_userqueue(ac_drm_device *dev, uint32_t ip_type, uint32_t doorbell_handle,
                                  uint32_t doorbell_offset, uint64_t queue_va, uint64_t queue_size,
                                  uint64_t wptr_va, uint64_t rptr_va, void *mqd_in,
                                  uint32_t *queue_id) TAIL;
-PROC int ac_drm_free_userqueue(int device_fd, uint32_t queue_id) TAIL;
-PROC int ac_drm_userq_signal(int device_fd, struct drm_amdgpu_userq_signal *signal_data) TAIL;
-PROC int ac_drm_userq_wait(int device_fd, struct drm_amdgpu_userq_wait *wait_data) TAIL;
+PROC int ac_drm_free_userqueue(ac_drm_device *dev, uint32_t queue_id) TAIL;
+PROC int ac_drm_userq_signal(ac_drm_device *dev, struct drm_amdgpu_userq_signal *signal_data) TAIL;
+PROC int ac_drm_userq_wait(ac_drm_device *dev, struct drm_amdgpu_userq_wait *wait_data) TAIL;
 
 #ifdef __cplusplus
 }

commit d27748a76f7dd9236bfcf9ef172dc13b8c0e170f

radeonsi: use ac_drm_device_get_cookie
Instead of the raw ac_drm_device pointer which will be different
for each call to ac_drm_device_initialize.

Fixes: a565f2994fe ("amd: move all uses of libdrm_amdgpu to ac_linux_drm")
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33081>

src/gallium/winsys/amdgpu/drm/amdgpu_winsys.c
@@ -126,7 +126,8 @@ static void amdgpu_winsys_destroy_locked(struct radeon_winsys *rws, bool locked)
 
    destroy = pipe_reference(&aws->reference, NULL);
    if (destroy && dev_tab) {
-      _mesa_hash_table_remove_key(dev_tab, aws->dev);
+      _mesa_hash_table_remove_key(dev_tab,
+                                  (void *)ac_drm_device_get_cookie(aws->dev));
       if (_mesa_hash_table_num_entries(dev_tab) == 0) {
          _mesa_hash_table_destroy(dev_tab, NULL);
          dev_tab = NULL;
@@ -403,7 +404,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
    }
 
    /* Lookup a winsys if we have already created one for this device. */
-   aws = util_hash_table_get(dev_tab, dev);
+   aws = util_hash_table_get(dev_tab, (void *)ac_drm_device_get_cookie(dev));
    if (aws) {
       struct amdgpu_screen_winsys *sws_iter;
 
@@ -516,7 +517,7 @@ amdgpu_winsys_create(int fd, const struct pipe_screen_config *config,
          return NULL;
       }
 
-      _mesa_hash_table_insert(dev_tab, dev, aws);
+      _mesa_hash_table_insert(dev_tab, (void *)ac_drm_device_get_cookie(dev), aws);
 
       if (aws->reserve_vmid) {
          r = ac_drm_vm_reserve_vmid(aws->dev, 0);