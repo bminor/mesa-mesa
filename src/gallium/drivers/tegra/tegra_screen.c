b3ae0e60544dd6927529efefd5504724017908e5
Qiang Yu <yuq825@gmail.com>
2025-01-22 10:07:17 +0800

    tegra: remove shader and compute get param
    Acked-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Acked-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33176>

--- src/gallium/drivers/tegra/tegra_screen.c
+++ src/gallium/drivers/tegra/tegra_screen.c
@@ -73,15 +73,6 @@ tegra_screen_get_device_vendor(struct pipe_screen *pscreen)
 }
 
 static int
-tegra_screen_get_shader_param(struct pipe_screen *pscreen, enum pipe_shader_type shader,
-                              enum pipe_shader_cap param)
-{
-   struct tegra_screen *screen = to_tegra_screen(pscreen);
-
-   return screen->gpu->get_shader_param(screen->gpu, shader, param);
-}
-
-static int
 tegra_screen_get_video_param(struct pipe_screen *pscreen,
                              enum pipe_video_profile profile,
                              enum pipe_video_entrypoint entrypoint,
@@ -91,17 +82,6 @@ tegra_screen_get_video_param(struct pipe_screen *pscreen,
 
    return screen->gpu->get_video_param(screen->gpu, profile, entrypoint,
                                        param);
-}
-
-static int
-tegra_screen_get_compute_param(struct pipe_screen *pscreen,
-                               enum pipe_compute_cap param,
-                               void *retp)
-{
-   struct tegra_screen *screen = to_tegra_screen(pscreen);
-
-   return screen->gpu->get_compute_param(screen->gpu, param,
-                                         retp);
 }
 
 static uint64_t
@@ -588,9 +568,7 @@ tegra_screen_create(int fd)
    screen->base.get_vendor = tegra_screen_get_vendor;
    screen->base.get_device_vendor = tegra_screen_get_device_vendor;
    screen->base.get_screen_fd = tegra_screen_get_fd;
-   screen->base.get_shader_param = tegra_screen_get_shader_param;
    screen->base.get_video_param = tegra_screen_get_video_param;
-   screen->base.get_compute_param = tegra_screen_get_compute_param;
    screen->base.get_timestamp = tegra_screen_get_timestamp;
    screen->base.context_create = tegra_screen_context_create;
    screen->base.is_format_supported = tegra_screen_is_format_supported;
