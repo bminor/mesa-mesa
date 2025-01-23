bminor/mesa-mesa

commit d46b80249bd207ce460b74a9aa1e58cd584f58b2

panvk: enable subgroupSizeControl
This is trivial for us, the hardware only ever supports a single
subgroup size.

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32710>

src/panfrost/vulkan/panvk_physical_device.c
@@ -245,6 +245,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .EXT_sampler_filter_minmax = arch >= 10,
       .EXT_scalar_block_layout = true,
       .EXT_shader_module_identifier = true,
+      .EXT_subgroup_size_control = true,
       .EXT_tooling_info = true,
       .GOOGLE_decorate_string = true,
       .GOOGLE_hlsl_functionality1 = true,
@@ -356,8 +357,8 @@ get_features(const struct panvk_physical_device *device,
       .privateData = true,
       .shaderDemoteToHelperInvocation = false,
       .shaderTerminateInvocation = false,
-      .subgroupSizeControl = false,
-      .computeFullSubgroups = false,
+      .subgroupSizeControl = true,
+      .computeFullSubgroups = true,
       .synchronization2 = true,
       .textureCompressionASTC_HDR = false,
       .shaderZeroInitializeWorkgroupMemory = true,
@@ -773,10 +774,11 @@ get_device_properties(const struct panvk_instance *instance,
       /* Vulkan 1.3 properties */
       /* XXX: 1.3 support */
       /* XXX: VK_EXT_subgroup_size_control */
-      .minSubgroupSize = 8,
-      .maxSubgroupSize = 8,
-      .maxComputeWorkgroupSubgroups = 48,
-      .requiredSubgroupSizeStages = VK_SHADER_STAGE_ALL,
+      .minSubgroupSize = pan_subgroup_size(arch),
+      .maxSubgroupSize = pan_subgroup_size(arch),
+      .maxComputeWorkgroupSubgroups =
+         device->kmod.props.max_threads_per_wg / pan_subgroup_size(arch),
+      .requiredSubgroupSizeStages = VK_SHADER_STAGE_COMPUTE_BIT,
       /* XXX: VK_EXT_inline_uniform_block */
       .maxInlineUniformBlockSize = MAX_INLINE_UNIFORM_BLOCK_SIZE,
       .maxPerStageDescriptorInlineUniformBlocks =

commit e34e474f24bc0047cc0b30597781f34365fd1f7b

panvk: do not expose EXT_subgroup_size_control on bifrost
This exptension requires Vulkan 1.1, which we don't expose there yet.

While we're at it, put panvk into the normal sorted order of the list of
drivers.

Fixes: d46b80249bd ("panvk: enable subgroupSizeControl")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33180>

src/panfrost/vulkan/panvk_physical_device.c
@@ -245,7 +245,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .EXT_sampler_filter_minmax = arch >= 10,
       .EXT_scalar_block_layout = true,
       .EXT_shader_module_identifier = true,
-      .EXT_subgroup_size_control = true,
+      .EXT_subgroup_size_control = arch >= 10, /* requires vk1.1 */
       .EXT_tooling_info = true,
       .GOOGLE_decorate_string = true,
       .GOOGLE_hlsl_functionality1 = true,