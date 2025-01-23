11364cd133ecf2a66ba8fc1fc25a50931dee70ff
Marek Olšák <marek.olsak@amd.com>
2025-01-23 07:03:50 -0500

    mesa: remove a glapi workaround for Mesa 10.5 and older
    Reviewed-by: Adam Jackson <ajax@redhat.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33634>

--- src/mesa/main/context.c
+++ src/mesa/main/context.c
@@ -720,19 +720,6 @@ update_default_objects(struct gl_context *ctx)
 }
 
 
-/* XXX this is temporary and should be removed at some point in the
- * future when there's a reasonable expectation that the libGL library
- * contains the _glapi_new_nop_table() and _glapi_set_nop_handler()
- * functions which were added in Mesa 10.6.
- */
-#if !defined(_WIN32)
-/* Avoid libGL / driver ABI break */
-#define USE_GLAPI_NOP_FEATURES 0
-#else
-#define USE_GLAPI_NOP_FEATURES 1
-#endif
-
-
 /**
  * This function is called by the glapi no-op functions.  For each OpenGL
  * function/entrypoint there's a simple no-op function.  These "no-op"
@@ -748,7 +735,6 @@ update_default_objects(struct gl_context *ctx)
  *
  * \param name  the name of the OpenGL function
  */
-#if USE_GLAPI_NOP_FEATURES
 static void
 nop_handler(const char *name)
 {
@@ -765,7 +751,6 @@ nop_handler(const char *name)
    }
 #endif
 }
-#endif
 
 
 /**
@@ -776,19 +761,6 @@ static void GLAPIENTRY
 nop_glFlush(void)
 {
    /* don't record an error like we do in nop_handler() */
-}
-#endif
-
-
-#if !USE_GLAPI_NOP_FEATURES
-static int
-generic_nop(void)
-{
-   GET_CURRENT_CONTEXT(ctx);
-   _mesa_error(ctx, GL_INVALID_OPERATION,
-               "unsupported function called "
-               "(unsupported extension or deprecated function?)");
-   return 0;
 }
 #endif
 
@@ -813,20 +785,7 @@ glthread_nop(void)
 struct _glapi_table *
 _mesa_new_nop_table(unsigned numEntries, bool glthread)
 {
-   struct _glapi_table *table;
-
-#if !USE_GLAPI_NOP_FEATURES
-   table = malloc(numEntries * sizeof(_glapi_proc));
-   if (table) {
-      _glapi_proc *entry = (_glapi_proc *) table;
-      unsigned i;
-      for (i = 0; i < numEntries; i++) {
-         entry[i] = (_glapi_proc) generic_nop;
-      }
-   }
-#else
-   table = _glapi_new_nop_table(numEntries);
-#endif
+   struct _glapi_table *table = _glapi_new_nop_table(numEntries);
 
    if (glthread) {
       _glapi_proc *entry = (_glapi_proc *) table;
@@ -877,10 +836,7 @@ _mesa_alloc_dispatch_table(bool glthread)
    }
 #endif
 
-#if USE_GLAPI_NOP_FEATURES
    _glapi_set_nop_handler(nop_handler);
-#endif
-
    return table;
 }
 
