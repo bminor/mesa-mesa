db88a490b8d257b5409dc2161c28838a6bd53160
Rob Clark <robdclark@chromium.org>
2025-01-27 09:55:47 -0800

    tu: Avoid extraneous set_iova
    The GEM_NEW ccmd already passes the iova, so we don't need an extra
    SET_IOVA for newly created BOs.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33433>

--- src/freedreno/vulkan/tu_knl_drm_virtio.cc
+++ src/freedreno/vulkan/tu_knl_drm_virtio.cc
@@ -574,8 +574,6 @@ tu_bo_init(struct tu_device *dev,
 {
    assert(dev->physical_device->has_set_iova);
 
-   set_iova(dev, bo->res_id, iova);
-
    name = tu_debug_bos_add(dev, size, name);
 
    mtx_lock(&dev->bo_mutex);
@@ -834,6 +832,8 @@ virtio_bo_init_dmabuf(struct tu_device *dev,
    } else {
       *out_bo = bo;
    }
+
+   set_iova(dev, bo->res_id, iova);
 
 out_unlock:
    mtx_unlock(&dev->vma_mutex);
