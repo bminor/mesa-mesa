63c6f3e0f0859e7ca275dd13f3e71b5b3188f77d
Erik Faye-Lund <erik.faye-lund@collabora.com>
2025-01-27 17:54:35 +0100

    panvk: rename helper
    This function is no longer the only concept we have of "Vulkan version",
    so let's rename it to reflect that it's only about the API-versin. We
    don't really need to specify that it's about Vulkan versions, that seems
    pretty obvious here.
    
    Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33254>

--- src/panfrost/vulkan/panvk_physical_device.c
+++ src/panfrost/vulkan/panvk_physical_device.c
@@ -434,7 +434,7 @@ get_features(const struct panvk_physical_device *device,
 }
 
 static uint32_t
-get_vk_version(unsigned arch)
+get_api_version(unsigned arch)
 {
    const uint32_t version_override = vk_get_version_override();
    if (version_override)
@@ -473,7 +473,7 @@ get_device_properties(const struct panvk_instance *instance,
    assert(arch > 8 || device->kmod.props.max_threads_per_wg <= 1024);
 
    *properties = (struct vk_properties){
-      .apiVersion = get_vk_version(arch),
+      .apiVersion = get_api_version(arch),
       .driverVersion = vk_get_driver_version(),
       .vendorID = ARM_VENDOR_ID,
 
