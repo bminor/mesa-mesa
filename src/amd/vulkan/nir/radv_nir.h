ff225dee67e6bba605e7d4ca8eb4d8653c78f5c8
Georg Lehmann <dadschoorse@gmail.com>
2025-02-01 20:21:07 +0100

    radv: inline radv_nir_lower_poly_line_smooth
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33340>

--- src/amd/vulkan/nir/radv_nir.h
+++ src/amd/vulkan/nir/radv_nir.h
@@ -69,8 +69,6 @@ void radv_nir_lower_io(struct radv_device *device, nir_shader *nir);
 
 bool radv_nir_lower_io_to_mem(struct radv_device *device, struct radv_shader_stage *stage);
 
-void radv_nir_lower_poly_line_smooth(nir_shader *nir, const struct radv_graphics_state_key *gfx_state);
-
 bool radv_nir_lower_cooperative_matrix(nir_shader *shader, unsigned wave_size);
 
 bool radv_nir_lower_draw_id_to_zero(nir_shader *shader);
