f13b95ad5cbd529b85a9535c16c9383e4541cba1
Guilherme Gallo <guilherme.gallo@collabora.com>
2025-02-04 17:36:22 -0300

    ci: add support for structural tagging
    Make structural tagging functions available for both test and build
    scripts.
    
    Introduces the update_tag.sh helper for listing, checking, and updating
    deterministic tags.
    
    Also adds the ci_tag_build_time_check and ci_tag_test_time_check
    functions to validate tags during build and test phases, ensuring
    consistent component versioning.
    
    Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33421>

--- .gitlab-ci/setup-test-env.sh
+++ .gitlab-ci/setup-test-env.sh
@@ -187,6 +187,147 @@ function trap_err {
     error ${CURRENT_SECTION:-'unknown-section'}: ret code: $*
 }
 
+# ------ Structured tagging
+export _CI_TAG_CHECK_DIR="/mesa-ci-build-tag"
+
+_ci_tag_from_name_to_var() {
+    # Transforms MY_COMPONENT_TAG to my-component
+    echo "${1%_TAG}" | tr '[:upper:]' '[:lower:]' | tr '_' '-'
+}
+
+_ci_tag_check() (
+    x_off
+    _declared_name="${1}"
+    declare -n _declared="${_declared_name}"
+    _calculated="${2}"
+    local component_lower=$(_ci_tag_from_name_to_var "${_declared_name}")
+
+    if [ -z "${_declared:-}" ]; then
+        # Close the section
+        error "Fatal error"
+        _error_msg "Structured tag is not set: ${_declared_name}"
+        _error_msg ""
+        echo "If you are adding a new component, please run:"
+        echo "bin/ci/update_tag.py --include ${component_lower}"
+        echo "This will automatically update the YAML file for you."
+        echo "Or manually edit .gitlab-ci/conditional-build-image-tags.yml to add the new"
+        echo "component."
+        error ""
+        exit 2
+    fi
+
+    if [ "${_declared}" != "${_calculated}" ]; then
+        # Close the section
+        error "Fatal error"
+        _error_msg "Mismatch in declared and calculated tags:"
+        _error_msg "    ${_declared_name} from the YAML is \"${_declared}\""
+        _error_msg "    ... but I think it should be \"${_calculated}\""
+        _error_msg ""
+        echo "Usually this happens when you change what you want to be built without also"
+        echo "changing the YAML declaration. For example, you've bumped SKQP to version"
+        echo "1.2.3, but you still have 'SKQP_VER: 1.2.2' in"
+        echo ".gitlab-ci/conditional-build-image-tags.yml."
+        echo ""
+        echo "If you meant to change the component I'm talking about, please change the"
+        echo "tag and resubmit. You can also run:"
+        echo "bin/ci/update_tag.py --include ${component_lower}"
+        echo "to update the tag automatically."
+        echo ""
+        echo "If you didn't mean to change the component, please ping @mesa/ci-helpers and we"
+        echo "can help you figure out what's gone wrong."
+        echo ""
+        echo "But for now, I've got to fail this build. Sorry."
+        exit 2
+    fi
+    x_restore
+)
+
+_ci_tag_check_build() {
+    x_off
+    if [ -n "${NEW_TAG_DRY_RUN:-}" ]; then
+        echo "${2}"
+        exit 0
+    fi
+
+    _ci_tag_check "${1}" "${2}"
+
+    if [ -n "${CI_NOT_BUILDING_ANYTHING:-}" ]; then
+        exit 0
+    fi
+    x_restore
+}
+
+get_tag_file() {
+    x_off
+    # If no tag name is provided, return the directory
+    echo "${_CI_TAG_CHECK_DIR}/${1:-}"
+    x_restore
+}
+
+_ci_tag_write() (
+    set +x
+    local tag_name="${1}"
+    local tag_value="${2}"
+
+    mkdir -p "${_CI_TAG_CHECK_DIR}"
+    echo -n "${tag_value}" > "$(get_tag_file "${tag_name}")"
+)
+
+_ci_calculate_tag() {
+    x_off
+    # the args are files that can affect the build output
+    mapfile -t extra_files < <(printf '%s\n' "$@")
+    (
+        for extra_file in "${extra_files[@]}"; do
+            if [ ! -f "${extra_file}" ]; then
+                error "File '${extra_file}' does not exist"
+                exit 1
+            fi
+            cat "${extra_file}"
+        done
+    ) | md5sum | cut -d' ' -f1
+    x_restore
+}
+
+ci_tag_build_time_check() {
+    # Get the caller script and hash its contents plus the extra files
+    x_off
+    local tag_name="${1}"
+    local build_script_file="build-$(_ci_tag_from_name_to_var "${tag_name}").sh"
+    local build_script=".gitlab-ci/container/${build_script_file}"
+    shift
+    # now $@ has the extra files
+    local calculated_tag=$(_ci_calculate_tag "${build_script}" "$@")
+
+    _ci_tag_check_build "${tag_name}" "${calculated_tag}"
+    _ci_tag_write "${tag_name}" "${calculated_tag}"
+    x_restore
+}
+
+ci_tag_test_time_check() {
+    x_off
+    local tag_file=$(get_tag_file "${1}")
+    if [ ! -f "${tag_file}" ]; then
+        _error_msg "Structured tag file ${tag_file} does not exist"
+        _error_msg "Please run the ci_tag_build_time_check first and rebuild the image/rootfs"
+        exit 2
+    fi
+    _ci_tag_check "${1}" "$(cat "${tag_file}")"
+    x_restore
+}
+
+# Export all functions
+export -f _ci_calculate_tag
+export -f _ci_tag_check
+export -f _ci_tag_check_build
+export -f _ci_tag_from_name_to_var
+export -f _ci_tag_write
+export -f ci_tag_build_time_check
+export -f ci_tag_test_time_check
+export -f get_tag_file
+
+# Structured tagging ------
+
 export -f error
 export -f trap_err
 
