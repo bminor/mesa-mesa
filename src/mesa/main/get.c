337d19c2fce9cac8d74214d909ae4df5b3d7b3b2
Qiang Yu <yuq825@gmail.com>
2025-02-10 15:41:47 +0800

    mesa: implement EXT_mesh_shader glGet* values
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36751>

--- src/mesa/main/get.c
+++ src/mesa/main/get.c
@@ -180,6 +180,7 @@ enum value_location {
    LOC_CONTEXT,
    LOC_ARRAY,
    LOC_TEXUNIT,
+   LOC_CAPS,
    LOC_CUSTOM
 };
 
@@ -254,6 +255,8 @@ union value {
    LOC_CONTEXT, type, offsetof(struct gl_context, field)
 #define ARRAY_FIELD(field, type) \
    LOC_ARRAY, type, offsetof(struct gl_vertex_array_object, field)
+#define CAPS_FIELD(field, type) \
+   LOC_CAPS, type, offsetof(struct pipe_caps, field)
 #undef CONST /* already defined through windows.h */
 #define CONST(value) \
    LOC_CONTEXT, TYPE_CONST, value
@@ -294,6 +297,10 @@ union value {
 #define ARRAY_BOOL(field) ARRAY_FIELD(field, TYPE_BOOLEAN)
 #define ARRAY_UBYTE(field) ARRAY_FIELD(field, TYPE_UBYTE)
 #define ARRAY_SHORT(field) ARRAY_FIELD(field, TYPE_SHORT)
+
+/* pipe_caps fields */
+#define CAPS_UINT(field) CAPS_FIELD(field, TYPE_UINT)
+#define CAPS_BOOL(field) CAPS_FIELD(field, TYPE_BOOLEAN)
 
 #define EXT(f)					\
    offsetof(struct gl_extensions, f)
@@ -594,6 +601,7 @@ EXTRA_EXT(ARB_sparse_texture);
 EXTRA_EXT(KHR_shader_subgroup);
 EXTRA_EXT(OVR_multiview);
 EXTRA_EXT(NV_timeline_semaphore);
+EXTRA_EXT(EXT_mesh_shader);
 
 static const int extra_ARB_gl_spirv_or_es2_compat[] = {
    EXT(ARB_gl_spirv),
@@ -1686,6 +1694,9 @@ find_value(const char *func, GLenum pname, void **p, union value *v)
                   _mesa_enum_to_string(pname),
                   ctx->Texture.CurrentUnit);
       return &error_value;
+   case LOC_CAPS:
+      *p = ((char *) &ctx->screen->caps + d->offset);
+      return d;
    case LOC_CUSTOM:
       find_custom_value(ctx, d, v);
       *p = v;
@@ -3015,6 +3026,35 @@ find_value_indexed(const char *func, GLenum pname, GLuint index, union value *v)
       if (index >= ctx->Const.MaxViewports)
          goto invalid_value;
       v->value_int = ctx->ViewportArray[index].SwizzleW;
+      return TYPE_INT;
+   /* GL_EXT_mesh_shader */
+   case GL_MAX_TASK_WORK_GROUP_COUNT_EXT:
+      if (!ctx->Extensions.EXT_mesh_shader)
+         goto invalid_enum;
+      if (index >= 3)
+         goto invalid_value;
+      v->value_uint = ctx->screen->caps.mesh.max_task_work_group_count[index];
+      return TYPE_UINT;
+   case GL_MAX_MESH_WORK_GROUP_COUNT_EXT:
+      if (!ctx->Extensions.EXT_mesh_shader)
+         goto invalid_enum;
+      if (index >= 3)
+         goto invalid_value;
+      v->value_uint = ctx->screen->caps.mesh.max_mesh_work_group_count[index];
+      return TYPE_UINT;
+   case GL_MAX_TASK_WORK_GROUP_SIZE_EXT:
+      if (!ctx->Extensions.EXT_mesh_shader)
+         goto invalid_enum;
+      if (index >= 3)
+         goto invalid_value;
+      v->value_int = ctx->screen->caps.mesh.max_task_work_group_size[index];
+      return TYPE_INT;
+   case GL_MAX_MESH_WORK_GROUP_SIZE_EXT:
+      if (!ctx->Extensions.EXT_mesh_shader)
+         goto invalid_enum;
+      if (index >= 3)
+         goto invalid_value;
+      v->value_int = ctx->screen->caps.mesh.max_mesh_work_group_size[index];
       return TYPE_INT;
    }
 
