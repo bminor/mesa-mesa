17abc5f326115625e768a21eb8b35e73dcdd616d
Rhys Perry <pendingchaos02@gmail.com>
2025-02-10 12:50:41 +0000

    radv: improve radv_get_max_waves for multi-wave workgroups with LDS
    LDS isn't divided among SIMDs, and it doesn't make sense to launch a
    fraction of a compute workgroup.
    
    Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33873>

--- src/amd/vulkan/radv_shader.c
+++ src/amd/vulkan/radv_shader.c
@@ -2600,15 +2600,16 @@ radv_get_max_waves(const struct radv_device *device, const struct ac_shader_conf
    const uint8_t wave_size = info->wave_size;
    gl_shader_stage stage = info->stage;
    unsigned max_simd_waves = gpu_info->max_waves_per_simd;
-   unsigned lds_per_wave = 0;
+   unsigned lds_per_workgroup = 0;
+   unsigned waves_per_workgroup = 1;
 
    if (stage == MESA_SHADER_FRAGMENT) {
-      lds_per_wave = conf->lds_size * gpu_info->lds_encode_granularity + info->ps.num_inputs * 48;
-      lds_per_wave = align(lds_per_wave, gpu_info->lds_alloc_granularity);
+      lds_per_workgroup = conf->lds_size * gpu_info->lds_encode_granularity + info->ps.num_inputs * 48;
+      lds_per_workgroup = align(lds_per_workgroup, gpu_info->lds_alloc_granularity);
    } else if (stage == MESA_SHADER_COMPUTE || stage == MESA_SHADER_TASK) {
       unsigned max_workgroup_size = info->workgroup_size;
-      lds_per_wave = align(conf->lds_size * gpu_info->lds_encode_granularity, gpu_info->lds_alloc_granularity);
-      lds_per_wave /= DIV_ROUND_UP(max_workgroup_size, wave_size);
+      lds_per_workgroup = align(conf->lds_size * gpu_info->lds_encode_granularity, gpu_info->lds_alloc_granularity);
+      waves_per_workgroup = DIV_ROUND_UP(max_workgroup_size, wave_size);
    }
 
    if (conf->num_sgprs && gfx_level < GFX10) {
@@ -2626,13 +2627,14 @@ radv_get_max_waves(const struct radv_device *device, const struct ac_shader_conf
       max_simd_waves = MIN2(max_simd_waves, physical_vgprs / vgprs);
    }
 
-   unsigned simd_per_workgroup = gpu_info->num_simd_per_compute_unit;
+   unsigned simd_per_cu_wgp = gpu_info->num_simd_per_compute_unit;
    if (gfx_level >= GFX10)
-      simd_per_workgroup *= 2; /* like lds_size_per_workgroup, assume WGP on GFX10+ */
+      simd_per_cu_wgp *= 2; /* like lds_size_per_workgroup, assume WGP on GFX10+ */
 
-   unsigned max_lds_per_simd = gpu_info->lds_size_per_workgroup / simd_per_workgroup;
-   if (lds_per_wave)
-      max_simd_waves = MIN2(max_simd_waves, DIV_ROUND_UP(max_lds_per_simd, lds_per_wave));
+   if (lds_per_workgroup) {
+      unsigned max_cu_wgp_waves = gpu_info->lds_size_per_workgroup / lds_per_workgroup * waves_per_workgroup;
+      max_simd_waves = MIN2(max_simd_waves, DIV_ROUND_UP(max_cu_wgp_waves, simd_per_cu_wgp));
+   }
 
    return gfx_level >= GFX10 ? max_simd_waves * (wave_size / 32) : max_simd_waves;
 }
