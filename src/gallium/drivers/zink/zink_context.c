f725909179c56c19d09b37378e76bacb746a8b99
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2025-02-10 14:29:13 -0500

    zink: only enable unsynchronized_texture_subdata with HIC
    this is otherwise useless
    
    cc: mesa-stable
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33484>
    (cherry picked from commit 23040782614bed020156d2a43eadd47fa06ad387)

--- src/gallium/drivers/zink/zink_context.c
+++ src/gallium/drivers/zink/zink_context.c
@@ -5592,7 +5592,7 @@ zink_context_create(struct pipe_screen *pscreen, void *priv, unsigned flags)
                                                         .is_resource_busy = zink_context_is_resource_busy,
                                                         .driver_calls_flush_notify = !screen->driver_workarounds.track_renderpasses,
                                                         .unsynchronized_get_device_reset_status = true,
-                                                        .unsynchronized_texture_subdata = true,
+                                                        .unsynchronized_texture_subdata = screen->info.have_EXT_host_image_copy,
                                                         .parse_renderpass_info = screen->driver_workarounds.track_renderpasses,
                                                         .dsa_parse = zink_tc_parse_dsa,
                                                         .fs_parse = zink_tc_parse_fs,
