a4cffa91b847844eb9186e586d9952d3b0b38727
Daniel Schürmann <daniel@schuermann.dev>
2025-02-11 11:16:09 +0100

    nir: remove nir_lower_discard_if_to_cf option
    Since removing nir_intrinsic_discard{_if} it has no purpose anymore.
    
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Reviewed-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33492>

--- src/compiler/nir/nir.h
+++ src/compiler/nir/nir.h
@@ -5849,9 +5849,8 @@ bool nir_lower_interpolation(nir_shader *shader,
                              nir_lower_interpolation_options options);
 
 typedef enum {
-   nir_lower_discard_if_to_cf = (1 << 0),
-   nir_lower_demote_if_to_cf = (1 << 1),
-   nir_lower_terminate_if_to_cf = (1 << 2),
+   nir_lower_demote_if_to_cf = (1 << 0),
+   nir_lower_terminate_if_to_cf = (1 << 1),
 } nir_lower_discard_if_options;
 
 bool nir_lower_discard_if(nir_shader *shader, nir_lower_discard_if_options options);
