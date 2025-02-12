0e983886144b891dc4d89d0661a22134f631d5ed
Daniel Schürmann <daniel@schuermann.dev>
2025-02-12 12:37:34 +0100

    aco/ssa_elimination: refactor scratch_sgpr handling
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33527>

--- src/amd/compiler/aco_ssa_elimination.cpp
+++ src/amd/compiler/aco_ssa_elimination.cpp
@@ -11,16 +11,17 @@
 namespace aco {
 namespace {
 
-struct phi_info_item {
-   Definition def;
-   Operand op;
+struct phi_info {
+   std::vector<std::pair<Definition, Operand>> copies;
+   PhysReg scratch_sgpr;
+   bool needs_scratch_reg = false;
 };
 
 struct ssa_elimination_ctx {
    /* The outer vectors should be indexed by block index. The inner vectors store phi information
     * for each block. */
-   std::vector<std::vector<phi_info_item>> logical_phi_info;
-   std::vector<std::vector<phi_info_item>> linear_phi_info;
+   std::vector<phi_info> logical_phi_info;
+   std::vector<phi_info> linear_phi_info;
    Program* program;
 
    ssa_elimination_ctx(Program* program_)
@@ -48,9 +49,13 @@ collect_phi_info(ssa_elimination_ctx& ctx)
             Block::edge_vec& preds =
                phi->opcode == aco_opcode::p_phi ? block.logical_preds : block.linear_preds;
             uint32_t pred_idx = preds[i];
-            auto& info_vec = phi->opcode == aco_opcode::p_phi ? ctx.logical_phi_info[pred_idx]
-                                                              : ctx.linear_phi_info[pred_idx];
-            info_vec.push_back({phi->definitions[0], phi->operands[i]});
+            auto& info = phi->opcode == aco_opcode::p_phi ? ctx.logical_phi_info[pred_idx]
+                                                          : ctx.linear_phi_info[pred_idx];
+            info.copies.emplace_back(phi->definitions[0], phi->operands[i]);
+            if (phi->pseudo().needs_scratch_reg) {
+               info.needs_scratch_reg = true;
+               info.scratch_sgpr = phi->pseudo().scratch_sgpr;
+            }
          }
       }
    }
@@ -62,16 +67,17 @@ insert_parallelcopies(ssa_elimination_ctx& ctx)
    /* insert the parallelcopies from logical phis before branch */
    for (unsigned block_idx = 0; block_idx < ctx.program->blocks.size(); ++block_idx) {
       auto& logical_phi_info = ctx.logical_phi_info[block_idx];
-      if (logical_phi_info.empty())
+      if (logical_phi_info.copies.empty())
          continue;
 
       Block& block = ctx.program->blocks[block_idx];
       aco_ptr<Instruction> pc{create_instruction(aco_opcode::p_parallelcopy, Format::PSEUDO,
-                                                 logical_phi_info.size(), logical_phi_info.size())};
+                                                 logical_phi_info.copies.size(),
+                                                 logical_phi_info.copies.size())};
       unsigned i = 0;
-      for (auto& phi_info : logical_phi_info) {
-         pc->definitions[i] = phi_info.def;
-         pc->operands[i] = phi_info.op;
+      for (auto& pair : logical_phi_info.copies) {
+         pc->definitions[i] = pair.first;
+         pc->operands[i] = pair.second;
          i++;
       }
       pc->pseudo().needs_scratch_reg = false;
@@ -82,21 +88,21 @@ insert_parallelcopies(ssa_elimination_ctx& ctx)
    /* insert parallelcopies for the linear phis at the end of blocks just before the branch */
    for (unsigned block_idx = 0; block_idx < ctx.program->blocks.size(); ++block_idx) {
       auto& linear_phi_info = ctx.linear_phi_info[block_idx];
-      if (linear_phi_info.empty())
+      if (linear_phi_info.copies.empty())
          continue;
 
       Block& block = ctx.program->blocks[block_idx];
-      Block& succ = ctx.program->blocks[block.linear_succs[0]];
       aco_ptr<Instruction> pc{create_instruction(aco_opcode::p_parallelcopy, Format::PSEUDO,
-                                                 linear_phi_info.size(), linear_phi_info.size())};
+                                                 linear_phi_info.copies.size(),
+                                                 linear_phi_info.copies.size())};
       unsigned i = 0;
-      for (auto& phi_info : linear_phi_info) {
-         pc->definitions[i] = phi_info.def;
-         pc->operands[i] = phi_info.op;
+      for (auto& pair : linear_phi_info.copies) {
+         pc->definitions[i] = pair.first;
+         pc->operands[i] = pair.second;
          i++;
       }
-      pc->pseudo().scratch_sgpr = succ.instructions[0]->pseudo().scratch_sgpr;
-      pc->pseudo().needs_scratch_reg = succ.instructions[0]->pseudo().needs_scratch_reg;
+      pc->pseudo().scratch_sgpr = linear_phi_info.scratch_sgpr;
+      pc->pseudo().needs_scratch_reg = linear_phi_info.needs_scratch_reg;
       auto it = std::prev(block.instructions.end());
       block.instructions.insert(it, std::move(pc));
    }
