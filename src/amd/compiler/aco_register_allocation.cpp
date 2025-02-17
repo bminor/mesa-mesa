642b1f0dcebd7764792cdac8b088a6f0faf7afd8
Natalie Vock <natalie.vock@gmx.de>
2025-02-17 18:42:47 +0100

    aco/ra: Add utility to clear PhysRegInterval
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37381>

--- src/amd/compiler/aco_register_allocation.cpp
+++ src/amd/compiler/aco_register_allocation.cpp
@@ -356,6 +356,10 @@ public:
          fill(start, rc.size(), 0xFFFFFFFF);
    }
 
+   void block(PhysRegInterval interval) { fill(interval.lo(), interval.size, 0xFFFFFFFF); }
+
+   void clear(PhysRegInterval interval) { fill(interval.lo(), interval.size, 0); }
+
    bool is_blocked(PhysReg start) const
    {
       if (regs[start] == 0xFFFFFFFF)
