alexvorxx/Mesa-VirGL

commit 976f22a5da30c503b05e2873e4d265fb0d4ecb07

nvk: Implement CmdProcess/ExecuteGeneratedCommandsEXT
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31394>

src/nouveau/vulkan/nvk_cmd_buffer.h
@@ -198,7 +198,7 @@ struct nvk_cmd_push {
 struct nvk_cmd_buffer {
    struct vk_command_buffer vk;
 
-   struct {
+   struct nvk_cmd_state {
       uint64_t descriptor_buffers[NVK_MAX_SETS];
       struct nvk_graphics_state gfx;
       struct nvk_compute_state cs;
@@ -311,7 +311,22 @@ nvk_get_descriptors_state(struct nvk_cmd_buffer *cmd,
    default:
       unreachable("Unhandled bind point");
    }
-};
+}
+
+static inline struct nvk_descriptor_state *
+nvk_get_descriptor_state_for_stages(struct nvk_cmd_buffer *cmd,
+                                    VkShaderStageFlags stages)
+{
+   if (stages & VK_SHADER_STAGE_COMPUTE_BIT) {
+      assert(stages == VK_SHADER_STAGE_COMPUTE_BIT);
+      return &cmd->state.cs.descriptors;
+   } else if (stages & NVK_SHADER_STAGE_GRAPHICS_BITS) {
+      assert(!(stages & ~NVK_SHADER_STAGE_GRAPHICS_BITS));
+      return &cmd->state.gfx.descriptors;
+   } else {
+      unreachable("Unknown shader stage");
+   }
+}
 
 VkResult nvk_cmd_buffer_upload_alloc(struct nvk_cmd_buffer *cmd,
                                      uint32_t size, uint32_t alignment,

src/nouveau/vulkan/meson.build
@@ -13,6 +13,7 @@ nvk_files = files(
   'nvk_cmd_copy.c',
   'nvk_cmd_dispatch.c',
   'nvk_cmd_draw.c',
+  'nvk_cmd_indirect.c',
   'nvk_cmd_meta.c',
   'nvk_cmd_pool.c',
   'nvk_cmd_pool.h',

bminor/mesa-mesa

commit 39ae06e153250fe4871aecaafaa2583de24af268

nvk: Pull shaders from the state command buffer in nvk_cmd_process_cmds()
Found by the VKD3D test suite.

Fixes: 976f22a5da30 ("nvk: Implement CmdProcess/ExecuteGeneratedCommandsEXT")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33610>

src/nouveau/vulkan/nvk_cmd_buffer.h
@@ -365,6 +365,7 @@ nvk_cmd_buffer_get_cbuf_descriptor_addr(struct nvk_cmd_buffer *cmd,
                                         const struct nvk_cbuf *cbuf);
 
 VkResult nvk_cmd_flush_cs_qmd(struct nvk_cmd_buffer *cmd,
+                              const struct nvk_cmd_state *state,
                               uint32_t global_size[3],
                               uint64_t *qmd_addr_out,
                               uint64_t *root_desc_addr_out);

src/nouveau/vulkan/nvk_cmd_dispatch.c
@@ -202,13 +202,14 @@ nvk_cmd_upload_qmd(struct nvk_cmd_buffer *cmd,
 
 VkResult
 nvk_cmd_flush_cs_qmd(struct nvk_cmd_buffer *cmd,
+                     const struct nvk_cmd_state *state,
                      uint32_t global_size[3],
                      uint64_t *qmd_addr_out,
                      uint64_t *root_desc_addr_out)
 {
-   struct nvk_descriptor_state *desc = &cmd->state.cs.descriptors;
+   const struct nvk_descriptor_state *desc = &state->cs.descriptors;
 
-   return nvk_cmd_upload_qmd(cmd, cmd->state.cs.shader,
+   return nvk_cmd_upload_qmd(cmd, state->cs.shader,
                              desc, (void *)desc->root, global_size,
                              qmd_addr_out, root_desc_addr_out);
 }
@@ -257,7 +258,8 @@ nvk_CmdDispatchBase(VkCommandBuffer commandBuffer,
    nvk_flush_compute_state(cmd, base_workgroup, global_size);
 
    uint64_t qmd_addr = 0;
-   VkResult result = nvk_cmd_flush_cs_qmd(cmd, global_size, &qmd_addr, NULL);
+   VkResult result = nvk_cmd_flush_cs_qmd(cmd, &cmd->state, global_size,
+                                          &qmd_addr, NULL);
    if (result != VK_SUCCESS) {
       vk_command_buffer_set_error(&cmd->vk, result);
       return;
@@ -496,8 +498,8 @@ nvk_CmdDispatchIndirect(VkCommandBuffer commandBuffer,
    nvk_flush_compute_state(cmd, base_workgroup, global_size);
 
    uint64_t qmd_addr = 0, root_desc_addr = 0;
-   VkResult result = nvk_cmd_flush_cs_qmd(cmd, global_size, &qmd_addr,
-                                          &root_desc_addr);
+   VkResult result = nvk_cmd_flush_cs_qmd(cmd, &cmd->state, global_size,
+                                          &qmd_addr, &root_desc_addr);
    if (result != VK_SUCCESS) {
       vk_command_buffer_set_error(&cmd->vk, result);
       return;

src/nouveau/vulkan/nvk_cmd_indirect.c
@@ -947,8 +947,8 @@ nvk_cmd_process_cmds(struct nvk_cmd_buffer *cmd,
    uint64_t qmd_addr = 0;
    if (layout->stages & VK_SHADER_STAGE_COMPUTE_BIT) {
       uint32_t global_size[3] = { 0, 0, 0 };
-      VkResult result = nvk_cmd_flush_cs_qmd(cmd, global_size, &qmd_addr,
-                                             &push.root_addr);
+      VkResult result = nvk_cmd_flush_cs_qmd(cmd, state, global_size,
+                                             &qmd_addr, &push.root_addr);
       if (unlikely(result != VK_SUCCESS)) {
          vk_command_buffer_set_error(&cmd->vk, result);
          return;