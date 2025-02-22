5a0702f351d31e19f71f7dc4b240c74a45feccf2
Georg Lehmann <dadschoorse@gmail.com>
2025-02-22 13:04:51 +0100

    nir/builder: add nir_shader_phi_pass
    Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
    Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33672>

--- src/compiler/nir/nir_builder.h
+++ src/compiler/nir/nir_builder.h
@@ -81,6 +81,8 @@ typedef bool (*nir_alu_pass_cb)(struct nir_builder *,
                                 nir_alu_instr *, void *);
 typedef bool (*nir_tex_pass_cb)(struct nir_builder *,
                                 nir_tex_instr *, void *);
+typedef bool (*nir_phi_pass_cb)(struct nir_builder *,
+                                nir_phi_instr *, void *);
 
 /**
  * Iterates over all the instructions in a NIR function and calls the given pass
@@ -254,6 +256,36 @@ nir_shader_tex_pass(nir_shader *shader, nir_tex_pass_cb pass,
                nir_tex_instr *tex = nir_instr_as_tex(instr);
                func_progress |= pass(&b, tex, cb_data);
             }
+         }
+      }
+
+      if (func_progress) {
+         nir_metadata_preserve(impl, preserved);
+         progress = true;
+      } else {
+         nir_metadata_preserve(impl, nir_metadata_all);
+      }
+   }
+
+   return progress;
+}
+
+/* As above, but for phis */
+static inline bool
+nir_shader_phi_pass(nir_shader *shader,
+                    nir_phi_pass_cb pass,
+                    nir_metadata preserved,
+                    void *cb_data)
+{
+   bool progress = false;
+
+   nir_foreach_function_impl(impl, shader) {
+      bool func_progress = false;
+      nir_builder b = nir_builder_create(impl);
+
+      nir_foreach_block_safe(block, impl) {
+         nir_foreach_phi_safe(phi, block) {
+            func_progress |= pass(&b, phi, cb_data);
          }
       }
 
