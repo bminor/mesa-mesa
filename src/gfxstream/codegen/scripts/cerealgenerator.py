14bc2e2d395a77f2caeb75701985799b7397dcd2
Jason Macnak <natsu@google.com>
2025-02-24 14:16:41 -0800

    gfxstream: Remove duplicated boxed handle func declarations
    ... and fix up include paths.
    
    Test: cvd create --gpu_mode=gfxstream_guest_angle_host_swiftshader
    
    Reviewed-by: Aaron Ruby <aruby@qnx.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33740>

--- src/gfxstream/codegen/scripts/cerealgenerator.py
+++ src/gfxstream/codegen/scripts/cerealgenerator.py
@@ -381,6 +381,11 @@ class IOStream;
 #undef VK_ANDROID_external_memory_android_hardware_buffer
 """ % VULKAN_STREAM_TYPE_GUEST
 
+        reservedMarshalingHostIncludes = """
+#include "VulkanBoxedHandles.h"
+
+"""
+
         reservedmarshalImplIncludeGuest = """
 #include "Resources.h"
 """
@@ -496,6 +501,7 @@ using DlSymFunc = void* (void*, const char*);
         decoderSnapshotImplIncludes = f"""
 #include <mutex>
 
+#include "VulkanBoxedHandles.h"
 #include "VulkanHandleMapping.h"
 #include "VkDecoderGlobalState.h"
 #include "VkReconstruction.h"
@@ -535,6 +541,7 @@ class BumpPool;
 #include "VkDecoderGlobalState.h"
 #include "VkDecoderSnapshot.h"
 
+#include "VulkanBoxedHandles.h"
 #include "VulkanDispatch.h"
 #include "%s.h"
 
@@ -622,7 +629,7 @@ class BumpPool;
                            extraImpl=commonCerealImplIncludes)
             self.addCppModule("common", "goldfish_vk_reserved_marshaling",
                            extraHeader=vulkanStreamIncludeHost,
-                           extraImpl=commonCerealImplIncludes)
+                           extraImpl=commonCerealImplIncludes + reservedMarshalingHostIncludes)
             self.addCppModule("common", "goldfish_vk_deepcopy",
                            extraHeader=poolInclude,
                            extraImpl=commonCerealImplIncludes + deepcopyInclude)
