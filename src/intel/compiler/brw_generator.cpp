KiritakeKumi/mesa-pvr-zink-24

brw: extend the NOP+WHILE workaround
It turns out that we need to add a NOP not only in between two
consecutive WHILE instructions, but also after every control flow
instruction that immediately precedes a WHILE.

v2: Rebase after the renames.

Fixes: 5ca883505e29 ("brw: add a NOP in between WHILE instructions on LNL")
Signed-off-by: Paulo Zanoni <paulo.r.zanoni@intel.com>

src/intel/compiler/brw_generator.cpp
@@ -772,7 +772,7 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
 
    struct disasm_info *disasm_info = disasm_initialize(p->isa, cfg);
 
-   enum opcode prev_opcode = BRW_OPCODE_ILLEGAL;
+   brw_inst *prev_inst = NULL;
    foreach_block_and_inst (block, brw_inst, inst, cfg) {
       if (inst->opcode == SHADER_OPCODE_UNDEF)
          continue;
@@ -1090,17 +1090,19 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
          brw_CONT(p);
 	 break;
 
-      case BRW_OPCODE_WHILE:
-         /* On LNL and newer, if we don't put a NOP in between two consecutive
-          * WHILE instructions we may end up with misrendering or GPU hangs.
-          * See HSD 22020521218.
+      case BRW_OPCODE_WHILE: {
+         /* Workaround for an issue with branch prediction for WHILE
+          * instructions that may lead to misrendering or GPU hangs.
+          * See HSDs 22020521218 and 16026360541.
           */
-         if (devinfo->ver >= 20 && unlikely(prev_opcode == BRW_OPCODE_WHILE))
+         if (devinfo->ver >= 20 && prev_inst &&
+             unlikely(prev_inst->is_control_flow()))
             brw_NOP(p);
 
          brw_WHILE(p);
          loop_count++;
          break;
+      }
 
       case SHADER_OPCODE_RCP:
       case SHADER_OPCODE_RSQ:
@@ -1359,7 +1361,7 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
       case SHADER_OPCODE_LOAD_PAYLOAD:
          unreachable("Should be lowered by lower_load_payload()");
       }
-      prev_opcode = inst->opcode;
+      prev_inst = inst;
 
       if (multiple_instructions_emitted)
          continue;

bminor/mesa-mesa

Reviewed-by: Francisco Jerez <currojerez@riseup.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33021>
(cherry picked from commit fd10764cff590b2743dee8544c78d4229fa6f6d6)

src/intel/compiler/brw_generator.cpp
@@ -772,7 +772,7 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
 
    struct disasm_info *disasm_info = disasm_initialize(p->isa, cfg);
 
-   enum opcode prev_opcode = BRW_OPCODE_ILLEGAL;
+   fs_inst *prev_inst = NULL;
    foreach_block_and_inst (block, fs_inst, inst, cfg) {
       if (inst->opcode == SHADER_OPCODE_UNDEF)
          continue;
@@ -1091,11 +1091,12 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
 	 break;
 
       case BRW_OPCODE_WHILE:
-         /* On LNL and newer, if we don't put a NOP in between two consecutive
-          * WHILE instructions we may end up with misrendering or GPU hangs.
-          * See HSD 22020521218.
+         /* Workaround for an issue with branch prediction for WHILE
+          * instructions that may lead to misrendering or GPU hangs.
+          * See HSDs 22020521218 and 16026360541.
           */
-         if (devinfo->ver >= 20 && unlikely(prev_opcode == BRW_OPCODE_WHILE))
+         if (devinfo->ver >= 20 && prev_inst &&
+             unlikely(prev_inst->is_control_flow()))
             brw_NOP(p);
 
          brw_WHILE(p);
@@ -1376,7 +1377,7 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
       case SHADER_OPCODE_LOAD_PAYLOAD:
          unreachable("Should be lowered by lower_load_payload()");
       }
-      prev_opcode = inst->opcode;
+      prev_inst = inst;
 
       if (multiple_instructions_emitted)
          continue;