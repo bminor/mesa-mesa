bminor/mesa-mesa

commit 55c476efed01121b3a64a58c304aae8ef9a79475

panvk: advertise shaderFloat16
Signed-off-by: Benjamin Lee <benjamin.lee@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33637>

src/panfrost/vulkan/panvk_physical_device.c
@@ -316,7 +316,7 @@ get_features(const struct panvk_physical_device *device,
       .storagePushConstant8 = false,
       .shaderBufferInt64Atomics = false,
       .shaderSharedInt64Atomics = false,
-      .shaderFloat16 = false,
+      .shaderFloat16 = true,
       .shaderInt8 = true,
 
       .descriptorIndexing = false,

commit bcff33ff93b5e5104addafe2e7d58c2ab7141aa7

panvk: disable shaderFloat16 on bifrost
We're failing a bunch of tests on G52 otherwise. Let's just disable this
feature on older gens for now, until we've gotten this fixed.

Fixes: 55c476efed0 ("panvk: advertise shaderFloat16")
Reviewed-by: Benjamin Lee <benjamin.lee@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33881>

src/panfrost/vulkan/panvk_physical_device.c
@@ -319,7 +319,7 @@ get_features(const struct panvk_physical_device *device,
       .storagePushConstant8 = false,
       .shaderBufferInt64Atomics = false,
       .shaderSharedInt64Atomics = false,
-      .shaderFloat16 = true,
+      .shaderFloat16 = arch >= 10,
       .shaderInt8 = true,
 
       .descriptorIndexing = false,