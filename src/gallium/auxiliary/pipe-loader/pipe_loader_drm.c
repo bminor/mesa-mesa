27d2bd59253cc545d460221d04293a913ee75cbb
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2025-03-07 09:29:28 -0500

    gallium: wire up asahi driver
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Acked-by: Eric Engestrom <eric@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33984>

--- src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
+++ src/gallium/auxiliary/pipe-loader/pipe_loader_drm.c
@@ -87,6 +87,7 @@ static const struct drm_driver_descriptor *driver_descriptors[] = {
    &vc4_driver_descriptor,
    &panfrost_driver_descriptor,
    &panthor_driver_descriptor,
+   &asahi_driver_descriptor,
    &etnaviv_driver_descriptor,
    &tegra_driver_descriptor,
    &lima_driver_descriptor,
@@ -330,6 +331,9 @@ pipe_loader_get_compatible_render_capable_device_fds(int kms_only_fd, unsigned i
    bool is_platform_device;
    struct pipe_loader_device *dev;
    const char * const drivers[] = {
+#if defined GALLIUM_ASAHI
+      "asahi",
+#endif
 #if defined GALLIUM_ETNAVIV
       "etnaviv",
 #endif
