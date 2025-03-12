06369caa40262bae9dad1cbfad7365e27e6be5e8
Yiwei Zhang <zzyiwei@chromium.org>
2025-03-11 21:28:24 -0700

    pan/kmod: set DRM_RDWR for exported dma-bufs
    This allows the exported fds to be mapped for writing. This is needed
    for virtgpu native ctx support where the fds are mapped rw when the
    mappings are added to the guest by kvm. This aligns with other mesa
    drivers, and unblocks the extended testing with venus on top.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34017>

--- src/panfrost/lib/kmod/pan_kmod.h
+++ src/panfrost/lib/kmod/pan_kmod.h
@@ -565,7 +565,8 @@ pan_kmod_bo_export(struct pan_kmod_bo *bo)
 {
    int fd;
 
-   if (drmPrimeHandleToFD(bo->dev->fd, bo->handle, DRM_CLOEXEC, &fd)) {
+   if (drmPrimeHandleToFD(bo->dev->fd, bo->handle, DRM_CLOEXEC | DRM_RDWR,
+                          &fd)) {
       mesa_loge("drmPrimeHandleToFD() failed (err=%d)", errno);
       return -1;
    }
