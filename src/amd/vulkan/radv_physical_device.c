bminor/mesa-mesa

commit c942d957b00429aedc65226e29ff2dfe441fc801

radv: fail to initialize when the AMD GPU generation is unsupported
Better to be conservative than allowing something that isn't supposed
to be working.

Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33031>

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
@@ -23,9 +23,22 @@
 #include "xf86drm.h"
 
 static bool
+radv_is_gpu_supported(const struct radeon_info *info)
+{
+   /* Unknown GPU generations aren't supported. */
+   if (info->gfx_level > GFX12)
+      return false;
+
+   return true;
+}
+
+static bool
 do_winsys_init(struct radv_amdgpu_winsys *ws, int fd)
 {
    if (!ac_query_gpu_info(fd, ws->dev, &ws->info, true))
+      return false;
+
+   if (!radv_is_gpu_supported(&ws->info))
       return false;
 
    /*

commit 61feea6954a7526836ccbd30c657e6afc11fb4f5

radv: Move support check out of winsys.
To get the right error code. Mostly shouldn't be winsys dependent
anyway, outside of the idea that if we explicitly emulate a device
we should just assume th euser knows what they're doing.

Fixes: c942d957b00 ("radv: fail to initialize when the AMD GPU generation is unsupported")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12792
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33964>

src/amd/vulkan/radv_physical_device.c
@@ -1953,6 +1953,20 @@ radv_get_physical_device_properties(struct radv_physical_device *pdev)
    p->shaderBinaryVersion = 1;
 }
 
+static bool
+radv_is_gpu_supported(const struct radeon_info *info)
+{
+   /* AMD CDNA isn't supported. */
+   if (info->gfx_level == GFX9 && !info->has_graphics)
+      return false;
+
+   /* Unknown GPU generations aren't supported. */
+   if (info->gfx_level > GFX12)
+      return false;
+
+   return true;
+}
+
 static VkResult
 radv_physical_device_try_create(struct radv_instance *instance, drmDevicePtr drm_device,
                                 struct radv_physical_device **pdev_out)
@@ -2000,7 +2014,7 @@ radv_physical_device_try_create(struct radv_instance *instance, drmDevicePtr drm
       drmFreeVersion(version);
 
       if (instance->debug_flags & RADV_DEBUG_STARTUP)
-         fprintf(stderr, "radv: info: Found compatible device '%s'.\n", path);
+         fprintf(stderr, "radv: info: Found device '%s'.\n", path);
    }
 #endif
 
@@ -2062,6 +2076,14 @@ radv_physical_device_try_create(struct radv_instance *instance, drmDevicePtr drm
    pdev->local_fd = fd;
    pdev->ws->query_info(pdev->ws, &pdev->info);
    pdev->info.family_overridden = drm_device == NULL;
+
+   /* Allow all devices on a virtual winsys, otherwise do a basic support check. */
+   if (!radv_is_gpu_supported(&pdev->info) && drm_device) {
+      if (instance->debug_flags & RADV_DEBUG_STARTUP)
+         fprintf(stderr, "radv: info: device '%s' is not supported by RADV.\n", pdev->info.name);
+      result = VK_ERROR_INCOMPATIBLE_DRIVER;
+      goto fail_wsi;
+   }
 
    if (drm_device) {
       pdev->addrlib = ac_addrlib_create(&pdev->info, &pdev->info.max_alignment);

src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.c
@@ -23,26 +23,9 @@
 #include "xf86drm.h"
 
 static bool
-radv_is_gpu_supported(const struct radeon_info *info)
-{
-   /* AMD CDNA isn't supported. */
-   if (info->gfx_level == GFX9 && !info->has_graphics)
-      return false;
-
-   /* Unknown GPU generations aren't supported. */
-   if (info->gfx_level > GFX12)
-      return false;
-
-   return true;
-}
-
-static bool
 do_winsys_init(struct radv_amdgpu_winsys *ws, int fd)
 {
    if (!ac_query_gpu_info(fd, ws->dev, &ws->info, true))
-      return false;
-
-   if (!radv_is_gpu_supported(&ws->info))
       return false;
 
    /*