d383d78e8c0544e6ebf9b23f57969bc6f5288fe7
Faith Ekstrand <faith.ekstrand@collabora.com>
2025-03-17 14:48:57 -0500

    nvk: Maxwell+ is now conformant
    This advertises Vulkan 1.4 conformance for Maxwell+ GPUs as well as
    enabling Vulkan on them by default.
    
    Backport-to: 25.1
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34582>

--- src/nouveau/vulkan/nvk_physical_device.c
+++ src/nouveau/vulkan/nvk_physical_device.c
@@ -1321,7 +1321,7 @@ nvk_create_drm_physical_device(struct vk_instance *_instance,
 
    bool conformant =
       nvkmd->dev_info.type == NV_DEVICE_TYPE_DIS &&
-      nvkmd->dev_info.cls_eng3d >= TURING_A &&
+      nvkmd->dev_info.cls_eng3d >= MAXWELL_A &&
       nvkmd->dev_info.cls_eng3d <= ADA_A;
 
    if (!conformant &&
