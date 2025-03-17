f80a69d7569845462872f062b053ea1e582b29d0
Agate, Jesse <Jesse.Agate@amd.com>
2025-03-17 16:54:26 -0400

    amd/vpelib: Add macro to perform ceil division
    Add macro to perform ceil division
    
    Reviewed-by: Brendan Steve Leder <BrendanSteve.Leder@amd.com>
    Reviewed-by: Navid Assadian <Navid.Assadian@amd.com>
    Acked-by: ChuanYu Tseng <ChuanYu.Tseng@amd.com>
    Signed-off-by: Agate, Jesse <Jesse.Agate@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35012>

--- src/amd/vpelib/src/core/inc/common.h
+++ src/amd/vpelib/src/core/inc/common.h
@@ -53,6 +53,10 @@ extern "C" {
 #define max(x, y) (((x) > (y)) ? (x) : (y))
 #endif
 
+#ifndef int_divide_with_ceil
+#define int_divide_with_ceil(x, y) ((x + (y-1))/y)
+#endif
+
 bool vpe_find_color_space_from_table(
     const struct vpe_color_space *table, int table_size, const struct vpe_color_space *cs);
 
