1f4e3d56de6f599b4c324ccc71acb4d87927bbb5
Patrick Lerda <patrick9876@free.fr>
2025-03-21 14:28:15 +0100

    r600: add r600_nir_lower_txl_txf_array_or_cube metadata compatibility
    ../src/compiler/nir/nir_metadata.c:172: nir_metadata_check_validation_flag: Assertion `!(impl->valid_metadata & nir_metadata_not_properly_reset)' failed.
    
    Signed-off-by: Patrick Lerda <patrick9876@free.fr>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34155>

--- src/gallium/drivers/r600/sfn/sfn_nir_lower_tex.cpp
+++ src/gallium/drivers/r600/sfn/sfn_nir_lower_tex.cpp
@@ -197,7 +197,7 @@ r600_nir_lower_txl_txf_array_or_cube_impl(nir_function_impl *impl)
          }
       }
    }
-   return progress;
+   return nir_progress(progress, impl, nir_metadata_control_flow);
 }
 
 bool
