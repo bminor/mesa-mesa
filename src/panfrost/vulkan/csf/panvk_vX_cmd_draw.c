168cf64d706bff89d016e2b027155e8200172c1c
Mary Guillemard <mary.guillemard@collabora.com>
2025-03-21 10:10:03 +0100

    panvk: Use spd variant instead of pos_points when checking for vs shader presence
    This result in the same thing and will avoid us needing specific
    per-arch checks for v12+.
    
    Signed-off-by: Mary Guillemard <mary.guillemard@collabora.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34032>

--- src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
+++ src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -1812,7 +1812,7 @@ panvk_cmd_draw(struct panvk_cmd_buffer *cmdbuf, struct panvk_draw_info *draw)
    VkResult result;
 
    /* If there's no vertex shader, we can skip the draw. */
-   if (!panvk_priv_mem_dev_addr(vs->spds.pos_points))
+   if (!panvk_priv_mem_dev_addr(vs->spd))
       return;
 
    /* Needs to be done before get_fs() is called because it depends on
@@ -1978,7 +1978,7 @@ panvk_cmd_draw_indirect(struct panvk_cmd_buffer *cmdbuf,
    VkResult result;
 
    /* If there's no vertex shader, we can skip the draw. */
-   if (!panvk_priv_mem_dev_addr(vs->spds.pos_points))
+   if (!panvk_priv_mem_dev_addr(vs->spd))
       return;
 
    /* Needs to be done before get_fs() is called because it depends on
