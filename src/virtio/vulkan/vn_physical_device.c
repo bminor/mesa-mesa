bminor/mesa-mesa

commit fde5cebec53aa9a0ceaafef2ec9ba85fd897f40c

venus: fix image format cache miss with AHB usage query
should skip updating cache key instead of marking as a miss

Fixes: e48645250c6 ("venus: image format properties cache")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33757>

src/virtio/vulkan/vn_physical_device.c
@@ -2246,6 +2246,9 @@ vn_image_get_image_format_key(
             _mesa_sha1_update(&sha1_ctx, &src->sType,
                               sizeof(VkStructureType));
             break;
+         case VK_STRUCTURE_TYPE_ANDROID_HARDWARE_BUFFER_USAGE_ANDROID:
+            /* no need to update cache key since handled outside the cache */
+            break;
          default:
             physical_dev->image_format_cache.debug.cache_skip_count++;
             return false;

commit ea6dc035d893b973c5dc7dd097e26e1d5ddf13e7

venus: fix ahb usage caching
Test: dEQP-VK.api.external.memory.android_hardware_buffer.*

Fixes: fde5cebec53 ("venus: fix image format cache miss with AHB usage query")
Acked-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34135>

src/virtio/vulkan/vn_physical_device.c
@@ -2489,6 +2489,9 @@ vn_image_init_format_from_cache(
                   cache_entry->properties.filter_cubic.filterCubicMinmax;
                break;
             }
+            case VK_STRUCTURE_TYPE_ANDROID_HARDWARE_BUFFER_USAGE_ANDROID:
+               /* no-op here since handled outside the cache */
+               break;
             default:
                unreachable("unexpected format props pNext");
             }
@@ -2573,6 +2576,9 @@ vn_image_store_format_in_cache(
                *((VkFilterCubicImageViewImageFormatPropertiesEXT *)src);
             break;
          }
+         case VK_STRUCTURE_TYPE_ANDROID_HARDWARE_BUFFER_USAGE_ANDROID:
+            /* no-op here since handled outside the cache */
+            break;
          default:
             unreachable("unexpected format props pNext");
          }