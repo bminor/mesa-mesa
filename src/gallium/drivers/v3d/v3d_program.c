bminor/mesa-mesa

commit 332b313547f454034a10a92bd03f71a1cc46625c

v3d: enable framebuffer fetch
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33766>

src/gallium/drivers/v3d/v3d_program.c
@@ -690,11 +690,13 @@ v3d_update_compiled_fs(struct v3d_context *v3d, uint8_t prim_mode)
                  */
                 key->cbufs |= 1 << i;
 
-                /* If logic operations are enabled then we might emit color
-                 * reads and we need to know the color buffer format and
-                 * swizzle for that.
+                /* When emitting color reads (in the case of logic ops and
+                 * load_output) we need to know the color buffer format and
+                 * swizzle.
                  */
-                if (key->logicop_func != PIPE_LOGICOP_COPY) {
+                if (key->logicop_func != PIPE_LOGICOP_COPY ||
+                    s->info.fs.uses_fbfetch_output) {
+
                         key->color_fmt[i].format = cbuf->format;
                         memcpy(key->color_fmt[i].swizzle,
                                v3d_get_format_swizzle(&v3d->screen->devinfo,

src/broadcom/compiler/v3d_compiler.h
@@ -439,7 +439,7 @@ struct v3d_fs_key {
         uint8_t uint_color_rb;
 
         /* Color format information per render target. Only set when logic
-         * operations are enabled.
+         * operations are enabled or when fbfetch is in use.
          */
         struct {
                 enum pipe_format format;

src/gallium/drivers/v3d/v3d_screen.c
@@ -338,6 +338,8 @@ v3d_init_screen_caps(struct v3d_screen *screen)
         caps->max_texture_array_layers = V3D_MAX_ARRAY_LAYERS;
 
         caps->max_render_targets = V3D_MAX_RENDER_TARGETS(screen->devinfo.ver);
+        caps->fbfetch = caps->max_render_targets;
+        caps->fbfetch_coherent = true;
 
         caps->vendor_id = 0x14E4;
 

src/gallium/drivers/v3d/v3dx_draw.c
@@ -560,6 +560,9 @@ emit_shader_state_record(struct v3d_context *v3d,
                 shader.fragment_shader_does_z_writes =
                         v3d->prog.fs->prog_data.fs->writes_z;
 
+                shader.enable_sample_rate_shading =
+                        job->msaa && v3d->prog.fs->prog_data.fs->force_per_sample_msaa;
+
                 /* Set if the EZ test must be disabled (due to shader side
                  * effects and the early_z flag not being present in the
                  * shader).

commit 53484dc64afb46a2a99a0410a3bf9be77dd47c29

v3d: Fix fbfetch with discards.
We can't do earlyz_with_discard when using fbfetch as we can have TLB reads
that occur before the discard. This can result in implicit Z writes which make
the setmsf instruction emitted as a result of the discard invalid.

Fixes: 332b313547f ("v3d: enable framebuffer fetch")

Reviewed-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34151>

src/gallium/drivers/v3d/v3d_program.c
@@ -675,6 +675,7 @@ v3d_update_compiled_fs(struct v3d_context *v3d, uint8_t prim_mode)
 
         key->swap_color_rb = v3d->swap_color_rb;
         key->can_earlyz_with_discard = s->info.fs.uses_discard &&
+                !s->info.fs.uses_fbfetch_output &&
                 (!v3d->zsa || !job->zsbuf || !v3d->zsa->base.depth_enabled ||
                  !v3d->zsa->base.depth_writemask) &&
                 !(v3d->active_queries && v3d->current_oq);