bminor/mesa-mesa

commit 104f9c6654583c67251fb442c49b3cf86b416efb

radeonsi/vce: Remove support for FW 50 and older
Reviewed-by: Leo Liu <leo.liu@amd.com>
Acked-by: Leo Liu <leo.liu@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32829>

src/gallium/drivers/radeonsi/radeon_vce.c
@@ -17,11 +17,6 @@
 
 #include <stdio.h>
 
-#define FW_40_2_2  ((40 << 24) | (2 << 16) | (2 << 8))
-#define FW_50_0_1  ((50 << 24) | (0 << 16) | (1 << 8))
-#define FW_50_1_2  ((50 << 24) | (1 << 16) | (2 << 8))
-#define FW_50_10_2 ((50 << 24) | (10 << 16) | (2 << 8))
-#define FW_50_17_3 ((50 << 24) | (17 << 16) | (3 << 8))
 #define FW_52_0_3  ((52 << 24) | (0 << 16) | (3 << 8))
 #define FW_52_4_3  ((52 << 24) | (4 << 16) | (3 << 8))
 #define FW_52_8_3  ((52 << 24) | (8 << 16) | (3 << 8))
@@ -460,30 +455,7 @@ struct pipe_video_codec *si_vce_create_encoder(struct pipe_context *context,
       goto error;
    }
 
-   switch (sscreen->info.vce_fw_version) {
-   case FW_40_2_2:
-      si_vce_40_2_2_init(enc);
-      break;
-
-   case FW_50_0_1:
-   case FW_50_1_2:
-   case FW_50_10_2:
-   case FW_50_17_3:
-      si_vce_50_init(enc);
-      break;
-
-   case FW_52_0_3:
-   case FW_52_4_3:
-   case FW_52_8_3:
-      si_vce_52_init(enc);
-      break;
-
-   default:
-      if ((sscreen->info.vce_fw_version & (0xff << 24)) >= FW_53) {
-         si_vce_52_init(enc);
-      } else
-         goto error;
-   }
+   si_vce_52_init(enc);
 
    return &enc->base;
 
@@ -500,11 +472,6 @@ error:
 bool si_vce_is_fw_version_supported(struct si_screen *sscreen)
 {
    switch (sscreen->info.vce_fw_version) {
-   case FW_40_2_2:
-   case FW_50_0_1:
-   case FW_50_1_2:
-   case FW_50_10_2:
-   case FW_50_17_3:
    case FW_52_0_3:
    case FW_52_4_3:
    case FW_52_8_3:

src/gallium/drivers/radeonsi/radeon_vce.h
@@ -415,22 +415,7 @@ bool si_vce_is_fw_version_supported(struct si_screen *sscreen);
 void si_vce_add_buffer(struct rvce_encoder *enc, struct pb_buffer_lean *buf, unsigned usage,
                        enum radeon_bo_domain domain, signed offset);
 
-/* init vce fw 40.2.2 specific callbacks */
-void si_vce_40_2_2_init(struct rvce_encoder *enc);
-
-/* init vce fw 50 specific callbacks */
-void si_vce_50_init(struct rvce_encoder *enc);
-
 /* init vce fw 52 specific callbacks */
 void si_vce_52_init(struct rvce_encoder *enc);
-
-/* get parameters for vce 40.2.2 */
-void si_vce_40_2_2_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic);
-
-/* get parameters for vce 50 */
-void si_vce_50_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic);
-
-/* get parameters for vce 52 */
-void si_vce_52_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic);
 
 #endif

src/gallium/drivers/radeonsi/radeon_vce_52.c
@@ -145,7 +145,7 @@ static void get_vui_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture
    enc->enc_pic.vui.max_dec_frame_buffering = 0x00000003;
 }
 
-void si_vce_52_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic)
+static void get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic)
 {
    get_rate_control_param(enc, pic);
    get_motion_estimation_param(enc, pic);
@@ -657,5 +657,5 @@ void si_vce_52_init(struct rvce_encoder *enc)
    enc->config = config;
    enc->encode = encode;
    enc->destroy = destroy;
-   enc->si_get_pic_param = si_vce_52_get_param;
+   enc->si_get_pic_param = get_param;
 }

src/gallium/drivers/radeonsi/meson.build
@@ -84,8 +84,6 @@ files_libradeonsi = files(
   'radeon_uvd_enc_1_1.c',
   'radeon_vce.c',
   'radeon_vce.h',
-  'radeon_vce_40_2_2.c',
-  'radeon_vce_50.c',
   'radeon_vce_52.c',
   'radeon_vcn.h',
   'radeon_vcn.c',

src/gallium/drivers/radeonsi/radeon_vce_50.c
@@ -1,224 +1,0 @@
-/**************************************************************************
- *
- * Copyright 2013 Advanced Micro Devices, Inc.
- *
- * SPDX-License-Identifier: MIT
- *
- **************************************************************************/
-
-#include "pipe/p_video_codec.h"
-#include "radeon_vce.h"
-#include "radeon_video.h"
-#include "si_pipe.h"
-#include "util/u_memory.h"
-#include "util/u_video.h"
-#include "vl/vl_video_buffer.h"
-
-#include <stdio.h>
-
-static void rate_control(struct rvce_encoder *enc)
-{
-   uint32_t target_bits_picture =
-      enc->pic.rate_ctrl[0].target_bitrate *
-      ((float)enc->pic.rate_ctrl[0].frame_rate_den /
-      enc->pic.rate_ctrl[0].frame_rate_num);
-   uint32_t peak_bits_picture_integer =
-      enc->pic.rate_ctrl[0].peak_bitrate *
-      ((float)enc->pic.rate_ctrl[0].frame_rate_den /
-      enc->pic.rate_ctrl[0].frame_rate_num);
-   uint32_t peak_bits_picture_fraction =
-      (((enc->pic.rate_ctrl[0].peak_bitrate *
-      (uint64_t)enc->pic.rate_ctrl[0].frame_rate_den) %
-      enc->pic.rate_ctrl[0].frame_rate_num) << 32) /
-      enc->pic.rate_ctrl[0].frame_rate_num;
-
-   RVCE_BEGIN(0x04000005);                                 // rate control
-   RVCE_CS(enc->pic.rate_ctrl[0].rate_ctrl_method);           // encRateControlMethod
-   RVCE_CS(enc->pic.rate_ctrl[0].target_bitrate);             // encRateControlTargetBitRate
-   RVCE_CS(enc->pic.rate_ctrl[0].peak_bitrate);               // encRateControlPeakBitRate
-   RVCE_CS(enc->pic.rate_ctrl[0].frame_rate_num);             // encRateControlFrameRateNum
-   RVCE_CS(0x00000000);                                    // encGOPSize
-   RVCE_CS(enc->pic.quant_i_frames);                       // encQP_I
-   RVCE_CS(enc->pic.quant_p_frames);                       // encQP_P
-   RVCE_CS(enc->pic.quant_b_frames);                       // encQP_B
-   RVCE_CS(enc->pic.rate_ctrl[0].vbv_buffer_size);            // encVBVBufferSize
-   RVCE_CS(enc->pic.rate_ctrl[0].frame_rate_den);             // encRateControlFrameRateDen
-   RVCE_CS(0x00000000);                                    // encVBVBufferLevel
-   RVCE_CS(0x00000000);                                    // encMaxAUSize
-   RVCE_CS(0x00000000);                                    // encQPInitialMode
-   RVCE_CS(target_bits_picture);                           // encTargetBitsPerPicture
-   RVCE_CS(peak_bits_picture_integer);                     // encPeakBitsPerPictureInteger
-   RVCE_CS(peak_bits_picture_fraction);                    // encPeakBitsPerPictureFractional
-   RVCE_CS(0x00000000);                                    // encMinQP
-   RVCE_CS(0x00000033);                                    // encMaxQP
-   RVCE_CS(0x00000000);                                    // encSkipFrameEnable
-   RVCE_CS(0x00000000);                                    // encFillerDataEnable
-   RVCE_CS(0x00000000);                                    // encEnforceHRD
-   RVCE_CS(0x00000000);                                    // encBPicsDeltaQP
-   RVCE_CS(0x00000000);                                    // encReferenceBPicsDeltaQP
-   RVCE_CS(0x00000000);                                    // encRateControlReInitDisable
-   RVCE_CS(0x00000000);                                    // encLCVBRInitQPFlag
-   RVCE_CS(0x00000000); // encLCVBRSATDBasedNonlinearBitBudgetFlag
-   RVCE_END();
-}
-
-static void encode(struct rvce_encoder *enc)
-{
-   signed luma_offset, chroma_offset, bs_offset;
-   unsigned bs_idx = enc->bs_idx++;
-   int i;
-
-   enc->task_info(enc, 0x00000003, 0, 0, bs_idx);
-
-   RVCE_BEGIN(0x05000001);                                      // context buffer
-   RVCE_READWRITE(enc->cpb.res->buf, enc->cpb.res->domains, 0); // encodeContextAddressHi/Lo
-   RVCE_END();
-
-   bs_offset = -(signed)(bs_idx * enc->bs_size);
-
-   RVCE_BEGIN(0x05000004);                                   // video bitstream buffer
-   RVCE_WRITE(enc->bs_handle, RADEON_DOMAIN_GTT, bs_offset); // videoBitstreamRingAddressHi/Lo
-   RVCE_CS(enc->bs_size);                                    // videoBitstreamRingSize
-   RVCE_END();
-
-   if (enc->dual_pipe) {
-      unsigned aux_offset =
-         enc->cpb.res->buf->size - RVCE_MAX_AUX_BUFFER_NUM * RVCE_MAX_BITSTREAM_OUTPUT_ROW_SIZE * 2;
-      RVCE_BEGIN(0x05000002); // auxiliary buffer
-      for (i = 0; i < 8; ++i) {
-         RVCE_CS(aux_offset);
-         aux_offset += RVCE_MAX_BITSTREAM_OUTPUT_ROW_SIZE;
-      }
-      for (i = 0; i < 8; ++i)
-         RVCE_CS(RVCE_MAX_BITSTREAM_OUTPUT_ROW_SIZE);
-      RVCE_END();
-   }
-
-   RVCE_BEGIN(0x03000001);                   // encode
-   RVCE_CS(enc->pic.frame_num ? 0x0 : 0x11); // insertHeaders
-   RVCE_CS(0x00000000);                      // pictureStructure
-   RVCE_CS(enc->bs_size);                    // allowedMaxBitstreamSize
-   RVCE_CS(0x00000000);                      // forceRefreshMap
-   RVCE_CS(0x00000000);                      // insertAUD
-   RVCE_CS(0x00000000);                      // endOfSequence
-   RVCE_CS(0x00000000);                      // endOfStream
-   RVCE_READ(enc->handle, RADEON_DOMAIN_VRAM,
-             (uint64_t)enc->luma->u.legacy.level[0].offset_256B * 256); // inputPictureLumaAddressHi/Lo
-   RVCE_READ(enc->handle, RADEON_DOMAIN_VRAM,
-             (uint64_t)enc->chroma->u.legacy.level[0].offset_256B * 256);              // inputPictureChromaAddressHi/Lo
-   RVCE_CS(align(enc->luma->u.legacy.level[0].nblk_y, 16));       // encInputFrameYPitch
-   RVCE_CS(enc->luma->u.legacy.level[0].nblk_x * enc->luma->bpe); // encInputPicLumaPitch
-   RVCE_CS(enc->chroma->u.legacy.level[0].nblk_x * enc->chroma->bpe); // encInputPicChromaPitch
-   if (enc->dual_pipe)
-      RVCE_CS(0x00000000); // encInputPic(Addr|Array)Mode,encDisable(TwoPipeMode|MBOffloading)
-   else
-      RVCE_CS(0x00010000); // encInputPic(Addr|Array)Mode,encDisable(TwoPipeMode|MBOffloading)
-   RVCE_CS(0x00000000);    // encInputPicTileConfig
-   RVCE_CS(enc->pic.picture_type);                                   // encPicType
-   RVCE_CS(enc->pic.picture_type == PIPE_H2645_ENC_PICTURE_TYPE_IDR);// encIdrFlag
-   RVCE_CS(0x00000000);                                              // encIdrPicId
-   RVCE_CS(0x00000000);                                              // encMGSKeyPic
-   RVCE_CS(!enc->pic.not_referenced);                                // encReferenceFlag
-   RVCE_CS(0x00000000);                                              // encTemporalLayerIndex
-   RVCE_CS(0x00000000); // num_ref_idx_active_override_flag
-   RVCE_CS(0x00000000); // num_ref_idx_l0_active_minus1
-   RVCE_CS(0x00000000); // num_ref_idx_l1_active_minus1
-
-   i = enc->pic.frame_num - enc->pic.ref_idx_l0_list[0];
-   if (i > 1 && enc->pic.picture_type == PIPE_H2645_ENC_PICTURE_TYPE_P) {
-      RVCE_CS(0x00000001); // encRefListModificationOp
-      RVCE_CS(i - 1);      // encRefListModificationNum
-   } else {
-      RVCE_CS(0x00000000); // encRefListModificationOp
-      RVCE_CS(0x00000000); // encRefListModificationNum
-   }
-
-   for (i = 0; i < 3; ++i) {
-      RVCE_CS(0x00000000); // encRefListModificationOp
-      RVCE_CS(0x00000000); // encRefListModificationNum
-   }
-   for (i = 0; i < 4; ++i) {
-      RVCE_CS(0x00000000); // encDecodedPictureMarkingOp
-      RVCE_CS(0x00000000); // encDecodedPictureMarkingNum
-      RVCE_CS(0x00000000); // encDecodedPictureMarkingIdx
-      RVCE_CS(0x00000000); // encDecodedRefBasePictureMarkingOp
-      RVCE_CS(0x00000000); // encDecodedRefBasePictureMarkingNum
-   }
-
-   // encReferencePictureL0[0]
-   RVCE_CS(0x00000000); // pictureStructure
-   if (enc->pic.picture_type == PIPE_H2645_ENC_PICTURE_TYPE_P ||
-       enc->pic.picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B) {
-      struct rvce_cpb_slot *l0 = si_l0_slot(enc);
-      si_vce_frame_offset(enc, l0, &luma_offset, &chroma_offset);
-      RVCE_CS(l0->picture_type);  // encPicType
-      RVCE_CS(l0->frame_num);     // frameNumber
-      RVCE_CS(l0->pic_order_cnt); // pictureOrderCount
-      RVCE_CS(luma_offset);       // lumaOffset
-      RVCE_CS(chroma_offset);     // chromaOffset
-   } else {
-      RVCE_CS(0x00000000); // encPicType
-      RVCE_CS(0x00000000); // frameNumber
-      RVCE_CS(0x00000000); // pictureOrderCount
-      RVCE_CS(0xffffffff); // lumaOffset
-      RVCE_CS(0xffffffff); // chromaOffset
-   }
-
-   // encReferencePictureL0[1]
-   RVCE_CS(0x00000000); // pictureStructure
-   RVCE_CS(0x00000000); // encPicType
-   RVCE_CS(0x00000000); // frameNumber
-   RVCE_CS(0x00000000); // pictureOrderCount
-   RVCE_CS(0xffffffff); // lumaOffset
-   RVCE_CS(0xffffffff); // chromaOffset
-
-   // encReferencePictureL1[0]
-   RVCE_CS(0x00000000); // pictureStructure
-   if (enc->pic.picture_type == PIPE_H2645_ENC_PICTURE_TYPE_B) {
-      struct rvce_cpb_slot *l1 = si_l1_slot(enc);
-      si_vce_frame_offset(enc, l1, &luma_offset, &chroma_offset);
-      RVCE_CS(l1->picture_type);  // encPicType
-      RVCE_CS(l1->frame_num);     // frameNumber
-      RVCE_CS(l1->pic_order_cnt); // pictureOrderCount
-      RVCE_CS(luma_offset);       // lumaOffset
-      RVCE_CS(chroma_offset);     // chromaOffset
-   } else {
-      RVCE_CS(0x00000000); // encPicType
-      RVCE_CS(0x00000000); // frameNumber
-      RVCE_CS(0x00000000); // pictureOrderCount
-      RVCE_CS(0xffffffff); // lumaOffset
-      RVCE_CS(0xffffffff); // chromaOffset
-   }
-
-   si_vce_frame_offset(enc, si_current_slot(enc), &luma_offset, &chroma_offset);
-   RVCE_CS(luma_offset);            // encReconstructedLumaOffset
-   RVCE_CS(chroma_offset);          // encReconstructedChromaOffset
-   RVCE_CS(0x00000000);             // encColocBufferOffset
-   RVCE_CS(0x00000000);             // encReconstructedRefBasePictureLumaOffset
-   RVCE_CS(0x00000000);             // encReconstructedRefBasePictureChromaOffset
-   RVCE_CS(0x00000000);             // encReferenceRefBasePictureLumaOffset
-   RVCE_CS(0x00000000);             // encReferenceRefBasePictureChromaOffset
-   RVCE_CS(0x00000000);             // pictureCount
-   RVCE_CS(enc->pic.frame_num);     // frameNumber
-   RVCE_CS(enc->pic.pic_order_cnt); // pictureOrderCount
-   RVCE_CS(0x00000000);             // numIPicRemainInRCGOP
-   RVCE_CS(0x00000000);             // numPPicRemainInRCGOP
-   RVCE_CS(0x00000000);             // numBPicRemainInRCGOP
-   RVCE_CS(0x00000000);             // numIRPicRemainInRCGOP
-   RVCE_CS(0x00000000);             // enableIntraRefresh
-   RVCE_END();
-}
-
-void si_vce_50_get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_desc *pic)
-{
-}
-
-void si_vce_50_init(struct rvce_encoder *enc)
-{
-   si_vce_40_2_2_init(enc);
-
-   /* only the two below are different */
-   enc->rate_control = rate_control;
-   enc->encode = encode;
-   enc->si_get_pic_param = si_vce_50_get_param;
-}

commit a2b4617c00ea5c7dd86b95d2294e02ab843c898b

radeonsi/vce: Support old VCE firmware
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12866
Fixes: 104f9c66545 ("radeonsi/vce: Remove support for FW 50 and older")
Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34152>

src/gallium/drivers/radeonsi/radeon_vce.c
@@ -419,6 +419,7 @@ struct pipe_video_codec *si_vce_create_encoder(struct pipe_context *context,
       goto error;
    }
 
+   enc->fw_version = (sscreen->info.vce_fw_version & (0xff << 24)) >> 24;
    si_vce_52_init(enc);
 
    return &enc->base;
@@ -435,17 +436,8 @@ error:
  */
 bool si_vce_is_fw_version_supported(struct si_screen *sscreen)
 {
-   switch (sscreen->info.vce_fw_version) {
-   case FW_52_0_3:
-   case FW_52_4_3:
-   case FW_52_8_3:
-      return true;
-   default:
-      if ((sscreen->info.vce_fw_version & (0xff << 24)) >= FW_53)
-         return true;
-      else
-         return false;
-   }
+   unsigned version = (sscreen->info.vce_fw_version & (0xff << 24)) >> 24;
+   return version >= 40;
 }
 
 /**

src/gallium/drivers/radeonsi/radeon_vce.h
@@ -343,6 +343,7 @@ struct rvce_encoder {
 
    bool use_vm;
    bool dual_pipe;
+   unsigned fw_version;
 };
 
 struct rvce_output_unit_segment {

src/gallium/drivers/radeonsi/radeon_vce_52.c
@@ -198,7 +198,7 @@ static void get_param(struct rvce_encoder *enc, struct pipe_h264_enc_picture_des
    enc->enc_pic.i_remain = pic->i_remain;
    enc->enc_pic.pic_order_cnt = pic->pic_order_cnt;
    enc->enc_pic.not_referenced = pic->not_referenced;
-   enc->enc_pic.addrmode_arraymode_disrdo_distwoinstants = 0x01000201;
+   enc->enc_pic.addrmode_arraymode_disrdo_distwoinstants = enc->fw_version >= 52 ? 0x01000201 : 0;
    enc->enc_pic.is_idr = (pic->picture_type == PIPE_H2645_ENC_PICTURE_TYPE_IDR);
    enc->enc_pic.eo.enc_idr_pic_id = pic->idr_pic_id;
    enc->enc_pic.ec.enc_vbaq_mode =
@@ -309,10 +309,12 @@ static void create(struct rvce_encoder *enc)
 
    RVCE_CS(enc->enc_pic.addrmode_arraymode_disrdo_distwoinstants);
 
-   RVCE_CS(enc->enc_pic.ec.enc_pre_encode_context_buffer_offset);
-   RVCE_CS(enc->enc_pic.ec.enc_pre_encode_input_luma_buffer_offset);
-   RVCE_CS(enc->enc_pic.ec.enc_pre_encode_input_chroma_buffer_offset);
-   RVCE_CS(enc->enc_pic.ec.enc_pre_encode_mode_chromaflag_vbaqmode_scenechangesensitivity);
+   if (enc->fw_version >= 52) {
+      RVCE_CS(enc->enc_pic.ec.enc_pre_encode_context_buffer_offset);
+      RVCE_CS(enc->enc_pic.ec.enc_pre_encode_input_luma_buffer_offset);
+      RVCE_CS(enc->enc_pic.ec.enc_pre_encode_input_chroma_buffer_offset);
+      RVCE_CS(enc->enc_pic.ec.enc_pre_encode_mode_chromaflag_vbaqmode_scenechangesensitivity);
+   }
    RVCE_END();
 }
 
@@ -373,7 +375,7 @@ static void encode(struct rvce_encoder *enc)
       enc->enc_pic.eo.enc_input_pic_swizzle_mode = enc->luma->u.gfx9.swizzle_mode;
    }
 
-   enc->enc_pic.eo.enc_disable_two_pipe_mode = !enc->dual_pipe;
+   enc->enc_pic.eo.enc_disable_two_pipe_mode = enc->fw_version >= 50 ? !enc->dual_pipe : 0;
    RVCE_CS(enc->enc_pic.eo.enc_input_pic_addr_array_disable2pipe_disablemboffload);
    RVCE_CS(enc->enc_pic.eo.enc_input_pic_tile_config);
    RVCE_CS(enc->enc_pic.picture_type);                                    // encPicType
@@ -457,17 +459,18 @@ static void encode(struct rvce_encoder *enc)
    RVCE_CS(enc->enc_pic.eo.num_ir_pic_remain_in_rcgop);
    RVCE_CS(enc->enc_pic.eo.enable_intra_refresh);
 
-   RVCE_CS(enc->enc_pic.eo.aq_variance_en);
-   RVCE_CS(enc->enc_pic.eo.aq_block_size);
-   RVCE_CS(enc->enc_pic.eo.aq_mb_variance_sel);
-   RVCE_CS(enc->enc_pic.eo.aq_frame_variance_sel);
-   RVCE_CS(enc->enc_pic.eo.aq_param_a);
-   RVCE_CS(enc->enc_pic.eo.aq_param_b);
-   RVCE_CS(enc->enc_pic.eo.aq_param_c);
-   RVCE_CS(enc->enc_pic.eo.aq_param_d);
-   RVCE_CS(enc->enc_pic.eo.aq_param_e);
-
-   RVCE_CS(enc->enc_pic.eo.context_in_sfb);
+   if (enc->fw_version >= 52) {
+      RVCE_CS(enc->enc_pic.eo.aq_variance_en);
+      RVCE_CS(enc->enc_pic.eo.aq_block_size);
+      RVCE_CS(enc->enc_pic.eo.aq_mb_variance_sel);
+      RVCE_CS(enc->enc_pic.eo.aq_frame_variance_sel);
+      RVCE_CS(enc->enc_pic.eo.aq_param_a);
+      RVCE_CS(enc->enc_pic.eo.aq_param_b);
+      RVCE_CS(enc->enc_pic.eo.aq_param_c);
+      RVCE_CS(enc->enc_pic.eo.aq_param_d);
+      RVCE_CS(enc->enc_pic.eo.aq_param_e);
+      RVCE_CS(enc->enc_pic.eo.context_in_sfb);
+   }
    RVCE_END();
 }
 
@@ -498,8 +501,10 @@ static void rate_control(struct rvce_encoder *enc)
    RVCE_CS(enc->enc_pic.rc.b_pics_delta_qp);
    RVCE_CS(enc->enc_pic.rc.ref_b_pics_delta_qp);
    RVCE_CS(enc->enc_pic.rc.rc_reinit_disable);
-   RVCE_CS(enc->enc_pic.rc.enc_lcvbr_init_qp_flag);
-   RVCE_CS(enc->enc_pic.rc.lcvbrsatd_based_nonlinear_bit_budget_flag);
+   if (enc->fw_version >= 50) {
+      RVCE_CS(enc->enc_pic.rc.enc_lcvbr_init_qp_flag);
+      RVCE_CS(enc->enc_pic.rc.lcvbrsatd_based_nonlinear_bit_budget_flag);
+   }
    RVCE_END();
 }
 