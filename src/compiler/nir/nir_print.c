a530890e75547833cfafe92d35ddc43c9fee38b5
Timur Kristóf <timur.kristof@gmail.com>
2025-04-01 14:21:03 +0200

    nir/print: Fix variable mode for arrayed output load intrinsics.
    This helps print the names of varyings correctly.
    
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34317>

--- src/compiler/nir/nir_print.c
+++ src/compiler/nir/nir_print.c
@@ -1383,6 +1383,8 @@ print_intrinsic_instr(nir_intrinsic_instr *instr, print_state *state)
             break;
 
          case nir_intrinsic_load_output:
+         case nir_intrinsic_load_per_vertex_output:
+         case nir_intrinsic_load_per_primitive_output:
          case nir_intrinsic_store_output:
          case nir_intrinsic_store_per_primitive_output:
          case nir_intrinsic_store_per_vertex_output:
