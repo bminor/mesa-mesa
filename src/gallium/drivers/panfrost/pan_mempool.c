bminor/mesa-mesa

commit 22985caf3ff3d5845d5c03c4516be12f6d004d5e

panfrost: sanity-check alignment
The page-alignment should always be a positive power of two. Let's
assert that, to avoid confusion.

Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
CID: 1605086
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32380>

src/gallium/drivers/panfrost/pan_mempool.c
@@ -137,7 +137,9 @@ panfrost_pool_alloc_aligned(struct panfrost_pool *pool, size_t sz,
 #ifdef PAN_DBG_OVERFLOW
    if (unlikely(pool->dev->debug & PAN_DBG_OVERFLOW) &&
        !(pool->create_flags & PAN_BO_INVISIBLE)) {
-      unsigned aligned = ALIGN_POT(sz, sysconf(_SC_PAGESIZE));
+      long alignment = sysconf(_SC_PAGESIZE);
+      assert(alignment > 0 && util_is_power_of_two_nonzero(alignment));
+      unsigned aligned = ALIGN_POT(sz, alignment);
       unsigned bo_size = aligned + PAN_GUARD_SIZE;
 
       bo = panfrost_pool_alloc_backing(pool, bo_size);

commit 1471279203a2675bca066229a46fe6ab87567b68

panfrost: avoid accidental aliasing
We already have a variable call "alignment" here, and aliasing it
breaks things. Whoops, let's rename the variable to page_size to
avoid this.

Fixes: 22985caf3ff ("panfrost: sanity-check alignment")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34156>

src/gallium/drivers/panfrost/pan_mempool.c
@@ -142,9 +142,9 @@ panfrost_pool_alloc_aligned(struct panfrost_pool *pool, size_t sz,
 #ifdef PAN_DBG_OVERFLOW
    if (unlikely(pool->dev->debug & PAN_DBG_OVERFLOW) &&
        !(pool->create_flags & PAN_BO_INVISIBLE)) {
-      long alignment = sysconf(_SC_PAGESIZE);
-      assert(alignment > 0 && util_is_power_of_two_nonzero(alignment));
-      unsigned aligned = ALIGN_POT(sz, alignment);
+      long page_size = sysconf(_SC_PAGESIZE);
+      assert(page_size > 0 && util_is_power_of_two_nonzero(page_size));
+      unsigned aligned = ALIGN_POT(sz, page_size);
       unsigned bo_size = aligned + PAN_GUARD_SIZE;
 
       bo = panfrost_pool_alloc_backing(pool, bo_size);