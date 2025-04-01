ee0ee282b9015ffdd88c9250fd5a25098b9b04c6
Daniel Schürmann <daniel@schuermann.dev>
2025-04-01 11:46:39 +0200

    aco: simplify Operand() constructor
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34359>

--- src/amd/compiler/aco_ir.h
+++ src/amd/compiler/aco_ir.h
@@ -460,12 +460,11 @@ static constexpr PhysReg scc{253};
  */
 class Operand final {
 public:
-   constexpr Operand()
-       : reg_(PhysReg{128}), isTemp_(false), isFixed_(true), isPrecolored_(false),
-         isConstant_(false), isKill_(false), isUndef_(true), isFirstKill_(false),
-         isLateKill_(false), isClobbered_(false), isCopyKill_(false), is16bit_(false),
-         is24bit_(false), signext(false), constSize(0)
-   {}
+   constexpr Operand() noexcept
+   {
+      isUndef_ = true;
+      setFixed(PhysReg{128});
+   }
 
    explicit Operand(Temp r) noexcept
    {
