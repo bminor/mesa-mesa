8742927d8f7bd65a529957230efd6d853e4fc4c9
Juan A. Suarez Romero <jasuarez@igalia.com>
2025-04-03 13:18:24 +0200

    v3dv: don't check if DRM device is master
    This was added to ensure we can get its resources, but they can be
    obtained also from non master.
    
    Fixes: 2af12c5b36e ("v3dv: Check multiple DRM primary nodes before picking the display fd")
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12641
    Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
    Reviewed-by: Maíra Canal <mcanal@igalia.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34366>

--- src/broadcom/vulkan/v3dv_device.c
+++ src/broadcom/vulkan/v3dv_device.c
@@ -1513,12 +1513,7 @@ try_display_device(struct v3dv_instance *instance, const char *path,
     * drivers for different types of connectors and the one with a connected
     * output may not be vc4, which unlike Raspberry Pi 4, doesn't drive the
     * DSI output for example.
-    *
-    * If the display device isn't the DRM master, we can't get its resources.
     */
-   if (!drmIsMaster(*fd))
-      goto fail;
-
    drmModeResPtr mode_res = drmModeGetResources(*fd);
    if (!mode_res) {
       mesa_loge("Failed to get DRM mode resources: %s\n", strerror(errno));
