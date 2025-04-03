bminor/mesa-mesa

commit b0a98d3b1324ae0c04b987ff60501de766e47b64

ir3: Detect empty fragment shaders
Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33735>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -5483,6 +5483,17 @@ fixup_tg4(struct ir3_context *ctx)
    }
 }
 
+static bool
+is_empty(struct ir3 *ir)
+{
+   foreach_block (block, &ir->block_list) {
+      foreach_instr (instr, &block->instr_list) {
+         return instr->opc == OPC_END;
+      }
+   }
+   return true;
+}
+
 static void
 collect_tex_prefetches(struct ir3_context *ctx, struct ir3 *ir)
 {
@@ -6028,6 +6039,10 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
 
    if (ctx->so->type == MESA_SHADER_VERTEX && ctx->compiler->gen >= 6) {
       so->constlen = MAX2(so->constlen, 8);
+   }
+
+   if (so->type == MESA_SHADER_FRAGMENT) {
+      so->empty = is_empty(ir) && so->num_sampler_prefetch == 0;
    }
 
    if (gl_shader_stage_is_compute(so->type)) {

src/freedreno/ir3/ir3_shader.h
@@ -853,6 +853,8 @@ struct ir3_shader_variant {
 
    bool post_depth_coverage;
 
+   bool empty;
+
    /* Are we using split or merged register file? */
    bool mergedregs;
 

commit f5019ee0d4b2a5bc2447ad44659eeb6565b80662

ir3: Fix shaders that write only color classified as empty
Shader may have zero instructions and no prefetches but have inputs
that without modifications are used as output.

Fixed vkd3d test:
 test_depth_bias_behaviour

Fixes: b0a98d3b1324ae0c04b987ff60501de766e47b64
("ir3: Detect empty fragment shaders")

Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34348>

src/freedreno/ir3/ir3_compiler_nir.c
@@ -6053,7 +6053,8 @@ ir3_compile_shader_nir(struct ir3_compiler *compiler,
    }
 
    if (so->type == MESA_SHADER_FRAGMENT) {
-      so->empty = is_empty(ir) && so->num_sampler_prefetch == 0;
+      so->empty = is_empty(ir) && so->outputs_count == 0 &&
+                  so->num_sampler_prefetch == 0;
       so->writes_only_color = !ctx->s->info.writes_memory && !so->has_kill &&
                               !so->writes_pos && !so->writes_smask &&
                               !so->writes_stencilref;