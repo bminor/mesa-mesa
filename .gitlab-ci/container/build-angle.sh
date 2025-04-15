b1af5780d1378ff6a4b2e0396b45d1665105d611
Collabora's Gfx CI Team <gfx-ci-bot-collabora@contact.daniels.if.required.fdo.invalid>
2025-04-15 00:09:22 +0000

    Uprev ANGLE to a3f2545f6bb3
    https://github.com/google/angle/compare/3818d37d5e94317f01810053b8f28c1f1e8b98e6...a3f2545f6bb3e8d27827dceb2b4e901673995ad1
    
    Also disable -Werror, because we're not necessarily using the same
    toolchain or dependencies as the upstream builds.
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34515>

--- .gitlab-ci/container/build-angle.sh
+++ .gitlab-ci/container/build-angle.sh
@@ -13,7 +13,7 @@ uncollapsed_section_start angle "Building ANGLE"
 # setting up the environment variables locally
 ci_tag_build_time_check "ANGLE_TAG"
 
-ANGLE_REV="3818d37d5e94317f01810053b8f28c1f1e8b98e6"
+ANGLE_REV="a3f2545f6bb3e8d27827dceb2b4e901673995ad1"
 
 # Set ANGLE_ARCH based on DEBIAN_ARCH if it hasn't been explicitly defined
 if [[ -z "${ANGLE_ARCH:-}" ]]; then
@@ -93,6 +93,7 @@ is_component_build=false
 is_debug=false
 target_cpu="${ANGLE_ARCH}"
 target_os="${ANGLE_TARGET}"
+treat_warnings_as_errors=false
 EOF
 
 case "$ANGLE_TARGET" in
