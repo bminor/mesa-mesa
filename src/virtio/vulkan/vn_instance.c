bminor/mesa-mesa

commit 25b8f4f7143f2e022df245ba2e1a2ab3ed4a2f90

venus: handle device probing properly.
Currently if you try to probe the virtio ICD on a non-virtio system
it will fail in CreateInstance which causes the loader to spit on the
screen.

However instance creation shouldn't fail, the driver should just
not enumerate any devices in this case. It's a bit tricky to ensure
this, but return instance and then handle instance destruction
and fail device enumeration.

Cc: mesa-stable
Reviewed-by: Ryan Neph
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32266>

src/virtio/vulkan/vn_instance.c
@@ -276,6 +276,7 @@ vn_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
       return vn_error(NULL, result);
    }
 
+   VkInstance instance_handle = vn_instance_to_handle(instance);
    /* ring_idx = 0 reserved for CPU timeline */
    instance->ring_idx_used_mask = 0x1;
 
@@ -294,6 +295,10 @@ vn_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
    }
 
    result = vn_instance_init_renderer(instance);
+   if (result == VK_ERROR_INITIALIZATION_FAILED) {
+      *pInstance = instance_handle;
+      return VK_SUCCESS;
+   }
    if (result != VK_SUCCESS)
       goto out_mtx_destroy;
 
@@ -333,7 +338,6 @@ vn_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
       local_create_info.pApplicationInfo = &local_app_info;
    }
 
-   VkInstance instance_handle = vn_instance_to_handle(instance);
    result = vn_call_vkCreateInstance(instance->ring.ring, pCreateInfo, NULL,
                                      &instance_handle);
    if (result != VK_SUCCESS)
@@ -407,16 +411,18 @@ vn_DestroyInstance(VkInstance _instance,
    mtx_destroy(&instance->physical_device.mutex);
    mtx_destroy(&instance->ring_idx_mutex);
 
-   vn_call_vkDestroyInstance(instance->ring.ring, _instance, NULL);
+   if (instance->renderer) {
+      vn_call_vkDestroyInstance(instance->ring.ring, _instance, NULL);
 
-   vn_instance_fini_ring(instance);
+      vn_instance_fini_ring(instance);
 
-   vn_renderer_shmem_pool_fini(instance->renderer,
-                               &instance->reply_shmem_pool);
+      vn_renderer_shmem_pool_fini(instance->renderer,
+                                  &instance->reply_shmem_pool);
 
-   vn_renderer_shmem_pool_fini(instance->renderer, &instance->cs_shmem_pool);
+      vn_renderer_shmem_pool_fini(instance->renderer, &instance->cs_shmem_pool);
 
-   vn_renderer_destroy(instance->renderer, alloc);
+      vn_renderer_destroy(instance->renderer, alloc);
+   }
 
    driDestroyOptionCache(&instance->dri_options);
    driDestroyOptionInfo(&instance->available_dri_options);

src/virtio/vulkan/vn_physical_device.c
@@ -1500,6 +1500,10 @@ enumerate_physical_devices(struct vn_instance *instance,
    struct vn_physical_device *physical_devs = NULL;
    VkResult result;
 
+   if (!instance->renderer) {
+       *out_count = 0;
+       return VK_SUCCESS;
+   }
    uint32_t count = 0;
    result = vn_call_vkEnumeratePhysicalDevices(
       ring, vn_instance_to_handle(instance), &count, NULL);

commit 2a4675ee9fa2742da9826d1b5fb064b6a0066713

venus: fix missing renderer destructions
With failed compatibility check, the created renderer must be destroyed
within vn_instance_init_renderer.

Cc: mesa-stable
Fixes: 25b8f4f7143 ("venus: handle device probing properly.")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34613>

src/virtio/vulkan/vn_instance.c
@@ -181,7 +181,7 @@ vn_instance_init_renderer(struct vn_instance *instance)
          vn_log(instance, "wire format version %d != %d",
                 renderer_info->wire_format_version, version);
       }
-      return VK_ERROR_INITIALIZATION_FAILED;
+      goto out_renderer_destroy;
    }
 
    version = vn_info_vk_xml_version();
@@ -197,7 +197,7 @@ vn_instance_init_renderer(struct vn_instance *instance)
                 VK_VERSION_MINOR(VN_MIN_RENDERER_VERSION),
                 VK_VERSION_PATCH(VN_MIN_RENDERER_VERSION));
       }
-      return VK_ERROR_INITIALIZATION_FAILED;
+      goto out_renderer_destroy;
    }
 
    uint32_t spec_version =
@@ -226,6 +226,12 @@ vn_instance_init_renderer(struct vn_instance *instance)
    }
 
    return VK_SUCCESS;
+
+out_renderer_destroy:
+   vn_renderer_destroy(instance->renderer, alloc);
+   /* needed by stub instance creation */
+   instance->renderer = NULL;
+   return VK_ERROR_INITIALIZATION_FAILED;
 }
 
 /* instance commands */
@@ -309,6 +315,7 @@ vn_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,
 
    result = vn_instance_init_renderer(instance);
    if (result == VK_ERROR_INITIALIZATION_FAILED) {
+      assert(!instance->renderer);
       *pInstance = instance_handle;
       return VK_SUCCESS;
    }