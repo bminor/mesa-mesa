de1ce0f75e98f6fcdc8b7a7b495f54a2060a5896
Karol Herbst <kherbst@redhat.com>
2025-04-20 13:52:33 +0200

    rusticl: allow packagers to enable freedreno by default
    The msm driver hasn't yet passed conformance, so it's not part of the
    auto set at this point in time. But this will allow distribution to
    enable it by default, if they find it suitable.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35618>

--- src/gallium/frontends/rusticl/mesa/pipe/device.rs
+++ src/gallium/frontends/rusticl/mesa/pipe/device.rs
@@ -64,6 +64,8 @@ fn get_enabled_devs() -> HashMap<String, u32> {
     let default_devs: &[&str] = &[
         #[cfg(any(rusticl_enable_asahi, rusticl_enable_auto))]
         "asahi",
+        #[cfg(rusticl_enable_freedreno)]
+        "freedreno",
         #[cfg(rusticl_enable_radeonsi)]
         "radeonsi",
     ];
