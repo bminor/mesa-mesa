6cf01adf6b51fcdb9397681e46b8fbd4e54cb03a
Danylo Piliaiev <dpiliaiev@igalia.com>
2025-04-21 11:23:51 +0200

    tu,freedreno: Don't fallback to LINEAR with DRM_FORMAT_MOD_QCOM_COMPRESSED
    DRM_FORMAT_MOD_QCOM_COMPRESSED forces the image to be UBWC regardless
    of what's better for perf, we should respect that.
    
    The regression is seen in GTK4 when it tries to create tiny swapchain
    images.
    
    Fixes: fc50fb35b0526a10da5e5fe72d4291c5175a9759
    ("tu,freedreno: Enable linear mipmap tail for UBWC images")
    
    Signed-off-by: Danylo Piliaiev <dpiliaiev@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34628>
    (cherry picked from commit 36f22cc951c3249ebe36ab722739317d49e17f84)

--- src/freedreno/fdl/fd_layout_test.c
+++ src/freedreno/fdl/fd_layout_test.c
@@ -34,7 +34,7 @@ fdl_test_layout(const struct testcase *testcase, const struct fd_dev_id *dev_id)
                   MAX2(testcase->layout.nr_samples, 1), testcase->layout.width0,
                   MAX2(testcase->layout.height0, 1),
                   MAX2(testcase->layout.depth0, 1), mip_levels,
-                  MAX2(testcase->array_size, 1), testcase->is_3d, false, NULL);
+                  MAX2(testcase->array_size, 1), testcase->is_3d, false, false, NULL);
    } else {
       assert(fd_dev_gen(dev_id) >= 5);
       fdl5_layout(&layout, testcase->format,
