alexvorxx/zink-xlib-termux

commit e1483d022b28218bd53aea1c4917739cf0398b3e

radv: clear unwritten color attachments for monolithic PS earlier
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28976>

src/amd/vulkan/radv_shader_info.c
@@ -914,6 +914,9 @@ gather_shader_info_fs(const struct radv_device *device, const nir_shader *nir,
    if (!info->has_epilog) {
       info->ps.mrt0_is_dual_src = gfx_state->ps.epilog.mrt0_is_dual_src;
       info->ps.spi_shader_col_format = gfx_state->ps.epilog.spi_shader_col_format;
+
+      /* Clear color attachments that aren't exported by the FS to match IO shader arguments. */
+      info->ps.spi_shader_col_format &= info->ps.colors_written;
    }
 
    const bool export_alpha_and_mrtz =

src/amd/vulkan/radv_pipeline_graphics.c
@@ -4113,15 +4113,7 @@ radv_graphics_pipeline_init(struct radv_graphics_pipeline *pipeline, struct radv
       /* Make sure to clear color attachments without exports because MRT holes are removed during
        * compilation for optimal performance.
        */
-      blend.spi_shader_col_format =
-         radv_compact_spi_shader_col_format(blend.spi_shader_col_format & ps->info.ps.colors_written);
-
-      /* In presence of MRT holes (ie. the FS exports MRT1 but not MRT0), the compiler will remap
-       * them, so that only MRT0 is exported and the driver will compact SPI_SHADER_COL_FORMAT to
-       * match what the FS actually exports. Though, to make sure the hw remapping works as
-       * expected, we should also clear color attachments without exports in CB_SHADER_MASK.
-       */
-      blend.cb_shader_mask &= ps->info.ps.colors_written;
+      blend.spi_shader_col_format = radv_compact_spi_shader_col_format(blend.spi_shader_col_format);
    }
 
    unsigned custom_blend_mode = extra ? extra->custom_blend_mode : 0;

bminor/mesa-mesa

commit 55ad0fd35c600538150f4c94ca2f03024cb0076b

radv: do not clear unwritten color attachments with dual-source blending
This is incorrect because the color format at slot 0 needs to be
replicated to the slot 1. But with dual-source blending the colors
written mask is only 0xf and this was clearing the color format at
slot 1.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13082
Fixes: e1483d022b2 ("radv: clear unwritten color attachments for monolithic PS earlier")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34773>

src/amd/vulkan/radv_shader_info.c
@@ -998,7 +998,8 @@ gather_shader_info_fs(const struct radv_device *device, const nir_shader *nir,
       info->ps.spi_shader_col_format = gfx_state->ps.epilog.spi_shader_col_format;
 
       /* Clear color attachments that aren't exported by the FS to match IO shader arguments. */
-      info->ps.spi_shader_col_format &= info->ps.colors_written;
+      if (!info->ps.mrt0_is_dual_src)
+         info->ps.spi_shader_col_format &= info->ps.colors_written;
 
       info->ps.cb_shader_mask = ac_get_cb_shader_mask(info->ps.spi_shader_col_format);
    }