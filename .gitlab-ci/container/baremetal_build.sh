bminor/mesa-mesa

commit 13db56320bbe2312b0f902bf677d9ab19e5893c0

ci/baremetal: Use container rootfs
Switch baremetal to use the new artifacts from the test-gl and test-vk
containers.

Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34451>

.gitlab-ci/container/baremetal_build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2154 # arch is assigned in previous scripts
 
 set -e
 set -o xtrace
@@ -6,11 +7,12 @@ set -o xtrace
 # Fetch the arm-built rootfs image and unpack it in our x86_64 container (saves
 # network transfer, disk usage, and runtime on test jobs)
 
-# shellcheck disable=SC2154 # arch is assigned in previous scripts
-if curl --fail -L -s "${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${ARTIFACTS_SUFFIX}/${arch}/done"; then
-  ARTIFACTS_URL="${ARTIFACTS_PREFIX}/${FDO_UPSTREAM_REPO}/${ARTIFACTS_SUFFIX}/${arch}"
+S3_PATH="https://${S3_HOST}/${S3_KERNEL_BUCKET}"
+
+if curl -L --retry 3 -f --retry-delay 10 -s --head "${S3_PATH}/${FDO_UPSTREAM_REPO}/${LAVA_DISTRIBUTION_TAG}"; then
+  ARTIFACTS_URL="${S3_PATH}/${FDO_UPSTREAM_REPO}/${LAVA_DISTRIBUTION_TAG}"
 else
-  ARTIFACTS_URL="${ARTIFACTS_PREFIX}/${CI_PROJECT_PATH}/${ARTIFACTS_SUFFIX}/${arch}"
+  ARTIFACTS_URL="${S3_PATH}/${CI_PROJECT_PATH}/${LAVA_DISTRIBUTION_TAG}"
 fi
 
 curl -L --retry 4 -f --retry-all-errors --retry-delay 60 \

.gitlab-ci/container/gitlab-ci.yml
@@ -696,19 +696,17 @@ kernel+rootfs_arm32:
     # Don't want the .container rules
     - .container+build-rules
   variables:
-    FDO_DISTRIBUTION_TAG: "${MESA_IMAGE_TAG}--${MESA_ROOTFS_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
-    ARTIFACTS_PREFIX: "https://${S3_HOST}/${S3_KERNEL_BUCKET}"
-    ARTIFACTS_SUFFIX: "${MESA_ROOTFS_TAG}--${KERNEL_TAG}--${MESA_ARTIFACTS_TAG}--${MESA_TEMPLATES_COMMIT}"
+    FDO_DISTRIBUTION_TAG: "${MESA_IMAGE_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
     MESA_ARTIFACTS_TAG: *debian-arm64_build
-    MESA_ROOTFS_TAG: *kernel-rootfs
 
 debian/baremetal_arm32_test-gl:
   extends:
     - .debian/baremetal_arm_test
   needs:
-    - kernel+rootfs_arm32
+    - debian/arm32_test-gl
   variables:
-    MESA_IMAGE_TAG: &baremetal-arm32_test-gl "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    MESA_IMAGE_TAG: &baremetal-arm32_test-gl "${DEBIAN_TEST_GL_TAG}--${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    LAVA_DISTRIBUTION_TAG: !reference [debian/arm32_test-gl, variables, LAVA_DISTRIBUTION_TAG]
 
 debian/baremetal_arm64_test-gl:
   extends:
@@ -716,7 +714,8 @@ debian/baremetal_arm64_test-gl:
   needs:
     - debian/arm64_test-gl
   variables:
-    MESA_IMAGE_TAG: &baremetal-arm64_test-gl "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    MESA_IMAGE_TAG: &baremetal-arm64_test-gl "${DEBIAN_TEST_GL_TAG}--${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    LAVA_DISTRIBUTION_TAG: !reference [debian/arm64_test-gl, variables, LAVA_DISTRIBUTION_TAG]
 
 debian/baremetal_arm64_test-vk:
   extends:
@@ -724,12 +723,11 @@ debian/baremetal_arm64_test-vk:
   needs:
     - debian/arm64_test-vk
   variables:
-    MESA_IMAGE_TAG: &baremetal-arm64_test-vk "${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    MESA_IMAGE_TAG: &baremetal-arm64_test-vk "${DEBIAN_TEST_VK_TAG}--${DEBIAN_BASE_TAG}--${PKG_REPO_REV}"
+    LAVA_DISTRIBUTION_TAG: !reference [debian/arm64_test-vk, variables, LAVA_DISTRIBUTION_TAG]
 
 .use-debian/baremetal_arm32_test-gl:
-  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${MESA_ROOTFS_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
-  extends:
-    - .use-debian/baremetal_arm_test
+  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
   variables:
     MESA_IMAGE_PATH: "debian/baremetal_arm32_test-gl"
     MESA_IMAGE_TAG: *baremetal-arm32_test-gl
@@ -737,9 +735,7 @@ debian/baremetal_arm64_test-vk:
     - debian/baremetal_arm_test
 
 .use-debian/baremetal_arm64_test-gl:
-  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${MESA_ROOTFS_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
-  extends:
-    - .use-debian/baremetal_arm_test
+  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
   variables:
     MESA_IMAGE_PATH: "debian/baremetal_arm64_test-gl"
     MESA_IMAGE_TAG: *baremetal-arm64_test-gl
@@ -747,9 +743,7 @@ debian/baremetal_arm64_test-vk:
     - debian/baremetal_arm_test
 
 .use-debian/baremetal_arm64_test-vk:
-  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${MESA_ROOTFS_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
-  extends:
-    - .use-debian/baremetal_arm_test
+  image: "$CI_REGISTRY_IMAGE/${MESA_IMAGE_PATH}:${MESA_IMAGE_TAG}--${KERNEL_TAG}--${MESA_TEMPLATES_COMMIT}"
   variables:
     MESA_IMAGE_PATH: "debian/baremetal_arm64_test-vk"
     MESA_IMAGE_TAG: *baremetal-arm64_test-vk

commit c093a09660bf528e83dfc8626d59dd0447540a96

ci/baremetal: Fix rootfs URL detection
The previous check used a directory path without the filename, causing
`curl` to return 404 and always fall back to the fork's URL. Add the
rootfs filename to ensure the check works as intended.

Fixes: 13db56320bb ("ci/baremetal: Use container rootfs")

Signed-off-by: Valentine Burley <valentine.burley@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34873>

.gitlab-ci/container/baremetal_build.sh
@@ -9,7 +9,7 @@ set -o xtrace
 
 S3_PATH="https://${S3_HOST}/${S3_KERNEL_BUCKET}"
 
-if curl -L --retry 3 -f --retry-delay 10 -s --head "${S3_PATH}/${FDO_UPSTREAM_REPO}/${LAVA_DISTRIBUTION_TAG}"; then
+if curl -L --retry 3 -f --retry-delay 10 -s --head "${S3_PATH}/${FDO_UPSTREAM_REPO}/${LAVA_DISTRIBUTION_TAG}/lava-rootfs.tar.zst"; then
   ARTIFACTS_URL="${S3_PATH}/${FDO_UPSTREAM_REPO}/${LAVA_DISTRIBUTION_TAG}"
 else
   ARTIFACTS_URL="${S3_PATH}/${CI_PROJECT_PATH}/${LAVA_DISTRIBUTION_TAG}"