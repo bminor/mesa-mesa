786bd1c6f20806583c0894643051283e9001cc9f
Bo Hu <bohu@google.com>
2025-05-15 11:50:28 -0700

    gfxstream: [vulkan snapshot]: update to support vkCmdCopyBuffer
    Reviewed-by: Marcin Radomski <dextero@google.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35323>

--- src/gfxstream/codegen/scripts/cereal/decodersnapshot.py
+++ src/gfxstream/codegen/scripts/cereal/decodersnapshot.py
@@ -291,6 +291,11 @@ def api_special_implementation_vkCmdCopyBufferToImage(api, cgen):
     cgen.stmt("apiCallInfo->depends.push_back( (uint64_t)(uintptr_t)unboxed_to_boxed_non_dispatchable_VkBuffer(srcBuffer))")
     cgen.stmt("apiCallInfo->depends.push_back( (uint64_t)(uintptr_t)unboxed_to_boxed_non_dispatchable_VkImage(dstImage))")
 
+def api_special_implementation_vkCmdCopyBuffer(api, cgen):
+    cgen.stmt("std::lock_guard<std::mutex> lock(mReconstructionMutex)")
+    cgen.stmt("apiCallInfo->depends.push_back( (uint64_t)(uintptr_t)unboxed_to_boxed_non_dispatchable_VkBuffer(srcBuffer))")
+    cgen.stmt("apiCallInfo->depends.push_back( (uint64_t)(uintptr_t)unboxed_to_boxed_non_dispatchable_VkBuffer(dstBuffer))")
+
 def api_special_implementation_vkCmdBindVertexBuffers(api, cgen):
     cgen.stmt("std::lock_guard<std::mutex> lock(mReconstructionMutex)")
     cgen.beginFor("uint32_t i = 0", "i < bindingCount", "++i")
@@ -349,6 +354,7 @@ apiSpecialImplementation = {
     "vkCmdBindVertexBuffers": api_special_implementation_vkCmdBindVertexBuffers,
     "vkCmdBindPipeline": api_special_implementation_vkCmdBindPipeline,
     "vkCmdCopyBufferToImage": api_special_implementation_vkCmdCopyBufferToImage,
+    "vkCmdCopyBuffer": api_special_implementation_vkCmdCopyBuffer,
     "vkCmdPipelineBarrier": api_special_implementation_vkCmdPipelineBarrier,
     "vkCmdBeginRenderPass": api_special_implementation_vkCmdBeginRenderPass,
     "vkCmdBeginRenderPass2": api_special_implementation_vkCmdBeginRenderPass,
