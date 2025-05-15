b1cb11337a237e91db975411128e9426d6abfe0e
Frank Binns <frank.binns@imgtec.com>
2025-05-15 11:19:24 +0100

    pvr: advertise KHR_spirv_1_4
    Signed-off-by: Frank Binns <frank.binns@imgtec.com>
    Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37512>

--- src/imagination/vulkan/pvr_device.c
+++ src/imagination/vulkan/pvr_device.c
@@ -198,6 +198,7 @@ static void pvr_physical_device_get_supported_extensions(
       .KHR_shader_expect_assume = false,
       .KHR_shader_float_controls = true,
       .KHR_shader_subgroup_extended_types = true,
+      .KHR_spirv_1_4 = true,
       .KHR_swapchain = PVR_USE_WSI_PLATFORM,
       .KHR_swapchain_mutable_format = PVR_USE_WSI_PLATFORM,
       .KHR_timeline_semaphore = true,
