joshuaswarren/mesa-1

panvk/v9+: Set up limited texture descs for storage use
Storage access to images using LEA_TEX[_IMM] has limitations on some
fields in the texture descriptors, making them incompatible with the
descriptors required for texture access, specifically in the case
non-zero levels.

This change sets up two sets of texture descriptors for image views of
storage images, then picks the correct one when writing the image view
descriptors.

Backport-to: 25.0
Backport-to: 25.1
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Tested-by: Heiko Stuebner <heiko@sntech.de>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34839>

src/panfrost/vulkan/panvk_vX_image_view.c
@@ -131,6 +131,15 @@ prepare_tex_descs(struct panvk_image_view *view)
       .size = tex_payload_size * (can_preload_other_aspect ? 2 : plane_count),
    };
 
+#if PAN_ARCH >= 9
+   uint32_t storage_payload_size = 0;
+   if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+      /* We'll need a second set of Texture Descriptors for storage use. */
+      storage_payload_size = tex_payload_size * plane_count;
+      alloc_info.size += storage_payload_size;
+   }
+#endif
+
    view->mem = panvk_pool_alloc_mem(&dev->mempools.rw, alloc_info);
    if (!panvk_priv_mem_host_addr(view->mem))
       return panvk_error(dev, VK_ERROR_OUT_OF_DEVICE_MEMORY);
@@ -139,6 +148,15 @@ prepare_tex_descs(struct panvk_image_view *view)
       .gpu = panvk_priv_mem_dev_addr(view->mem),
       .cpu = panvk_priv_mem_host_addr(view->mem),
    };
+
+#if PAN_ARCH >= 9
+   struct panfrost_ptr storage_ptr = ptr;
+   if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+      uint32_t storage_payload_offset = alloc_info.size - storage_payload_size;
+      storage_ptr.gpu += storage_payload_offset;
+      storage_ptr.cpu += storage_payload_offset;
+   }
+#endif
 
    if (plane_count > 1) {
       memset(pview.planes, 0, sizeof(pview.planes));
@@ -152,12 +170,25 @@ prepare_tex_descs(struct panvk_image_view *view)
          pview.format = vk_format_to_pipe_format(plane_format);
 
          GENX(panfrost_new_texture)(&pview, &view->descs.tex[plane], &ptr);
+#if PAN_ARCH >= 9
+         if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+            GENX(panfrost_new_storage_texture)(
+               &pview, &view->descs.storage_tex[plane], &storage_ptr);
+            storage_ptr.cpu += tex_payload_size;
+            storage_ptr.gpu += tex_payload_size;
+         }
+#endif
 
          ptr.cpu += tex_payload_size;
          ptr.gpu += tex_payload_size;
       }
    } else {
       GENX(panfrost_new_texture)(&pview, &view->descs.tex[0], &ptr);
+#if PAN_ARCH >= 9
+      if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT)
+         GENX(panfrost_new_storage_texture)(&pview, &view->descs.storage_tex[0],
+                                            &storage_ptr);
+#endif
    }
 
    if (!can_preload_other_aspect)

bminor/mesa-mesa

(cherry picked from commit 7451bc3bef0b983063cb996a25a06312a751ac9b)
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35025>

src/panfrost/vulkan/panvk_vX_image_view.c
@@ -113,6 +113,9 @@ prepare_tex_descs(struct panvk_image_view *view)
    if (pview.format == PIPE_FORMAT_Z32_FLOAT_S8X24_UINT)
       pview.format = PIPE_FORMAT_Z32_FLOAT;
 
+   uint32_t tex_payload_size =
+      GENX(panfrost_estimate_texture_payload_size)(&pview);
+
    struct panvk_pool_alloc_info alloc_info = {
 #if PAN_ARCH == 6
       .alignment = pan_alignment(SURFACE_WITH_STRIDE),
@@ -122,9 +125,15 @@ prepare_tex_descs(struct panvk_image_view *view)
       .alignment = pan_alignment(PLANE),
 #endif
 
-      .size = GENX(panfrost_estimate_texture_payload_size)(&pview) *
-              (can_preload_other_aspect ? 2 : 1),
+      .size = tex_payload_size * (can_preload_other_aspect ? 2 : 1),
    };
+
+#if PAN_ARCH >= 9
+   if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+      /* We'll need a second Texture Descriptor for storage use. */
+      alloc_info.size += tex_payload_size;
+   }
+#endif
 
    view->mem = panvk_pool_alloc_mem(&dev->mempools.rw, alloc_info);
    if (!panvk_priv_mem_host_addr(view->mem))
@@ -135,7 +144,21 @@ prepare_tex_descs(struct panvk_image_view *view)
       .cpu = panvk_priv_mem_host_addr(view->mem),
    };
 
+#if PAN_ARCH >= 9
+   struct panfrost_ptr storage_ptr = ptr;
+   if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+      uint32_t storage_payload_offset = alloc_info.size - tex_payload_size;
+      storage_ptr.gpu += storage_payload_offset;
+      storage_ptr.cpu += storage_payload_offset;
+   }
+#endif
+
    GENX(panfrost_new_texture)(&pview, &view->descs.tex, &ptr);
+#if PAN_ARCH >= 9
+   if (view->vk.usage & VK_IMAGE_USAGE_STORAGE_BIT)
+      GENX(panfrost_new_storage_texture)(&pview, &view->descs.storage_tex,
+                                         &storage_ptr);
+#endif
 
    if (!can_preload_other_aspect)
       return VK_SUCCESS;
@@ -158,8 +181,8 @@ prepare_tex_descs(struct panvk_image_view *view)
       assert(!"Invalid format");
    }
 
-   ptr.cpu += alloc_info.size / 2;
-   ptr.gpu += alloc_info.size / 2;
+   ptr.cpu += tex_payload_size;
+   ptr.gpu += tex_payload_size;
 
    GENX(panfrost_new_texture)(&pview, &view->descs.other_aspect_tex, &ptr);
    return VK_SUCCESS;