ValveSoftware/steamos_mesa

commit 8fb966688bc1053a48e8ee7f7394ce030bcfd345

st/mesa: Disable blending for integer formats.
Blending isn't valid for integer formats.  Rather than having drivers
worry about this, just disable blending in this case.  This hopefully
will increase hits in the CSO cache as well, by eliminating most of the
meaningless fields in this case.

Reviewed-by: Marek Olšák <marek.olsak@amd.com>

src/mesa/state_tracker/st_atom_blend.c
@@ -171,6 +171,7 @@ st_update_blend( struct st_context *st )
       /* blending enabled */
       for (i = 0, j = 0; i < num_state; i++) {
          if (!(ctx->Color.BlendEnabled & (1 << i)) ||
+             (ctx->DrawBuffer->_IntegerBuffers & (1 << i)) ||
              !blend->rt[i].colormask)
             continue;
 

bminor/mesa-mesa

commit d04d9da98c9f70ec7db59750558f10dad215b2f4

st/mesa: fix _IntegerBuffers bitfield use
Previously we were assuming that all color attachments were active.

Fixes: 8fb966688bc1 ("st/mesa: Disable blending for integer formats.")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13168
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35014>

src/mesa/main/blend.c
@@ -1136,7 +1136,7 @@ _mesa_update_clamp_fragment_color(struct gl_context *ctx,
     * - there is an integer colorbuffer
     */
    if (!drawFb || !drawFb->_HasSNormOrFloatColorBuffer ||
-       drawFb->_IntegerBuffers)
+       drawFb->_IntegerDrawBuffers)
       clamp = GL_FALSE;
    else
       clamp = _mesa_get_clamp_fragment_color(ctx, drawFb);

src/mesa/main/buffers.c
@@ -739,6 +739,28 @@ updated_drawbuffers(struct gl_context *ctx, struct gl_framebuffer *fb)
    }
 }
 
+static void
+update_drawbuffer_mask(struct gl_context *ctx, struct gl_framebuffer *fb,
+                       GLbitfield *buffers, GLbitfield *draw_buffers)
+{
+   *draw_buffers = 0;
+   for (unsigned i = 0; i < fb->_NumColorDrawBuffers; i++) {
+      gl_buffer_index buf = fb->_ColorDrawBufferIndexes[i];
+      if (buf < BUFFER_COLOR0)
+         continue;
+
+      if (*buffers & (1 << (buf - BUFFER_COLOR0)))
+         *draw_buffers |= (1 << i);
+   }
+}
+
+void
+_mesa_update_drawbuffer_masks(struct gl_context *ctx,
+                              struct gl_framebuffer *fb)
+{
+   update_drawbuffer_mask(ctx, fb, &ctx->DrawBuffer->_IntegerBuffers,
+                          &ctx->DrawBuffer->_IntegerDrawBuffers);
+}
 
 /**
  * Helper function to set the GL_DRAW_BUFFER state for the given context and
@@ -816,6 +838,8 @@ _mesa_drawbuffers(struct gl_context *ctx, struct gl_framebuffer *fb,
       }
       fb->_NumColorDrawBuffers = count;
    }
+
+   _mesa_update_drawbuffer_masks(ctx, fb);
 
    /* set remaining outputs to BUFFER_NONE */
    for (buf = fb->_NumColorDrawBuffers; buf < ctx->Const.MaxDrawBuffers; buf++) {

src/mesa/main/buffers.h
@@ -52,6 +52,10 @@ _mesa_readbuffer(struct gl_context *ctx, struct gl_framebuffer *fb,
 extern void
 _mesa_update_draw_buffers(struct gl_context *ctx);
 
+extern void
+_mesa_update_drawbuffer_masks(struct gl_context *ctx,
+                              struct gl_framebuffer *fb);
+
 extern GLenum
 _mesa_back_to_front_if_single_buffered(const struct gl_framebuffer *fb,
                                        GLenum buffer);

src/mesa/main/draw_validate.c
@@ -155,16 +155,8 @@ _mesa_update_valid_to_render_state(struct gl_context *ctx)
          /* If drawing to integer-valued color buffers, there must be an
           * active fragment shader (GL_EXT_texture_integer).
           */
-         if (ctx->DrawBuffer->_IntegerBuffers) {
-            for (int i = 0; i < ctx->DrawBuffer->_NumColorDrawBuffers; i++) {
-               gl_buffer_index buf = ctx->DrawBuffer->_ColorDrawBufferIndexes[i];
-               if (buf < BUFFER_COLOR0)
-                  continue;
-
-               if (ctx->DrawBuffer->_IntegerBuffers & (1 << (buf - BUFFER_COLOR0)))
-                  return;
-            }
-         }
+         if (ctx->DrawBuffer->_IntegerDrawBuffers)
+            return;
       }
    }
 

src/mesa/main/fbobject.c
@@ -1732,6 +1732,8 @@ _mesa_test_framebuffer_completeness(struct gl_context *ctx,
       return;
    }
 
+   _mesa_update_drawbuffer_masks(ctx, fb);
+
    /* Provisionally set status = COMPLETE ... */
    fb->_Status = GL_FRAMEBUFFER_COMPLETE_EXT;
 

src/mesa/main/framebuffer.c
@@ -1056,6 +1056,6 @@ _mesa_is_multisample_enabled(const struct gl_context *ctx)
 bool
 _mesa_is_alpha_test_enabled(const struct gl_context *ctx)
 {
-   bool buffer0_is_integer = ctx->DrawBuffer->_IntegerBuffers & 0x1;
+   bool buffer0_is_integer = ctx->DrawBuffer->_IntegerDrawBuffers & 0x1;
    return (ctx->Color.AlphaEnabled && !buffer0_is_integer);
 }

src/mesa/main/get.c
@@ -1266,7 +1266,7 @@ find_custom_value(struct gl_context *ctx, const struct value_desc *d, union valu
       break;
    /* GL_EXT_textrue_integer */
    case GL_RGBA_INTEGER_MODE_EXT:
-      v->value_int = (ctx->DrawBuffer->_IntegerBuffers != 0);
+      v->value_int = (ctx->DrawBuffer->_IntegerDrawBuffers != 0);
       break;
    /* GL_ATI_meminfo & GL_NVX_gpu_memory_info */
    case GL_VBO_FREE_MEMORY_ATI:

src/mesa/main/mtypes.h
@@ -2691,7 +2691,8 @@ struct gl_framebuffer
     */
    bool _HasAttachments;
 
-   GLbitfield _IntegerBuffers;  /**< Which color buffers are integer valued */
+   GLbitfield _IntegerBuffers;  /**< Which color buffer attachments are integer valued */
+   GLbitfield _IntegerDrawBuffers;  /**< Which color draw buffers are integer valued */
    GLbitfield _BlendForceAlphaToOne;  /**< Which color buffers need blend factor adjustment */
    GLbitfield _IsRGB;  /**< Which color buffers have an RGB base format? */
    GLbitfield _FP32Buffers; /**< Which color buffers are FP32 */

src/mesa/state_tracker/st_atom_blend.c
@@ -175,8 +175,8 @@ blend_per_rt(const struct st_context *st, unsigned num_cb)
       /* this can only happen if GL_ARB_draw_buffers_blend is enabled */
       return GL_TRUE;
    }
-   if (ctx->DrawBuffer->_IntegerBuffers &&
-       (ctx->DrawBuffer->_IntegerBuffers != cb_mask)) {
+   if (ctx->DrawBuffer->_IntegerDrawBuffers &&
+       (ctx->DrawBuffer->_IntegerDrawBuffers != cb_mask)) {
       /* If there is a mix of integer/non-integer buffers then blending
        * must be handled on a per buffer basis. */
       return GL_TRUE;
@@ -280,7 +280,7 @@ st_update_blend( struct st_context *st )
       /* blending enabled */
       for (i = 0, j = 0; i < num_state; i++) {
          if (!(ctx->Color.BlendEnabled & (1 << i)) ||
-             (ctx->DrawBuffer->_IntegerBuffers & (1 << i)) ||
+             (ctx->DrawBuffer->_IntegerDrawBuffers & (1 << i)) ||
              !blend->rt[i].colormask)
             continue;
 
@@ -340,7 +340,7 @@ st_update_blend( struct st_context *st )
       blend->dither = ctx->Color.DitherFlag;
 
    if (_mesa_is_multisample_enabled(ctx) &&
-       !(ctx->DrawBuffer->_IntegerBuffers & 0x1)) {
+       !(ctx->DrawBuffer->_IntegerDrawBuffers & 0x1)) {
       /* Unlike in gallium/d3d10 these operations are only performed
        * if both msaa is enabled and we have a multisample buffer.
        */

src/mesa/state_tracker/st_atom_depth.c
@@ -129,7 +129,7 @@ st_update_depth_stencil_alpha(struct st_context *st)
    }
 
    if (ctx->Color.AlphaEnabled && !st->lower_alpha_test &&
-       !(ctx->DrawBuffer->_IntegerBuffers & 0x1)) {
+       !(ctx->DrawBuffer->_IntegerDrawBuffers & 0x1)) {
       dsa->alpha_enabled = 1;
       dsa->alpha_func = func_to_gallium(ctx->Color.AlphaFunc);
       dsa->alpha_ref_value = ctx->Color.AlphaRefUnclamped;