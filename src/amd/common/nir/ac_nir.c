fe2c93a78893419bd90757c5ebaf17f33c05a976
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2025-05-28 15:41:20 +0200

    ac/nir: enable 64-bit lowering for bitfield_extract
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35187>

--- src/amd/common/nir/ac_nir.c
+++ src/amd/common/nir/ac_nir.c
@@ -87,7 +87,8 @@ void ac_nir_set_options(struct radeon_info *info, bool use_llvm,
    options->has_shfr32 = true;
    options->has_mul24_relaxed = true;
    options->lower_int64_options = nir_lower_imul64 | nir_lower_imul_high64 | nir_lower_imul_2x32_64 | nir_lower_divmod64 |
-                                  nir_lower_minmax64 | nir_lower_iabs64 | nir_lower_iadd_sat64 | nir_lower_conv64;
+                                  nir_lower_minmax64 | nir_lower_iabs64 | nir_lower_iadd_sat64 | nir_lower_conv64 |
+                                  nir_lower_bitfield_extract64;
    options->divergence_analysis_options = nir_divergence_view_index_uniform;
    options->optimize_quad_vote_to_reduce = !use_llvm;
    options->lower_fisnormal = true;
