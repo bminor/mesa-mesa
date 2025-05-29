aerisarn/mesa-uwp

commit 17a62ff9934c9c3f8721e05ea1485b7e1e078a9f

panfrost: legalize afbc before blitting
If we legalize AFBC late, we end up in a situation while we might need
to do a blit while inside a previous blit operation, but u_blitter
state isn't saved recursively, and that leads to crashes.

This patch solves this issue by splitting panfrost_blit into two
functions and legalizing AFBC early.

Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24942>

src/gallium/drivers/panfrost/pan_resource.c
@@ -864,7 +864,7 @@ pan_blit_from_staging(struct pipe_context *pctx,
    blit.mask = util_format_get_mask(blit.src.format);
    blit.filter = PIPE_TEX_FILTER_NEAREST;
 
-   panfrost_blit(pctx, &blit);
+   panfrost_blit_no_afbc_legalization(pctx, &blit);
 }
 
 static void
@@ -884,7 +884,7 @@ pan_blit_to_staging(struct pipe_context *pctx, struct panfrost_transfer *trans)
    blit.mask = util_format_get_mask(blit.dst.format);
    blit.filter = PIPE_TEX_FILTER_NEAREST;
 
-   panfrost_blit(pctx, &blit);
+   panfrost_blit_no_afbc_legalization(pctx, &blit);
 }
 
 static void
@@ -1342,7 +1342,7 @@ pan_resource_modifier_convert(struct panfrost_context *ctx,
                   &blit.dst.box);
          blit.src.box = blit.dst.box;
 
-         panfrost_blit(&ctx->base, &blit);
+         panfrost_blit_no_afbc_legalization(&ctx->base, &blit);
       }
    }
 

src/gallium/drivers/panfrost/pan_resource.h
@@ -197,4 +197,7 @@ void pan_legalize_afbc_format(struct panfrost_context *ctx,
 void pan_dump_resource(struct panfrost_context *ctx,
                        struct panfrost_resource *rsc);
 
+void panfrost_blit_no_afbc_legalization(struct pipe_context *pipe,
+                                        const struct pipe_blit_info *info);
+
 #endif /* PAN_RESOURCE_H */

src/gallium/drivers/panfrost/pan_blit.c
@@ -29,6 +29,7 @@
 
 #include "util/format/u_format.h"
 #include "pan_context.h"
+#include "pan_resource.h"
 #include "pan_util.h"
 
 void
@@ -80,6 +81,18 @@ panfrost_blitter_save(struct panfrost_context *ctx,
 }
 
 void
+panfrost_blit_no_afbc_legalization(struct pipe_context *pipe,
+                                   const struct pipe_blit_info *info)
+{
+   struct panfrost_context *ctx = pan_context(pipe);
+
+   panfrost_blitter_save(ctx, info->render_condition_enable
+                                 ? PAN_RENDER_BLIT_COND
+                                 : PAN_RENDER_BLIT);
+   util_blitter_blit(ctx->blitter, info);
+}
+
+void
 panfrost_blit(struct pipe_context *pipe, const struct pipe_blit_info *info)
 {
    struct panfrost_context *ctx = pan_context(pipe);
@@ -91,11 +104,13 @@ panfrost_blit(struct pipe_context *pipe, const struct pipe_blit_info *info)
       unreachable("Unsupported blit\n");
 
    /* Legalize here because it could trigger a recursive blit otherwise */
-   pan_legalize_afbc_format(ctx, pan_resource(info->dst.resource),
-                            info->dst.format, true);
+   struct panfrost_resource *src = pan_resource(info->src.resource);
+   enum pipe_format src_view_format = util_format_linear(info->src.format);
+   pan_legalize_afbc_format(ctx, src, src_view_format, false);
 
-   panfrost_blitter_save(ctx, info->render_condition_enable
-                                 ? PAN_RENDER_BLIT_COND
-                                 : PAN_RENDER_BLIT);
-   util_blitter_blit(ctx->blitter, info);
+   struct panfrost_resource *dst = pan_resource(info->dst.resource);
+   enum pipe_format dst_view_format = util_format_linear(info->dst.format);
+   pan_legalize_afbc_format(ctx, dst, dst_view_format, true);
+
+   panfrost_blit_no_afbc_legalization(pipe, info);
 }

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -50,6 +50,7 @@
 #include "pan_jm.h"
 #include "pan_job.h"
 #include "pan_pool.h"
+#include "pan_resource.h"
 #include "pan_shader.h"
 #include "pan_texture.h"
 #include "pan_util.h"

bminor/mesa-mesa

commit 104ea2e4cf0ab5d949c6c58a9fea1f0230dd7892

panfrost: legalize afbc before zs and rt clears
In panfrost_clear_depth_stencil and panfrost_clear_render_target, we
start the blit context before binding the clear targets. If we don't
legalize AFBC beforehand, we get a recursive blit crash. panfrost_clear
does not need this because the resource should already be legalized in
panfrost_batch_add_surface.

Fixes the following piglit tests with pan_force_afbc_packing:
 - spec@arb_clear_texture@arb_clear_texture-base-formats
 - spec@arb_clear_texture@arb_clear_texture-simple
 - spec@arb_clear_texture@arb_clear_texture-sized-formats

Fixes: 17a62ff9934 ("panfrost: legalize afbc before blitting")
Signed-off-by: Olivia Lee <olivia.lee@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34992>

src/gallium/drivers/panfrost/pan_resource.c
@@ -70,6 +70,11 @@ panfrost_clear_depth_stencil(struct pipe_context *pipe,
    if (render_condition_enabled && !panfrost_render_condition_check(ctx))
       return;
 
+   /* Legalize here because it could trigger a recursive blit otherwise */
+   struct panfrost_resource *rdst = pan_resource(dst->texture);
+   enum pipe_format dst_view_format = util_format_linear(dst->format);
+   pan_legalize_format(ctx, rdst, dst_view_format, true, false);
+
    panfrost_blitter_save(
       ctx, render_condition_enabled ? PAN_RENDER_COND : PAN_RENDER_BASE);
    util_blitter_clear_depth_stencil(ctx->blitter, dst, clear_flags, depth,
@@ -87,6 +92,11 @@ panfrost_clear_render_target(struct pipe_context *pipe,
 
    if (render_condition_enabled && !panfrost_render_condition_check(ctx))
       return;
+
+   /* Legalize here because it could trigger a recursive blit otherwise */
+   struct panfrost_resource *rdst = pan_resource(dst->texture);
+   enum pipe_format dst_view_format = util_format_linear(dst->format);
+   pan_legalize_format(ctx, rdst, dst_view_format, true, false);
 
    panfrost_blitter_save(
       ctx, (render_condition_enabled ? PAN_RENDER_COND : PAN_RENDER_BASE) | PAN_SAVE_FRAGMENT_CONSTANT);

src/gallium/drivers/panfrost/pan_resource.h
@@ -137,6 +137,9 @@ enum {
    PAN_RENDER_CLEAR = PAN_SAVE_FRAGMENT_STATE | PAN_SAVE_FRAGMENT_CONSTANT,
 };
 
+/* Callers should ensure that all AFBC/AFRC resources that will be used in the
+ * blit operation are legalized before calling blitter operations, otherwise
+ * we may trigger a recursive blit */
 void panfrost_blitter_save(struct panfrost_context *ctx,
                            const enum panfrost_blitter_op blitter_op);
 