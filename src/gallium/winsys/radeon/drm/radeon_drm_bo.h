aerisarn/mesa-uwp

commit 6d913a2bcc69309d07ca9f75c91fed8f92004aad

r300,r600,radeonsi: switch to pb_buffer_lean
to remove pb_buffer::vtbl from all buffer structures

Reviewed-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/26643>

src/gallium/winsys/radeon/drm/radeon_drm_bo.h
@@ -13,7 +13,7 @@
 #include "pipebuffer/pb_slab.h"
 
 struct radeon_bo {
-   struct pb_buffer base;
+   struct pb_buffer_lean base;
    union {
       struct {
          struct pb_cache_entry cache_entry;
@@ -70,7 +70,7 @@ static inline void
 radeon_ws_bo_reference(struct radeon_winsys *rws, struct radeon_bo **dst,
                        struct radeon_bo *src)
 {
-   radeon_bo_reference(rws, (struct pb_buffer**)dst, (struct pb_buffer*)src);
+   radeon_bo_reference(rws, (struct pb_buffer_lean**)dst, (struct pb_buffer_lean*)src);
 }
 
 void *radeon_bo_do_map(struct radeon_bo *bo);

src/gallium/drivers/r300/r300_context.h
@@ -294,14 +294,14 @@ struct r300_query {
     bool begin_emitted;
 
     /* The buffer where query results are stored. */
-    struct pb_buffer *buf;
+    struct pb_buffer_lean *buf;
 };
 
 struct r300_surface {
     struct pipe_surface base;
 
     /* Winsys buffer backing the texture. */
-    struct pb_buffer *buf;
+    struct pb_buffer_lean *buf;
 
     enum radeon_bo_domain domain;
 
@@ -392,7 +392,7 @@ struct r300_resource
     struct pipe_resource b;
 
     /* Winsys buffer backing this resource. */
-    struct pb_buffer *buf;
+    struct pb_buffer_lean *buf;
     enum radeon_bo_domain domain;
 
     /* Constant buffers and SWTCL vertex and index buffers are in user
@@ -456,7 +456,7 @@ struct r300_context {
     /* Draw module. Used mostly for SW TCL. */
     struct draw_context* draw;
     /* Vertex buffer for SW TCL. */
-    struct pb_buffer *vbo;
+    struct pb_buffer_lean *vbo;
     /* Offset and size into the SW TCL VBO. */
     size_t draw_vbo_offset;
 

src/gallium/drivers/r300/r300_emit.c
@@ -770,8 +770,8 @@ void r300_emit_query_end(struct r300_context* r300)
     query->num_results += query->num_pipes;
 
     /* XXX grab all the results and reset the counter. */
-    if (query->num_results >= query->buf->base.size / 4 - 4) {
-        query->num_results = (query->buf->base.size / 4) / 2;
+    if (query->num_results >= query->buf->size / 4 - 4) {
+        query->num_results = (query->buf->size / 4) / 2;
         fprintf(stderr, "r300: Rewinding OQBO...\n");
     }
 }

src/gallium/drivers/r300/r300_render.c
@@ -950,7 +950,7 @@ static bool r300_render_allocate_vertices(struct vbuf_render* render,
 
     DBG(r300, DBG_DRAW, "r300: render_allocate_vertices (size: %d)\n", size);
 
-    if (!r300->vbo || size + r300->draw_vbo_offset > r300->vbo->base.size) {
+    if (!r300->vbo || size + r300->draw_vbo_offset > r300->vbo->size) {
 	radeon_bo_reference(r300->rws, &r300->vbo, NULL);
         r300->vbo = NULL;
         r300render->vbo_ptr = NULL;
@@ -1056,7 +1056,7 @@ static void r300_render_draw_elements(struct vbuf_render* render,
 {
     struct r300_render* r300render = r300_render(render);
     struct r300_context* r300 = r300render->r300;
-    unsigned max_index = (r300->vbo->base.size - r300->draw_vbo_offset) /
+    unsigned max_index = (r300->vbo->size - r300->draw_vbo_offset) /
                          (r300render->r300->vertex_info.size * 4) - 1;
     struct pipe_resource *index_buffer = NULL;
     unsigned index_buffer_offset;

src/gallium/drivers/r300/r300_screen_buffer.c
@@ -114,7 +114,7 @@ r300_buffer_transfer_map( struct pipe_context *context,
         if (r300->rws->cs_is_buffer_referenced(&r300->cs, rbuf->buf, RADEON_USAGE_READWRITE) ||
             !r300->rws->buffer_wait(r300->rws, rbuf->buf, 0, RADEON_USAGE_READWRITE)) {
             unsigned i;
-            struct pb_buffer *new_buf;
+            struct pb_buffer_lean *new_buf;
 
             /* Create a new one in the same pipe_resource. */
             new_buf = r300->rws->buffer_create(r300->rws, rbuf->b.width0,

src/gallium/drivers/r300/r300_texture.c
@@ -1033,7 +1033,7 @@ r300_texture_create_object(struct r300_screen *rscreen,
                            enum radeon_bo_layout microtile,
                            enum radeon_bo_layout macrotile,
                            unsigned stride_in_bytes_override,
-                           struct pb_buffer *buffer)
+                           struct pb_buffer_lean *buffer)
 {
     struct radeon_winsys *rws = rscreen->rws;
     struct r300_resource *tex = NULL;
@@ -1142,7 +1142,7 @@ struct pipe_resource *r300_texture_from_handle(struct pipe_screen *screen,
 {
     struct r300_screen *rscreen = r300_screen(screen);
     struct radeon_winsys *rws = rscreen->rws;
-    struct pb_buffer *buffer;
+    struct pb_buffer_lean *buffer;
     struct radeon_bo_metadata tiling = {};
 
     /* Support only 2D textures without mipmaps */

src/gallium/drivers/r300/r300_texture_desc.c
@@ -606,17 +606,17 @@ void r300_texture_desc_init(struct r300_screen *rscreen,
     r300_setup_miptree(rscreen, tex, true);
     /* If the required buffer size is larger than the given max size,
      * try again without the alignment for the CBZB clear. */
-    if (tex->buf && tex->tex.size_in_bytes > tex->buf->base.size) {
+    if (tex->buf && tex->tex.size_in_bytes > tex->buf->size) {
         r300_setup_miptree(rscreen, tex, false);
 
         /* Make sure the buffer we got is large enough. */
-        if (tex->tex.size_in_bytes > tex->buf->base.size) {
+        if (tex->tex.size_in_bytes > tex->buf->size) {
             fprintf(stderr,
                 "r300: I got a pre-allocated buffer to use it as a texture "
                 "storage, but the buffer is too small. I'll use the buffer "
                 "anyway, because I can't crash here, but it's dangerous. "
                 "This can be a DDX bug. Got: %"PRIu64"B, Need: %uB, Info:\n",
-                tex->buf->base.size, tex->tex.size_in_bytes);
+                tex->buf->size, tex->tex.size_in_bytes);
             r300_tex_print_info(tex, "texture_desc_init");
             /* Oops, what now. Apps will break if we fail this,
              * so just pretend everything's okay. */

src/gallium/drivers/r600/r600_buffer_common.c
@@ -34,7 +34,7 @@
 #include <stdio.h>
 
 bool r600_rings_is_buffer_referenced(struct r600_common_context *ctx,
-				     struct pb_buffer *buf,
+				     struct pb_buffer_lean *buf,
 				     unsigned usage)
 {
 	if (ctx->ws->cs_is_buffer_referenced(&ctx->gfx.cs, buf, usage)) {
@@ -166,7 +166,7 @@ void r600_init_resource_fields(struct r600_common_screen *rscreen,
 bool r600_alloc_resource(struct r600_common_screen *rscreen,
 			 struct r600_resource *res)
 {
-	struct pb_buffer *old_buf, *new_buf;
+	struct pb_buffer_lean *old_buf, *new_buf;
 
 	/* Allocate a new resource. */
 	new_buf = rscreen->ws->buffer_create(rscreen->ws, res->bo_size,
@@ -195,8 +195,8 @@ bool r600_alloc_resource(struct r600_common_screen *rscreen,
 	/* Print debug information. */
 	if (rscreen->debug_flags & DBG_VM && res->b.b.target == PIPE_BUFFER) {
 		fprintf(stderr, "VM start=0x%"PRIX64"  end=0x%"PRIX64" | Buffer %"PRIu64" bytes\n",
-			res->gpu_address, res->gpu_address + res->buf->base.size,
-			res->buf->base.size);
+			res->gpu_address, res->gpu_address + res->buf->size,
+			res->buf->size);
 	}
 	return true;
 }

bminor/mesa-mesa

commit b1d81a7df18817c0442605637ce55eef0998bc1c

radeonsi: Fix violation of aliasing rules in radeon_ws_bo_reference
Applications using Mesa built with LLVM 20.1.4 fail to start with
strange segmentfaults/bus errors when radeonsi driver is used. The last
piece of stacktrace looks like

  - pipe_reference_described
  - pipe_reference
  - radeon_bo_reference
  - radeon_ws_bo_reference
  - radeon_lookup_or_add_real_buffer

Coredump shows the pointer dst passed to pipe_reference_described() is
either unaligned or even invalid, which is the reason of crashing. The
crash goes away when Mesa is built without optimization.

Looking through the related functions, it's found that
radeon_ws_bo_reference() contains unsafe type cast from radeon_bo to
pb_buffer_lean: though the former's first field is just the later, this
violates strict aliasing rules as pb_buffer_lean isn't compatible with
radeon_bo. Such violation ultimately results in miscompilation.

Let's take the address of pb_buffer_lean field, avoiding the unsafe
cast. It's still required to cast pb_buffer_lean back to radeon_bo since
radeon_bo_reference may update the pointer, which is safe as radeon_bo
contains a pb_buffer_lean member and C language permits access members
through a pointer in type of the container.

Fixes: 6d913a2bcc69 ("r300,r600,radeonsi: switch to pb_buffer_lean")
Link: https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Aliasing-Type-Rules.html
Signed-off-by: Yao Zi <ziyao@disroot.org>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35249>

src/gallium/winsys/radeon/drm/radeon_drm_bo.h
@@ -70,7 +70,11 @@ static inline void
 radeon_ws_bo_reference(struct radeon_winsys *rws, struct radeon_bo **dst,
                        struct radeon_bo *src)
 {
-   radeon_bo_reference(rws, (struct pb_buffer_lean**)dst, (struct pb_buffer_lean*)src);
+   struct pb_buffer_lean *p_dst_base = &(*dst)->base;
+
+   radeon_bo_reference(rws, &p_dst_base, (struct pb_buffer_lean*)src);
+
+   *dst = (struct radeon_bo *)p_dst_base;
 }
 
 void *radeon_bo_do_map(struct radeon_bo *bo);