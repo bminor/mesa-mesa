c4b3f8cf74e8b10f1dade92787c11f64a76c63ca
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2025-05-30 11:46:51 -0400

    agx: add headers for uniform abi
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35658>

--- src/asahi/lib/agx_abi.h
+++ src/asahi/lib/agx_abi.h
@@ -21,6 +21,36 @@
 #define AGX_ABI_FOUT_WRITE_SAMPLES (7)
 #define AGX_ABI_FOUT_COLOUR(rt)    (2 * (4 + (4 * rt)))
 
+#define AGX_ABI_VUNI_VBO_BASE(vbo) (4 * (vbo))
+#define AGX_ABI_VUNI_VBO_CLAMP(nr_vbos, vbo)                                   \
+   (AGX_ABI_VUNI_VBO_BASE(nr_vbos) + 2 * (vbo))
+
+#define AGX_ABI_VUNI_FIRST_VERTEX(nr_vbos)                                     \
+   AGX_ABI_VUNI_VBO_CLAMP(nr_vbos, nr_vbos)
+
+#define AGX_ABI_VUNI_BASE_INSTANCE(nr_vbos)                                    \
+   (AGX_ABI_VUNI_FIRST_VERTEX(nr_vbos) + 2)
+
+#define AGX_ABI_VUNI_DRAW_ID(nr_vbos) (AGX_ABI_VUNI_FIRST_VERTEX(nr_vbos) + 4)
+
+#define AGX_ABI_VUNI_INPUT_ASSEMBLY(nr_vbos)                                   \
+   (AGX_ABI_VUNI_FIRST_VERTEX(nr_vbos) + 8)
+
+#define AGX_ABI_VUNI_COUNT(nr_vbos, hw)                                        \
+   (hw ? (AGX_ABI_VUNI_BASE_INSTANCE(nr_vbos) + 5)                             \
+       : (AGX_ABI_VUNI_INPUT_ASSEMBLY(nr_vbos) + 4))
+
+#define AGX_ABI_VUNI_COUNT_VK(nr_vbos)                                         \
+   ALIGN_POT(AGX_ABI_VUNI_INPUT_ASSEMBLY(nr_vbos) + 4, 4)
+
+#define AGX_ABI_FUNI_EMRT_HEAP (0)
+#define AGX_ABI_FUNI_BLEND_R   (AGX_ABI_FUNI_EMRT_HEAP + 4)
+#define AGX_ABI_FUNI_BLEND_G   (AGX_ABI_FUNI_BLEND_R + 2)
+#define AGX_ABI_FUNI_BLEND_B   (AGX_ABI_FUNI_BLEND_R + 4)
+#define AGX_ABI_FUNI_BLEND_A   (AGX_ABI_FUNI_BLEND_R + 6)
+#define AGX_ABI_FUNI_ROOT      (12)
+#define AGX_ABI_FUNI_COUNT     (16)
+
 /* This address is in our reservation, and can be
  * addressed with only small integers in the low/high. That lets us do some
  * robustness optimization even without soft fault.
