alexvorxx/Mesa-VirGL

commit 6ae401aa868066eecbe6307c6e8173525f566580

vulkan: Add null descriptor bits to vk_pipeline_robustness_state
Fixes: c9eac89da835 ("nvk: Advertise VK_EXT_robustness2")
Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30663>

src/vulkan/runtime/vk_pipeline.c
@@ -295,6 +295,8 @@ vk_pipeline_robustness_state_fill(const struct vk_device *device,
    rs->storage_buffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_DEVICE_DEFAULT_EXT;
    rs->vertex_inputs = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_DEVICE_DEFAULT_EXT;
    rs->images = VK_PIPELINE_ROBUSTNESS_IMAGE_BEHAVIOR_DEVICE_DEFAULT_EXT;
+   rs->null_uniform_buffer_descriptor = device->enabled_features.nullDescriptor;
+   rs->null_storage_buffer_descriptor = device->enabled_features.nullDescriptor;
 
    const VkPipelineRobustnessCreateInfoEXT *shader_info =
       vk_find_struct_const(shader_stage_pNext,

src/vulkan/runtime/vk_pipeline.h
@@ -76,6 +76,8 @@ struct vk_pipeline_robustness_state {
    VkPipelineRobustnessBufferBehaviorEXT uniform_buffers;
    VkPipelineRobustnessBufferBehaviorEXT vertex_inputs;
    VkPipelineRobustnessImageBehaviorEXT images;
+   bool null_uniform_buffer_descriptor;
+   bool null_storage_buffer_descriptor;
 };
 
 /** Hash VkPipelineShaderStageCreateInfo info

src/vulkan/runtime/vk_shader.c
@@ -393,6 +393,17 @@ vk_common_CreateShadersEXT(VkDevice _device,
       .uniform_buffers = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_DISABLED_EXT,
       .vertex_inputs = VK_PIPELINE_ROBUSTNESS_BUFFER_BEHAVIOR_DISABLED_EXT,
       .images = VK_PIPELINE_ROBUSTNESS_IMAGE_BEHAVIOR_DISABLED_EXT,
+      /* From the Vulkan 1.3.292 spec:
+       *
+       *    "This extension [VK_EXT_robustness2] also adds support for “null
+       *    descriptors”, where VK_NULL_HANDLE can be used instead of a valid
+       *    handle. Accesses to null descriptors have well-defined behavior,
+       *    and do not rely on robustness."
+       *
+       * For now, default these to true.
+       */
+      .null_uniform_buffer_descriptor = true,
+      .null_storage_buffer_descriptor = true,
    };
 
    /* From the Vulkan 1.3.274 spec:

bminor/mesa-mesa

commit 347e957b44746bbe417d77f19113463da6817d58

vulkan/pipeline: Hash null descriptor robustness info
Fixes: 6ae401aa8680 ("vulkan: Add null descriptor bits to vk_pipeline_robustness_state")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34967>

src/vulkan/runtime/vk_pipeline.c
@@ -256,6 +256,10 @@ vk_pipeline_hash_shader_stage(VkPipelineCreateFlags2KHR pipeline_flags,
       _mesa_sha1_update(&ctx, &rstate->uniform_buffers, sizeof(rstate->uniform_buffers));
       _mesa_sha1_update(&ctx, &rstate->vertex_inputs, sizeof(rstate->vertex_inputs));
       _mesa_sha1_update(&ctx, &rstate->images, sizeof(rstate->images));
+      _mesa_sha1_update(&ctx, &rstate->null_uniform_buffer_descriptor,
+                        sizeof(rstate->null_uniform_buffer_descriptor));
+      _mesa_sha1_update(&ctx, &rstate->null_storage_buffer_descriptor,
+                        sizeof(rstate->null_storage_buffer_descriptor));
    }
 
    _mesa_sha1_update(&ctx, info->pName, strlen(info->pName));