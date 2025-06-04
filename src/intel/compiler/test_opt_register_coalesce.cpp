bminor/mesa-mesa

commit 0e3e5146cf140095cae9da0c20a7feed17eb9ef7

intel/brw: Use correct instruction for value change check when coalescing
When we have partial VGRF MOVs with offsets, we will reach
`channels_remaining == 0` with `inst` that is not writing the whole VGRF.
Currently, even though we check `can_coalesce_vars()` for each offset
separately, it will always check if the dst value is not changed only
for the offset from the instruction that satisfied the
`channels_remaining == 0` condition.

Instead, we should remember and use the correct instruction for each
written offset separately.

Cc: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/10916
Signed-off-by: Sviatoslav Peleshko <sviatoslav.peleshko@globallogic.com>
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Matt Turner <mattst88@gmail.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35062>

src/intel/compiler/brw_opt_register_coalesce.cpp
@@ -282,8 +282,8 @@ brw_opt_register_coalesce(brw_shader &s)
       if (inst->opcode == SHADER_OPCODE_LOAD_PAYLOAD) {
          for (int i = 0; i < src_size; i++) {
             dst_reg_offset[i] = inst->dst.offset / REG_SIZE + i;
+            mov[i] = inst;
          }
-         mov[0] = inst;
          channels_remaining -= regs_written(inst);
       } else {
          const int offset = inst->src[0].offset / REG_SIZE;
@@ -297,9 +297,10 @@ brw_opt_register_coalesce(brw_shader &s)
             channels_remaining = -1;
             continue;
          }
-         for (unsigned i = 0; i < MAX2(inst->size_written / REG_SIZE, 1); i++)
+         for (unsigned i = 0; i < MAX2(inst->size_written / REG_SIZE, 1); i++) {
             dst_reg_offset[offset + i] = inst->dst.offset / REG_SIZE + i;
-         mov[offset] = inst;
+            mov[offset + i] = inst;
+         }
          channels_remaining -= regs_written(inst);
       }
 
@@ -318,7 +319,7 @@ brw_opt_register_coalesce(brw_shader &s)
          dst_var[i] = live.var_from_vgrf[dst_reg] + dst_reg_offset[i];
          src_var[i] = live.var_from_vgrf[src_reg] + i;
 
-         if (!can_coalesce_vars(devinfo, live, ips, s.cfg, inst, dst_var[i], src_var[i]) ||
+         if (!can_coalesce_vars(devinfo, live, ips, s.cfg, mov[i], dst_var[i], src_var[i]) ||
              would_violate_eot_restriction(s, s.cfg, dst_reg, src_reg)) {
             can_coalesce = false;
             src_reg = ~0u;

src/intel/compiler/test_opt_register_coalesce.cpp
@@ -84,3 +84,28 @@ TEST_F(RegisterCoalesceTest, InterfereButContainEachOther)
 
    EXPECT_SHADERS_MATCH(bld, exp);
 }
+
+TEST_F(RegisterCoalesceTest, ChangingTemporaryCompoundRegisterNotChangesOriginal)
+{
+   brw_builder bld = make_shader();
+
+   brw_reg src = vgrf(bld, BRW_TYPE_F, 2);
+   brw_reg tmp = vgrf(bld, BRW_TYPE_F, 2);
+   brw_reg dst = vgrf(bld, BRW_TYPE_F, 2);
+
+   brw_reg one = brw_imm_f(1.0);
+   brw_reg two = brw_imm_f(2.0);
+
+   bld.MOV(src, one);
+   bld.MOV(offset(src, bld, 1), two);
+
+   bld.MOV(offset(tmp, bld, 1), offset(src, bld, 1));
+
+   bld.MOV(tmp, src);
+   bld.ADD(offset(tmp, bld, 1), offset(tmp, bld, 1), one);
+
+   bld.ADD(dst, src, one);
+   bld.ADD(offset(dst, bld, 1), offset(src, bld, 1), two);
+
+   EXPECT_NO_PROGRESS(brw_opt_register_coalesce, bld);
+}

commit a51d061c00b6b49853881783ffa02aca3ee105f4

brw: don't generate invalid instructions
0e3e5146cf ("intel/brw: Use correct instruction for value change check
when coalescing") enabled some new cases that exposed a pre-existing
bug that would turn something like this :

      mul.sat(16) %789:F, %787:F, %788:F
      mov.g.f0.0(16) %790:F, %789:F
      (+f0.0) sel(16) %800:UD, %790:UD, 0u

into this :

      mul.sat(16) %790:F, %787:F, %788:F
      mov.g.f0.0(16) null:F, null<8,8,1>:F
      (+f0.0) sel(16) %800:UD, %790:UD, 0u

The mov[] array can contain the same instruction because it's repeated
for each REG_SIZE writes and a SIMD16 instruction will write 2
REG_SIZE.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 0e3e5146cf ("intel/brw: Use correct instruction for value change check when coalescing")
Cc: mesa-stable
Reviewed-by: Matt Turner <mattst88@gmail.com>
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35276>

src/intel/compiler/brw_opt_register_coalesce.cpp
@@ -332,7 +332,7 @@ brw_opt_register_coalesce(brw_shader &s)
 
       progress = true;
 
-      for (int i = 0; i < src_size; i++) {
+      for (int i = 0; i < src_size; i += regs_written(mov[i])) {
          if (!mov[i])
             continue;
 

src/intel/compiler/test_opt_register_coalesce.cpp
@@ -109,3 +109,53 @@ TEST_F(RegisterCoalesceTest, ChangingTemporaryCompoundRegisterNotChangesOriginal
 
    EXPECT_NO_PROGRESS(brw_opt_register_coalesce, bld);
 }
+
+TEST_F(RegisterCoalesceTest, MovWithFlagRegisterWrite)
+{
+   brw_builder bld = make_shader(MESA_SHADER_COMPUTE, 16);
+   brw_builder exp = make_shader(MESA_SHADER_COMPUTE, 16);
+
+   /**
+    *         mul.sat(16)    %789:F,  %787:F, %788:F
+    *         mov.g.f0.0(16) %790:F,  %789:F
+    * (+f0.0) sel(16)        %800:UD, %790:UD, 0u
+    */
+
+   brw_reg src0  = vgrf(bld, exp, BRW_TYPE_F);
+   brw_reg src1  = vgrf(bld, exp, BRW_TYPE_F);
+   brw_reg vgrf0 = vgrf(bld, exp, BRW_TYPE_F);
+   brw_reg vgrf1 = vgrf(bld, exp, BRW_TYPE_F);
+   brw_reg vgrf2 = vgrf(bld, exp, BRW_TYPE_F);
+
+   {
+      brw_inst *mul = bld.MUL(vgrf0, src0, src1);
+      mul->saturate = true;
+
+      brw_inst *mov = bld.MOV(vgrf1, vgrf0);
+      mov->conditional_mod = BRW_CONDITIONAL_G;
+
+      brw_inst *sel = bld.SEL(vgrf2, vgrf1, brw_imm_f(0.0));
+      sel->predicate = BRW_PREDICATE_NORMAL;
+   }
+
+   EXPECT_PROGRESS(brw_opt_register_coalesce, bld);
+
+   /**
+    *         mul.sat(16)    %789:F,  %787:F, %788:F
+    *         mov.g.f0.0(16) null:F,  %789:F
+    * (+f0.0) sel(16)        %800:UD, %789:UD, 0u
+    */
+
+   {
+      brw_inst *mul = exp.MUL(vgrf0, src0, src1);
+      mul->saturate = true;
+
+      brw_inst *mov = exp.MOV(brw_null_reg(), vgrf0);
+      mov->conditional_mod = BRW_CONDITIONAL_G;
+
+      brw_inst *sel = exp.SEL(vgrf2, vgrf0, brw_imm_f(0.0));
+      sel->predicate = BRW_PREDICATE_NORMAL;
+   }
+
+   EXPECT_SHADERS_MATCH(bld, exp);
+}