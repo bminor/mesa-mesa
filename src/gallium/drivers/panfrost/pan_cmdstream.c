bminor/mesa-mesa

commit c1ce2dcc66df091026a87c9e2ed58a75b8fcf67a

pan/bi: Enable ARB_shader_clock extension support
Introduces GCLK instruction to read the system timer/counter. Currently
only counter is supported.

Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34342>

src/gallium/drivers/panfrost/pan_screen.c
@@ -514,7 +514,7 @@ panfrost_init_screen_caps(struct panfrost_screen *screen)
     * work to turn on, since CYCLE_COUNT_START needs to be issued. In
     * kbase, userspace requests this via BASE_JD_REQ_PERMON. There is not
     * yet way to request this with mainline TODO */
-   caps->shader_clock = false;
+   caps->shader_clock = dev->arch >= 6;
 
    caps->vs_instanceid = true;
    caps->texture_multisample = true;

src/panfrost/compiler/bifrost_compile.c
@@ -2187,6 +2187,7 @@ bi_emit_intrinsic(bi_builder *b, nir_intrinsic_instr *instr)
       break;
 
    case nir_intrinsic_shader_clock:
+      assert(nir_intrinsic_memory_scope(instr) == SCOPE_SUBGROUP);
       bi_ld_gclk_u64_to(b, dst, BI_SOURCE_CYCLE_COUNTER);
       bi_split_def(b, &instr->def);
       break;

src/panfrost/compiler/valhall/ISA.xml
@@ -733,6 +733,20 @@
     <value desc="Retrieve previous state">retrieve</value>
   </enum>
 
+  <enum name="Source">
+    <desc>
+      Clock source for LD_GCLK instruction.
+    </desc>
+    <reserved/>
+    <reserved/>
+    <reserved/>
+    <reserved/>
+    <reserved/>
+    <reserved/>
+    <value desc="Cycle counter">cycle_counter</value>
+    <value desc="System timestamp">system_timestamp</value>
+  </enum>
+
   <enum name="Source format">
     <desc>
       In-memory format of varyings.
@@ -1016,6 +1030,13 @@
     <src>Vertex ID</src>
     <src>Instance ID</src>
     <src>Index and table</src>
+  </ins>
+
+  <ins name="LD_GCLK_U64" title="Global clock load" opcode="0x44" unused="true" unit="LS">
+    <desc>Load the 64-bit global clock, either a cycle counter or the system clock.</desc>
+    <sr write="true"/>
+    <sr_count count="2"/>
+    <slot/>
   </ins>
 
   <ins name="LD_TEX_IMM" title="Load immediate texture" opcode="0x66" opcode2="1" message="attribute" unit="LS">

src/panfrost/compiler/valhall/va_pack.c
@@ -678,6 +678,19 @@ va_pack_byte_offset_8(const bi_instr *I)
 }
 
 static uint64_t
+va_pack_gclk(const bi_instr *I)
+{
+   switch (I->source) {
+   case BI_SOURCE_CYCLE_COUNTER:
+      return VA_SOURCE_CYCLE_COUNTER;
+   case BI_SOURCE_SYSTEM_TIMESTAMP:
+      return VA_SOURCE_SYSTEM_TIMESTAMP;
+   }
+
+   invalid_instruction(I, "source");
+}
+
+static uint64_t
 va_pack_load(const bi_instr *I, bool buffer_descriptor)
 {
    const uint8_t load_lane_identity[8] = {
@@ -919,6 +932,10 @@ va_pack_instr(const bi_instr *I, unsigned arch)
 
       break;
    }
+
+   case BI_OPCODE_LD_GCLK_U64:
+      hex |= va_pack_gclk(I);
+      break;
 
    case BI_OPCODE_TEX_GRADIENT:
    case BI_OPCODE_TEX_SINGLE:

commit 95c508d950c9f541d2745dafd4c0c76aeb624175

panfrost: Fix shader_clock support for v6+
This code sets PANFROST_JD_REQ_CYCLE_COUNT when using LD_GCLK to enable
shader_clock

Fixes: c1ce2dcc ("pan/bi: Enable ARB_shader_clock extension support")
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Signed-off-by: Ashley Smith <ashley.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35363>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -2978,6 +2978,9 @@ panfrost_update_shader_state(struct panfrost_batch *batch,
    unsigned dirty_3d = ctx->dirty;
    unsigned dirty = ctx->dirty_shader[st];
 
+   if (ss->info.has_shader_clk_instr)
+      batch->need_job_req_cycle_count = true;
+
    if (dirty & (PAN_DIRTY_STAGE_TEXTURE | PAN_DIRTY_STAGE_SHADER)) {
       batch->textures[st] = panfrost_emit_texture_descriptors(batch, st);
    }

src/gallium/drivers/panfrost/pan_screen.c
@@ -667,11 +667,9 @@ panfrost_init_screen_caps(struct panfrost_screen *screen)
    caps->anisotropic_filter =
       panfrost_device_gpu_rev(dev) >= dev->model->min_rev_anisotropic;
 
-   /* Compile side is done for Bifrost, Midgard TODO. Needs some kernel
-    * work to turn on, since CYCLE_COUNT_START needs to be issued. In
-    * kbase, userspace requests this via BASE_JD_REQ_PERMON. There is not
-    * yet way to request this with mainline TODO */
-   caps->shader_clock = dev->arch >= 6;
+   /* Compile side is TODO for Midgard. */
+   caps->shader_clock = dev->arch >= 6 &&
+      dev->kmod.props.gpu_can_query_timestamp;
 
    caps->vs_instanceid = true;
    caps->texture_multisample = true;

src/panfrost/compiler/bifrost_compile.c
@@ -2318,9 +2318,9 @@ bi_emit_intrinsic(bi_builder *b, nir_intrinsic_instr *instr)
       break;
 
    case nir_intrinsic_shader_clock:
-      assert(nir_intrinsic_memory_scope(instr) == SCOPE_SUBGROUP);
       bi_ld_gclk_u64_to(b, dst, BI_SOURCE_CYCLE_COUNTER);
       bi_split_def(b, &instr->def);
+      b->shader->info.has_ld_gclk_instr = true;
       break;
 
    case nir_intrinsic_ddx:
@@ -6326,6 +6326,7 @@ bi_compile_variant(nir_shader *nir,
 
    info->ubo_mask |= ctx->ubo_mask;
    info->tls_size = MAX2(info->tls_size, ctx->info.tls_size);
+   info->has_shader_clk_instr = ctx->info.has_ld_gclk_instr;
 
    if (idvs == BI_IDVS_VARYING) {
       info->vs.secondary_enable = (binary->size > offset);

src/panfrost/compiler/compiler.h
@@ -886,6 +886,7 @@ struct bi_shader_info {
    unsigned tls_size;
    unsigned work_reg_count;
    unsigned push_offset;
+   bool has_ld_gclk_instr;
 };
 
 /* State of index-driven vertex shading for current shader */

src/panfrost/util/pan_ir.h
@@ -281,6 +281,9 @@ struct pan_shader_info {
    /* Floating point controls that the driver should try to honour */
    bool ftz_fp16, ftz_fp32;
 
+   /* True if the shader contains a shader_clock instruction. */
+   bool has_shader_clk_instr;
+
    unsigned sampler_count;
    unsigned texture_count;
    unsigned ubo_count;