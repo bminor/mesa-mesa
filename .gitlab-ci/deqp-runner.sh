bminor/mesa-mesa

commit 8d08cde667954e71592cee70933a38afe04deb7b

ci/piglit: Use structured tagging for Piglit
Structured tagging (cf. mesa/mesa!33421) captures a checksum of the
thing we think we're building, and verifies this through the chain.

When we run container builds, we check that the tag we've captured in
the CI variables matches the calculated checksum, to make sure the
declared tags are consistent and we always have traceability.

When we run tests, we check the tags again between what was declared in
the CI variables and what we're actually running from the test
container. This makes sure that we're always testing what we think we're
testing.

As a side advantage, the rule inheritance we need to make this work
means that we can start doing more optional downloads via overlays,
instead of pulling a whole container full of stuff we might not ever
use.

Signed-off-by: Daniel Stone <daniels@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34539>

.gitlab-ci/deqp-runner.sh
@@ -31,6 +31,16 @@ if [ -n "$ANGLE_TAG" ]; then
   export LD_LIBRARY_PATH=/angle:$LD_LIBRARY_PATH
 fi
 
+if [ -n "$PIGLIT_TAG" ]; then
+  # Are we using the right Piglit version?
+  ci_tag_test_time_check "PIGLIT_TAG"
+elif [ -d "/piglit" ]; then
+  # The job does not inherit from .test-piglit, so we move it out of the way.
+  # This makes sure that we can both do the right version checks when needed,
+  # and also optimise our dependencies so we don't pull unneeded stuff.
+  mv /piglit /piglit.unused
+fi
+
 # Ensure Mesa Shader Cache resides on tmpfs.
 SHADER_CACHE_HOME=${XDG_CACHE_HOME:-${HOME}/.cache}
 SHADER_CACHE_DIR=${MESA_SHADER_CACHE_DIR:-${SHADER_CACHE_HOME}/mesa_shader_cache}

.gitlab-ci/common/export-gitlab-job-env-for-dut.sh
@@ -108,6 +108,7 @@ VARS=(
     PIGLIT_REPLAY_SUBCOMMAND
     PIGLIT_RESULTS
     PIGLIT_RUNNER_OPTIONS
+    PIGLIT_TAG
     PIGLIT_TESTS
     PIGLIT_TRACES_FILE
     PIPELINE_ARTIFACTS_BASE

.gitlab-ci/conditional-build-image-tags.yml
@@ -1,2 +1,3 @@
 variables:
   CONDITIONAL_BUILD_ANGLE_TAG: ab19bccfd3858c539ba8cb8d9b52a003
+  CONDITIONAL_BUILD_PIGLIT_TAG: e2cb48af623fb6c8b9793679de33d5be

.gitlab-ci/container/build-piglit.sh
@@ -10,6 +10,10 @@ uncollapsed_section_start piglit "Building piglit"
 # DEBIAN_TEST_VK_TAG
 # KERNEL_ROOTFS_TAG
 
+# Do a very early check to make sure the tag is correct without the need of
+# setting up the environment variables locally
+ci_tag_build_time_check "PIGLIT_TAG"
+
 REV="0ecdebb0f5927728ddeeb851639a559b0f7d6590"
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit

.gitlab-ci/container/gitlab-ci.yml
@@ -78,17 +78,29 @@
     # should make CI_BUILD_COMPONENTS="angle piglit"
     CI_BUILD_COMPONENTS: "angle"
 
+.container-builds-piglit:
+  variables:
+    PIGLIT_TAG: "${CONDITIONAL_BUILD_PIGLIT_TAG}"
+
 .container-builds-x86_64:
   extends:
     - .container-builds-angle
+    - .container-builds-piglit
   variables:
-    CI_BUILD_COMPONENTS: "angle"
+    CI_BUILD_COMPONENTS: "angle piglit"
+
+.container-builds-arm32:
+  extends:
+    - .container-builds-piglit
+  variables:
+    CI_BUILD_COMPONENTS: "piglit"
 
 .container-builds-arm64:
   extends:
     - .container-builds-angle
+    - .container-builds-piglit
   variables:
-    CI_BUILD_COMPONENTS: "angle"
+    CI_BUILD_COMPONENTS: "angle piglit"
 
 .use-base-image:
   extends:
@@ -405,7 +417,9 @@ debian/arm64_test-base:
 
 # Debian based x86_64 test image for GL
 debian/x86_64_test-gl:
-  extends: .use-debian/x86_64_test-base
+  extends:
+    - .use-debian/x86_64_test-base
+    - .container-builds-x86_64
   variables:
     MESA_IMAGE_TAG: &debian-x86_64_test-gl ${DEBIAN_TEST_GL_TAG}
 
@@ -423,7 +437,9 @@ debian/x86_64_test-gl:
 
 # Debian based x86_64 test image for VK
 debian/x86_64_test-vk:
-  extends: .use-debian/x86_64_test-base
+  extends:
+    - .use-debian/x86_64_test-base
+    - .container-builds-x86_64
   variables:
     MESA_IMAGE_TAG: &debian-x86_64_test-vk ${DEBIAN_TEST_VK_TAG}
 
@@ -484,7 +500,9 @@ debian/x86_64_pyutils:
 debian/arm32_test-gl:
   tags:
     - $FDO_RUNNER_JOB_PRIORITY_TAG_AARCH64
-  extends: .use-debian/arm32_test-base
+  extends:
+    - .use-debian/arm32_test-base
+    - .container-builds-arm32
   variables:
     MESA_IMAGE_TAG: &debian-arm32_test-gl ${DEBIAN_TEST_GL_TAG}
 
@@ -506,7 +524,9 @@ debian/arm32_test-vk:
     - when: never # There are currently no arm32 VK jobs
   tags:
     - $FDO_RUNNER_JOB_PRIORITY_TAG_AARCH64
-  extends: .use-debian/arm32_test-base
+  extends:
+    - .use-debian/arm32_test-base
+    - .container-builds-arm32
   variables:
     MESA_IMAGE_TAG: &debian-arm32_test-vk ${DEBIAN_TEST_VK_TAG}
 
@@ -548,7 +568,9 @@ debian/arm64_test-gl:
 debian/arm64_test-vk:
   tags:
     - $FDO_RUNNER_JOB_PRIORITY_TAG_AARCH64
-  extends: .use-debian/arm64_test-base
+  extends:
+    - .use-debian/arm64_test-base
+    - .container-builds-arm64
   variables:
     MESA_IMAGE_TAG: &debian-arm64_test-vk ${DEBIAN_TEST_VK_TAG}
 

.gitlab-ci/image-tags.yml
@@ -23,10 +23,10 @@ variables:
 
   DEBIAN_BUILD_TAG: "20250324-android"
 
-  DEBIAN_TEST_ANDROID_TAG: "20250415-anglit"
-  DEBIAN_TEST_GL_TAG: "20250415-anglit"
-  DEBIAN_TEST_VK_TAG: "20250416-vkvideo"
-  KERNEL_ROOTFS_TAG: "20250415-anglit"
+  DEBIAN_TEST_ANDROID_TAG: "20250416-pigtag"
+  DEBIAN_TEST_GL_TAG: "20250416-pigtag"
+  DEBIAN_TEST_VK_TAG: "20250416-pigtag"
+  KERNEL_ROOTFS_TAG: "20250416-pigtag"
 
   DEBIAN_PYUTILS_TAG: "20250321-s3cp-fix5"
 

.gitlab-ci/lava/lava-gitlab-ci.yml
@@ -140,6 +140,8 @@ variables:
     - .lava-test:x86_64
 
 .lava-piglit-traces:
+  extends:
+    - .test-piglit
   variables:
     HWCI_TEST_SCRIPT: "/install/piglit/piglit-traces.sh"
     # until we overcome Infrastructure issues, give traces extra 5 min before timeout

.gitlab-ci/piglit/piglit-traces.sh
@@ -12,6 +12,9 @@ set -ex
 # Our rootfs may not have "less", which apitrace uses during apitrace dump
 export PAGER=cat  # FIXME: export everywhere
 
+# Check we're using the version of Piglit we think we are
+ci_tag_test_time_check "PIGLIT_TAG"
+
 INSTALL=$(realpath -s "$PWD"/install)
 
 export PIGLIT_REPLAY_DESCRIPTION_FILE="$INSTALL/$PIGLIT_TRACES_FILE"

commit 9e007063f2b2bb1c4d5e2af48f12ad2192f81555

ci/piglit: avoid copying /piglit across filesystems when unused
This `mv` copies the folder from the read-only docker image to the
run time overlayfs, which is expensive and unnecessary, and slow on
devices using network disks.

`rm` by comparison is instant, and there is no reason to keep the folder
around if we're taking it away from the expected path anyway.

Fixes: 8d08cde667954e71592c ("ci/piglit: Use structured tagging for Piglit")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35500>

.gitlab-ci/deqp-runner.sh
@@ -35,10 +35,10 @@ if [ -n "$PIGLIT_TAG" ]; then
   # Are we using the right Piglit version?
   ci_tag_test_time_check "PIGLIT_TAG"
 elif [ -d "/piglit" ]; then
-  # The job does not inherit from .test-piglit, so we move it out of the way.
+  # The job does not inherit from .test-piglit, so we remove it.
   # This makes sure that we can both do the right version checks when needed,
   # and also optimise our dependencies so we don't pull unneeded stuff.
-  mv /piglit /piglit.unused
+  rm -r /piglit
 fi
 
 # Ensure Mesa Shader Cache resides on tmpfs.