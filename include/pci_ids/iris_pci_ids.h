alexvorxx/zink-xlib-termux

commit c1d3fa007cabdcb69f37acdfaa8154402cb6e760

intel/dev: Add BMG PCI IDs (with FORCE_PROBE set)
Ref: bspec 68090
Signed-off-by: Jordan Justen <jordan.l.justen@intel.com>
Reviewed-by: Tapani Pälli <tapani.palli@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29457>

include/pci_ids/iris_pci_ids.h
@@ -271,3 +271,9 @@ CHIPSET(0x7d41, arl_u, "ARL", "Intel(R) Graphics")
 CHIPSET(0x7d51, arl_h, "ARL", "Intel(R) Graphics")
 CHIPSET(0x7d67, arl_u, "ARL", "Intel(R) Graphics")
 CHIPSET(0x7dd1, arl_h, "ARL", "Intel(R) Graphics")
+
+CHIPSET(0xe202, bmg, "BMG G21", "Intel(R) Graphics", FORCE_PROBE)
+CHIPSET(0xe20b, bmg, "BMG G21", "Intel(R) Graphics", FORCE_PROBE)
+CHIPSET(0xe20c, bmg, "BMG G21", "Intel(R) Graphics", FORCE_PROBE)
+CHIPSET(0xe20d, bmg, "BMG G21", "Intel(R) Graphics", FORCE_PROBE)
+CHIPSET(0xe212, bmg, "BMG G21", "Intel(R) Graphics", FORCE_PROBE)

src/intel/dev/intel_device_info.c
@@ -1225,7 +1225,7 @@ static const struct intel_device_info intel_device_info_arl_h = {
    .cooperative_matrix_configurations = { { 0 }, },             \
    .has_flat_ccs = true
 
-UNUSED static const struct intel_device_info intel_device_info_bmg = {
+static const struct intel_device_info intel_device_info_bmg = {
    XE2_FEATURES,
    .platform = INTEL_PLATFORM_BMG,
    .has_local_mem = true,

bminor/mesa-mesa

commit 270035fdce11b89ff0795fd94f2ef92a3c6b686c

intel/dev: update BMG device names
Ref: https://www.intel.com/content/www/us/en/products/sku/241676/intel-arc-b570-graphics/specifications.html
Ref: https://www.intel.com/content/www/us/en/products/sku/241598/intel-arc-b580-graphics/specifications.html
Ref: https://www.intel.com/content/www/us/en/products/sku/242615/intel-arc-pro-b50-graphics/specifications.html
Ref: https://www.intel.com/content/www/us/en/products/sku/243916/intel-arc-pro-b60-graphics/specifications.html

Fixes: c1d3fa007ca ("intel/dev: Add BMG PCI IDs (with FORCE_PROBE set)")
Fixes: d3ec4670317 ("intel/dev: Add BMG 0xe211 PCI ID")
Acked-by: Tapani Pälli <tapani.palli@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35539>

include/pci_ids/iris_pci_ids.h
@@ -274,12 +274,12 @@ CHIPSET(0x7dd1, arl_h, "ARL", "Intel(R) Graphics")
 CHIPSET(0xb640, arl_u, "ARL", "Intel(R) Graphics")
 
 CHIPSET(0xe202, bmg, "BMG G21", "Intel(R) Graphics")
-CHIPSET(0xe20b, bmg, "BMG G21", "Intel(R) Graphics")
-CHIPSET(0xe20c, bmg, "BMG G21", "Intel(R) Graphics")
+CHIPSET(0xe20b, bmg, "BMG G21", "Intel(R) Arc(tm) B580 Graphics")
+CHIPSET(0xe20c, bmg, "BMG G21", "Intel(R) Arc(tm) B570 Graphics")
 CHIPSET(0xe20d, bmg, "BMG G21", "Intel(R) Graphics")
 CHIPSET(0xe210, bmg, "BMG G21", "Intel(R) Graphics")
-CHIPSET(0xe211, bmg, "BMG G21", "Intel(R) Graphics")
-CHIPSET(0xe212, bmg, "BMG G21", "Intel(R) Graphics")
+CHIPSET(0xe211, bmg, "BMG G21", "Intel(R) Arc(tm) Pro B60 Graphics")
+CHIPSET(0xe212, bmg, "BMG G21", "Intel(R) Arc(tm) Pro B50 Graphics")
 CHIPSET(0xe215, bmg, "BMG", "Intel(R) Graphics")
 CHIPSET(0xe216, bmg, "BMG", "Intel(R) Graphics")
 CHIPSET(0xe220, bmg, "BMG G31", "Intel(R) Graphics")