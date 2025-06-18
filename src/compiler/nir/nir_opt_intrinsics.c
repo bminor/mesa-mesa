aerisarn/mesa-uwp

commit 5c70a55bf3fbc5f32e81843a0d49e137f35a30b6

nir/opt_intrinsics: optimize (exclusive_scan(op, a) op a) to inclusive scan
D3D only has exclusive scans so some games use this pattern.

Reviewed-by: Faith Ekstrand <faith.ekstrand@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24557>

src/compiler/nir/nir_opt_intrinsics.c
@@ -241,6 +241,55 @@ opt_intrinsics_alu(nir_builder *b, nir_alu_instr *alu,
 }
 
 static bool
+try_opt_exclusive_scan_to_inclusive(nir_intrinsic_instr *intrin)
+{
+   if (intrin->dest.ssa.num_components != 1)
+      return false;
+
+   nir_foreach_use_including_if(src, &intrin->dest.ssa) {
+      if (src->is_if || src->parent_instr->type != nir_instr_type_alu)
+         return false;
+
+      nir_alu_instr *alu = nir_instr_as_alu(src->parent_instr);
+
+      if (alu->op != (nir_op)nir_intrinsic_reduction_op(intrin))
+         return false;
+
+      /* Don't reassociate exact float operations. */
+      if (nir_alu_type_get_base_type(nir_op_infos[alu->op].output_type) == nir_type_float &&
+          alu->op != nir_op_fmax && alu->op != nir_op_fmin && alu->exact)
+         return false;
+
+      if (alu->dest.dest.ssa.num_components != 1)
+         return false;
+
+      nir_alu_src *alu_src = list_entry(src, nir_alu_src, src);
+      unsigned src_index = alu_src - alu->src;
+
+      assert(src_index < 2 && nir_op_infos[alu->op].num_inputs == 2);
+
+      nir_ssa_scalar scan_scalar = nir_ssa_scalar_resolved(intrin->src[0].ssa, 0);
+      nir_ssa_scalar op_scalar = nir_ssa_scalar_resolved(alu->src[!src_index].src.ssa,
+                                                         alu->src[!src_index].swizzle[0]);
+
+      if (scan_scalar.def != op_scalar.def || scan_scalar.comp != op_scalar.comp)
+         return false;
+   }
+
+   /* Convert to inclusive scan. */
+   intrin->intrinsic = nir_intrinsic_inclusive_scan;
+
+   nir_foreach_use_including_if_safe(src, &intrin->dest.ssa) {
+      /* Remove alu. */
+      nir_alu_instr *alu = nir_instr_as_alu(src->parent_instr);
+      nir_ssa_def_rewrite_uses(&alu->dest.dest.ssa, &intrin->dest.ssa);
+      nir_instr_remove(&alu->instr);
+   }
+
+   return true;
+}
+
+static bool
 opt_intrinsics_intrin(nir_builder *b, nir_intrinsic_instr *intrin,
                       const struct nir_shader_compiler_options *options)
 {
@@ -282,7 +331,8 @@ opt_intrinsics_intrin(nir_builder *b, nir_intrinsic_instr *intrin,
       }
       return progress;
    }
-
+   case nir_intrinsic_exclusive_scan:
+      return try_opt_exclusive_scan_to_inclusive(intrin);
    default:
       return false;
    }

bminor/mesa-mesa

commit e9c886c331aaf0b68415afb9a647d5057d691ac6

nir/opt_intrinsic: fix inclusive scan rewrite with multiple uses
Modifying the iterated list is a footgun, so just create a new instruction.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13364
Fixes: 5c70a55bf3f ("nir/opt_intrinsics: optimize (exclusive_scan(op, a) op a) to inclusive scan")

Reviewed-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35577>

src/compiler/nir/nir_opt_intrinsics.c
@@ -260,10 +260,12 @@ opt_intrinsics_alu(nir_builder *b, nir_alu_instr *alu,
 }
 
 static bool
-try_opt_exclusive_scan_to_inclusive(nir_intrinsic_instr *intrin)
+try_opt_exclusive_scan_to_inclusive(nir_builder *b, nir_intrinsic_instr *intrin)
 {
    if (intrin->def.num_components != 1)
       return false;
+
+   nir_op reduction_op = nir_intrinsic_reduction_op(intrin);
 
    nir_foreach_use_including_if(src, &intrin->def) {
       if (nir_src_is_if(src) || nir_src_parent_instr(src)->type != nir_instr_type_alu)
@@ -271,7 +273,7 @@ try_opt_exclusive_scan_to_inclusive(nir_intrinsic_instr *intrin)
 
       nir_alu_instr *alu = nir_instr_as_alu(nir_src_parent_instr(src));
 
-      if (alu->op != (nir_op)nir_intrinsic_reduction_op(intrin))
+      if (alu->op != reduction_op)
          return false;
 
       /* Don't reassociate exact float operations. */
@@ -309,13 +311,15 @@ try_opt_exclusive_scan_to_inclusive(nir_intrinsic_instr *intrin)
    }
 
    /* Convert to inclusive scan. */
-   intrin->intrinsic = nir_intrinsic_inclusive_scan;
+   nir_def *incl_scan = nir_inclusive_scan(b, intrin->src[0].ssa, .reduction_op = reduction_op);
 
    nir_foreach_use_including_if_safe(src, &intrin->def) {
       /* Remove alu. */
       nir_alu_instr *alu = nir_instr_as_alu(nir_src_parent_instr(src));
-      nir_def_replace(&alu->def, &intrin->def);
+      nir_def_replace(&alu->def, incl_scan);
    }
+
+   nir_instr_remove(&intrin->instr);
 
    return true;
 }
@@ -374,7 +378,7 @@ opt_intrinsics_intrin(nir_builder *b, nir_intrinsic_instr *intrin,
       return progress;
    }
    case nir_intrinsic_exclusive_scan:
-      return try_opt_exclusive_scan_to_inclusive(intrin);
+      return try_opt_exclusive_scan_to_inclusive(b, intrin);
    default:
       return false;
    }