bminor/mesa-mesa

commit 8f557b84f662cfb1c1fb19faac1293f7131437f6

ci: crnm: fix hyperlink format
While preparing a console link, if there is not padding defined and the text
is not strictly a string, we can have an exception that is also finishing the
execution. So, we need to strictly cast the text variable.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13342

Signed-off-by: Sergi Blanch-Torne <sergi.blanch.torne@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35511>

bin/ci/ci_run_n_monitor.py
@@ -611,7 +611,7 @@ def __job_duration_record(dict_item: tuple) -> str:
 
 
 def link2print(url: str, text: str, text_pad: int = 0) -> str:
-    text_pad = len(text) if text_pad < 1 else text_pad
+    text_pad = len(str(text)) if text_pad < 1 else text_pad
     return f"{URL_START}{url}\a{text:{text_pad}}{URL_END}"
 
 

commit 14fedcfb8d4819de801ddb6451b4369a85e04067

ci_run_n_monitor: fix padding in links
8f557b84f662cfb1c1fb was done because `text` is sometimes just an int,
but the fix was only applies to the padding calculation.

Unfortunately, the padding direction is also different between strings
and integers, which means the behaviour is now incoherent.

Let's convert `text` to a string before we start doing anything so that
everything afterwards is coherent.

Fixes: 8f557b84f662cfb1c1fb ("ci: crnm: fix hyperlink format")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35587>

bin/ci/ci_run_n_monitor.py
@@ -611,7 +611,8 @@ def __job_duration_record(dict_item: tuple) -> str:
 
 
 def link2print(url: str, text: str, text_pad: int = 0) -> str:
-    text_pad = len(str(text)) if text_pad < 1 else text_pad
+    text = str(text)
+    text_pad = len(text) if text_pad < 1 else text_pad
     return f"{URL_START}{url}\a{text:{text_pad}}{URL_END}"
 
 