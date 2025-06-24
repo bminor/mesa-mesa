bminor/mesa-mesa

commit bba4785f60401ef99b7158fed0d3562a112d6f06

pan/format: Add render target support for R8G8B8/B8G8R8 formats
Add render target support to the existing R8G8B8/B8G8R8 formats
and add the B8G8R8_UNORM format.

Signed-off-by: Ryan Mckeever <ryan.mckeever@collabora.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34889>

src/panfrost/lib/pan_format.c
@@ -420,7 +420,8 @@ const struct pan_format GENX(pan_pipe_format)[PIPE_FORMAT_COUNT] = {
    FMT(R16_UNORM,               R16_UNORM,       R001, L, VTR_I),
    FMT(R8G8_UNORM,              RG8_UNORM,       RG01, L, VTR_I),
    FMT(R16G16_UNORM,            RG16_UNORM,      RG01, L, VTR_I),
-   FMT(R8G8B8_UNORM,            RGB8_UNORM,      RGB1, L, V____),
+   FMT(R8G8B8_UNORM,            RGB8_UNORM,      RGB1, L, VTR_I),
+   FMT(B8G8R8_UNORM,            RGB8_UNORM,      BGR1, L, VTR_I),
 
    /* 32-bit NORM is not texturable in v7 onwards. It's renderable
     * everywhere, but rendering without texturing is not useful.
@@ -483,8 +484,8 @@ const struct pan_format GENX(pan_pipe_format)[PIPE_FORMAT_COUNT] = {
    FMT(L8_SRGB,                 R8_UNORM,        RRR1, S, VTR_I),
    FMT(R8_SRGB,                 R8_UNORM,        R001, S, VTR_I),
    FMT(R8G8_SRGB,               RG8_UNORM,       RG01, S, VTR_I),
-   FMT(R8G8B8_SRGB,             RGB8_UNORM,      RGB1, S, V____),
-   FMT(B8G8R8_SRGB,             RGB8_UNORM,      BGR1, S, V____),
+   FMT(R8G8B8_SRGB,             RGB8_UNORM,      RGB1, S, VTR_I),
+   FMT(B8G8R8_SRGB,             RGB8_UNORM,      BGR1, S, VTR_I),
    FMT(R8G8B8A8_SRGB,           RGBA8_UNORM,     RGBA, S, VTR_I),
    FMT(A8B8G8R8_SRGB,           RGBA8_UNORM,     ABGR, S, VTR_I),
    FMT(X8B8G8R8_SRGB,           RGBA8_UNORM,     ABG1, S, VTR_I),

commit f3f67823c47b3b6a7f5f49fcb9ff9f26fc10ddc8

pan/format: Add an entry for BGR8 to the blendable table
Without it, BGR8 gets a NULL entry, and it trips out the GPU when it
gets passed a blend descriptor with RGB8 as a format to convert to.

Fixes: bba4785f6040 ("pan/format: Add render target support for R8G8B8/B8G8R8 formats")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35715>

src/panfrost/lib/pan_format.c
@@ -84,6 +84,7 @@ const struct pan_blendable_format
       BFMT_SRGB(R8, R8),
       BFMT_SRGB(R8G8, R8G8),
       BFMT_SRGB(R8G8B8, R8G8B8),
+      BFMT_SRGB(B8G8R8, R8G8B8),
 
       BFMT_SRGB(B8G8R8A8, R8G8B8A8),
       BFMT_SRGB(B8G8R8X8, R8G8B8A8),