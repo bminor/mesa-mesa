0d33739216c6d67e370e506029f859270c5e3f52
Yiwei Zhang <zzyiwei@chromium.org>
2025-06-27 00:08:56 -0700

    radv: use common ANB swapchain gralloc usage query
    The additional usage bits added for rgba8 and rgb565 are not needed as
    those are conditionally added based on the consumer side of the Android
    surface (e.g. display, encoder, etc).
    
    Acked-by: Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35785>

--- src/amd/vulkan/radv_android.c
+++ src/amd/vulkan/radv_android.c
@@ -14,12 +14,7 @@
 
 #if DETECT_OS_ANDROID
 #include <libsync.h>
-#include <hardware/gralloc.h>
 #include <vulkan/vk_android_native_buffer.h>
-
-#if ANDROID_API_LEVEL >= 26
-#include <hardware/gralloc1.h>
-#endif
 #endif /* DETECT_OS_ANDROID */
 
 #include "util/os_file.h"
@@ -29,15 +24,6 @@
 #include "vk_util.h"
 
 #if DETECT_OS_ANDROID
-
-/* If any bits in test_mask are set, then unset them and return true. */
-static inline bool
-unmask32(uint32_t *inout_mask, uint32_t test_mask)
-{
-   uint32_t orig_mask = *inout_mask;
-   *inout_mask &= ~test_mask;
-   return *inout_mask != orig_mask;
-}
 
 VkResult
 radv_image_from_gralloc(VkDevice device_h, const VkImageCreateInfo *base_info,
@@ -144,161 +130,6 @@ fail_create_image:
    return result;
 }
 
-VkResult
-radv_GetSwapchainGrallocUsageANDROID(VkDevice device_h, VkFormat format, VkImageUsageFlags imageUsage,
-                                     int *grallocUsage)
-{
-   VK_FROM_HANDLE(radv_device, device, device_h);
-   struct radv_physical_device *pdev = radv_device_physical(device);
-   VkPhysicalDevice pdev_h = radv_physical_device_to_handle(pdev);
-   VkResult result;
-
-   *grallocUsage = 0;
-
-   /* WARNING: Android Nougat's libvulkan.so hardcodes the VkImageUsageFlags
-    * returned to applications via VkSurfaceCapabilitiesKHR::supportedUsageFlags.
-    * The relevant code in libvulkan/swapchain.cpp contains this fun comment:
-    *
-    *     TODO(jessehall): I think these are right, but haven't thought hard
-    *     about it. Do we need to query the driver for support of any of
-    *     these?
-    *
-    * Any disagreement between this function and the hardcoded
-    * VkSurfaceCapabilitiesKHR:supportedUsageFlags causes tests
-    * dEQP-VK.wsi.android.swapchain.*.image_usage to fail.
-    */
-
-   const VkPhysicalDeviceImageFormatInfo2 image_format_info = {
-      .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_IMAGE_FORMAT_INFO_2,
-      .format = format,
-      .type = VK_IMAGE_TYPE_2D,
-      .tiling = VK_IMAGE_TILING_OPTIMAL,
-      .usage = imageUsage,
-   };
-
-   VkImageFormatProperties2 image_format_props = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_FORMAT_PROPERTIES_2,
-   };
-
-   /* Check that requested format and usage are supported. */
-   result = radv_GetPhysicalDeviceImageFormatProperties2(pdev_h, &image_format_info, &image_format_props);
-   if (result != VK_SUCCESS) {
-      return vk_errorf(device, result,
-                       "radv_GetPhysicalDeviceImageFormatProperties2 failed "
-                       "inside %s",
-                       __func__);
-   }
-
-   if (unmask32(&imageUsage, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT))
-      *grallocUsage |= GRALLOC_USAGE_HW_RENDER;
-
-   if (unmask32(&imageUsage, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT |
-                                VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT))
-      *grallocUsage |= GRALLOC_USAGE_HW_TEXTURE;
-
-   /* All VkImageUsageFlags not explicitly checked here are unsupported for
-    * gralloc swapchains.
-    */
-   if (imageUsage != 0) {
-      return vk_errorf(device, VK_ERROR_FORMAT_NOT_SUPPORTED,
-                       "unsupported VkImageUsageFlags(0x%x) for gralloc "
-                       "swapchain",
-                       imageUsage);
-   }
-
-   /*
-    * FINISHME: Advertise all display-supported formats. Mostly
-    * DRM_FORMAT_ARGB2101010 and DRM_FORMAT_ABGR2101010, but need to check
-    * what we need for 30-bit colors.
-    */
-   if (format == VK_FORMAT_B8G8R8A8_UNORM || format == VK_FORMAT_B5G6R5_UNORM_PACK16) {
-      *grallocUsage |= GRALLOC_USAGE_HW_FB | GRALLOC_USAGE_HW_COMPOSER | GRALLOC_USAGE_EXTERNAL_DISP;
-   }
-
-   if (*grallocUsage == 0)
-      return VK_ERROR_FORMAT_NOT_SUPPORTED;
-
-   return VK_SUCCESS;
-}
-
-VkResult
-radv_GetSwapchainGrallocUsage2ANDROID(VkDevice device_h, VkFormat format, VkImageUsageFlags imageUsage,
-                                      VkSwapchainImageUsageFlagsANDROID swapchainImageUsage,
-                                      uint64_t *grallocConsumerUsage, uint64_t *grallocProducerUsage)
-{
-   /* Before level 26 (Android 8.0/Oreo) the loader uses
-    * vkGetSwapchainGrallocUsageANDROID. */
-#if ANDROID_API_LEVEL >= 26
-   VK_FROM_HANDLE(radv_device, device, device_h);
-   struct radv_physical_device *pdev = radv_device_physical(device);
-   VkPhysicalDevice pdev_h = radv_physical_device_to_handle(pdev);
-   VkResult result;
-
-   *grallocConsumerUsage = 0;
-   *grallocProducerUsage = 0;
-
-   if (swapchainImageUsage & VK_SWAPCHAIN_IMAGE_USAGE_SHARED_BIT_ANDROID)
-      return vk_errorf(device, VK_ERROR_FORMAT_NOT_SUPPORTED,
-                       "The Vulkan loader tried to query shared presentable image support");
-
-   const VkPhysicalDeviceImageFormatInfo2 image_format_info = {
-      .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_IMAGE_FORMAT_INFO_2,
-      .format = format,
-      .type = VK_IMAGE_TYPE_2D,
-      .tiling = VK_IMAGE_TILING_OPTIMAL,
-      .usage = imageUsage,
-   };
-
-   VkImageFormatProperties2 image_format_props = {
-      .sType = VK_STRUCTURE_TYPE_IMAGE_FORMAT_PROPERTIES_2,
-   };
-
-   /* Check that requested format and usage are supported. */
-   result = radv_GetPhysicalDeviceImageFormatProperties2(pdev_h, &image_format_info, &image_format_props);
-   if (result != VK_SUCCESS) {
-      return vk_errorf(device, result,
-                       "radv_GetPhysicalDeviceImageFormatProperties2 failed "
-                       "inside %s",
-                       __func__);
-   }
-
-   if (unmask32(&imageUsage, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)) {
-      *grallocProducerUsage |= GRALLOC1_PRODUCER_USAGE_GPU_RENDER_TARGET;
-      *grallocConsumerUsage |= GRALLOC1_CONSUMER_USAGE_CLIENT_TARGET;
-   }
-
-   if (unmask32(&imageUsage, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT |
-                                VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT)) {
-      *grallocConsumerUsage |= GRALLOC1_CONSUMER_USAGE_GPU_TEXTURE;
-   }
-
-   if (imageUsage != 0) {
-      return vk_errorf(device, VK_ERROR_FORMAT_NOT_SUPPORTED,
-                       "unsupported VkImageUsageFlags(0x%x) for gralloc "
-                       "swapchain",
-                       imageUsage);
-   }
-
-   /*
-    * FINISHME: Advertise all display-supported formats. Mostly
-    * DRM_FORMAT_ARGB2101010 and DRM_FORMAT_ABGR2101010, but need to check
-    * what we need for 30-bit colors.
-    */
-   if (format == VK_FORMAT_B8G8R8A8_UNORM || format == VK_FORMAT_B5G6R5_UNORM_PACK16) {
-      *grallocProducerUsage |= GRALLOC1_PRODUCER_USAGE_GPU_RENDER_TARGET;
-      *grallocConsumerUsage |= GRALLOC1_CONSUMER_USAGE_HWCOMPOSER;
-   }
-
-   if (!*grallocProducerUsage && !*grallocConsumerUsage)
-      return VK_ERROR_FORMAT_NOT_SUPPORTED;
-
-   return VK_SUCCESS;
-#else
-   *grallocConsumerUsage = 0;
-   *grallocProducerUsage = 0;
-   return VK_ERROR_FORMAT_NOT_SUPPORTED;
-#endif
-}
 #endif /* DETECT_OS_ANDROID */
 
 #if RADV_SUPPORT_ANDROID_HARDWARE_BUFFER
