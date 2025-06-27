e9f23edf17aa1da5b30c5a103a1692911998d692
Caterina Shablia <caterina.shablia@collabora.com>
2025-06-27 15:08:39 +0000

    panvk: adjust formatting in csf/panvk_queue.h
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37288>

--- src/panfrost/vulkan/jm/panvk_queue.h
+++ src/panfrost/vulkan/jm/panvk_queue.h
@@ -28,8 +28,7 @@ VkResult panvk_per_arch(create_gpu_queue)(
    uint32_t queue_idx, struct vk_queue **out_queue);
 void panvk_per_arch(destroy_gpu_queue)(struct vk_queue *vk_queue);
 VkResult panvk_per_arch(gpu_queue_submit)(struct vk_queue *vk_queue,
-                                      struct vk_queue_submit *vk_submit);
-VkResult panvk_per_arch(gpu_queue_check_status)(
-   struct vk_queue *vk_queue);
+                                          struct vk_queue_submit *vk_submit);
+VkResult panvk_per_arch(gpu_queue_check_status)(struct vk_queue *vk_queue);
 
 #endif
