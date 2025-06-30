aerisarn/mesa-uwp

commit 6585209cdd938b24418b606b1c4de68753f89dc7

nir/lower_bit_size: mask bitz/bitnz src1 like shifts
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/23298>

src/compiler/nir/nir_lower_bit_size.c
@@ -64,7 +64,10 @@ lower_alu_instr(nir_builder *bld, nir_alu_instr *alu, unsigned bit_size)
       if (nir_alu_type_get_type_size(type) == 0)
          src = convert_to_bit_size(bld, src, type, bit_size);
 
-      if (i == 1 && (op == nir_op_ishl || op == nir_op_ishr || op == nir_op_ushr)) {
+      if (i == 1 && (op == nir_op_ishl || op == nir_op_ishr || op == nir_op_ushr ||
+                     op == nir_op_bitz || op == nir_op_bitz8 || op == nir_op_bitz16 ||
+                     op == nir_op_bitz32 || op == nir_op_bitnz || op == nir_op_bitnz8 ||
+                     op == nir_op_bitnz16 || op == nir_op_bitnz32)) {
          assert(util_is_power_of_two_nonzero(dst_bit_size));
          src = nir_iand(bld, src, nir_imm_int(bld, dst_bit_size - 1));
       }

bminor/mesa-mesa

commit 08859cbe50eb2a0bcf8b1a9d2e0cdb716bd670e6

nir/lower_bit_size: fix bitz/bitnz
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Fixes: 6585209cdd93 ("nir/lower_bit_size: mask bitz/bitnz src1 like shifts")
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35770>

src/compiler/nir/nir_lower_bit_size.c
@@ -69,8 +69,9 @@ lower_alu_instr(nir_builder *bld, nir_alu_instr *alu, unsigned bit_size)
                      op == nir_op_bitz || op == nir_op_bitz8 || op == nir_op_bitz16 ||
                      op == nir_op_bitz32 || op == nir_op_bitnz || op == nir_op_bitnz8 ||
                      op == nir_op_bitnz16 || op == nir_op_bitnz32)) {
-         assert(util_is_power_of_two_nonzero(dst_bit_size));
-         src = nir_iand(bld, src, nir_imm_int(bld, dst_bit_size - 1));
+         unsigned src0_bit_size = alu->src[0].src.ssa->bit_size;
+         assert(util_is_power_of_two_nonzero(src0_bit_size));
+         src = nir_iand(bld, src, nir_imm_int(bld, src0_bit_size - 1));
       }
 
       srcs[i] = src;