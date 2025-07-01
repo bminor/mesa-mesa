bminor/mesa-mesa

commit e103afe7bec5eb509bf11ea62148a3db61a9eeaa

brw: run the nir_opt_offsets pass and set the maximum offset size
Perf A/B testing on DG2: no changes
Perf A/B testing on BMG: +2.1% Blackops3, +1.5% Cyberpunk

DG2 stats (mostly insignificant):
Assassins Creed Valhalla:
 Totals from 1169 (55.67% of 2100) affected shaders:
 Instrs: 509237 -> 509215 (-0.00%)
 Cycle count: 30614325 -> 30607419 (-0.02%); split: -0.03%, +0.00%
 Non SSA regs after NIR: 83434 -> 85909 (+2.97%)

Blackops 3:
 Totals from 1045 (64.63% of 1617) affected shaders:
 Instrs: 527312 -> 527310 (-0.00%)
 Cycle count: 496912222 -> 496902846 (-0.00%); split: -0.00%, +0.00%
 Non SSA regs after NIR: 106883 -> 109095 (+2.07%)

Cyberpunk:
 Totals from 706 (56.03% of 1260) affected shaders:
 Instrs: 345976 -> 345974 (-0.00%); split: -0.00%, +0.00%
 Cycle count: 9775138 -> 9775472 (+0.00%); split: -0.00%, +0.00%
 Max live registers: 40295 -> 40297 (+0.00%)
 Non SSA regs after NIR: 93245 -> 94718 (+1.58%)

Fortnite:
 Totals from 4210 (55.98% of 7521) affected shaders:
 Instrs: 2205471 -> 2205469 (-0.00%)
 Cycle count: 91451040 -> 91450956 (-0.00%); split: -0.00%, +0.00%
 Non SSA regs after NIR: 952354 -> 961664 (+0.98%)

LNL stats (notable changes):
Assassins Creed Valhalla:
 Totals from 1684 (83.57% of 2015) affected shaders:
 Instrs: 774305 -> 764501 (-1.27%); split: -1.27%, +0.01%
 Cycle count: 58845842 -> 58699250 (-0.25%); split: -0.98%, +0.73%
 Spill count: 625 -> 638 (+2.08%)
 Fill count: 1490 -> 1503 (+0.87%)
 Scratch Memory Size: 41984 -> 44032 (+4.88%)
 Max live registers: 196424 -> 197561 (+0.58%); split: -0.10%, +0.68%

Blackops 3:
 Totals from 1125 (76.53% of 1470) affected shaders:
 Instrs: 781749 -> 773275 (-1.08%); split: -1.08%, +0.00%
 Subgroup size: 22896 -> 22912 (+0.07%)
 Cycle count: 659864454 -> 654641032 (-0.79%); split: -1.10%, +0.31%
 Max live registers: 116772 -> 116854 (+0.07%); split: -0.01%, +0.08%
 Non SSA regs after NIR: 172648 -> 168260 (-2.54%); split: -2.55%, +0.01%

Control:

src/intel/compiler/brw_nir_lower_immediate_offsets.c
@@ -1,0 +1,78 @@
+/*
+ * Copyright (c) 2025 Intel Corporation
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "compiler/nir/nir_builder.h"
+#include "brw_eu.h"
+#include "brw_nir.h"
+
+static bool
+lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
+{
+   unsigned max_bits = 0;
+
+   switch (intrin->intrinsic) {
+   case nir_intrinsic_load_shared:
+   case nir_intrinsic_store_shared:
+   case nir_intrinsic_shared_atomic:
+   case nir_intrinsic_shared_atomic_swap:
+   case nir_intrinsic_load_shared_block_intel:
+   case nir_intrinsic_store_shared_block_intel:
+   case nir_intrinsic_load_shared_uniform_block_intel:
+      max_bits = LSC_ADDRESS_OFFSET_FLAT_BITS;
+      break;
+   case nir_intrinsic_load_ssbo_intel:
+   case nir_intrinsic_load_ubo_uniform_block_intel:
+   case nir_intrinsic_load_ssbo_uniform_block_intel:
+   case nir_intrinsic_store_ssbo_intel:
+   case nir_intrinsic_store_ssbo_block_intel: {
+      nir_src *binding = nir_get_io_index_src(intrin);
+      const bool has_resource =
+         binding->ssa->parent_instr->type == nir_instr_type_intrinsic &&
+         nir_instr_as_intrinsic(binding->ssa->parent_instr)->intrinsic ==
+         nir_intrinsic_resource_intel;
+      bool ss_binding = false;
+      if (has_resource) {
+         nir_intrinsic_instr *resource =
+            nir_instr_as_intrinsic(binding->ssa->parent_instr);
+         ss_binding = (nir_intrinsic_resource_access_intel(resource) &
+                       nir_resource_intel_bindless) != 0;
+      }
+      max_bits = ss_binding ?
+         LSC_ADDRESS_OFFSET_SS_BITS : LSC_ADDRESS_OFFSET_BTI_BITS;
+      break;
+   }
+   default:
+      return false;
+   }
+
+   assert(nir_intrinsic_has_base(intrin));
+
+   b->cursor = nir_before_instr(&intrin->instr);
+
+   const int32_t min = u_intN_min(max_bits);
+   const int32_t max = u_intN_max(max_bits);
+
+   const int32_t base = nir_intrinsic_base(intrin);
+   if ((base % 4) == 0 && base >= min && base <= max)
+      return false;
+
+   int32_t new_base = CLAMP(base, min, max);
+   new_base -= new_base % 4;
+
+   assert(new_base >= min && new_base <= max);
+
+   nir_src *offset_src = nir_get_io_offset_src(intrin);
+   nir_src_rewrite(offset_src, nir_iadd_imm(b, offset_src->ssa, base - new_base));
+   nir_intrinsic_set_base(intrin, new_base);
+
+   return true;
+}
+
+bool
+brw_nir_lower_immediate_offsets(nir_shader *shader)
+{
+   return nir_shader_intrinsics_pass(shader, lower_immediate_offsets,
+                                     nir_metadata_control_flow, NULL);
+}

src/intel/compiler/brw_nir.c
@@ -1756,6 +1756,58 @@ get_mem_access_size_align(nir_intrinsic_op intrin, uint8_t bytes,
    }
 }
 
+static bool
+brw_nir_ssbo_intel_instr(nir_builder *b,
+                         nir_intrinsic_instr *intrin,
+                         void *cb_data)
+{
+   switch (intrin->intrinsic) {
+   case nir_intrinsic_load_ssbo: {
+      b->cursor = nir_before_instr(&intrin->instr);
+      nir_def *value = nir_load_ssbo_intel(
+         b,
+         intrin->def.num_components,
+         intrin->def.bit_size,
+         intrin->src[0].ssa,
+         intrin->src[1].ssa,
+         .access = nir_intrinsic_access(intrin),
+         .align_mul = nir_intrinsic_align_mul(intrin),
+         .align_offset = nir_intrinsic_align_offset(intrin),
+         .base = 0);
+      value->loop_invariant = intrin->def.loop_invariant;
+      value->divergent = intrin->def.divergent;
+      nir_def_replace(&intrin->def, value);
+      return true;
+   }
+
+   case nir_intrinsic_store_ssbo: {
+      b->cursor = nir_instr_remove(&intrin->instr);
+      nir_store_ssbo_intel(
+         b,
+         intrin->src[0].ssa,
+         intrin->src[1].ssa,
+         intrin->src[2].ssa,
+         .access = nir_intrinsic_access(intrin),
+         .align_mul = nir_intrinsic_align_mul(intrin),
+         .align_offset = nir_intrinsic_align_offset(intrin),
+         .base = 0);
+      return true;
+   }
+
+   default:
+      return false;
+   }
+}
+
+static bool
+brw_nir_ssbo_intel(nir_shader *shader)
+{
+   return nir_shader_intrinsics_pass(shader,
+                                     brw_nir_ssbo_intel_instr,
+                                     nir_metadata_control_flow,
+                                     NULL);
+}
+
 static void
 brw_vectorize_lower_mem_access(nir_shader *nir,
                                const struct brw_compiler *compiler,
@@ -1808,7 +1860,6 @@ brw_vectorize_lower_mem_access(nir_shader *nir,
       }
    }
 
-
    struct brw_mem_access_cb_data cb_data = {
       .devinfo = compiler->devinfo,
    };
@@ -1835,6 +1886,23 @@ brw_vectorize_lower_mem_access(nir_shader *nir,
       OPT(nir_opt_cse);
       OPT(nir_opt_algebraic);
       OPT(nir_opt_constant_folding);
+   }
+
+   /* Do this after the vectorization & brw_nir_rebase_const_offset_ubo_loads
+    * so that we maximize the offset put into the messages.
+    */
+   if (compiler->devinfo->ver >= 20) {
+      OPT(brw_nir_ssbo_intel);
+
+      const nir_opt_offsets_options offset_options = {
+         .buffer_max        = UINT32_MAX,
+         .shared_max        = UINT32_MAX,
+         .shared_atomic_max = UINT32_MAX,
+         .uniform_max       = UINT32_MAX,
+      };
+      OPT(nir_opt_offsets, &offset_options);
+
+      OPT(brw_nir_lower_immediate_offsets);
    }
 }
 

src/intel/compiler/brw_nir.h
@@ -215,6 +215,8 @@ bool brw_nir_lower_texture(nir_shader *nir,
 
 bool brw_nir_lower_sample_index_in_coord(nir_shader *nir);
 
+bool brw_nir_lower_immediate_offsets(nir_shader *shader);
+
 bool brw_nir_lower_mem_access_bit_sizes(nir_shader *shader,
                                         const struct
                                         intel_device_info *devinfo);

src/intel/compiler/meson.build
@@ -73,6 +73,7 @@ libintel_compiler_brw_files = files(
   'brw_nir_lower_alpha_to_coverage.c',
   'brw_nir_lower_fs_barycentrics.c',
   'brw_nir_lower_fs_msaa.c',
+  'brw_nir_lower_immediate_offsets.c',
   'brw_nir_lower_intersection_shader.c',
   'brw_nir_lower_ray_queries.c',
   'brw_nir_lower_rt_intrinsics.c',

commit 343f3dd3c1b97473233fa7f2aa2ca5df2c0b0c90

brw: fix non constant BTI accesses with offsets
Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: e103afe7be ("brw: run the nir_opt_offsets pass and set the maximum offset size")
Reviewed-by: Ian Romanick <ian.d.romanick@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35822>

src/intel/compiler/brw_eu_emit.c
@@ -1530,7 +1530,8 @@ brw_send_indirect_split_message(struct brw_codegen *p,
       if (ex_desc_imm_inst) {
          /* Write the immediate extended descriptor immediate value, but only
           * the part used for encoding an offset. This matches to bits
-          * 12:15-19:31 as described in BSpec 70586.
+          * 12:15-19:31 as described in BSpec 70586 (extended descriptor
+          * format) & BSpec 56890 (SEND instruction format).
           */
          assert(devinfo->ver >= 20);
          brw_eu_inst_set_bits(send, 127, 124, GET_BITS(ex_desc_imm_inst, 31, 28));

src/intel/compiler/brw_lower_logical_sends.cpp
@@ -1455,10 +1455,9 @@ setup_lsc_surface_descriptors(const brw_builder &bld, brw_inst *inst,
          inst->src[1] = brw_imm_ud(lsc_bti_ex_desc(devinfo, surface.ud,
                                                    base_offset_bits));
       } else {
+         assert(base_offset == 0);
          const brw_builder ubld = bld.uniform();
-         brw_reg tmp = ubld.OR(
-            ubld.SHL(surface, brw_imm_ud(24)),
-            brw_imm_ud(base_offset << 12));
+         brw_reg tmp = ubld.SHL(surface, brw_imm_ud(24));
          inst->src[1] = component(tmp, 0);
       }
       break;

src/intel/compiler/brw_nir_lower_immediate_offsets.c
@@ -33,14 +33,38 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
          nir_instr_as_intrinsic(binding->ssa->parent_instr)->intrinsic ==
          nir_intrinsic_resource_intel;
       bool ss_binding = false;
+      bool bti_is_const;
       if (has_resource) {
          nir_intrinsic_instr *resource =
             nir_instr_as_intrinsic(binding->ssa->parent_instr);
          ss_binding = (nir_intrinsic_resource_access_intel(resource) &
                        nir_resource_intel_bindless) != 0;
+         bti_is_const = nir_src_is_const(resource->src[1]);
+      } else {
+         bti_is_const = nir_src_is_const(*nir_get_io_index_src(intrin));
       }
-      max_bits = ss_binding ?
-         LSC_ADDRESS_OFFSET_SS_BITS : LSC_ADDRESS_OFFSET_BTI_BITS;
+      /* The BTI index and the base offset got into the extended descriptor
+       * (see BSpec 63997 for the format).
+       *
+       * When the BTI index constant, the extended descriptor is encoded into
+       * the SEND instruction (no need to use the address register, see BSpec
+       * 56890). This is referred to as the extended descriptor immediate.
+       *
+       * When BTI is not a constant, the extended descriptor is put into the
+       * address register but only the BTI index part of it. The base offset
+       * needs to go in the SEND instruction (see programming note on BSpec
+       * 63997).
+       *
+       * When the extended descriptor is coming from the address register,
+       * some of the bits in the SEND instruction cannot be used for the
+       * immediate extended descriptor part and that includes bits you would
+       * want to use for the base offset... Slow clap to the HW design here.
+       *
+       * So put set max bits to 0 in that case and set the base offset to 0
+       * since it's unusable.
+       */
+      max_bits = ss_binding ? LSC_ADDRESS_OFFSET_SS_BITS :
+         bti_is_const ? LSC_ADDRESS_OFFSET_BTI_BITS : 0;
       break;
    }
    default:
@@ -49,7 +73,21 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 
    assert(nir_intrinsic_has_base(intrin));
 
+   if (nir_intrinsic_base(intrin) == 0)
+      return false;
+
    b->cursor = nir_before_instr(&intrin->instr);
+
+   nir_src *offset_src = nir_get_io_offset_src(intrin);
+
+   if (max_bits == 0) {
+      nir_src_rewrite(
+         offset_src,
+         nir_iadd_imm(
+            b, offset_src->ssa, nir_intrinsic_base(intrin)));
+      nir_intrinsic_set_base(intrin, 0);
+      return true;
+   }
 
    const int32_t min = u_intN_min(max_bits);
    const int32_t max = u_intN_max(max_bits);
@@ -63,8 +101,9 @@ lower_immediate_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 
    assert(new_base >= min && new_base <= max);
 
-   nir_src *offset_src = nir_get_io_offset_src(intrin);
-   nir_src_rewrite(offset_src, nir_iadd_imm(b, offset_src->ssa, base - new_base));
+   nir_src_rewrite(
+      offset_src, nir_iadd_imm(
+         b, offset_src->ssa, base - new_base));
    nir_intrinsic_set_base(intrin, new_base);
 
    return true;