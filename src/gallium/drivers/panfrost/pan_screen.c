bminor/mesa-mesa

commit 423f3fd485012c903ed32f1af1fa6b942f58c8ca

panfrost: enable 8x and 16x msaa modes when supported
8x and 16x MSAA should work now, so we no longer need to deal with those
separately. We have more accurate checks for when it's supported or not
in place already.

2x MSAA isn't supported at all until v12, which we haven't finished
wiring up yet. So let's leave that one for now... We'll need some more
fixes for that anyway.

Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34455>

src/gallium/drivers/panfrost/pan_screen.c
@@ -70,7 +70,6 @@ static const struct debug_named_value panfrost_debug_options[] = {
    {"gl3",        PAN_DBG_GL3,        "Enable experimental GL 3.x implementation, up to 3.3"},
    {"noafbc",     PAN_DBG_NO_AFBC,    "Disable AFBC support"},
    {"nocrc",      PAN_DBG_NO_CRC,     "Disable transaction elimination"},
-   {"msaa16",     PAN_DBG_MSAA16,     "Enable MSAA 8x and 16x support"},
    {"linear",     PAN_DBG_LINEAR,     "Force linear textures"},
    {"strict_import", PAN_DBG_STRICT_IMPORT, "Use the explicit WSI stride and fail if it's not properly aligned"},
    {"nocache",    PAN_DBG_NO_CACHE,   "Disable BO cache"},
@@ -177,28 +176,8 @@ panfrost_is_format_supported(struct pipe_screen *screen,
        MAX2(sample_count, 1) > max_msaa)
       return false;
 
-   /* MSAA 2x gets rounded up to 4x. MSAA 8x/16x only supported on v5+.
-    * TODO: debug MSAA 8x/16x */
-
-   switch (sample_count) {
-   case 0:
-   case 1:
-   case 4:
-      break;
-   case 2:
-      if (dev->arch >= 12)
-         break;
-      else
-         return false;
-   case 8:
-   case 16:
-      if (dev->debug & PAN_DBG_MSAA16)
-         break;
-      else
-         return false;
-   default:
+   if (sample_count == 2 && dev->arch < 12)
       return false;
-   }
 
    if (MAX2(sample_count, 1) != MAX2(storage_sample_count, 1))
       return false;

src/panfrost/lib/pan_util.h
@@ -42,7 +42,7 @@
 #define PAN_DBG_NO_CRC  0x0080
 #define PAN_DBG_GL3     0x0100
 #define PAN_DBG_NO_AFBC 0x0200
-#define PAN_DBG_MSAA16  0x0400
+/* 0x400 unused */
 #define PAN_DBG_STRICT_IMPORT 0x0800
 #define PAN_DBG_LINEAR   0x1000
 #define PAN_DBG_NO_CACHE 0x2000

commit e2a2663638669e56960b89ba438cec590b70bcda

panfrost: disable 16x MSAA on t760 before r1p0
Versions prior to the r1p0 revision of Mali-T760 has a bug that prevents
16x MSAA from working properly. Let's detect this GPU, and disable the
functionality for it.

It's a bit awkward that we can't easily do this as part of the pan_model
infrastructure, but we'd otherwise have to introduce a new quirk that
depends on the GPU revision, which isn't something the current pan_model
infrastructure is prepared for. So let's just do it in get_max_msaa()
instead.

And while we're at it, let's also make the max_4x_msaa quirk do the
MIN2() properly. This can currently only happen on V4 GPUs when the
pan_allow_128bit_rts_v4 driconf is enabled, but it should be fixed in
either case.

Fixes: 423f3fd4850 ("panfrost: enable 8x and 16x msaa modes when supported")
Reviewed-by: Daniel Stone <daniels@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35871>

src/gallium/drivers/panfrost/pan_screen.c
@@ -151,8 +151,15 @@ get_max_msaa(struct panfrost_device *dev, enum pipe_format format)
                                         max_cbuf_atts, format_size);
    assert(format_size > 16 || max_msaa >= 4);
 
+   /* t760 (GPU ID 0x750 - not a typo) has a HW issue in versions before
+    * the r1p0 version, which prevents 16x MSAA from working properly.
+    */
+   if (panfrost_device_gpu_id(dev) == 0x750 &&
+       panfrost_device_gpu_rev(dev) < 0x1000)
+      max_msaa = MIN2(max_msaa, 8);
+
    if (dev->model->quirks.max_4x_msaa)
-      max_msaa = 4;
+      max_msaa = MIN2(max_msaa, 4);
 
    return max_msaa;
 }