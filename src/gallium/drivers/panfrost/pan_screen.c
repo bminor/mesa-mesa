Grima04/mesa-turnip-kgsl

commit 6bba718027e1a720f2531573b6be5a89a7722278

panfrost: Advertise SAMPLE_SHADING
Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8774>

src/gallium/drivers/panfrost/pan_screen.c
@@ -114,6 +114,7 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
         case PIPE_CAP_MIXED_COLORBUFFER_FORMATS:
         case PIPE_CAP_MIXED_FRAMEBUFFER_SIZES:
         case PIPE_CAP_FRONTEND_NOOP:
+        case PIPE_CAP_SAMPLE_SHADING:
                 return 1;
 
         case PIPE_CAP_MAX_RENDER_TARGETS:
@@ -123,10 +124,6 @@ panfrost_get_param(struct pipe_screen *screen, enum pipe_cap param)
 
         case PIPE_CAP_MAX_DUAL_SOURCE_RENDER_TARGETS:
                 return 1;
-
-        case PIPE_CAP_SAMPLE_SHADING:
-                /* WIP */
-                return is_gl3 ? 1 : 0;
 
         case PIPE_CAP_OCCLUSION_QUERY:
         case PIPE_CAP_PRIMITIVE_RESTART:

bminor/mesa-mesa

commit 504e511c4466743d89259488f467b91745087681

panfrost: limit sample_shading to bifrost and later
We haven't wired this up in the Midgard compiler, so we can't expose
sample shading on Midgard GPUs. This all seems fixable, because the KILL
instruction can update the coverage without the kill-flag (yeah, a bit
confusing naming), but until someone puts in the time to wire up that,
let's just disable the functionality to avoid crashes.

Fixes: 6bba718027e ("panfrost: Advertise SAMPLE_SHADING")
Reviewed-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35881>

src/gallium/drivers/panfrost/pan_screen.c
@@ -639,7 +639,7 @@ panfrost_init_screen_caps(struct panfrost_screen *screen)
    caps->depth_clip_disable = true;
    caps->mixed_framebuffer_sizes = true;
    caps->frontend_noop = true;
-   caps->sample_shading = true;
+   caps->sample_shading = dev->arch >= 6;
    caps->fragment_shader_derivatives = true;
    caps->framebuffer_no_attachment = true;
    caps->quads_follow_provoking_vertex_convention = true;