8884032394d03c2947c6f23be22fe5ca18bd74ca
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2025-07-04 10:03:14 -0400

    zink: fix acquire semaphore sync
    the semaphore stage is VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
    so the src access barrier must also use this in order to ensure it happens
    after the acquire
    
    cc: mesa-stable
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35940>
    (cherry picked from commit 69b5abee148522e7792b85ec58f50660272e6317)

--- src/gallium/drivers/zink/zink_kopper.c
+++ src/gallium/drivers/zink/zink_kopper.c
@@ -616,6 +616,14 @@ kopper_acquire(struct zink_screen *screen, struct zink_resource *res, uint64_t t
       res->layout = VK_IMAGE_LAYOUT_UNDEFINED;
       cdt->swapchain->images[res->obj->dt_idx].init = true;
    }
+   res->obj->unordered_read = true;
+   res->obj->unordered_write = true;
+   res->obj->access = 0;
+   res->obj->unordered_access = 0;
+   /* this is the stage used by the acquire semaphore */
+   res->obj->unordered_access_stage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+   res->obj->access_stage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+   res->obj->ordered_access_is_copied = true;
    if (timeout == UINT64_MAX) {
       res->obj->indefinite_acquire = true;
       p_atomic_inc(&cdt->swapchain->num_acquires);
