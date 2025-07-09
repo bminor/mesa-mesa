5423643b87a9144b1de89647aafa93bee6ab0b63
Juston Li <justonli@google.com>
2025-07-09 16:07:11 -0700

    anv/android: refactor anb resolve to fix align assertion
    Retrieving memory requirement size and alignment via
    anv_image_get_memory_requirements() return's 0 before surfaces are added
    by resolve_anb_image() and will assert in align64() when align is 0:
    
    Abort message: '../src/util/u_math.h:713: uint64_t align64(uint64_t, uint64_t): assertion "util_is_power_of_two_nonzero64(alignment)" failed'
    
    Refactor out anv_image_bind_from_gralloc() into resolve_anb_image() so
    the checks are performed after the surface is adds.
    
    Resolving also requires API 29 so return VK_ERROR_EXTENSION_NOT_PRESENT
    without it.
    
    Fixes: 43cb986d9e7 ("anv/android: resolve ANB swapchain images on bind")
    Signed-off-by: Juston Li <justonli@google.com>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Acked-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36060>
    (cherry picked from commit e1ca09317e100a96c4a8fd1c1be19c3fe2696989)

--- src/intel/vulkan/anv_android.h
+++ src/intel/vulkan/anv_android.h
@@ -49,10 +49,6 @@ VkResult anv_image_init_from_gralloc(struct anv_device *device,
                                      const VkImageCreateInfo *base_info,
                                      const VkNativeBufferANDROID *gralloc_info);
 
-VkResult anv_image_bind_from_gralloc(struct anv_device *device,
-                                     struct anv_image *image,
-                                     const VkNativeBufferANDROID *gralloc_info);
-
 unsigned anv_ahb_format_for_vk_format(VkFormat vk_format);
 
 VkResult anv_import_ahw_memory(VkDevice device_h,
