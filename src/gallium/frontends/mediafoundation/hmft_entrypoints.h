6d1cb645d87c22deb189ea117e8c6c72af6ee12d
Pohsiang (John) Hsu <pohhsu@microsoft.com>
2025-07-09 13:35:20 -0700

    mediafoundation: fix build after updating sdk to 26100.4188
    Reviewed-by: Sil Vilerino <sivileri@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36082>

--- src/gallium/frontends/mediafoundation/hmft_entrypoints.h
+++ src/gallium/frontends/mediafoundation/hmft_entrypoints.h
@@ -267,6 +267,19 @@ DEFINE_CODECAPI_GUID( AVEncVideoEnableFramePsnrYuv,
                       0xf5,
                       0xf0 )
 #define CODECAPI_AVEncVideoEnableFramePsnrYuv DEFINE_CODECAPI_GUIDNAMED( AVEncVideoEnableFramePsnrYuv )
+
+typedef struct _MFSampleExtensionPsnrYuv
+{
+   FLOAT psnrY;   // PSNR for Y plane
+   FLOAT psnrU;   // PSNR for U plane
+   FLOAT psnrV;   // PSNR for V plane
+} MFSampleExtensionPsnrYuv;
+
+// MFSampleExtension_FramePsnrYuv {1C633A3D-566F-4752-833B-2907DF5415E1}
+// Type: IMFMediaBuffer
+// A MFSampleExtensionPsnrYuv structure that specifies the PSNR data of YUV planes of an encoded video frame.
+DEFINE_GUID( MFSampleExtension_FramePsnrYuv, 0x1c633a3d, 0x566f, 0x4752, 0x83, 0x3b, 0x29, 0x07, 0xdf, 0x54, 0x15, 0xe1 );
+
 #endif
 
 #ifndef CODECAPI_AVEncVideoEnableSpatialAdaptiveQuantization
@@ -309,6 +322,12 @@ DEFINE_CODECAPI_GUID( AVEncVideoOutputQPMapBlockSize,
                       0x34,
                       0xef )
 #define CODECAPI_AVEncVideoOutputQPMapBlockSize DEFINE_CODECAPI_GUIDNAMED( AVEncVideoOutputQPMapBlockSize )
+
+// MFSampleExtension_VideoEncodeQPMap {2C68A331-B712-49CA-860A-3A1D58237D88}
+// Type: IMFMediaBuffer
+// The QP map of an encoded video frame.
+DEFINE_GUID( MFSampleExtension_VideoEncodeQPMap, 0x2c68a331, 0xb712, 0x49ca, 0x86, 0x0a, 0x3a, 0x1d, 0x58, 0x23, 0x7d, 0x88 );
+
 #endif
 
 #ifndef CODECAPI_AVEncVideoOutputBitsUsedMapBlockSize
@@ -330,34 +349,12 @@ DEFINE_CODECAPI_GUID( AVEncVideoOutputBitsUsedMapBlockSize,
                       0xc3,
                       0x6e )
 #define CODECAPI_AVEncVideoOutputBitsUsedMapBlockSize DEFINE_CODECAPI_GUIDNAMED( AVEncVideoOutputBitsUsedMapBlockSize )
-#endif
 
-#ifndef MFSampleExtension_FramePsnrYuv
-typedef struct _MFSampleExtensionPsnrYuv
-{
-   FLOAT psnrY;   // PSNR for Y plane
-   FLOAT psnrU;   // PSNR for U plane
-   FLOAT psnrV;   // PSNR for V plane
-} MFSampleExtensionPsnrYuv;
-
-// MFSampleExtension_FramePsnrYuv {1C633A3D-566F-4752-833B-2907DF5415E1}
-// Type: IMFMediaBuffer
-// A MFSampleExtensionPsnrYuv structure that specifies the PSNR data of YUV planes of an encoded video frame.
-DEFINE_GUID( MFSampleExtension_FramePsnrYuv, 0x1c633a3d, 0x566f, 0x4752, 0x83, 0x3b, 0x29, 0x07, 0xdf, 0x54, 0x15, 0xe1 );
-#endif
-
-#ifndef MFSampleExtension_VideoEncodeQPMap
-// MFSampleExtension_VideoEncodeQPMap {2C68A331-B712-49CA-860A-3A1D58237D88}
-// Type: IMFMediaBuffer
-// The QP map of an encoded video frame.
-DEFINE_GUID( MFSampleExtension_VideoEncodeQPMap, 0x2c68a331, 0xb712, 0x49ca, 0x86, 0x0a, 0x3a, 0x1d, 0x58, 0x23, 0x7d, 0x88 );
-#endif
-
-#ifndef MFSampleExtension_VideoEncodeBitsUsedMap
 // MFSampleExtension_VideoEncodeBitsUsedMap {6894263D-E6E2-4BCC-849D-8570365F5114}
 // Type: IMFMediaBuffer
 // The bits used map of an encoded video frame.
 DEFINE_GUID( MFSampleExtension_VideoEncodeBitsUsedMap, 0x6894263d, 0xe6e2, 0x4bcc, 0x84, 0x9d, 0x85, 0x70, 0x36, 0x5f, 0x51, 0x14 );
+
 #endif
 
 #if MFT_CODEC_H264ENC
