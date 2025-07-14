bminor/mesa-mesa

commit 85b6bd989ec501975259e42af2927caa16d8707b

panvk: Advertise support for VK_KHR_maintenance5
This is already supported, all we have to do is advertise it.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34648>

src/panfrost/vulkan/panvk_physical_device.c
@@ -242,6 +242,7 @@ get_device_extensions(const struct panvk_physical_device *device,
       .KHR_maintenance2 = true,
       .KHR_maintenance3 = true,
       .KHR_maintenance4 = has_vk1_1,
+      .KHR_maintenance5 = has_vk1_1,
       .KHR_map_memory2 = true,
       .KHR_multiview = arch >= 10,
       .KHR_pipeline_executable_properties = true,
@@ -472,6 +473,7 @@ get_features(const struct panvk_physical_device *device,
       .dynamicRenderingLocalRead = true,
       .shaderIntegerDotProduct = false,
       .maintenance4 = true,
+      .maintenance5 = true,
 
       /* Vulkan 1.4 */
       .shaderSubgroupRotate = true,

commit b530692529cc8c0d9c12c9a1f8dabc849d91b86c

panvk: Allow invalid enum values in get_image_format_properties
This is allowed with VK_KHR_maintenance5 which we support for v10+.

Fixes: 85b6bd989ec ("panvk: Advertise support for VK_KHR_maintenance5")

Reviewed-by: Olivia Lee <olivia.lee@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36065>

src/panfrost/vulkan/panvk_physical_device.c
@@ -865,7 +865,10 @@ get_image_format_properties(struct panvk_physical_device *physical_device,
       break;
    }
    default:
-      unreachable("bad VkPhysicalDeviceImageFormatInfo2");
+      /* VK_KHR_maintenance5: Physical-device-level functions can now be called
+       * with any value in the valid range for a type beyond the defined
+       * enumerants [...] */
+      goto unsupported;
    }
 
    /* For the purposes of these checks, we don't care about all the extra
@@ -892,8 +895,6 @@ get_image_format_properties(struct panvk_physical_device *physical_device,
       goto unsupported;
 
    switch (info->type) {
-   default:
-      unreachable("bad vkimage type");
    case VK_IMAGE_TYPE_1D:
       maxExtent.width = 1 << 16;
       maxExtent.height = 1;
@@ -911,6 +912,11 @@ get_image_format_properties(struct panvk_physical_device *physical_device,
       maxMipLevels = util_logbase2(maxExtent.width) + 1;
       maxArraySize = 1;
       break;
+   default:
+      /* VK_KHR_maintenance5: Physical-device-level functions can now be called
+       * with any value in the valid range for a type beyond the defined
+       * enumerants [...] */
+      goto unsupported;
    }
 
    if (ycbcr_info)