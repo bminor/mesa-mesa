0178a4bd9db1ac264541e108ed7ee9282140a9ef
Erik Faye-Lund <erik.faye-lund@collabora.com>
2025-07-16 08:37:56 +0200

    Revert "lima: make fp16 render-targets opt-in with driconf"
    This is no longer needed, due to the previous commit.
    
    This reverts commit 1617778c3898499d44b41057d30eb8d7b8616f37.
    
    Backport-to: 25.1
    Reviewed-by: Vasily Khoruzhick <anarsoul@gmail.com>
    Acked-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36156>

--- src/gallium/drivers/lima/driinfo_lima.h
+++ src/gallium/drivers/lima/driinfo_lima.h
@@ -1,4 +1,1 @@
 /* lima specific driconf options */
-DRI_CONF_SECTION_MISCELLANEOUS
-   DRI_CONF_OPT_B(lima_allow_fp16_rts, false, "Allow using FP16 render-targets")
-DRI_CONF_SECTION_END
--- src/gallium/drivers/lima/lima_screen.c
+++ src/gallium/drivers/lima/lima_screen.c
@@ -246,14 +246,9 @@ lima_screen_is_format_supported(struct pipe_screen *pscreen,
       if (!lima_format_pixel_supported(format))
          return false;
 
-      if (util_format_is_float(format)) {
-         if (!lima_screen(pscreen)->allow_fp16_rts)
-            return false;
-
-         /* multisample unsupported with half float target */
-         if (sample_count > 1)
-            return false;
-      }
+      /* multisample unsupported with half float target */
+      if (sample_count > 1 && util_format_is_float(format))
+         return false;
    }
 
    if (usage & PIPE_BIND_DEPTH_STENCIL) {
@@ -585,9 +580,6 @@ lima_screen_create(int fd, const struct pipe_screen_config *config,
 
    driParseConfigFiles(config->options, config->options_info, 0,
                        "lima", NULL, NULL, NULL, 0, NULL, 0);
-
-   screen->allow_fp16_rts = driQueryOptionb(config->options,
-                                            "lima_allow_fp16_rts");
 
    if (!lima_screen_query_info(screen))
       goto err_out0;
