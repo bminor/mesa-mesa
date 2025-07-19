d02d5fa695429bc34ff8493636454b8e0a95ac20
Eric Engestrom <eric@igalia.com>
2025-07-19 14:02:40 +0200

    docs/linkcheck: ignore sourceforge subdomains as well
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36240>

--- docs/conf.py
+++ docs/conf.py
@@ -141,6 +141,7 @@ linkcheck_ignore = [
     r'https://github.com/.*#.*',  # needs JS eval
     r'https://www.intel.com/.*',  # intel.com is blocking the linkcheck user-agent; maybe it can be customized to look like a browser?
     r'https://sourceforge.net/.*',  # blocking the linkcheck user-agent
+    r'https://.*\.sourceforge\.net/.*',  # blocking the linkcheck user-agent
     r'https://stackoverflow.com/.*',  # blocking the linkcheck user-agent
 ]
 linkcheck_exclude_documents = [r'relnotes/.*']
