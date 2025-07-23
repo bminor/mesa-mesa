dc9fb92c1f7e1d1b53735177a1d794ab56e3aa17
Christoph Pillmayer <christoph.pillmayer@arm.com>
2025-07-23 14:38:05 +0000

    vk/sync: Pass dependencyFlags in vk_common_CmdPipelineBarrier
    Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
    Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    
    Fixes: 97f0a449 ("vulkan: implement legacy entrypoints on top of VK_KHR_synchronization2")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36321>
    (cherry picked from commit a501d26dee634961e024b2bba9bc40d73241808b)

--- src/vulkan/runtime/vk_synchronization.c
+++ src/vulkan/runtime/vk_synchronization.c
@@ -202,6 +202,7 @@ vk_common_CmdPipelineBarrier(
 
    VkDependencyInfo dep_info = {
       .sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO,
+      .dependencyFlags = dependencyFlags,
       .memoryBarrierCount = memoryBarrierCount,
       .pMemoryBarriers = memory_barriers,
       .bufferMemoryBarrierCount = bufferMemoryBarrierCount,
