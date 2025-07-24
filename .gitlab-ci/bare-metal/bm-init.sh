c25b14fa2310d8c46f4ede3197f923d28883934a
Guilherme Gallo <guilherme.gallo@collabora.com>
2025-07-24 15:21:23 -0300

    ci/bare-metal: Fix exit code variable
    `bm-init.sh` executes `init-stage1.sh` and then an `export`, but the
    latter command overwrite the exit code `$?`, so we need to put the first
    exit code into a variable.
    
    Signed-off-by: Guilherme Gallo <guilherme.gallo@collabora.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36356>

--- .gitlab-ci/bare-metal/bm-init.sh
+++ .gitlab-ci/bare-metal/bm-init.sh
@@ -4,11 +4,12 @@
 
 # First stage: very basic setup to bring up network and /dev etc
 /init-stage1.sh
+INIT_STAGE1_EXIT_CODE=$?
 
 export CURRENT_SECTION=dut_boot
 
 # Second stage: run jobs
-test $? -eq 0 && /init-stage2.sh
+test $INIT_STAGE1_EXIT_CODE -eq 0 && /init-stage2.sh
 
 # Wait until the job would have timed out anyway, so we don't spew a "init
 # exited" panic.
