1ddbe7e818cef939d87dc99fab847d2c1a3fe042
Marek Olšák <marek.olsak@amd.com>
2025-07-29 16:17:45 -0400

    radeonsi/gfx12: print swizzle modes for AMD_TEST=imagecopy
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36578>

--- src/gallium/drivers/radeonsi/si_test_image_copy_region.c
+++ src/gallium/drivers/radeonsi/si_test_image_copy_region.c
@@ -414,7 +414,19 @@ static void print_image_attrs(struct si_screen *sscreen, struct si_texture *tex)
 {
    const char *mode;
 
-   if (sscreen->info.gfx_level >= GFX9) {
+   if (sscreen->info.gfx_level >= GFX12) {
+      static const char *modes[32] = {
+         [ADDR3_LINEAR] = "LINEAR",
+         [ADDR3_256B_2D] = "256B_2D",
+         [ADDR3_4KB_2D] = "4KB_2D",
+         [ADDR3_64KB_2D] = "64KB_2D",
+         [ADDR3_256KB_2D] = "256KB_2D",
+         [ADDR3_4KB_3D] = "4KB_3D",
+         [ADDR3_64KB_3D] = "64KB_3D",
+         [ADDR3_256KB_3D] = "256KB_3D",
+      };
+      mode = modes[tex->surface.u.gfx9.swizzle_mode];
+   } else if (sscreen->info.gfx_level >= GFX9) {
       static const char *modes[32] = {
          [ADDR_SW_LINEAR] = "LINEAR",
          [ADDR_SW_4KB_S_X] = "4KB_S_X",
