alexvorxx/zink-xlib-termux

commit a7f86e38ca25c844d69b230283deba599dc9c289

zink: free sparse page for miptail on uncommit
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29818>

src/gallium/drivers/zink/zink_bo.c
@@ -1137,6 +1137,11 @@ zink_bo_commit(struct zink_context *ctx, struct zink_resource *res, unsigned lev
                            util_dynarray_append(&ctx->bs->tracked_semaphores, VkSemaphore, cur_sem);
                         else
                            ok = false;
+                        ok = sparse_backing_free(screen, backing[i]->bo, backing[i], backing_start[i], backing_size[i]);
+                        if (!ok) {
+                           /* Couldn't allocate tracking data structures, so we have to leak */
+                           fprintf(stderr, "zink: leaking sparse backing memory\n");
+                        }
                      }
                      goto out;
                   } else {

bminor/mesa-mesa

commit b08a23550ffa9c0880c694375d3ce69411b5eedc

zink: do not overwrite existing error for miptail on uncommit
a7f86e38ca25c844d69b230283deba599dc9c289 added the call to
sparse_backing_free. It reuses the ok variable and overwrites the
existing value

Fixes: a7f86e38ca25c844d69b230283deba599dc9c289
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36467>

src/gallium/drivers/zink/zink_bo.c
@@ -1141,10 +1141,10 @@ zink_bo_commit(struct zink_context *ctx, struct zink_resource *res, unsigned lev
                            util_dynarray_append(&ctx->bs->tracked_semaphores, VkSemaphore, cur_sem);
                         else
                            ok = false;
-                        ok = sparse_backing_free(screen, backing[i]->bo, backing[i], backing_start[i], backing_size[i]);
-                        if (!ok) {
+                        if (!sparse_backing_free(screen, backing[i]->bo, backing[i], backing_start[i], backing_size[i])) {
                            /* Couldn't allocate tracking data structures, so we have to leak */
                            fprintf(stderr, "zink: leaking sparse backing memory\n");
+                           ok = false;
                         }
                      }
                      goto out;