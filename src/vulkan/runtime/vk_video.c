aerisarn/mesa-uwp

commit 8243145f02c94315de898102f741edfb340c01b3

vulkan/video: add a h265 level translator.
This just converts the protocol level to the spec level encoding.

Reviewed-by: Hyunjun Ko <zzoon@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/25874>

src/vulkan/runtime/vk_video.c
@@ -1357,4 +1357,13 @@ vk_video_get_h265_nal_unit(StdVideoH265PictureType pic_type, bool irap_pic_flag)
    return 0;
 }
 
+static const uint8_t vk_video_h265_levels[] = {10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62};
+
+static uint8_t
+vk_video_get_h265_level(StdVideoH265LevelIdc level)
+{
+   assert(level <= STD_VIDEO_H265_LEVEL_IDC_6_2);
+   return vk_video_h265_levels[level];
+}
+
 #endif

bminor/mesa-mesa

commit 2c3bb204a580c1cdec231e80e372a8ce7854ff39

vulkan/video: Fix h265 level values
From spec:
"general_level_idc shall be set equal to a value of 30 times the level number."

Fixes: 8243145f02c ("vulkan/video: add a h265 level translator.")
Acked-by: Dave Airlie <airlied@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36285>

src/vulkan/runtime/vk_video.c
@@ -1779,7 +1779,9 @@ vk_video_get_h265_nal_unit(const StdVideoEncodeH265PictureInfo *pic_info)
    return 0;
 }
 
-static const uint8_t vk_video_h265_levels[] = {10, 20, 21, 30, 31, 40, 41, 50, 51, 52, 60, 61, 62};
+static const uint8_t vk_video_h265_levels[] = {
+   30, 60, 63, 90, 93, 120, 123, 150, 153, 156, 180, 183, 186
+};
 
 static uint8_t
 vk_video_get_h265_level(StdVideoH265LevelIdc level)