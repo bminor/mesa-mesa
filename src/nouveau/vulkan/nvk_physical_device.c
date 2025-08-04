a49c5c07e922ed06178f2fd8ba5dea0561fedeca
Mel Henning <mhenning@darkrefraction.com>
2025-08-04 13:26:39 -0400

    nvk: Expose transfer-only queues
    Reviewed-by: Thomas H.P. Andersen <phomes@gmail.com>
    Reviewed-by: Mary Guillemard <mary@mary.zone>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36617>

--- src/nouveau/vulkan/nvk_physical_device.c
+++ src/nouveau/vulkan/nvk_physical_device.c
@@ -1580,6 +1580,13 @@ nvk_create_drm_physical_device(struct vk_instance *_instance,
                      VK_QUEUE_SPARSE_BINDING_BIT,
       .queue_count = 1,
    };
+   if (pdev->info.has_transfer_queue) {
+      pdev->queue_families[pdev->queue_family_count++] = (struct nvk_queue_family) {
+         .queue_flags = VK_QUEUE_TRANSFER_BIT |
+                        VK_QUEUE_SPARSE_BINDING_BIT,
+         .queue_count = 2,
+      };
+   }
    assert(pdev->queue_family_count <= ARRAY_SIZE(pdev->queue_families));
 
    pdev->vk.supported_sync_types = nvkmd->sync_types;
