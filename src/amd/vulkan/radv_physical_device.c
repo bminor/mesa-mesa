bminor/mesa-mesa

commit 07881b085e6742e3664671cbe09b4220dbb56308

vulkan: Enable PresentWait2 on many physical devices
PresentWait2 should be possible on any physical device, as it adds a
surface capability query that depends on common wsi code.

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35414>

src/amd/vulkan/radv_physical_device.c
@@ -566,8 +566,10 @@ radv_physical_device_get_supported_extensions(const struct radv_physical_device 
        * we can also expose the extension that way. */
       .KHR_present_id =
          instance->drirc.enable_khr_present_wait || wsi_common_vk_instance_supports_present_wait(&instance->vk),
+      .KHR_present_id2 = true,
       .KHR_present_wait =
          instance->drirc.enable_khr_present_wait || wsi_common_vk_instance_supports_present_wait(&instance->vk),
+      .KHR_present_wait2 = true,
       .KHR_push_descriptor = true,
       .KHR_ray_query = radv_enable_rt(pdev),
       .KHR_ray_tracing_maintenance1 = radv_enable_rt(pdev),

src/broadcom/vulkan/v3dv_device.c
@@ -193,6 +193,8 @@ get_device_extensions(const struct v3dv_physical_device *device,
       .KHR_swapchain                        = true,
       .KHR_swapchain_mutable_format         = true,
       .KHR_incremental_present              = true,
+      .KHR_present_id2                      = true,
+      .KHR_present_wait2                    = true,
 #endif
       .KHR_variable_pointers                = true,
       .KHR_vertex_attribute_divisor         = true,

src/freedreno/vulkan/tu_device.cc
@@ -224,8 +224,10 @@ get_device_extensions(const struct tu_physical_device *device,
        * we can also expose the extension that way. */
       .KHR_present_id = (driQueryOptionb(&device->instance->dri_options, "vk_khr_present_wait") ||
                          wsi_common_vk_instance_supports_present_wait(&device->instance->vk)),
+      .KHR_present_id2 = true,
       .KHR_present_wait = (driQueryOptionb(&device->instance->dri_options, "vk_khr_present_wait") ||
                            wsi_common_vk_instance_supports_present_wait(&device->instance->vk)),
+      .KHR_present_wait2 = true,
 #endif
       .KHR_push_descriptor = true,
       .KHR_ray_query = has_raytracing,

src/imagination/vulkan/pvr_device.c
@@ -186,6 +186,8 @@ static void pvr_physical_device_get_supported_extensions(
       .KHR_get_memory_requirements2 = true,
       .KHR_image_format_list = true,
       .KHR_index_type_uint8 = true,
+      .KHR_present_id2 = PVR_USE_WSI_PLATFORM,
+      .KHR_present_wait2 = PVR_USE_WSI_PLATFORM,
       .KHR_shader_expect_assume = true,
       .KHR_swapchain = PVR_USE_WSI_PLATFORM,
       .KHR_timeline_semaphore = true,

src/intel/vulkan/anv_physical_device.c
@@ -220,6 +220,8 @@ get_device_extensions(const struct anv_physical_device *device,
          driQueryOptionb(&device->instance->dri_options, "vk_khr_present_wait") ||
          wsi_common_vk_instance_supports_present_wait(&device->instance->vk),
       .KHR_push_descriptor                   = true,
+      .KHR_present_id2                       = true,
+      .KHR_present_wait2                     = true,
       .KHR_ray_query                         = rt_enabled,
       .KHR_ray_tracing_maintenance1          = rt_enabled,
       .KHR_ray_tracing_pipeline              = rt_enabled,

src/nouveau/vulkan/nvk_physical_device.c
@@ -146,6 +146,8 @@ nvk_get_device_extensions(const struct nvk_instance *instance,
                         wsi_common_vk_instance_supports_present_wait(&instance->vk),
       .KHR_present_wait = driQueryOptionb(&instance->dri_options, "vk_khr_present_wait") ||
                           wsi_common_vk_instance_supports_present_wait(&instance->vk),
+      .KHR_present_id2 = true,
+      .KHR_present_wait2 = true,
 #endif
       .KHR_push_descriptor = true,
       .KHR_relaxed_block_layout = true,

src/panfrost/vulkan/panvk_physical_device.c
@@ -273,6 +273,8 @@ get_device_extensions(const struct panvk_physical_device *device,
       .KHR_spirv_1_4 = arch >= 10,
       .KHR_storage_buffer_storage_class = true,
 #ifdef PANVK_USE_WSI_PLATFORM
+      .KHR_present_id2 = true,
+      .KHR_present_wait2 = true,
       .KHR_swapchain = true,
 #endif
       .KHR_synchronization2 = true,

commit 07fe297a3e70d3a8a9a908317eea4455885f2fa3

vulkan: fix missing presentId2/presentWait2 enable features
Fixes recent VKCTS changes for dEQP-VK.info.device_mandatory_features.

Fixes: 07881b085e6 ("vulkan: Enable PresentWait2 on many physical devices")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36547>

src/amd/vulkan/radv_physical_device.c
@@ -1376,6 +1376,12 @@ radv_physical_device_get_features(const struct radv_physical_device *pdev, struc
 
       /* VK_KHR_video_encode_av1 */
       .videoEncodeAV1 = true,
+
+      /* VK_KHR_present_id2 */
+      .presentId2 = true,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = true,
    };
 }
 

src/broadcom/vulkan/v3dv_device.c
@@ -510,6 +510,12 @@ get_features(const struct v3dv_physical_device *physical_device,
 #ifdef V3DV_USE_WSI_PLATFORM
       /* VK_EXT_swapchain_maintenance1 */
       .swapchainMaintenance1 = true,
+
+      /* VK_KHR_present_id2 */
+      .presentId2 = true,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = true,
 #endif
 
       /* VK_KHR_shader_relaxed_extended_instruction */

src/freedreno/vulkan/tu_device.cc
@@ -753,6 +753,12 @@ tu_get_features(struct tu_physical_device *pdevice,
 #ifdef TU_USE_WSI_PLATFORM
    /* VK_EXT_swapchain_maintenance1 */
    features->swapchainMaintenance1 = true;
+
+   /* VK_KHR_present_id2 */
+   features->presentId2 = true;
+
+   /* VK_KHR_present_wait2 */
+   features->presentWait2 = true;
 #endif
 
    /* VK_EXT_texel_buffer_alignment */

src/imagination/vulkan/pvr_device.c
@@ -283,6 +283,12 @@ static void pvr_physical_device_get_supported_features(
 
       /* VK_KHR_shader_expect_assume */
       .shaderExpectAssume = false,
+
+      /* VK_KHR_present_id2 */
+      .presentId2 = PVR_USE_WSI_PLATFORM,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = PVR_USE_WSI_PLATFORM,
    };
 }
 

src/intel/vulkan/anv_physical_device.c
@@ -974,6 +974,12 @@ get_features(const struct anv_physical_device *pdevice,
 
       /* VK_KHR_maintenance9 */
       .maintenance9 = true,
+
+      /* VK_KHR_present_id2 */
+      .presentId2 = true,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = true,
    };
 
    /* The new DOOM and Wolfenstein games require depthBounds without

src/nouveau/vulkan/nvk_physical_device.c
@@ -717,6 +717,12 @@ nvk_get_device_features(const struct nv_device_info *info,
 
       /* VK_NV_shader_sm_builtins */
       .shaderSMBuiltins = true,
+
+      /* VK_KHR_present_id2 */
+      .presentId2 = supported_extensions->KHR_present_id2,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = supported_extensions->KHR_present_wait2,
    };
 }
 

src/panfrost/vulkan/panvk_vX_physical_device.c
@@ -495,6 +495,14 @@ panvk_per_arch(get_physical_device_features)(
 
       /* VK_EXT_mutable_descriptor_type */
       .mutableDescriptorType = PAN_ARCH >= 9,
+
+#ifdef PANVK_USE_WSI_PLATFORM
+      /* VK_KHR_present_id2 */
+      .presentId2 = true,
+
+      /* VK_KHR_present_wait2 */
+      .presentWait2 = true,
+#endif
    };
 }
 