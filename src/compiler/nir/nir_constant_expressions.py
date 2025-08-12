a34756bbedacdf2558c966466a71bfd3715fa295
Yonggang Luo <luoyonggang@gmail.com>
2025-08-13 02:34:31 +0800

    Revert "nir: Temporarily disable optimizations for MSVC ARM64"
    This reverts commit 55d153b9f598c77510a483ba768eca2103bf850b.
    
    The msvc bug is https://developercommunity.visualstudio.com/t/Stack-overflow-compiling-C-code-to-ARM64/916235
    
    and Fixed In: Visual Studio 2022 version 17.7
    
    Signed-off-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Jesse Natalie <jenatali@microsoft.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36767>

--- src/compiler/nir/nir_constant_expressions.py
+++ src/compiler/nir/nir_constant_expressions.py
@@ -566,11 +566,6 @@ struct ${type}${width}_vec {
 </%def>
 
 % for name, op in sorted(opcodes.items()):
-% if op.name == "fsat":
-#if defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
-#pragma optimize("", off) /* Temporary work-around for MSVC compiler bug, present in VS2019 16.9.2 */
-#endif
-% endif
 static void
 evaluate_${name}(nir_const_value *_dst_val,
                  UNUSED unsigned num_components,
@@ -594,11 +589,6 @@ evaluate_${name}(nir_const_value *_dst_val,
       ${evaluate_op(op, 0, execution_mode)}
    % endif
 }
-% if op.name == "fsat":
-#if defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC))
-#pragma optimize("", on) /* Temporary work-around for MSVC compiler bug, present in VS2019 16.9.2 */
-#endif
-% endif
 % endfor
 
 void
