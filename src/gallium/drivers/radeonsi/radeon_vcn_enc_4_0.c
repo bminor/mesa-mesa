734d42d72f34712e32a28c03781d766cf8143e66
David Rosca <david.rosca@amd.com>
2025-08-19 14:17:06 +0200

    radeonsi/vcn: Fix compatibility with old FW for encode
    The interface version can't be higher than the FW version, otherwise
    FW will reject all encode jobs.
    
    Cc: mesa-stable
    Reviewed-by: Ruijing Dong <ruijing.dong@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36845>
    (cherry picked from commit e945bb3649543bfbcd2e683e308acb989d50b5e8)

--- src/gallium/drivers/radeonsi/radeon_vcn_enc_4_0.c
+++ src/gallium/drivers/radeonsi/radeon_vcn_enc_4_0.c
@@ -718,6 +718,9 @@ static void radeon_enc_header_av1(struct radeon_encoder *enc)
 
 void radeon_enc_4_0_init(struct radeon_encoder *enc)
 {
+   struct si_screen *sscreen = (struct si_screen *)enc->screen;
+   uint32_t minor_version;
+
    radeon_enc_3_0_init(enc);
 
    enc->session_init = radeon_enc_session_init;
@@ -743,7 +746,10 @@ void radeon_enc_4_0_init(struct radeon_encoder *enc)
       enc->encode_params = radeon_enc_av1_encode_params;
    }
 
+   minor_version =
+      MIN2(sscreen->info.vcn_enc_minor_version, RENCODE_FW_INTERFACE_MINOR_VERSION);
+
    enc->enc_pic.session_info.interface_version =
       ((RENCODE_FW_INTERFACE_MAJOR_VERSION << RENCODE_IF_MAJOR_VERSION_SHIFT) |
-      (RENCODE_FW_INTERFACE_MINOR_VERSION << RENCODE_IF_MINOR_VERSION_SHIFT));
+      (minor_version << RENCODE_IF_MINOR_VERSION_SHIFT));
 }
