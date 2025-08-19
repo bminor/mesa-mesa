bminor/mesa-mesa

commit 8ee54400733f1f26e175c0065ad69e6a49402969

nir/uub: improve ishl/imul with constant sources
fossil-db (navi21):
Totals from 1 (0.00% of 79653) affected shaders:
Instrs: 1339 -> 1338 (-0.07%)
CodeSize: 7244 -> 7240 (-0.06%)
Latency: 19827 -> 19822 (-0.03%)
InvThroughput: 9913 -> 9911 (-0.02%)
SALU: 419 -> 418 (-0.24%)

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35514>

src/compiler/nir/nir_range_analysis.c
@@ -1861,12 +1861,29 @@ get_alu_uub(struct analysis_state *state, struct uub_query q, uint32_t *result, 
       uint32_t src1 = MIN2(src[1], q.scalar.def->bit_size - 1u);
       if (util_last_bit64(src[0]) + src1 <= q.scalar.def->bit_size) /* check overflow */
          *result = src[0] << src1;
+
+      nir_scalar src1_scalar = nir_scalar_chase_alu_src(q.scalar, 1);
+      if (nir_scalar_is_const(src1_scalar)) {
+         uint32_t const_val = 1u << (nir_scalar_as_uint(src1_scalar) & (q.scalar.def->bit_size - 1u));
+         *result = MIN2(*result, max / const_val * const_val);
+      }
       break;
    }
-   case nir_op_imul:
+   case nir_op_imul: {
       if (src[0] == 0 || (src[0] * src[1]) / src[0] == src[1]) /* check overflow */
          *result = src[0] * src[1];
+
+      nir_scalar src0_scalar = nir_scalar_chase_alu_src(q.scalar, 0);
+      nir_scalar src1_scalar = nir_scalar_chase_alu_src(q.scalar, 1);
+      if (nir_scalar_is_const(src0_scalar)) {
+         uint32_t const_val = nir_scalar_as_uint(src0_scalar);
+         *result = MIN2(*result, max / const_val * const_val);
+      } else if (nir_scalar_is_const(src1_scalar)) {
+         uint32_t const_val = nir_scalar_as_uint(src1_scalar);
+         *result = MIN2(*result, max / const_val * const_val);
+      }
       break;
+   }
    case nir_op_ushr: {
       nir_scalar src1_scalar = nir_scalar_chase_alu_src(q.scalar, 1);
       uint32_t mask = q.scalar.def->bit_size - 1u;

commit de3d04dd72c134e09364aad607fde30dc7e17563

nir/uub: guard against division by 0
Fixes: 8ee54400733 ("nir/uub: improve ishl/imul with constant sources")

Reviewed-by: Konstantin Seurer <konstantin.seurer@gmail.com>
Reviewed-by: Karol Herbst <kherbst@redhat.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36805>

src/compiler/nir/nir_range_analysis.c
@@ -1941,10 +1941,10 @@ get_alu_uub(struct analysis_state *state, struct uub_query q, uint32_t *result, 
       nir_scalar src1_scalar = nir_scalar_chase_alu_src(q.scalar, 1);
       if (nir_scalar_is_const(src0_scalar)) {
          uint32_t const_val = nir_scalar_as_uint(src0_scalar);
-         *result = MIN2(*result, max / const_val * const_val);
+         *result = const_val ? MIN2(*result, max / const_val * const_val) : 0;
       } else if (nir_scalar_is_const(src1_scalar)) {
          uint32_t const_val = nir_scalar_as_uint(src1_scalar);
-         *result = MIN2(*result, max / const_val * const_val);
+         *result = const_val ? MIN2(*result, max / const_val * const_val) : 0;
       }
       break;
    }