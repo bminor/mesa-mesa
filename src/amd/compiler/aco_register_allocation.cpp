bminor/mesa-mesa

commit 08f088479a40b2f8c76064f4f9939c53a6d03a9b

aco/ra: set late-kill for operands of temporary p_create_vector
Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13543
Fixes: c279dd6e6141 ("aco: Support vector-aligned ops fixed to defs")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36469>

src/amd/compiler/aco_register_allocation.cpp
@@ -2423,6 +2423,8 @@ handle_vector_operands(ra_ctx& ctx, RegisterFile& register_file,
    for (unsigned i = 0; i < num_operands; i++) {
       vec->operands[i] = instr->operands[operand_index + i];
       vec->operands[i].setFixed(ctx.assignments[vec->operands[i].tempId()].reg);
+      /* The operands might not be late-kill if they are tied to a definition. */
+      vec->operands[i].setLateKill(true);
    }
    Temp vec_temp = ctx.program->allocateTmp(rc);
    ctx.assignments.emplace_back();

commit 219c53e6fca818e42ee5837c4e7f6e5253ea989f

aco/ra: don't clear lateKill operands in get_reg_create_vector()
Fixes: 08f088479a40b2f8c76064f4f9939c53a6d03a9b ('aco/ra: set late-kill for operands of temporary p_create_vector')
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36871>

src/amd/compiler/aco_register_allocation.cpp
@@ -2166,7 +2166,7 @@ get_reg_create_vector(ra_ctx& ctx, const RegisterFile& reg_file, Temp temp,
    tmp_file.fill_killed_operands(instr.get());
 
    for (unsigned i = 0; i < instr->operands.size(); i++) {
-      if ((correct_pos_mask >> i) & 1u && instr->operands[i].isKill())
+      if ((correct_pos_mask >> i) & 1u && instr->operands[i].isKillBeforeDef())
          tmp_file.clear(instr->operands[i]);
    }
 