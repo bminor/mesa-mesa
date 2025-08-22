33c0b86201c7483a3066853c8d11c9905e85da7e
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2025-08-22 10:57:52 +0300

    brw: fix INTEL_DEBUG=spill_fs
    We need to dirty the instruction BRW_DEPENDENCY_INSTRUCTIONS &
    BRW_DEPENDENCY_VARIABLES if anything was spilled.
    
    Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
    Fixes: a6b0783375 ("brw: Use brw_ip_ranges in scheduling / regalloc")
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13233
    Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36925>
    (cherry picked from commit e6ca709a4ee42bbb5b9c4db289867918162c6e9e)

--- src/intel/compiler/brw_reg_allocate.cpp
+++ src/intel/compiler/brw_reg_allocate.cpp
@@ -1318,6 +1318,7 @@ brw_reg_alloc::assign_regs(bool allow_spilling, bool spill_all)
          int reg = choose_spill_reg();
          if (reg != -1) {
             spill_reg(reg);
+            spilled++;
             continue;
          }
       }
