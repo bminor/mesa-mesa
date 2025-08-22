bminor/mesa-mesa

commit e7ac1094f69830cb76e622525629ace68b757a10

ir3: rematerialize preamble defs in block dominated by sources
Preamble defs were rematerialized at the end of the preamble. However,
when some of the sources were defined inside control flow, this would
lead to these sources not dominating their use. Fix this by finding the
block that is dominated by all sources and inserting the new instruction
there.

Also make sure we only de-duplicate instructions if the new instruction
is dominated by the existing one.

Fixes a NIR validation error in Devil may cry 5.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Fixes: fdfe86aa529 ("ir3: Expand preamble rematerialization")
Fixes: 6a744ddebc2 ("ir3: Initial support for pushing globals with ldg.k")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/33270>

src/freedreno/ir3/ir3_nir_opt_preamble.c
@@ -367,6 +367,53 @@ ir3_def_is_rematerializable_for_preamble(nir_def *def,
    }
 }
 
+struct find_insert_block_state {
+   nir_block *insert_block;
+};
+
+static bool
+find_dominated_src(nir_src *src, void *data)
+{
+   struct find_insert_block_state *state = data;
+   nir_block *src_block = src->ssa->parent_instr->block;
+
+   if (!state->insert_block) {
+      state->insert_block = src_block;
+      return true;
+   } else if (nir_block_dominates(state->insert_block, src_block)) {
+      state->insert_block = src_block;
+      return true;
+   } else if (nir_block_dominates(src_block, state->insert_block)) {
+      return true;
+   } else {
+      state->insert_block = NULL;
+      return false;
+   }
+}
+
+/* Find the block where instr can be inserted. This is the block that is
+ * dominated by all its sources. If instr doesn't have any sources, return dflt.
+ */
+static nir_block *
+find_insert_block(nir_instr *instr, nir_block *dflt)
+{
+   struct find_insert_block_state state = {
+      .insert_block = NULL,
+   };
+
+   if (nir_foreach_src(instr, find_dominated_src, &state)) {
+      return state.insert_block ? state.insert_block : dflt;
+   }
+
+   return NULL;
+}
+
+static bool
+dominates(const nir_instr *old_instr, const nir_instr *new_instr)
+{
+   return nir_block_dominates(old_instr->block, new_instr->block);
+}
+
 static nir_def *
 _rematerialize_def(nir_builder *b, struct hash_table *remap_ht,
                    struct set *instr_set, nir_def **preamble_defs,
@@ -405,17 +452,29 @@ _rematerialize_def(nir_builder *b, struct hash_table *remap_ht,
 
    nir_instr *instr = nir_instr_clone_deep(b->shader, def->parent_instr,
                                            remap_ht);
+
+   /* Find a legal place to insert the new instruction. We cannot simply put it
+    * at the end of the preamble since the original instruction and its sources
+    * may be defined inside control flow.
+    */
+   nir_metadata_require(b->impl, nir_metadata_dominance);
+   nir_block *insert_block =
+      find_insert_block(instr, nir_cursor_current_block(b->cursor));
+
+   /* Since the preamble control flow was reconstructed from the original one,
+    * we must be able to find a legal place to insert the instruction.
+    */
+   assert(insert_block);
+   b->cursor = nir_after_block(insert_block);
+   nir_builder_instr_insert(b, instr);
+
    if (instr_set) {
       nir_instr *other_instr =
-         nir_instr_set_add_or_rewrite(instr_set, instr, NULL);
+         nir_instr_set_add_or_rewrite(instr_set, instr, dominates);
       if (other_instr) {
          instr = other_instr;
          _mesa_hash_table_insert(remap_ht, def, nir_instr_def(other_instr));
-      } else {
-         nir_builder_instr_insert(b, instr);
       }
-   } else {
-      nir_builder_instr_insert(b, instr);
    }
 
    return nir_instr_def(instr);

commit 24cdb0b636dc6b6a7eee4e9175c61459b0bc88dd

ir3: emit descriptor prefetch in block dominated by its sources
Descriptor prefetches may be generated for instructions in control flow.
This means we cannot simply emit prefetches at the end of the preamble
because that may not be dominated by all their sources. This commit uses
the helpers introduced by e7ac1094f69 ("ir3: rematerialize preamble defs
in block dominated by sources") to find the correct block to insert
prefetches.

Fixes NIR validation errors in Dying Light 2.

Signed-off-by: Job Noorman <jnoorman@igalia.com>
Fixes: 4e2a0a5ad09 ("ir3: Add descriptor prefetching optimization on a7xx")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36885>

src/freedreno/ir3/ir3_nir_opt_preamble.c
@@ -401,23 +401,28 @@ struct find_insert_block_state {
 };
 
 static bool
-find_dominated_src(nir_src *src, void *data)
+find_dominated_block(nir_block *block, struct find_insert_block_state *state)
 {
-   struct find_insert_block_state *state = data;
-   nir_block *src_block = nir_def_block(src->ssa);
-
    if (!state->insert_block) {
-      state->insert_block = src_block;
+      state->insert_block = block;
       return true;
-   } else if (nir_block_dominates(state->insert_block, src_block)) {
-      state->insert_block = src_block;
+   } else if (nir_block_dominates(state->insert_block, block)) {
+      state->insert_block = block;
       return true;
-   } else if (nir_block_dominates(src_block, state->insert_block)) {
+   } else if (nir_block_dominates(block, state->insert_block)) {
       return true;
    } else {
       state->insert_block = NULL;
       return false;
    }
+}
+
+static bool
+find_dominated_src(nir_src *src, void *data)
+{
+   struct find_insert_block_state *state = data;
+   nir_block *src_block = nir_def_block(src->ssa);
+   return find_dominated_block(src_block, state);
 }
 
 /* Find the block where instr can be inserted. This is the block that is
@@ -435,6 +440,22 @@ find_insert_block(nir_instr *instr, nir_block *dflt)
    }
 
    return NULL;
+}
+
+static nir_block *
+find_insert_block_for_defs(nir_def *defs[], unsigned n)
+{
+   struct find_insert_block_state state = {
+      .insert_block = NULL,
+   };
+
+   for (unsigned i = 0; i < n; i++) {
+      if (!find_dominated_block(nir_def_block(defs[i]), &state)) {
+         return NULL;
+      }
+   }
+
+   return state.insert_block;
 }
 
 static bool
@@ -608,6 +629,20 @@ static bool
 emit_descriptor_prefetch(nir_builder *b, nir_instr *instr, nir_def **descs,
                          struct prefetch_state *state)
 {
+   nir_block *insert_block = nir_def_block(descs[0]);
+
+   if (descs[1]) {
+      insert_block = find_insert_block_for_defs(descs, 2);
+
+      /* Since the preamble control flow was reconstructed from the original
+       * one, and the two descriptor defs were used by the same instruction, we
+       * must be able to find a legal place to insert the prefetch.
+       */
+      assert(insert_block);
+   }
+
+   b->cursor = nir_after_block(insert_block);
+
    if (instr->type == nir_instr_type_tex) {
       nir_tex_instr *tex = nir_instr_as_tex(instr);
       int sampler_index =
@@ -783,8 +818,6 @@ ir3_nir_opt_prefetch_descriptors(nir_shader *nir, struct ir3_shader_variant *v)
                                                   preamble_defs);
          }
 
-         /* ir3_rematerialize_def_for_preamble may have moved the cursor. */
-         b.cursor = nir_after_impl(preamble);
          progress |= emit_descriptor_prefetch(&b, instr, preamble_descs, &state);
 
          if (state.sampler.num_prefetches == MAX_PREFETCHES &&