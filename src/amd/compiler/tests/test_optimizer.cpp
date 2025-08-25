bminor/mesa-mesa

commit 345bf8a2f2807a6a6ff3a4bdab5f2da6d94a9661

aco/optimizer: remove label_vop3p
Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34858>

src/amd/compiler/aco_optimizer.cpp
@@ -70,9 +70,8 @@ enum Label {
    label_b2i = 1 << 27,
    label_fcanonicalize = 1 << 28,
    label_constant_16bit = 1 << 29,
-   label_usedef = 1 << 30,   /* generic label */
-   label_vop3p = 1ull << 31, /* 1ull to prevent sign extension */
-   label_canonicalized = 1ull << 32,
+   label_usedef = 1 << 30,           /* generic label */
+   label_canonicalized = 1ull << 32, /* 1ull to prevent sign extension */
    label_extract = 1ull << 33,
    label_insert = 1ull << 34,
    label_dpp16 = 1ull << 35,
@@ -83,8 +82,8 @@ enum Label {
 };
 
 static constexpr uint64_t instr_usedef_labels =
-   label_vec | label_mul | label_add_sub | label_vop3p | label_bitwise | label_uniform_bitwise |
-   label_minmax | label_usedef | label_extract | label_dpp16 | label_dpp8 | label_f2f32;
+   label_vec | label_mul | label_add_sub | label_bitwise | label_uniform_bitwise | label_minmax |
+   label_usedef | label_extract | label_dpp16 | label_dpp8 | label_f2f32;
 static constexpr uint64_t instr_mod_labels =
    label_omod2 | label_omod4 | label_omod5 | label_clamp | label_insert | label_f2f16;
 
@@ -371,14 +370,6 @@ struct ssa_info {
    }
 
    bool is_usedef() { return label & label_usedef; }
-
-   void set_vop3p(Instruction* vop3p_instr)
-   {
-      add_label(label_vop3p);
-      instr = vop3p_instr;
-   }
-
-   bool is_vop3p() { return label & label_vop3p; }
 
    void set_fcanonicalize(Temp tmp)
    {
@@ -1671,13 +1662,9 @@ label_instruction(opt_ctx& ctx, aco_ptr<Instruction>& instr)
             ctx.info[instr->definitions[0].tempId()].set_canonicalized();
       }
 
-      if (instr->isVOPC()) {
+      if (instr->isVOPC() || instr->isVOP3P()) {
          ctx.info[instr->definitions[0].tempId()].set_usedef(instr.get());
          check_sdwa_extract(ctx, instr);
-         return;
-      }
-      if (instr->isVOP3P()) {
-         ctx.info[instr->definitions[0].tempId()].set_vop3p(instr.get());
          return;
       }
    }
@@ -3480,7 +3467,8 @@ combine_vop3p(opt_ctx& ctx, aco_ptr<Instruction>& instr)
        !vop3p->opsel_lo[1] && !vop3p->opsel_hi[1]) {
 
       ssa_info& info = ctx.info[instr->operands[0].tempId()];
-      if (info.is_vop3p() && instr_info.can_use_output_modifiers[(int)info.instr->opcode]) {
+      if (info.is_usedef() && info.instr->isVOP3P() &&
+          instr_info.can_use_output_modifiers[(int)info.instr->opcode]) {
          VALU_instruction* candidate = &ctx.info[instr->operands[0].tempId()].instr->valu();
          candidate->clamp = true;
          propagate_swizzles(candidate, vop3p->opsel_lo[0], vop3p->opsel_hi[0]);
@@ -3500,7 +3488,7 @@ combine_vop3p(opt_ctx& ctx, aco_ptr<Instruction>& instr)
          continue;
 
       ssa_info& info = ctx.info[op.tempId()];
-      if (info.is_vop3p() && info.instr->opcode == aco_opcode::v_pk_mul_f16 &&
+      if (info.is_usedef() && info.instr->opcode == aco_opcode::v_pk_mul_f16 &&
           (info.instr->operands[0].constantEquals(0x3C00) ||
            info.instr->operands[1].constantEquals(0x3C00))) {
 
@@ -3558,7 +3546,7 @@ combine_vop3p(opt_ctx& ctx, aco_ptr<Instruction>& instr)
          if (!op_instr)
             continue;
 
-         if (ctx.info[instr->operands[i].tempId()].is_vop3p()) {
+         if (op_instr->isVOP3P()) {
             if (fadd) {
                if (op_instr->opcode != aco_opcode::v_pk_mul_f16 ||
                    op_instr->definitions[0].isPrecise())
@@ -3641,7 +3629,7 @@ combine_vop3p(opt_ctx& ctx, aco_ptr<Instruction>& instr)
       fma->definitions[0] = instr->definitions[0];
       fma->pass_flags = instr->pass_flags;
       instr = std::move(fma);
-      ctx.info[instr->definitions[0].tempId()].set_vop3p(instr.get());
+      ctx.info[instr->definitions[0].tempId()].set_usedef(instr.get());
       decrease_uses(ctx, mul_instr);
       return;
    }

commit 8903bb4618da772d06413e0cfaa433a67525352d

aco/optimizer: don't apply packed clamp to v_fma_mix
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13758
Fixes: 345bf8a2f28 ("aco/optimizer: remove label_vop3p")
Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36963>

src/amd/compiler/aco_optimizer.cpp
@@ -3346,8 +3346,11 @@ combine_vop3p(opt_ctx& ctx, aco_ptr<Instruction>& instr)
        !vop3p->opsel_lo[1] && !vop3p->opsel_hi[1]) {
 
       Instruction* op_instr = ctx.info[instr->operands[0].tempId()].parent_instr;
-      if (op_instr->isVOP3P() &&
-          instr_info.alu_opcode_infos[(int)op_instr->opcode].output_modifiers) {
+      const aco_alu_opcode_info& opcode_info = instr_info.alu_opcode_infos[(int)op_instr->opcode];
+      aco_type op_type = opcode_info.def_types[0];
+      if (op_instr->isVOP3P() && op_type.num_components == 2 &&
+          op_type.base_type == aco_base_type_float && op_type.bit_size == 16 &&
+          opcode_info.output_modifiers) {
          op_instr->valu().clamp = true;
          propagate_swizzles(&op_instr->valu(), vop3p->opsel_lo[0], vop3p->opsel_hi[0]);
          instr->definitions[0].swapTemp(op_instr->definitions[0]);

src/amd/compiler/tests/test_optimizer.cpp
@@ -2048,3 +2048,33 @@ BEGIN_TEST(optimizer.trans_no_omod)
 
    finish_opt_test();
 END_TEST
+
+BEGIN_TEST(optimizer.pk_clamp_fma_mix)
+   //>> v1: %a:v[0],  v1: %b:v[1],  v2b: %c:v[2][0:16] = p_startpgm
+   if (!setup_cs("v1 v1 v2b", GFX12))
+      return;
+
+   //! v2b: %cvt0 = v_fma_mixlo_f16 1.0, %a, %b
+   //! v1: %clamp0 = v_pk_mul_f16 %cvt0.xx, 1.0.xx clamp
+   //! p_unit_test 0, %clamp0
+   Temp add0 = bld.vop2(aco_opcode::v_add_f32, bld.def(v1), inputs[0], inputs[1]);
+   Temp cvt0 = bld.vop1(aco_opcode::v_cvt_f16_f32, bld.def(v2b), add0);
+   Builder::Result clamp0 =
+      bld.vop3p(aco_opcode::v_pk_mul_f16, bld.def(v1), cvt0, Operand::c16(0x3c00), 0, 0);
+   clamp0->valu().clamp = true;
+
+   writeout(0, clamp0);
+
+   //! v1: %add1 = v_fma_mix_f32 1.0, lo(%c), %a
+   //! v1: %clamp1 = v_pk_mul_f16 %add1, 1.0.xx clamp
+   //! p_unit_test 1, %clamp1
+   Temp cvt1 = bld.vop1(aco_opcode::v_cvt_f32_f16, bld.def(v1), inputs[2]);
+   Temp add1 = bld.vop2(aco_opcode::v_add_f32, bld.def(v1), inputs[0], cvt1);
+   Builder::Result clamp1 =
+      bld.vop3p(aco_opcode::v_pk_mul_f16, bld.def(v1), add1, Operand::c16(0x3c00), 0, 0x1);
+   clamp1->valu().clamp = true;
+
+   writeout(1, clamp1);
+
+   finish_opt_test();
+END_TEST