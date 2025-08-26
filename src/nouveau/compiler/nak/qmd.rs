7b09f7d1566fd2e5542535e97178b753c933306b
Faith Ekstrand <faith.ekstrand@collabora.com>
2025-08-26 18:38:16 -0400

    nak: NAK_MAX_QMD_SIZE_B should be 384
    Also add a static assert so we don't miss this again.
    
    Fixes: 00a845a698e3 ("nak/qmd: QMD versions 4.0 and 5.0 are both 384B")
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37015>
    (cherry picked from commit 0a14ce7f501f70d9796270b5715dbecc36522102)

--- src/nouveau/compiler/nak/qmd.rs
+++ src/nouveau/compiler/nak/qmd.rs
@@ -67,6 +67,8 @@ macro_rules! qmd_impl_common {
         }
 
         const GLOBAL_SIZE_LAYOUT: nak_qmd_dispatch_size_layout = {
+            assert!(NAK_MAX_QMD_SIZE_B as usize >= size_of::<Self>());
+
             let w = paste! {$c::[<$s _CTA_RASTER_WIDTH>]};
             let h = paste! {$c::[<$s _CTA_RASTER_HEIGHT>]};
             let d = paste! {$c::[<$s _CTA_RASTER_DEPTH>]};
