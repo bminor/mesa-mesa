bminor/mesa-mesa

commit 00a845a698e3a86b71f0eb7a6ddf1d611826151e

nak/qmd: QMD versions 4.0 and 5.0 are both 384B
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Backport-to: 25.2
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36995>

src/nouveau/compiler/nak/qmd.rs
@@ -472,12 +472,12 @@ mod qmd_4_0 {
 
     #[repr(transparent)]
     pub struct Qmd4_0 {
-        qmd: [u32; 64],
+        qmd: [u32; 96],
     }
 
     impl QMD for Qmd4_0 {
         fn new() -> Self {
-            let mut qmd = [0; 64];
+            let mut qmd = [0; 96];
             let mut bv = QMDBitView::new(&mut qmd);
             qmd_init!(bv, clcbc0, QMDV04_00, 4, 0);
             Self { qmd }
@@ -514,12 +514,12 @@ mod qmd_5_0 {
     }
 
     pub struct Qmd5_0 {
-        qmd: [u32; 64],
+        qmd: [u32; 96],
     }
 
     impl QMD for Qmd5_0 {
         fn new() -> Self {
-            let mut qmd = [0; 64];
+            let mut qmd = [0; 96];
             let mut bv = QMDBitView::new(&mut qmd);
             qmd_init!(bv, clcdc0, QMDV05_00, 5, 0);
             set_field!(bv, clcdc0, QMDV05_00, QMD_TYPE, 0x2);

commit 0a14ce7f501f70d9796270b5715dbecc36522102

nak: NAK_MAX_QMD_SIZE_B should be 384
Also add a static assert so we don't miss this again.

Fixes: 00a845a698e3 ("nak/qmd: QMD versions 4.0 and 5.0 are both 384B")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37015>

src/nouveau/compiler/nak.h
@@ -252,7 +252,7 @@ struct nak_qmd_info {
 };
 
 #define NAK_QMD_ALIGN_B 256
-#define NAK_MAX_QMD_SIZE_B 256
+#define NAK_MAX_QMD_SIZE_B 384
 #define NAK_MAX_QMD_DWORDS (NAK_MAX_QMD_SIZE_B / 4)
 
 uint32_t nak_qmd_size_B(const struct nv_device_info *dev);

src/nouveau/compiler/nak/qmd.rs
@@ -68,6 +68,7 @@ macro_rules! qmd_impl_common {
 
         const GLOBAL_SIZE_LAYOUT: nak_qmd_dispatch_size_layout = {
             assert!(paste! {$c::[<$s _MAX_BIT>]} < size_of::<Self>() * 8);
+            assert!(NAK_MAX_QMD_SIZE_B as usize >= size_of::<Self>());
 
             let w = paste! {$c::[<$s _CTA_RASTER_WIDTH>]};
             let h = paste! {$c::[<$s _CTA_RASTER_HEIGHT>]};