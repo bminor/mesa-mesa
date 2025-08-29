alexvorxx/zink-xlib-termux

commit b013d54e4ff9cb066cb77ead0a7fd76d5df962ad

nak/lower_cf: Flag phis as convergent when possible
Because we go in and out of SSA, all the phis get re-created and the new
phis will default to divergent.  This little pass attempts to prove as
many of the phis convergent as possible.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/29591>

src/nouveau/compiler/nak_nir_lower_cf.c
@@ -373,6 +373,55 @@ lower_cf_list(nir_builder *b, nir_def *esc_reg, struct scope *parent_scope,
    }
 }
 
+static void
+recompute_phi_divergence_impl(nir_function_impl *impl)
+{
+   bool progress;
+   do {
+      progress = false;
+      nir_foreach_block_unstructured(block, impl) {
+         nir_foreach_instr(instr, block) {
+            if (instr->type != nir_instr_type_phi)
+               break;
+
+            nir_phi_instr *phi = nir_instr_as_phi(instr);
+
+            bool divergent = false;
+            nir_foreach_phi_src(phi_src, phi) {
+               /* There is a tricky case we need to care about here where a
+                * convergent block has a divergent dominator.  This can happen
+                * if, for instance, you have the following loop:
+                *
+                *    loop {
+                *       if (div) {
+                *          %20 = load_ubo(0, 0);
+                *       } else {
+                *          terminate;
+                *       }
+                *    }
+                *    use(%20);
+                *
+                * In this case, the load_ubo() dominates the use() even though
+                * the load_ubo() exists in divergent control-flow.  In this
+                * case, we simply flag the whole phi divergent because we
+                * don't want to deal with inserting a r2ur somewhere.
+                */
+               if (phi_src->pred->divergent || phi_src->src.ssa->divergent ||
+                   phi_src->src.ssa->parent_instr->block->divergent) {
+                  divergent = true;
+                  break;
+               }
+            }
+
+            if (divergent != phi->def.divergent) {
+               phi->def.divergent = divergent;
+               progress = true;
+            }
+         }
+      }
+   } while(progress);
+}
+
 static bool
 lower_cf_func(nir_function *func)
 {
@@ -422,6 +471,7 @@ lower_cf_func(nir_function *func)
    nir_sort_unstructured_blocks(new_impl);
    nir_repair_ssa_impl(new_impl);
    nir_lower_reg_intrinsics_to_ssa_impl(new_impl);
+   recompute_phi_divergence_impl(new_impl);
 
    return true;
 }

bminor/mesa-mesa

commit 26e32417b954f63a812bb93644a213997fa5c031

nir: Add an option to make lower_phis_to_regs_block() less clever
Right now it tries to place reg_write instructions as far up the
predecessor chain as possible.  This is useful for a bunch of the passes
that call it since it ensures they don't get placed in dead blocks or in
single successors and things like that.  But it screws up NAK's control
flow lowering so we need the option to turn it off and make the pass
place the reg_write instructions in the most obvious place possible.

Fixes: b013d54e4ff9 ("nak/lower_cf: Flag phis as convergent when possible")
Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36914>

src/compiler/nir/nir.h
@@ -6161,7 +6161,8 @@ nir_rewrite_uses_to_load_reg(nir_builder *b, nir_def *old,
 bool nir_convert_from_ssa(nir_shader *shader,
                           bool phi_webs_only, bool consider_divergence);
 
-bool nir_lower_phis_to_regs_block(nir_block *block);
+bool nir_lower_phis_to_regs_block(nir_block *block,
+                                  bool place_writes_in_imm_preds);
 bool nir_lower_ssa_defs_to_regs_block(nir_block *block);
 
 bool nir_rematerialize_deref_in_use_blocks(nir_deref_instr *instr);

src/compiler/nir/nir_from_ssa.c
@@ -1152,18 +1152,30 @@ place_phi_read(nir_builder *b, nir_def *reg,
  * temporary for each phi, all movs inserted in any particular block have
  * unique destinations so the order of operations does not matter.
  *
- * The one intelligent thing this pass does is that it places the moves from
- * the phi sources as high up the predecessor tree as possible instead of in
- * the exact predecessor.  This means that, in particular, it will crawl into
- * the deepest nesting of any if-ladders.  In order to ensure that doing so is
+ * If place_writes_in_imm_preds is set, we don't try to be clever and
+ * reg_write instructions are placed in the immediate predecessor block as
+ * given by the phi source.  If unset, we try to place the moves from the phi
+ * sources as high up the predecessor tree as possible instead of in the exact
+ * predecessor.  This means that, in particular, it will crawl into the
+ * deepest nesting of any if-ladders.  In order to ensure that doing so is
  * safe, it stops as soon as one of the predecessors has multiple successors.
+ * This can be useful for passes which don't want store_reg intrinsics to be
+ * placed in unreachable blocks or blocks with a single predecessor and single
+ * successor, this simplifying the pass logic.
+ *
+ * place_writes_in_imm_preds should be set if the caller wants reg_load/store
+ * instructions to map directly to the original phis.  This can be useful if,
+ * for instance, you want to guarantee that uniform registers are only ever
+ * written from uniform control flow or if you want to accurately be able to
+ * re-construct the original phis afterwards.
  */
 bool
-nir_lower_phis_to_regs_block(nir_block *block)
+nir_lower_phis_to_regs_block(nir_block *block, bool place_writes_in_imm_preds)
 {
    nir_builder b = nir_builder_create(nir_cf_node_get_function(&block->cf_node));
-   struct set *visited_blocks = _mesa_set_create(NULL, _mesa_hash_pointer,
-                                                 _mesa_key_pointer_equal);
+   struct set *visited_blocks = NULL;
+   if (!place_writes_in_imm_preds)
+      visited_blocks = _mesa_pointer_set_create(NULL);
 
    bool progress = false;
    nir_foreach_phi_safe(phi, block) {
@@ -1174,10 +1186,14 @@ nir_lower_phis_to_regs_block(nir_block *block)
       nir_def_rewrite_uses(&phi->def, nir_load_reg(&b, reg));
 
       nir_foreach_phi_src(src, phi) {
-
-         _mesa_set_add(visited_blocks, nir_def_block(src->src.ssa));
-         place_phi_read(&b, reg, src->src.ssa, src->pred, visited_blocks);
-         _mesa_set_clear(visited_blocks, NULL);
+         if (place_writes_in_imm_preds) {
+            b.cursor = nir_after_block_before_jump(src->pred);
+            nir_store_reg(&b, src->src.ssa, reg);
+         } else {
+            _mesa_set_add(visited_blocks, nir_def_block(src->src.ssa));
+            place_phi_read(&b, reg, src->src.ssa, src->pred, visited_blocks);
+            _mesa_set_clear(visited_blocks, NULL);
+         }
       }
 
       nir_instr_remove(&phi->instr);
@@ -1185,7 +1201,8 @@ nir_lower_phis_to_regs_block(nir_block *block)
       progress = true;
    }
 
-   _mesa_set_destroy(visited_blocks, NULL);
+   if (!place_writes_in_imm_preds)
+      _mesa_set_destroy(visited_blocks, NULL);
 
    return progress;
 }

src/compiler/nir/nir_lower_continue_constructs.c
@@ -49,7 +49,7 @@ lower_loop_continue_block(nir_builder *b, nir_loop *loop, bool *repair_ssa)
          break;
    }
 
-   nir_lower_phis_to_regs_block(header);
+   nir_lower_phis_to_regs_block(header, false);
 
    if (num_continue == 0) {
       /* this loop doesn't continue at all. delete the continue construct */
@@ -66,7 +66,7 @@ lower_loop_continue_block(nir_builder *b, nir_loop *loop, bool *repair_ssa)
       nir_cf_reinsert(&extracted,
                       nir_after_block_before_jump(single_predecessor));
    } else {
-      nir_lower_phis_to_regs_block(cont);
+      nir_lower_phis_to_regs_block(cont, false);
       *repair_ssa = true;
 
       /* As control flow has to re-converge before executing the continue

src/compiler/nir/nir_lower_goto_ifs.c
@@ -923,7 +923,7 @@ nir_lower_goto_ifs_impl(nir_function_impl *impl)
     * if we don't have any phi nodes to fix up.
     */
    nir_foreach_block_unstructured(block, impl)
-      nir_lower_phis_to_regs_block(block);
+      nir_lower_phis_to_regs_block(block, false);
 
    nir_cf_list cf_list;
    nir_cf_extract(&cf_list, nir_before_impl(impl),

src/compiler/nir/nir_lower_shader_calls.c
@@ -863,9 +863,9 @@ duplicate_loop_bodies(nir_function_impl *impl, nir_instr *resume_instr)
        * serious trouble if we don't do this.
        */
       nir_convert_loop_to_lcssa(loop);
-      nir_lower_phis_to_regs_block(nir_loop_first_block(loop));
+      nir_lower_phis_to_regs_block(nir_loop_first_block(loop), false);
       nir_lower_phis_to_regs_block(
-         nir_cf_node_as_block(nir_cf_node_next(&loop->cf_node)));
+         nir_cf_node_as_block(nir_cf_node_next(&loop->cf_node)), false);
 
       nir_cf_list cf_list;
       nir_cf_list_extract(&cf_list, &loop->body);

src/compiler/nir/nir_opt_if.c
@@ -215,9 +215,9 @@ opt_peel_loop_initial_if(nir_loop *loop)
       nir_cf_node_as_block(nir_cf_node_next(&nif->cf_node));
 
    /* Get rid of phis in the header block since we will be duplicating it */
-   nir_lower_phis_to_regs_block(header_block);
+   nir_lower_phis_to_regs_block(header_block, false);
    /* Get rid of phis after the if since dominance will change */
-   nir_lower_phis_to_regs_block(after_if_block);
+   nir_lower_phis_to_regs_block(after_if_block, false);
 
    /* Get rid of SSA defs in the pieces we're about to move around */
    nir_lower_ssa_defs_to_regs_block(header_block);

src/compiler/nir/nir_opt_loop.c
@@ -86,7 +86,7 @@ opt_loop_merge_break_continue(nir_if *nif)
     * merge the statement after the branch
     */
    if ((then_break && else_break) || (then_cont && else_cont)) {
-      nir_lower_phis_to_regs_block(last_then->successors[0]);
+      nir_lower_phis_to_regs_block(last_then->successors[0], false);
       nir_instr_remove_v(nir_block_last_instr(last_then));
       nir_instr *jump = nir_block_last_instr(last_else);
       nir_instr_remove_v(jump);
@@ -154,7 +154,7 @@ opt_loop_terminator(nir_if *nif)
          return false;
 
       /* We are about to move the predecessor. */
-      nir_lower_phis_to_regs_block(continue_from_blk->successors[0]);
+      nir_lower_phis_to_regs_block(continue_from_blk->successors[0], false);
    }
 
    /* Even though this if statement has a jump on one side, we may still have
@@ -221,7 +221,7 @@ opt_loop_last_block(nir_block *block, bool is_trivial_continue, bool is_trivial_
     * control-flow will naturally take us to the same target block.
     */
    if ((has_break && is_trivial_break) || (has_continue && is_trivial_continue)) {
-      nir_lower_phis_to_regs_block(block->successors[0]);
+      nir_lower_phis_to_regs_block(block->successors[0], false);
       nir_instr_remove_v(nir_block_last_instr(block));
       return true;
    }
@@ -269,7 +269,7 @@ opt_loop_last_block(nir_block *block, bool is_trivial_continue, bool is_trivial_
       nir_remove_single_src_phis_block(nir_cf_node_cf_tree_next(prev));
 
       /* We are about to remove one predecessor. */
-      nir_lower_phis_to_regs_block(block->successors[0]);
+      nir_lower_phis_to_regs_block(block->successors[0], false);
 
       nir_cf_list tmp;
       nir_cf_extract(&tmp, nir_after_cf_node(prev), nir_after_block_before_jump(block));
@@ -417,9 +417,9 @@ opt_loop_peel_initial_break(nir_loop *loop)
    }
 
    /* Lower loop header and LCSSA-phis to regs. */
-   nir_lower_phis_to_regs_block(header_block);
+   nir_lower_phis_to_regs_block(header_block, false);
    nir_lower_ssa_defs_to_regs_block(header_block);
-   nir_lower_phis_to_regs_block(exit_block);
+   nir_lower_phis_to_regs_block(exit_block, false);
 
    /* Extract the loop header including the first break. */
    nir_cf_list tmp;

src/compiler/nir/nir_opt_loop_unroll.c
@@ -56,7 +56,7 @@ loop_prepare_for_unroll(nir_loop *loop)
    /* Lower phis at the top level of the loop body */
    foreach_list_typed_safe(nir_cf_node, node, node, &loop->body) {
       if (nir_cf_node_block == node->type) {
-         nir_lower_phis_to_regs_block(nir_cf_node_as_block(node));
+         nir_lower_phis_to_regs_block(nir_cf_node_as_block(node), false);
       }
    }
 
@@ -64,7 +64,7 @@ loop_prepare_for_unroll(nir_loop *loop)
    nir_block *block_after_loop =
       nir_cf_node_as_block(nir_cf_node_next(&loop->cf_node));
 
-   nir_lower_phis_to_regs_block(block_after_loop);
+   nir_lower_phis_to_regs_block(block_after_loop, false);
 
    /* Remove jump if it's the last instruction in the loop */
    nir_instr *last_instr = nir_block_last_instr(nir_loop_last_block(loop));

src/nouveau/compiler/nak_nir_lower_cf.c
@@ -448,7 +448,7 @@ lower_cf_func(nir_function *func)
     * motion way easier.
     */
    nir_foreach_block(block, old_impl)
-      nir_lower_phis_to_regs_block(block);
+      nir_lower_phis_to_regs_block(block, false);
 
    /* We create a whole new nir_function_impl and copy the contents over */
    func->impl = NULL;