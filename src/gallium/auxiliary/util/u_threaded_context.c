bminor/mesa-mesa

commit 2eb45daa9c86f4b8bd602ddef7a67233f56f1edf

gallium: de-pointerize pipe_surface
this allows eliminating surface refcounting and objects
which, relatively speaking, don't serve much purpose

see MR for details

Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34054>

src/gallium/auxiliary/util/u_threaded_context.c
@@ -27,6 +27,7 @@
 #include "util/u_threaded_context.h"
 #include "util/u_cpu_detect.h"
 #include "util/format/u_format.h"
+#include "util/u_framebuffer.h"
 #include "util/u_inlines.h"
 #include "util/u_memory.h"
 #include "util/u_upload_mgr.h"
@@ -1415,12 +1416,8 @@ tc_call_set_framebuffer_state(struct pipe_context *pipe, void *call)
    struct pipe_framebuffer_state *p = &to_call(call, tc_framebuffer)->state;
 
    pipe->set_framebuffer_state(pipe, p);
+   util_unreference_framebuffer_state(p);
 
-   unsigned nr_cbufs = p->nr_cbufs;
-   for (unsigned i = 0; i < nr_cbufs; i++)
-      tc_drop_surface_reference(p->cbufs[i]);
-   tc_drop_surface_reference(p->zsbuf);
-   tc_drop_resource_reference(p->resolve);
    return call_size(tc_framebuffer);
 }
 
@@ -1433,12 +1430,7 @@ tc_set_framebuffer_state(struct pipe_context *_pipe,
       tc_add_call(tc, TC_CALL_set_framebuffer_state, tc_framebuffer);
    unsigned nr_cbufs = fb->nr_cbufs;
 
-   p->state.width = fb->width;
-   p->state.height = fb->height;
-   p->state.samples = fb->samples;
-   p->state.layers = fb->layers;
-   p->state.nr_cbufs = nr_cbufs;
-   p->state.viewmask = fb->viewmask;
+   p->state = *fb;
 
    /* when unbinding, mark attachments as used for the current batch */
    for (unsigned i = 0; i < tc->nr_cbufs; i++) {
@@ -1449,11 +1441,11 @@ tc_set_framebuffer_state(struct pipe_context *_pipe,
    tc_set_resource_batch_usage_persistent(tc, tc->fb_resolve, false);
 
    for (unsigned i = 0; i < nr_cbufs; i++) {
-      p->state.cbufs[i] = NULL;
-      pipe_surface_reference(&p->state.cbufs[i], fb->cbufs[i]);
+      /* ref for cmd */
+      struct pipe_resource *ref = NULL;
+      pipe_resource_reference(&ref, fb->cbufs[i].texture);
       /* full tracking requires storing the fb attachment resources */
-      if (fb->cbufs[i])
-         pipe_resource_reference(&tc->fb_resources[i], fb->cbufs[i]->texture);
+      pipe_resource_reference(&tc->fb_resources[i], fb->cbufs[i].texture);
       tc_set_resource_batch_usage_persistent(tc, tc->fb_resources[i], true);
    }
    tc->nr_cbufs = nr_cbufs;
@@ -1469,8 +1461,7 @@ tc_set_framebuffer_state(struct pipe_context *_pipe,
       uint8_t zsbuf = tc->renderpass_info_recording->has_draw ?
                       0 :
                       tc->renderpass_info_recording->data8[3];
-      bool zsbuf_changed = tc->fb_resources[PIPE_MAX_COLOR_BUFS] !=
-                           (fb->zsbuf ? fb->zsbuf->texture : NULL);
+      bool zsbuf_changed = tc->fb_resources[PIPE_MAX_COLOR_BUFS] != fb->zsbuf.texture;
 
       if (tc->seen_fb_state) {
          /* this is the end of a renderpass, so increment the renderpass info */
@@ -1489,16 +1480,19 @@ tc_set_framebuffer_state(struct pipe_context *_pipe,
       /* future fb state changes will increment the index */
       tc->seen_fb_state = true;
    }
-   pipe_resource_reference(&tc->fb_resources[PIPE_MAX_COLOR_BUFS],
-                           fb->zsbuf ? fb->zsbuf->texture : NULL);
+   /* ref for cmd */
+   struct pipe_resource *zsref = NULL;
+   pipe_resource_reference(&zsref, fb->zsbuf.texture);
+   pipe_resource_reference(&tc->fb_resources[PIPE_MAX_COLOR_BUFS], fb->zsbuf.texture);
+
+   /* ref for cmd */
+   struct pipe_resource *rref = NULL;
+   pipe_resource_reference(&rref, fb->resolve);
    pipe_resource_reference(&tc->fb_resolve, fb->resolve);
    tc_set_resource_batch_usage_persistent(tc, tc->fb_resources[PIPE_MAX_COLOR_BUFS], true);
    tc_set_resource_batch_usage_persistent(tc, tc->fb_resolve, true);
    tc->in_renderpass = false;
-   p->state.zsbuf = NULL;
-   pipe_surface_reference(&p->state.zsbuf, fb->zsbuf);
    p->state.resolve = NULL;
-   pipe_resource_reference(&p->state.resolve, fb->resolve);
 }
 
 struct tc_tess_state {

commit 7984a16e27bb30cbe3f6a6361603557c24322e1d

tc: don't unset resolve resource in set_framebuffer_state
this breaks the whole mechanism

Fixes: 2eb45daa9c8 ("gallium: de-pointerize pipe_surface")

Acked-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37166>

src/gallium/auxiliary/util/u_threaded_context.c
@@ -1596,7 +1596,6 @@ tc_set_framebuffer_state(struct pipe_context *_pipe,
    tc_set_resource_batch_usage_persistent(tc, tc->fb_resources[PIPE_MAX_COLOR_BUFS], true);
    tc_set_resource_batch_usage_persistent(tc, tc->fb_resolve, true);
    tc->in_renderpass = false;
-   p->state.resolve = NULL;
 }
 
 struct tc_tess_state {