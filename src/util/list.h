Grima04/mesa-turnip-kgsl

commit 372e83b95f9277fe1d09b1c65c70a38918843b6f

list: add some iterator debug
Debugging use of unsafe iterators when you should have used the _safe
version sucks.  Add some DEBUG build support to catch and assert if
someone does that.

I didn't update the UPPERCASE verions of the iterators.  They should
probably be deprecated/removed.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>

src/util/list.h
@@ -43,6 +43,11 @@
 #include <assert.h>
 #include "c99_compat.h"
 
+#ifdef DEBUG
+#  define list_assert(cond, msg)  assert(cond && msg)
+#else
+#  define list_assert(cond, msg)  (void)(0 && (cond))
+#endif
 
 struct list_head
 {
@@ -212,21 +217,27 @@ static inline void list_validate(const struct list_head *list)
 	pos = container_of(pos->member.prev, pos, member))
 
 #define list_for_each_entry(type, pos, head, member)                    \
-   for (type *pos = LIST_ENTRY(type, (head)->next, member);             \
+   for (type *pos = LIST_ENTRY(type, (head)->next, member),             \
+	     *__next = LIST_ENTRY(type, pos->member.next, member);      \
 	&pos->member != (head);                                         \
-	pos = LIST_ENTRY(type, pos->member.next, member))
+	pos = LIST_ENTRY(type, pos->member.next, member),               \
+	list_assert(pos == __next, "use _safe iterator"),               \
+	__next = LIST_ENTRY(type, __next->member.next, member))
 
 #define list_for_each_entry_safe(type, pos, head, member)               \
    for (type *pos = LIST_ENTRY(type, (head)->next, member),             \
 	     *__next = LIST_ENTRY(type, pos->member.next, member);      \
 	&pos->member != (head);                                         \
 	pos = __next,                                                   \
-        __next = LIST_ENTRY(type, __next->member.next, member))
+	__next = LIST_ENTRY(type, __next->member.next, member))
 
 #define list_for_each_entry_rev(type, pos, head, member)                \
-   for (type *pos = LIST_ENTRY(type, (head)->prev, member);             \
+   for (type *pos = LIST_ENTRY(type, (head)->prev, member),             \
+	     *__prev = LIST_ENTRY(type, pos->member.prev, member);      \
 	&pos->member != (head);                                         \
-	pos = LIST_ENTRY(type, pos->member.prev, member))
+	pos = LIST_ENTRY(type, pos->member.prev, member),               \
+	list_assert(pos == __prev, "use _safe iterator"),               \
+	__prev = LIST_ENTRY(type, __prev->member.prev, member))
 
 #define list_for_each_entry_safe_rev(type, pos, head, member)           \
    for (type *pos = LIST_ENTRY(type, (head)->prev, member),             \

bminor/mesa-mesa

commit 68632230336d79434a04a93e894a81dff105118b

util/list: Fix next instruction removal usecase for non safe iterators
Introducing this iterator debug information breaks the usecase of removing
elements in the list other than the current element.

Fixes: 372e83b95f9

Reviewed-by: Rob Clark <rob.clark@oss.qualcomm.com>
Reviewed-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37061>

src/util/list.h
@@ -44,6 +44,9 @@
 
 #define list_assert(cond, msg)  assert(cond && msg)
 
+#define list_validate_iter(item, msg) \
+   list_assert(list_is_linked(item) && !list_is_empty(item), msg)
+
 struct list_head
 {
     struct list_head *prev;
@@ -234,12 +237,10 @@ static inline void list_move_to(struct list_head *item, struct list_head *loc) {
 	pos = list_container_of(pos->member.prev, pos, member))
 
 #define list_for_each_entry(type, pos, head, member)                    \
-   for (type *pos = list_entry((head)->next, type, member),        \
-	     *__next = list_entry(pos->member.next, type, member); \
+   for (type *pos = list_entry((head)->next, type, member);             \
 	&pos->member != (head);                                         \
-	pos = list_entry(pos->member.next, type, member),          \
-	list_assert(pos == __next, "use _safe iterator"),               \
-	__next = list_entry(__next->member.next, type, member))
+        list_validate_iter(&pos->member, "use _safe iterator"), \
+	pos = list_entry(pos->member.next, type, member))
 
 #define list_for_each_entry_safe(type, pos, head, member)               \
    for (type *pos = list_entry((head)->next, type, member),        \
@@ -249,12 +250,10 @@ static inline void list_move_to(struct list_head *item, struct list_head *loc) {
 	__next = list_entry(__next->member.next, type, member))
 
 #define list_for_each_entry_rev(type, pos, head, member)                \
-   for (type *pos = list_entry((head)->prev, type, member),        \
-	     *__prev = list_entry(pos->member.prev, type, member); \
+   for (type *pos = list_entry((head)->prev, type, member);             \
 	&pos->member != (head);                                         \
-	pos = list_entry(pos->member.prev, type, member),          \
-	list_assert(pos == __prev, "use _safe iterator"),               \
-	__prev = list_entry(__prev->member.prev, type, member))
+        list_validate_iter(&pos->member, "use _safe iterator"), \
+	pos = list_entry(pos->member.prev, type, member))
 
 #define list_for_each_entry_safe_rev(type, pos, head, member)           \
    for (type *pos = list_entry((head)->prev, type, member),        \