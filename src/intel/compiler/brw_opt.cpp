bminor/mesa-mesa

commit 47fe9d28e74b2f4ff6f3f6ec7ae54c3c6241af5a

brw: Enumerate SHADER_OPCODE_SEND sources and standardize how many
This introduces enums for SHADER_OPCODE_SEND[_GATHER] sources, similar
similar to what we've done for most of the newer logical opcodes.  This
allows us to use actual names for sources rather than remembering their
order, or leaving ourselves comments like /* ex_desc */ all over.  It
will also make it easier to add or reorder sources in the future.

While we're at it, we also standardize on the number of sources.
Previously, we allowed SHADER_OPCODE_SEND to have either 3 (monosend) or
4 (split send) sources, but this is mostly for haphazard historical
reasons.  We now specify all sources every time, eliminating the need
for careful inst->source checks before accessing the last source.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34040>

src/intel/compiler/brw_opt.cpp
@@ -341,7 +341,7 @@ brw_opt_split_sends(brw_shader &s)
    foreach_block_and_inst(block, brw_inst, send, s.cfg) {
       if (send->opcode != SHADER_OPCODE_SEND ||
           send->mlen <= reg_unit(s.devinfo) || send->ex_mlen > 0 ||
-          send->src[2].file != VGRF)
+          send->src[SEND_SRC_PAYLOAD1].file != VGRF)
          continue;
 
       /* Currently don't split sends that reuse a previously used payload. */
@@ -350,7 +350,8 @@ brw_opt_split_sends(brw_shader &s)
       if (lp->is_head_sentinel() || lp->opcode != SHADER_OPCODE_LOAD_PAYLOAD)
          continue;
 
-      if (lp->dst.file != send->src[2].file || lp->dst.nr != send->src[2].nr)
+      if (lp->dst.file != send->src[SEND_SRC_PAYLOAD1].file ||
+          lp->dst.nr != send->src[SEND_SRC_PAYLOAD1].nr)
          continue;
 
       /* Split either after the header (if present), or when consecutive
@@ -389,9 +390,9 @@ brw_opt_split_sends(brw_shader &s)
       lp1->dst = retype(brw_allocate_vgrf_units(s, lp1->size_written / REG_SIZE), lp1->dst.type);
       lp2->dst = retype(brw_allocate_vgrf_units(s, lp2->size_written / REG_SIZE), lp2->dst.type);
 
-      send->resize_sources(4);
-      send->src[2] = lp1->dst;
-      send->src[3] = lp2->dst;
+      send->resize_sources(SEND_NUM_SRCS);
+      send->src[SEND_SRC_PAYLOAD1] = lp1->dst;
+      send->src[SEND_SRC_PAYLOAD2] = lp2->dst;
       send->ex_mlen = lp2->size_written / REG_SIZE;
       send->mlen -= send->ex_mlen;
 
@@ -625,8 +626,8 @@ brw_opt_send_to_send_gather(brw_shader &s)
          brw_reg src;
          unsigned phys_len;
       } payload[2] = {
-         { inst->src[2], inst->mlen / unit },
-         { inst->src[3], inst->ex_mlen / unit },
+         { inst->src[SEND_SRC_PAYLOAD1], inst->mlen / unit },
+         { inst->src[SEND_SRC_PAYLOAD2], inst->ex_mlen / unit },
       };
 
       const unsigned num_payload_sources = payload[0].phys_len + payload[1].phys_len;
@@ -640,11 +641,11 @@ brw_opt_send_to_send_gather(brw_shader &s)
          continue;
       }
 
-      inst->resize_sources(3 + num_payload_sources);
+      inst->resize_sources(SEND_GATHER_SRC_PAYLOAD + num_payload_sources);
       /* Sources 0 and 1 remain the same.  Source 2 will be filled
        * after register allocation.
        */
-      inst->src[2] = {};
+      inst->src[SEND_GATHER_SRC_SCALAR] = {};
 
       int idx = 3;
       for (unsigned p = 0; p < ARRAY_SIZE(payload); p++) {
@@ -694,7 +695,7 @@ brw_opt_send_gather_to_send(brw_shader &s)
          continue;
 
       assert(inst->sources > 2);
-      assert(inst->src[2].file == BAD_FILE);
+      assert(inst->src[SEND_GATHER_SRC_SCALAR].file == BAD_FILE);
 
       const int num_payload_sources = inst->sources - 3;
       assert(num_payload_sources > 0);
@@ -707,7 +708,7 @@ brw_opt_send_gather_to_send(brw_shader &s)
        * and there's no need to use SEND_GATHER (which would set ARF scalar register
        * adding an extra instruction).
        */
-      const brw_reg *payload = &inst->src[3];
+      const brw_reg *payload = &inst->src[SEND_GATHER_SRC_PAYLOAD];
       brw_reg payload1       = payload[0];
       brw_reg payload2       = {};
       int payload1_len       = 0;
@@ -758,10 +759,10 @@ brw_opt_send_gather_to_send(brw_shader &s)
             continue;
       }
 
-      inst->resize_sources(4);
+      inst->resize_sources(SEND_NUM_SRCS);
       inst->opcode  = SHADER_OPCODE_SEND;
-      inst->src[2]  = payload1;
-      inst->src[3]  = payload2;
+      inst->src[SEND_SRC_PAYLOAD1] = payload1;
+      inst->src[SEND_SRC_PAYLOAD2] = payload2;
       inst->mlen    = payload1_len * unit;
       inst->ex_mlen = payload2_len * unit;
 

src/intel/compiler/brw_compile_fs.cpp
@@ -640,22 +640,23 @@ brw_emit_repclear_shader(brw_shader &s)
          bld.uniform().MOV(component(header, 2), brw_imm_ud(i));
 
       write = bld.emit(SHADER_OPCODE_SEND);
-      write->resize_sources(3);
+      write->resize_sources(SEND_NUM_SRCS);
 
       /* We can use a headerless message for the first render target */
       write->header_size = i == 0 ? 0 : 2;
       write->mlen = 1 + write->header_size;
 
       write->sfid = BRW_SFID_RENDER_CACHE;
-      write->src[0] = brw_imm_ud(
+      write->src[SEND_SRC_DESC] = brw_imm_ud(
          brw_fb_write_desc(
             s.devinfo, i,
             BRW_DATAPORT_RENDER_TARGET_WRITE_SIMD16_SINGLE_SOURCE_REPLICATED,
             i == key->nr_color_regions - 1, false) |
          brw_message_desc(s.devinfo, write->mlen,
                           0 /* rlen */, write->header_size));
-      write->src[1] = brw_imm_ud(0);
-      write->src[2] = i == 0 ? color_output : header;
+      write->src[SEND_SRC_EX_DESC] = brw_imm_ud(0);
+      write->src[SEND_SRC_PAYLOAD1] = i == 0 ? color_output : header;
+      write->src[SEND_SRC_PAYLOAD2] = brw_reg();
       write->check_tdr = true;
       write->send_has_side_effects = true;
 

src/intel/compiler/brw_eu_defines.h
@@ -567,6 +567,26 @@ enum opcode {
    SHADER_OPCODE_LOAD_REG,
 };
 
+enum send_srcs {
+   /** The 32-bit message descriptor (can be a register) */
+   SEND_SRC_DESC,
+   /** The 32-bit extended message descriptor (can be a register) */
+   SEND_SRC_EX_DESC,
+   /** The leading register for the first SEND payload */
+   SEND_SRC_PAYLOAD1,
+   /** The leading register for the second split-SEND payload */
+   SEND_SRC_PAYLOAD2,
+
+   SEND_NUM_SRCS
+};
+
+enum send_gather_srcs {
+   SEND_GATHER_SRC_DESC,
+   SEND_GATHER_SRC_EX_DESC,
+   SEND_GATHER_SRC_SCALAR,
+   SEND_GATHER_SRC_PAYLOAD
+};
+
 enum fb_write_logical_srcs {
    FB_WRITE_LOGICAL_SRC_COLOR0,      /* REQUIRED */
    FB_WRITE_LOGICAL_SRC_COLOR1,      /* for dual source blend messages */

src/intel/compiler/brw_from_nir.cpp
@@ -4937,10 +4937,13 @@ emit_rt_lsc_fence(const brw_builder &bld,
 
    const brw_builder ubld = bld.exec_all().group(8, 0);
    brw_reg tmp = ubld.vgrf(BRW_TYPE_UD);
-   brw_inst *send = ubld.emit(SHADER_OPCODE_SEND, tmp,
-                             brw_imm_ud(0) /* desc */,
-                             brw_imm_ud(0) /* ex_desc */,
-                             brw_vec8_grf(0, 0) /* payload */);
+   brw_reg srcs[SEND_NUM_SRCS] = {
+      [SEND_SRC_DESC]     = brw_imm_ud(0),
+      [SEND_SRC_EX_DESC]  = brw_imm_ud(0),
+      [SEND_SRC_PAYLOAD1] = brw_vec8_grf(0, 0),
+      [SEND_SRC_PAYLOAD2] = brw_reg(),
+   };
+   brw_inst *send = ubld.emit(SHADER_OPCODE_SEND, tmp, srcs, SEND_NUM_SRCS);
    send->sfid = BRW_SFID_UGM;
    send->desc = lsc_fence_msg_desc(devinfo, scope, flush_type, true);
    send->mlen = reg_unit(devinfo); /* g0 header */

src/intel/compiler/brw_generator.cpp
@@ -1143,9 +1143,15 @@ brw_generator::generate_code(const cfg_t *cfg, int dispatch_width,
          break;
 
       case SHADER_OPCODE_SEND:
+         generate_send(inst, dst, src[SEND_SRC_DESC], src[SEND_SRC_EX_DESC],
+                       src[SEND_SRC_PAYLOAD1], src[SEND_SRC_PAYLOAD2]);
+         send_count++;
+         break;
+
       case SHADER_OPCODE_SEND_GATHER:
-         generate_send(inst, dst, src[0], src[1], src[2],
-                       inst->ex_mlen > 0 ? src[3] : brw_null_reg());
+         generate_send(inst, dst,
+                       src[SEND_GATHER_SRC_DESC], src[SEND_GATHER_SRC_EX_DESC],
+                       src[SEND_GATHER_SRC_SCALAR], brw_null_reg());
          send_count++;
          break;
 

src/intel/compiler/brw_inst.cpp
@@ -210,7 +210,7 @@ brw_inst::is_control_source(unsigned arg) const
 
    case SHADER_OPCODE_SEND:
    case SHADER_OPCODE_SEND_GATHER:
-      return arg == 0 || arg == 1;
+      return arg < SEND_SRC_PAYLOAD1;
 
    case SHADER_OPCODE_MEMORY_LOAD_LOGICAL:
    case SHADER_OPCODE_MEMORY_STORE_LOGICAL:
@@ -246,10 +246,10 @@ brw_inst::is_payload(unsigned arg) const
       return arg == 0;
 
    case SHADER_OPCODE_SEND:
-      return arg == 2 || arg == 3;
+      return arg >= SEND_SRC_PAYLOAD1;
 
    case SHADER_OPCODE_SEND_GATHER:
-      return arg >= 2;
+      return arg >= SEND_GATHER_SRC_SCALAR;
 
    default:
       return false;
@@ -521,15 +521,15 @@ brw_inst::size_read(const struct intel_device_info *devinfo, int arg) const
 {
    switch (opcode) {
    case SHADER_OPCODE_SEND:
-      if (arg == 2) {
+      if (arg == SEND_SRC_PAYLOAD1) {
          return mlen * REG_SIZE;
-      } else if (arg == 3) {
+      } else if (arg == SEND_SRC_PAYLOAD2) {
          return ex_mlen * REG_SIZE;
       }
       break;
 
    case SHADER_OPCODE_SEND_GATHER:
-      if (arg >= 3) {
+      if (arg >= SEND_GATHER_SRC_PAYLOAD) {
          /* SEND_GATHER is Xe3+, so no need to pass devinfo around. */
          const unsigned reg_unit = 2;
          return REG_SIZE * reg_unit;

src/intel/compiler/brw_lower.cpp
@@ -522,9 +522,12 @@ brw_lower_sends_overlapping_payload(brw_shader &s)
 
    foreach_block_and_inst_safe (block, brw_inst, inst, s.cfg) {
       if (inst->opcode == SHADER_OPCODE_SEND && inst->ex_mlen > 0 &&
-          regions_overlap(inst->src[2], inst->mlen * REG_SIZE,
-                          inst->src[3], inst->ex_mlen * REG_SIZE)) {
-         const unsigned arg = inst->mlen < inst->ex_mlen ? 2 : 3;
+          regions_overlap(inst->src[SEND_SRC_PAYLOAD1],
+                          inst->mlen * REG_SIZE,
+                          inst->src[SEND_SRC_PAYLOAD2],
+                          inst->ex_mlen * REG_SIZE)) {
+         const unsigned arg = inst->mlen < inst->ex_mlen ?
+                              SEND_SRC_PAYLOAD1 : SEND_SRC_PAYLOAD2;
          const unsigned len = MIN2(inst->mlen, inst->ex_mlen);
 
          brw_reg tmp = retype(brw_allocate_vgrf_units(s, len), BRW_TYPE_UD);

commit 33d2c31d7ad98cc9aeda9fdbf47aa99a1d23de16

brw: don't use brw_null_reg() for unused SEND sources
Just avoiding the validation assert.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: 47fe9d28e7 ("brw: Enumerate SHADER_OPCODE_SEND sources and standardize how many")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13777
Reviewed-by: Sagar Ghuge <sagar.ghuge@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37112>

src/intel/compiler/brw_opt.cpp
@@ -739,7 +739,7 @@ brw_opt_send_gather_to_send(brw_shader &s)
                break;
          }
       } else {
-         payload2 = brw_null_reg();
+         payload2 = brw_reg();
       }
 
       if (payload1_len + payload2_len != num_payload_sources)