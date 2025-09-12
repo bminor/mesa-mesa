bminor/mesa-mesa

commit 7304423b5c664c7d3054aed552d6a448ad214931

radv: mark RADV_DEBUG=splitfma as deprecated
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37145>

src/amd/vulkan/radv_physical_device.c
@@ -224,7 +224,7 @@ radv_physical_device_init_cache_key(struct radv_physical_device *pdev)
    key->no_rt = !!(instance->debug_flags & RADV_DEBUG_NO_RT);
    key->ps_wave32 = pdev->ps_wave_size == 32;
    key->rt_wave64 = pdev->rt_wave_size == 64;
-   key->split_fma = !!(instance->debug_flags & RADV_DEBUG_SPLIT_FMA);
+   key->split_fma = instance->drirc.split_fma;
    key->ssbo_non_uniform = instance->drirc.ssbo_non_uniform;
    key->tex_non_uniform = instance->drirc.tex_non_uniform;
    key->lower_terminate_to_discard = instance->drirc.lower_terminate_to_discard;

src/amd/vulkan/radv_debug.h
@@ -45,7 +45,7 @@ enum {
    RADV_DEBUG_NO_NGGC = 1ull << 30,
    RADV_DEBUG_DUMP_PROLOGS = 1ull << 31,
    RADV_DEBUG_NO_DMA_BLIT = 1ull << 32,
-   RADV_DEBUG_SPLIT_FMA = 1ull << 33,
+   RADV_DEBUG_SPLIT_FMA = 1ull << 33, /* deprecated */
    RADV_DEBUG_DUMP_EPILOGS = 1ull << 34,
    RADV_DEBUG_NO_FMASK = 1ull << 35,
    RADV_DEBUG_SHADOW_REGS = 1ull << 36,

src/amd/vulkan/radv_instance.c
@@ -224,8 +224,7 @@ radv_init_dri_options(struct radv_instance *instance)
 
    instance->drirc.invariant_geom = driQueryOptionb(&instance->drirc.options, "radv_invariant_geom");
 
-   if (driQueryOptionb(&instance->drirc.options, "radv_split_fma"))
-      instance->debug_flags |= RADV_DEBUG_SPLIT_FMA;
+   instance->drirc.split_fma = driQueryOptionb(&instance->drirc.options, "radv_split_fma");
 
    if (driQueryOptionb(&instance->drirc.options, "radv_disable_dcc"))
       instance->debug_flags |= RADV_DEBUG_NO_DCC;
@@ -449,6 +448,12 @@ radv_CreateInstance(const VkInstanceCreateInfo *pCreateInfo, const VkAllocationC
       fprintf(stderr, "radv: RADV_DEBUG=invariantgeom is deprecated and will it be removed in future Mesa releases. "
                       "Please use radv_invariant_geom=true instead.\n");
       instance->drirc.invariant_geom = true;
+   }
+
+   if (instance->debug_flags & RADV_DEBUG_SPLIT_FMA) {
+      fprintf(stderr, "radv: RADV_DEBUG=splitfma is deprecated and will it be removed in future Mesa releases. "
+                      "Please use radv_split_fma=true instead.\n");
+      instance->drirc.split_fma = true;
    }
 
    *pInstance = radv_instance_to_handle(instance);

src/amd/vulkan/radv_instance.h
@@ -76,6 +76,7 @@ struct radv_instance {
       bool cooperative_matrix2_nv;
       bool no_dynamic_bounds;
       bool invariant_geom;
+      bool split_fma;
       char *app_layer;
       uint8_t override_graphics_shader_version;
       uint8_t override_compute_shader_version;

src/amd/vulkan/radv_shader.c
@@ -51,10 +51,8 @@
 static void
 get_nir_options_for_stage(struct radv_physical_device *pdev, mesa_shader_stage stage)
 {
-   const struct radv_instance *instance = radv_physical_device_instance(pdev);
    nir_shader_compiler_options *options = &pdev->nir_options[stage];
-   bool split_fma =
-      (stage <= MESA_SHADER_GEOMETRY || stage == MESA_SHADER_MESH) && instance->debug_flags & RADV_DEBUG_SPLIT_FMA;
+   const bool split_fma = (stage <= MESA_SHADER_GEOMETRY || stage == MESA_SHADER_MESH) && pdev->cache_key.split_fma;
 
    ac_nir_set_options(&pdev->info, pdev->use_llvm, options);
 

commit a658be8a2432b5fd65ec65317bbf605e02ca38ec

radv: get NIR options after initializing the physical device cache key
Otherwise, radv_split_fma isn't considered.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/13878
Fixes: 7304423b5c6 ("radv: mark RADV_DEBUG=splitfma as deprecated")
Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37293>

src/amd/vulkan/radv_physical_device.c
@@ -2384,8 +2384,6 @@ radv_physical_device_try_create(struct radv_instance *instance, drmDevicePtr drm
    radv_physical_device_get_supported_extensions(pdev, &pdev->vk.supported_extensions);
    radv_physical_device_get_features(pdev, &pdev->vk.supported_features);
 
-   radv_get_nir_options(pdev);
-
 #ifndef _WIN32
    if (drm_device) {
       struct stat primary_stat = {0}, render_stat = {0};
@@ -2412,6 +2410,8 @@ radv_physical_device_try_create(struct radv_instance *instance, drmDevicePtr drm
 #endif
 
    radv_physical_device_init_cache_key(pdev);
+
+   radv_get_nir_options(pdev);
 
    if (radv_device_get_cache_uuid(pdev, pdev->cache_uuid)) {
       result = vk_errorf(instance, VK_ERROR_INITIALIZATION_FAILED, "cannot generate UUID");