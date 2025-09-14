7ef5653b11a40806bd5278093a72d0e0953d5dfa
Collabora's Gfx CI Team <gfx-ci-bot-collabora@contact.daniels.if.required.fdo.invalid>
2025-09-14 00:09:13 +0000

    Uprev ANGLE to 538129c6b3c17dc864101c7a4af4b74b00706f82
    https://github.com/google/angle/compare/1df3b59f8730b56b4770595d4d69f36d5283333f...538129c6b3c17dc864101c7a4af4b74b00706f82
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37356>

--- .gitlab-ci/container/build-angle.sh
+++ .gitlab-ci/container/build-angle.sh
@@ -13,7 +13,7 @@ section_start angle "Building ANGLE"
 # setting up the environment variables locally
 ci_tag_build_time_check "ANGLE_TAG"
 
-ANGLE_REV="1df3b59f8730b56b4770595d4d69f36d5283333f"
+ANGLE_REV="538129c6b3c17dc864101c7a4af4b74b00706f82"
 DEPOT_REV="5982a1aeb33dc36382ed8c62eddf52a6135e7dd3"
 
 # Set ANGLE_ARCH based on DEBIAN_ARCH if it hasn't been explicitly defined
