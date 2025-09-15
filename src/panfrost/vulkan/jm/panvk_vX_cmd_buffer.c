alexvorxx/Mesa-VirGL

commit 0bc3502ca36b7b5f244e5de34813f46ce2ff4a1b

panvk: Implement a custom FB preload logic
This has several advantages over using pan_blitter for that:

- we can catch allocation failures and flag the command buffer invalid
- we can re-use the vk_meta_device object list to keep track of our
  preload shaders
- we can re-use surface descriptors instead of re-emitting them every
  time a preload is done

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Eric R. Smith <eric.smith@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/31441>

src/panfrost/vulkan/jm/panvk_vX_cmd_buffer.c
@@ -32,6 +32,7 @@
 #include "panvk_cmd_alloc.h"
 #include "panvk_cmd_buffer.h"
 #include "panvk_cmd_desc_state.h"
+#include "panvk_cmd_fb_preload.h"
 #include "panvk_cmd_pool.h"
 #include "panvk_cmd_push_constant.h"
 #include "panvk_device.h"
@@ -40,7 +41,6 @@
 #include "panvk_physical_device.h"
 #include "panvk_priv_bo.h"
 
-#include "pan_blitter.h"
 #include "pan_desc.h"
 #include "pan_encoder.h"
 #include "pan_props.h"
@@ -140,22 +140,16 @@ panvk_per_arch(cmd_close_batch)(struct panvk_cmd_buffer *cmdbuf)
                                  panfrost_sample_positions_offset(
                                     pan_sample_pattern(fbinfo->nr_samples));
 
+      if (batch->vtc_jc.first_tiler) {
+         VkResult result = panvk_per_arch(cmd_fb_preload)(cmdbuf);
+	 if (result != VK_SUCCESS)
+            return;
+      }
+
       for (uint32_t i = 0; i < batch->fb.layer_count; i++) {
          VkResult result;
 
          mali_ptr fbd = batch->fb.desc.gpu + (batch->fb.desc_stride * i);
-         if (batch->vtc_jc.first_tiler) {
-            cmdbuf->state.gfx.render.fb.info.bifrost.pre_post.dcds.gpu = 0;
-
-            ASSERTED unsigned num_preload_jobs = GENX(pan_preload_fb)(
-               &dev->blitter.cache, &cmdbuf->desc_pool.base,
-               &cmdbuf->state.gfx.render.fb.info, i, batch->tls.gpu, NULL);
-
-            /* Bifrost GPUs use pre frame DCDs to preload the FB content. We
-             * thus expect num_preload_jobs to be zero.
-             */
-            assert(!num_preload_jobs);
-         }
 
          result = panvk_per_arch(cmd_prepare_tiler_context)(cmdbuf, i);
          if (result != VK_SUCCESS)

src/panfrost/vulkan/csf/panvk_vX_cmd_buffer.c
@@ -40,7 +40,6 @@
 #include "panvk_physical_device.h"
 #include "panvk_priv_bo.h"
 
-#include "pan_blitter.h"
 #include "pan_desc.h"
 #include "pan_encoder.h"
 #include "pan_props.h"

src/panfrost/lib/pan_desc.c
@@ -750,7 +750,17 @@ GENX(pan_emit_fbd)(const struct pan_fb_info *fb, unsigned layer_idx,
                                                   force_clean_write);
       cfg.post_frame = pan_fix_frame_shader_mode(fb->bifrost.pre_post.modes[2],
                                                  force_clean_write);
+#if PAN_ARCH <= 7
+      /* On Bifrost, the layer_id is passed through a push_uniform, which forces
+       * us to have one pre/post DCD array per layer. */
+      cfg.frame_shader_dcds =
+         fb->bifrost.pre_post.dcds.gpu + (layer_idx * 3 * pan_size(DRAW));
+#else
+      /* On Valhall, layer_id is passed through the framebuffer frame_arg, which
+       * is preloaded in r62, so we can use the same pre/post DCD array for all
+       * layers. */
       cfg.frame_shader_dcds = fb->bifrost.pre_post.dcds.gpu;
+#endif
       cfg.tiler =
          PAN_ARCH >= 9 ? tiler_ctx->valhall.desc : tiler_ctx->bifrost.desc;
 #endif

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -16,6 +16,7 @@
 #include "panvk_cmd_alloc.h"
 #include "panvk_cmd_buffer.h"
 #include "panvk_cmd_desc_state.h"
+#include "panvk_cmd_fb_preload.h"
 #include "panvk_cmd_meta.h"
 #include "panvk_device.h"
 #include "panvk_entrypoints.h"
@@ -1977,23 +1978,6 @@ resolve_attachments(struct panvk_cmd_buffer *cmdbuf)
 static uint8_t
 prepare_fb_desc(struct panvk_cmd_buffer *cmdbuf, uint32_t layer, void *fbd)
 {
-   struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
-
-   memset(&cmdbuf->state.gfx.render.fb.info.bifrost.pre_post.dcds, 0,
-          sizeof(cmdbuf->state.gfx.render.fb.info.bifrost.pre_post.dcds));
-
-   if (cmdbuf->state.tls.desc.gpu) {
-      ASSERTED unsigned num_preload_jobs =
-         GENX(pan_preload_fb)(&dev->blitter.cache, &cmdbuf->desc_pool.base,
-                              &cmdbuf->state.gfx.render.fb.info, layer,
-                              cmdbuf->state.tls.desc.gpu, NULL);
-
-      /* Valhall GPUs use pre frame DCDs to preload the FB content. We
-       * thus expect num_preload_jobs to be zero.
-       */
-      assert(!num_preload_jobs);
-   }
-
    struct pan_tiler_context tiler_ctx = {
       .valhall.layer_offset = layer - (layer % MAX_LAYERS_PER_TILER_DESC),
    };
@@ -2092,11 +2076,11 @@ wait_finish_tiling(struct panvk_cmd_buffer *cmdbuf)
                   vt_sync_addr);
 }
 
-static void
+static VkResult
 issue_fragment_jobs(struct panvk_cmd_buffer *cmdbuf)
 {
    if (!cmdbuf->state.gfx.render.fbds.gpu)
-      return;
+      return VK_SUCCESS;
 
    struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
    struct pan_fb_info *fbinfo = &cmdbuf->state.gfx.render.fb.info;
@@ -2131,6 +2115,10 @@ issue_fragment_jobs(struct panvk_cmd_buffer *cmdbuf)
    uint32_t fbd_sz = calc_fbd_size(cmdbuf);
    struct panfrost_ptr fbds = cmdbuf->state.gfx.render.fbds;
    uint8_t fbd_flags = 0;
+
+   VkResult result = panvk_per_arch(cmd_fb_preload)(cmdbuf);
+   if (result != VK_SUCCESS)
+      return result;
 
    /* We prepare all FB descriptors upfront. */
    for (uint32_t i = 0; i < cmdbuf->state.gfx.render.layer_count; i++) {
@@ -2280,6 +2268,8 @@ issue_fragment_jobs(struct panvk_cmd_buffer *cmdbuf)
    memset(&cmdbuf->state.gfx.render.fbds, 0,
           sizeof(cmdbuf->state.gfx.render.fbds));
    cmdbuf->state.gfx.render.tiler = 0;
+
+   return VK_SUCCESS;
 }
 
 void

src/panfrost/vulkan/meson.build
@@ -72,6 +72,7 @@ common_per_arch_files = [
   panvk_entrypoints[0],
   'panvk_vX_blend.c',
   'panvk_vX_buffer_view.c',
+  'panvk_vX_cmd_fb_preload.c',
   'panvk_vX_cmd_desc_state.c',
   'panvk_vX_cmd_meta.c',
   'panvk_vX_cmd_push_constant.c',

src/panfrost/vulkan/panvk_cmd_fb_preload.h
@@ -1,0 +1,13 @@
+/*
+ * Copyright © 2021 Collabora Ltd.
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef PANVK_FB_PRELOAD_H
+#define PANVK_FB_PRELOAD_H
+
+#include "panvk_cmd_buffer.h"
+
+VkResult panvk_per_arch(cmd_fb_preload)(struct panvk_cmd_buffer *cmdbuf);
+
+#endif

src/panfrost/vulkan/panvk_device.h
@@ -20,7 +20,6 @@
 
 #include "kmod/pan_kmod.h"
 #include "util/pan_ir.h"
-#include "pan_blitter.h"
 
 #include "util/vma.h"
 
@@ -42,22 +41,6 @@ struct panvk_device {
 
    struct panvk_priv_bo *tiler_heap;
    struct panvk_priv_bo *sample_positions;
-
-   /* Access to the blitter pools are protected by the blitter
-    * shader/rsd locks. They can't be merged with other binary/desc
-    * pools unless we patch pan_blitter.c to support external pool locks.
-    *
-    * FIXME: The blitter infrastructure is only needed for FB preload.
-    * We should probably consider getting rid of the dependency we have
-    * on pan_desc.c and implement preload ourselves so we don't have
-    * to duplicate caches.
-    */
-   struct {
-      struct panvk_pool bin_pool;
-      struct panvk_pool desc_pool;
-      struct pan_blitter_cache cache;
-      struct pan_blend_shader_cache blend_shader_cache;
-   } blitter;
 
    struct vk_meta_device meta;
 

bminor/mesa-mesa

commit 023972827af13f9dbc91a334fc5b59bb1d5474d7

panvk/jm: Preload the FB even if we have no draws queued
There seems to be cases where we end up with a depth/stencil preload,
with no draw associated to it, so let's make sure we preload in that
case, otherwise we will write invalid data back.

Fixes: 0bc3502ca36b ("panvk: Implement a custom FB preload logic")
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Christoph Pillmayer <christoph.pillmayer@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37158>

src/panfrost/vulkan/jm/panvk_vX_cmd_buffer.c
@@ -149,11 +149,9 @@ panvk_per_arch(cmd_close_batch)(struct panvk_cmd_buffer *cmdbuf)
       fbinfo->first_provoking_vertex =
          cmdbuf->state.gfx.render.first_provoking_vertex != U_TRISTATE_NO;
 
-      if (batch->vtc_jc.first_tiler) {
-         VkResult result = panvk_per_arch(cmd_fb_preload)(cmdbuf, fbinfo);
-         if (result != VK_SUCCESS)
-            return;
-      }
+      VkResult result = panvk_per_arch(cmd_fb_preload)(cmdbuf, fbinfo);
+      if (result != VK_SUCCESS)
+         return;
 
       uint32_t view_mask = cmdbuf->state.gfx.render.view_mask;
       assert(view_mask == 0 || util_bitcount(view_mask) <= batch->fb.layer_count);

src/panfrost/vulkan/panvk_vX_cmd_fb_preload.c
@@ -422,6 +422,13 @@ cmd_emit_dcd(struct panvk_cmd_buffer *cmdbuf, struct pan_fb_info *fbinfo,
 
    struct mali_draw_packed dcd_base;
 
+   /* If we got a preload without any draw, we end up with a NULL TLS
+    * descriptor. Allocate a dummy one (no TLS, no WLS) to get things working. */
+   if (!batch->tls.cpu) {
+      panvk_per_arch(cmd_alloc_tls_desc)(cmdbuf, true);
+      GENX(pan_emit_tls)(&batch->tlsinfo, batch->tls.cpu);
+   }
+
    pan_pack(&dcd_base, DRAW, cfg) {
       cfg.thread_storage = batch->tls.gpu;
       cfg.state = rsd.gpu;