63b3545ba7adf684a63f1124514c8e714bb50275
Collabora's Gfx CI Team <gfx-ci-bot-collabora@contact.daniels.if.required.fdo.invalid>
2025-09-16 00:08:29 +0000

    Uprev Piglit to a70c33045c59310f972dbbdb33f322eb209971bc
    https://gitlab.freedesktop.org/mesa/piglit/-/compare/517270ccca11a795d2f29bd723c362eb6ef9ce8f...a70c33045c59310f972dbbdb33f322eb209971bc
    
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37387>

--- .gitlab-ci/container/build-piglit.sh
+++ .gitlab-ci/container/build-piglit.sh
@@ -13,7 +13,7 @@ section_start piglit "Building piglit"
 # setting up the environment variables locally
 ci_tag_build_time_check "PIGLIT_TAG"
 
-REV="517270ccca11a795d2f29bd723c362eb6ef9ce8f"
+REV="a70c33045c59310f972dbbdb33f322eb209971bc"
 
 git clone https://gitlab.freedesktop.org/mesa/piglit.git --single-branch --no-checkout /piglit
 pushd /piglit
