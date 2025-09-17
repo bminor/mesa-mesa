c0c70cb39285f1d4eaf4fea6b970cb58ab767a6a
Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
2025-09-17 12:39:05 -0400

    nir/lower_flatshade: clean up
    while in the area.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
    Reviewed-by: Dylan Baker <dylan.c.baker@intel.com>
    Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37448>

--- src/compiler/nir/nir_lower_flatshade.c
+++ src/compiler/nir/nir_lower_flatshade.c
@@ -1,56 +1,29 @@
 /*
  * Copyright © 2015 Red Hat
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice (including the next
- * paragraph) shall be included in all copies or substantial portions of the
- * Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * SPDX-License-Identifier: MIT
  */
 
 #include "nir.h"
 #include "nir_builder.h"
 
 static bool
-check_location(int location)
-{
-   return location == VARYING_SLOT_COL0 ||
-          location == VARYING_SLOT_COL1 ||
-          location == VARYING_SLOT_BFC0 ||
-          location == VARYING_SLOT_BFC1;
-}
-
-static bool
-lower_input_io(nir_builder *b, nir_intrinsic_instr *intr, void *data)
+lower(nir_builder *b, nir_intrinsic_instr *intr, void *data)
 {
    if (intr->intrinsic != nir_intrinsic_load_interpolated_input)
       return false;
-   ;
+
    nir_io_semantics sem = nir_intrinsic_io_semantics(intr);
-   if (!check_location(sem.location))
+   if (!(BITFIELD64_BIT(sem.location) & VARYING_BITS_COLOR))
       return false;
-   nir_def *interp = intr->src[0].ssa;
-   nir_intrinsic_instr *interp_intr = nir_def_as_intrinsic(interp);
-   if (nir_intrinsic_interp_mode(interp_intr) != INTERP_MODE_NONE)
+
+   nir_intrinsic_instr *interp = nir_src_as_intrinsic(intr->src[0]);
+   if (nir_intrinsic_interp_mode(interp) != INTERP_MODE_NONE)
       return false;
+
    b->cursor = nir_before_instr(&intr->instr);
    nir_def *load = nir_load_input(b, intr->num_components,
                                   intr->def.bit_size, intr->src[1].ssa);
-   nir_intrinsic_instr *new_intr = nir_def_as_intrinsic(load);
-   nir_intrinsic_copy_const_indices(new_intr, intr);
+   nir_intrinsic_copy_const_indices(nir_def_as_intrinsic(load), intr);
    nir_def_replace(&intr->def, load);
    return true;
 }
@@ -59,6 +32,5 @@ bool
 nir_lower_flatshade(nir_shader *shader)
 {
    assert(shader->info.io_lowered);
-   return nir_shader_intrinsics_pass(shader, lower_input_io, nir_metadata_all,
-                                     NULL);
+   return nir_shader_intrinsics_pass(shader, lower, nir_metadata_all, NULL);
 }
