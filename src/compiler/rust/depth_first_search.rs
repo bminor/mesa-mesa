f91f86ddedfc4db1b91edbec20efc7926a954044
Faith Ekstrand <faith.ekstrand@collabora.com>
2025-09-23 16:33:50 -0400

    compiler/rust: Implement dfs() non-recursively
    Recursion in Rust can be annoyingly expensive.  The recursive DFS
    implementation we have now is probably not terrible but these things
    bloat in debug builds more than you'd expect.  This replaceas it with a
    non-recursive implementation which uses a Vec instead.
    
    Reviewed-by: Mel Henning <mhenning@darkrefraction.com>
    Reviewed-by: Lorenzo Rossi <git@rossilorenzo.dev>
    Reviewed-by: Mary Guillemard <mary@mary.zone>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37536>

--- src/compiler/rust/depth_first_search.rs
+++ src/compiler/rust/depth_first_search.rs
@@ -25,23 +25,9 @@ pub trait DepthFirstSearch {
     }
 }
 
-fn dfs_impl<I, D>(dfs: &mut D, seen: &mut BitSet, id: usize)
-where
-    I: Iterator<Item = usize>,
-    D: DepthFirstSearch<ChildIter = I>,
-{
-    if seen.contains(id) {
-        return;
-    }
-
-    seen.insert(id);
-
-    let children = dfs.pre(id);
-    for child in children {
-        dfs.edge(id, child);
-        dfs_impl(dfs, seen, child);
-    }
-    dfs.post(id);
+struct DFSEntry<I: Iterator<Item = usize>> {
+    id: usize,
+    children: I,
 }
 
 pub fn dfs<I, D>(dfs: &mut D, start: usize)
@@ -50,5 +36,43 @@ where
     D: DepthFirstSearch<ChildIter = I>,
 {
     let mut seen = BitSet::new();
-    dfs_impl(dfs, &mut seen, start);
+    let mut stack = Vec::new();
+
+    seen.insert(start);
+    let children = dfs.pre(start);
+
+    stack.push(DFSEntry {
+        id: start,
+        children,
+    });
+    loop {
+        let Some(entry) = stack.last_mut() else {
+            break;
+        };
+
+        loop {
+            if let Some(id) = entry.children.next() {
+                dfs.edge(entry.id, id);
+
+                if seen.contains(id) {
+                    continue;
+                }
+
+                seen.insert(id);
+                let children = dfs.pre(id);
+
+                stack.push(DFSEntry { id, children });
+            } else {
+                dfs.post(entry.id);
+                stack.pop();
+            }
+
+            // We're only looping here because we want to make the
+            // seen.contains() case a fast path.  Both the case where we
+            // push the stack and recurse into a child or when we pop the
+            // stack want to continue in the next iteration of the outer
+            // loop.
+            break;
+        }
+    }
 }
