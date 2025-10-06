aerisarn/mesa-uwp

commit 137cd3b0fa939ffe66c37ffdcd2ad0e133c42b85

freedreno/drm: Move no_implicit_sync accounting
No need to duplicate this.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/20263>

src/freedreno/drm/freedreno_ringbuffer_sp.c
@@ -302,6 +302,9 @@ fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd, bool use_fence_fd)
 
    bool has_shared = fd_submit_sp_flush_prep(submit, in_fence_fd, out_fence);
 
+   if ((in_fence_fd != -1) || out_fence->use_fence_fd)
+      pipe->no_implicit_sync = true;
+
    /* The rule about skipping submit merging with shared buffers is only
     * needed for implicit-sync.
     */

src/freedreno/drm/msm/msm_ringbuffer_sp.c
@@ -105,7 +105,6 @@ flush_submit_list(struct list_head *submit_list)
    if (fd_submit->in_fence_fd != -1) {
       req.flags |= MSM_SUBMIT_FENCE_FD_IN;
       req.fence_fd = fd_submit->in_fence_fd;
-      pipe->no_implicit_sync = true;
    }
 
    if (pipe->no_implicit_sync) {

src/freedreno/drm/virtio/virtio_ringbuffer.c
@@ -183,10 +183,6 @@ flush_submit_list(struct list_head *submit_list)
     */
    out_fence->use_fence_fd = true;
 
-   if (fd_submit->in_fence_fd != -1) {
-      pipe->no_implicit_sync = true;
-   }
-
    if (pipe->no_implicit_sync) {
       req->flags |= MSM_SUBMIT_NO_IMPLICIT;
    }

bminor/mesa-mesa

commit e60d34fa780579079372a064bc82e52155d0c18a

freedreno: Disable explicit sync heuristic for Xwayland
Xwayland seems to mix implicit and explicit sync, depending on client
app.  This trips up the heuristic that disables implicit sync once it
starts seeing app using explicit sync.  This is not typical behavior,
so add a driconf override to disable the heuristic.

Fixes: 137cd3b0fa93 ("freedreno/drm: Move no_implicit_sync accounting")
Cc: mesa-stable
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37703>

src/freedreno/drm/freedreno_device.c
@@ -228,6 +228,12 @@ fd_device_version(struct fd_device *dev)
    return dev->version;
 }
 
+void
+fd_device_disable_explicit_sync_heuristic(struct fd_device *dev)
+{
+   dev->disable_explicit_sync_heuristic = true;
+}
+
 DEBUG_GET_ONCE_BOOL_OPTION(libgl, "LIBGL_DEBUG", false)
 
 bool

src/freedreno/drm/freedreno_drmif.h
@@ -179,6 +179,7 @@ enum fd_version {
    FD_VERSION_VA_SIZE = 9,
 };
 enum fd_version fd_device_version(struct fd_device *dev);
+void fd_device_disable_explicit_sync_heuristic(struct fd_device *dev);
 
 bool fd_has_syncobj(struct fd_device *dev);
 

src/freedreno/drm/freedreno_priv.h
@@ -222,6 +222,8 @@ struct fd_device {
 
    bool closefd; /* call close(fd) upon destruction */
 
+   bool disable_explicit_sync_heuristic;
+
    /* just for valgrind: */
    int bo_size;
 

src/freedreno/drm/freedreno_ringbuffer_sp.c
@@ -360,7 +360,7 @@ fd_submit_sp_flush(struct fd_submit *submit, int in_fence_fd, bool use_fence_fd)
 
    bool has_shared = fd_submit_sp_flush_prep(submit, in_fence_fd, out_fence);
 
-   if ((in_fence_fd != -1) || out_fence->use_fence_fd)
+   if (((in_fence_fd != -1) || out_fence->use_fence_fd) && !dev->disable_explicit_sync_heuristic)
       pipe->no_implicit_sync = true;
 
    /* The rule about skipping submit merging with shared buffers is only

src/gallium/drivers/freedreno/driinfo_freedreno.h
@@ -2,6 +2,7 @@
 
 DRI_CONF_SECTION_MISCELLANEOUS
    DRI_CONF_DISABLE_CONSERVATIVE_LRZ(false)
+   DRI_CONF_DISABLE_EXPLICIT_SYNC_HEURISTIC(false)
 DRI_CONF_SECTION_END
 
 DRI_CONF_SECTION_DEBUG

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -974,6 +974,8 @@ fd_screen_create(int fd,
          !driQueryOptionb(config->options, "disable_throttling");
    screen->driconf.dual_color_blend_by_location =
          driQueryOptionb(config->options, "dual_color_blend_by_location");
+   if (driQueryOptionb(config->options, "disable_explicit_sync_heuristic"))
+      fd_device_disable_explicit_sync_heuristic(dev);
 
    struct sysinfo si;
    sysinfo(&si);

src/util/00-mesa-defaults.conf
@@ -1300,6 +1300,15 @@ TODO: document the other workarounds.
         <application name="code" executable="vivaldi-bin">
             <option name="force_gl_vendor" value="angleisbroken"/>
         </application>
+        <!--
+            Xwayland mixes implicit and explicit sync, depending on the
+            client app.  This trips up the heuristic that switches to
+            explicit sync once it sees that the "app" is using explicit
+            sync, causing flickering for implicit sync clients
+         -->
+        <application name="Xwayland" executable="Xwayland">
+            <option name="disable_explicit_sync_heuristic" value="true"/>
+        </application>
     </device>
 
     <device driver="turnip">

src/util/driconf.h
@@ -591,6 +591,10 @@
    DRI_CONF_OPT_B(disable_conservative_lrz, def, \
                   "Disable conservative LRZ")
 
+#define DRI_CONF_DISABLE_EXPLICIT_SYNC_HEURISTIC(def) \
+   DRI_CONF_OPT_B(disable_explicit_sync_heuristic, def, \
+                  "Disable Explicit-sync heuristic")
+
 /**
  * \brief panfrost specific configuration options
  */