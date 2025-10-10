bminor/mesa-mesa

commit bf76e86bc82f114b876f764dc1272cbcf54a47fe

brw: Refactor clip/cull distance mask setting into a helper
This was copy pasted between 4 different stages.

Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37784>

src/intel/compiler/brw/brw_from_nir.cpp
@@ -7924,6 +7924,29 @@ brw_test_dispatch_packing(const brw_builder &bld)
    }
 }
 
+static void
+set_clip_cull_distance_masks(brw_shader &s)
+{
+   const shader_info &info = s.nir->info;
+
+   if (info.stage >= MESA_SHADER_FRAGMENT && info.stage != MESA_SHADER_MESH)
+      return;
+
+   struct brw_vue_prog_data *prog_data = brw_vue_prog_data(s.prog_data);
+
+   if (info.outputs_written &
+       (VARYING_BIT_CLIP_DIST0 | VARYING_BIT_CLIP_DIST1 |
+        VARYING_BIT_CULL_DIST0 | VARYING_BIT_CULL_DIST1)) {
+
+      prog_data->clip_distance_mask =
+         ((1 << info.clip_distance_array_size) - 1);
+
+      prog_data->cull_distance_mask =
+         ((1 << info.cull_distance_array_size) - 1)
+         << info.clip_distance_array_size;
+   }
+}
+
 void
 brw_from_nir(brw_shader *s)
 {
@@ -7940,6 +7963,8 @@ brw_from_nir(brw_shader *s)
 
    if (ENABLE_TEST_DISPATCH_PACKING)
       brw_test_dispatch_packing(ntb.bld);
+
+   set_clip_cull_distance_masks(*s);
 
    emit_shader_float_controls_execution_mode(ntb);
 

src/intel/compiler/brw/brw_compile_gs.cpp
@@ -180,18 +180,6 @@ brw_compile_gs(const struct brw_compiler *compiler,
                        params->base.archiver, debug_enabled,
                        key->base.robust_flags);
 
-   const bool has_clip_cull_dist =
-      nir->info.outputs_written & (VARYING_BIT_CLIP_DIST0 |
-                                   VARYING_BIT_CLIP_DIST1 |
-                                   VARYING_BIT_CULL_DIST0 |
-                                   VARYING_BIT_CULL_DIST1);
-   prog_data->base.clip_distance_mask = has_clip_cull_dist ?
-      ((1 << nir->info.clip_distance_array_size) - 1) : 0;
-   prog_data->base.cull_distance_mask =
-      (has_clip_cull_dist ?
-       ((1 << nir->info.cull_distance_array_size) - 1) : 0) <<
-      nir->info.clip_distance_array_size;
-
    prog_data->include_primitive_id =
       BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID);
 

src/intel/compiler/brw/brw_compile_mesh.cpp
@@ -1192,17 +1192,6 @@ brw_compile_mesh(const struct brw_compiler *compiler,
    prog_data->base.local_size[1] = nir->info.workgroup_size[1];
    prog_data->base.local_size[2] = nir->info.workgroup_size[2];
 
-   const bool has_clip_cull_dist =
-      nir->info.outputs_written & (VARYING_BIT_CLIP_DIST0 |
-                                   VARYING_BIT_CLIP_DIST1 |
-                                   VARYING_BIT_CULL_DIST0 |
-                                   VARYING_BIT_CULL_DIST1);
-   prog_data->clip_distance_mask = has_clip_cull_dist ?
-      (1 << nir->info.clip_distance_array_size) - 1 : 0;
-   prog_data->cull_distance_mask =
-      (has_clip_cull_dist ?
-       ((1 << nir->info.cull_distance_array_size) - 1) : 0) <<
-      nir->info.clip_distance_array_size;
    prog_data->primitive_type = nir->info.mesh.primitive_type;
 
    /* Apply this workaround before trying to pack indices because this can

src/intel/compiler/brw/brw_compile_tes.cpp
@@ -113,18 +113,6 @@ brw_compile_tes(const struct brw_compiler *compiler,
       return NULL;
    }
 
-   const bool has_clip_cull_dist =
-      nir->info.outputs_written & (VARYING_BIT_CLIP_DIST0 |
-                                   VARYING_BIT_CLIP_DIST1 |
-                                   VARYING_BIT_CULL_DIST0 |
-                                   VARYING_BIT_CULL_DIST1);
-   prog_data->base.clip_distance_mask = has_clip_cull_dist ?
-      ((1 << nir->info.clip_distance_array_size) - 1) : 0;
-   prog_data->base.cull_distance_mask =
-      (has_clip_cull_dist ?
-       ((1 << nir->info.cull_distance_array_size) - 1) : 0) <<
-      nir->info.clip_distance_array_size;
-
    prog_data->include_primitive_id =
       BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID);
 

src/intel/compiler/brw/brw_compile_vs.cpp
@@ -283,18 +283,6 @@ brw_compile_vs(const struct brw_compiler *compiler,
                        params->base.archiver, debug_enabled,
                        key->base.robust_flags);
 
-   const bool has_clip_cull_dist =
-      nir->info.outputs_written & (VARYING_BIT_CLIP_DIST0 |
-                                   VARYING_BIT_CLIP_DIST1 |
-                                   VARYING_BIT_CULL_DIST0 |
-                                   VARYING_BIT_CULL_DIST1);
-   prog_data->base.clip_distance_mask = has_clip_cull_dist ?
-      ((1 << nir->info.clip_distance_array_size) - 1) : 0;
-   prog_data->base.cull_distance_mask =
-      (has_clip_cull_dist ?
-       ((1 << nir->info.cull_distance_array_size) - 1) : 0) <<
-      nir->info.clip_distance_array_size;
-
    unsigned nr_attribute_slots = util_bitcount64(prog_data->inputs_read);
    /* gl_VertexID and gl_InstanceID are system values, but arrive via an
     * incoming vertex attribute.  So, add an extra slot.

commit dd9e002129fe546fd8052c793824cac4068da571

brw: Fix mesh shader asserts in clip/cull distance setting
mesh doesn't use brw_vue_prog_data.  Also, I had been catching TCS
shaders here, and shouldn't.

Fixes: bf76e86bc82 ("brw: Refactor clip/cull distance mask setting into a helper")
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37809>

src/intel/compiler/brw/brw_from_nir.cpp
@@ -7929,21 +7929,30 @@ set_clip_cull_distance_masks(brw_shader &s)
 {
    const shader_info &info = s.nir->info;
 
-   if (info.stage >= MESA_SHADER_FRAGMENT && info.stage != MESA_SHADER_MESH)
+   if (info.stage != MESA_SHADER_VERTEX &&
+       info.stage != MESA_SHADER_TESS_EVAL &&
+       info.stage != MESA_SHADER_GEOMETRY &&
+       info.stage != MESA_SHADER_MESH)
       return;
-
-   struct brw_vue_prog_data *prog_data = brw_vue_prog_data(s.prog_data);
 
    if (info.outputs_written &
        (VARYING_BIT_CLIP_DIST0 | VARYING_BIT_CLIP_DIST1 |
         VARYING_BIT_CULL_DIST0 | VARYING_BIT_CULL_DIST1)) {
 
-      prog_data->clip_distance_mask =
-         BITFIELD_MASK(info.clip_distance_array_size);
+      uint32_t clip_mask = BITFIELD_MASK(info.clip_distance_array_size);
 
-      prog_data->cull_distance_mask =
-         BITFIELD_RANGE(info.clip_distance_array_size,
-                        info.cull_distance_array_size);
+      uint32_t cull_mask = BITFIELD_RANGE(info.clip_distance_array_size,
+                                          info.cull_distance_array_size);
+
+      if (info.stage == MESA_SHADER_MESH) {
+         struct brw_mesh_prog_data *prog_data = brw_mesh_prog_data(s.prog_data);
+         prog_data->clip_distance_mask = clip_mask;
+         prog_data->cull_distance_mask = cull_mask;
+      } else {
+         struct brw_vue_prog_data *prog_data = brw_vue_prog_data(s.prog_data);
+         prog_data->clip_distance_mask = clip_mask;
+         prog_data->cull_distance_mask = cull_mask;
+      }
    }
 }
 