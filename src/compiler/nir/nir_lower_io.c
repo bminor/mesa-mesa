bminor/mesa-mesa

commit 354df09c8808ec14ac1275a54f8665d1d3f6abdf

nir: add global_amd to nir_get_io_offset_src/nir_get_io_index_src
This is needed for nir_opt_load_skip_helpers.

fossil-db (gfx1201):
Totals from 5 (0.01% of 79839) affected shaders:
Instrs: 2288 -> 2286 (-0.09%); split: -0.13%, +0.04%
CodeSize: 12372 -> 12364 (-0.06%); split: -0.10%, +0.03%
Latency: 18378 -> 20044 (+9.07%)

Signed-off-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Georg Lehmann <dadschoorse@gmail.com>
Fixes: 883b1ca364d ("aco: disable wqm for tex loads when not needed")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36850>

src/compiler/nir/nir_lower_io.c
@@ -974,6 +974,7 @@ nir_get_io_offset_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_load_per_primitive_output:
    case nir_intrinsic_load_interpolated_input:
    case nir_intrinsic_load_smem_amd:
+   case nir_intrinsic_load_global_amd:
    case nir_intrinsic_store_output:
    case nir_intrinsic_store_shared:
    case nir_intrinsic_store_task_payload:
@@ -1003,6 +1004,9 @@ nir_get_io_offset_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_store_ssbo_block_intel:
    case nir_intrinsic_store_buffer_amd:
    case nir_intrinsic_store_ssbo_intel:
+   case nir_intrinsic_store_global_amd:
+   case nir_intrinsic_global_atomic_amd:
+   case nir_intrinsic_global_atomic_swap_amd:
       return 2;
    case nir_intrinsic_load_ssbo_ir3:
       /* This intrinsic has 2 offsets (src1 bytes, src2 dwords), we return the
@@ -1046,6 +1050,9 @@ nir_get_io_index_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_load_per_primitive_output:
    case nir_intrinsic_load_interpolated_input:
    case nir_intrinsic_load_smem_amd:
+   case nir_intrinsic_load_global_amd:
+   case nir_intrinsic_global_atomic_amd:
+   case nir_intrinsic_global_atomic_swap_amd:
    case nir_intrinsic_ldc_nv:
    case nir_intrinsic_ldcx_nv:
    case nir_intrinsic_load_ssbo_intel:
@@ -1061,6 +1068,7 @@ nir_get_io_index_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_store_per_primitive_output:
    case nir_intrinsic_store_ssbo_block_intel:
    case nir_intrinsic_store_ssbo_intel:
+   case nir_intrinsic_store_global_amd:
       return 1;
    default:
       return -1;

commit e08911dff4800aecbb070a900e472ebcb3a60065

nir: fix nir_get_io_offset_src for global_atomic_swap_amd
Fixes: 354df09c880 ("nir: add global_amd to nir_get_io_offset_src/nir_get_io_index_src")

Reviewed-by: Rhys Perry <pendingchaos02@gmail.com>
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37821>

src/compiler/nir/nir_lower_io.c
@@ -1005,7 +1005,6 @@ nir_get_io_offset_src_number(const nir_intrinsic_instr *instr)
    case nir_intrinsic_store_ssbo_intel:
    case nir_intrinsic_store_global_amd:
    case nir_intrinsic_global_atomic_amd:
-   case nir_intrinsic_global_atomic_swap_amd:
       return 2;
    case nir_intrinsic_load_ssbo_ir3:
       /* This intrinsic has 2 offsets (src1 bytes, src2 dwords), we return the
@@ -1016,6 +1015,8 @@ nir_get_io_offset_src_number(const nir_intrinsic_instr *instr)
       /* This intrinsic has 2 offsets (src2 bytes, src3 dwords), we return the
        * dwords one for opt_offsets.
        */
+      return 3;
+   case nir_intrinsic_global_atomic_swap_amd:
       return 3;
    default:
       return -1;