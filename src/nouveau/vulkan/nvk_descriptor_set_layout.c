aerisarn/mesa-uwp

commit 6a74b3e3118298fc9961173d23d8536de29a1f84

nvk: Support VkDescriptorSetVariableDescriptorCountLayoutSupport
On cases with variable descriptor counts we need to calculate the
maximum supported variable count while taking into account the other existing
descriptors and their alignments in the set.

Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/24326>

src/nouveau/vulkan/nvk_descriptor_set_layout.c
@@ -256,6 +256,9 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice _device,
    const VkMutableDescriptorTypeCreateInfoEXT *mutable_info =
       vk_find_struct_const(pCreateInfo->pNext,
                            MUTABLE_DESCRIPTOR_TYPE_CREATE_INFO_EXT);
+   const VkDescriptorSetLayoutBindingFlagsCreateInfo *binding_flags =
+      vk_find_struct_const(pCreateInfo->pNext,
+                           DESCRIPTOR_SET_LAYOUT_BINDING_FLAGS_CREATE_INFO);
 
    /* Figure out the maximum alignment up-front.  Otherwise, we need to sort
     * the list of descriptors by binding number in order to get the size
@@ -274,10 +277,17 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice _device,
       max_align = MAX2(max_align, align);
    }
 
-   uint32_t buffer_size = 0;
+   uint64_t non_variable_size = 0;
+   uint32_t variable_stride = 0;
+   uint32_t variable_count = 0;
    uint8_t dynamic_buffer_count = 0;
+
    for (uint32_t i = 0; i < pCreateInfo->bindingCount; i++) {
       const VkDescriptorSetLayoutBinding *binding = &pCreateInfo->pBindings[i];
+
+      VkDescriptorBindingFlags flags = 0;
+      if (binding_flags != NULL && binding_flags->bindingCount > 0)
+         flags = binding_flags->pBindingFlags[i];
 
       if (binding->descriptorCount == 0)
          continue;
@@ -303,23 +313,59 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice _device,
          assert(stride <= UINT8_MAX);
          assert(util_is_power_of_two_nonzero(align));
 
-         /* Since we're aligning to the maximum and since this is just a
-          * check for whether or not the max buffer size is big enough, we
-          * keep buffer_size aligned to max_align.
-          */
-         buffer_size += stride * binding->descriptorCount;
-         buffer_size = ALIGN_POT(buffer_size, max_align);
+         if (flags & VK_DESCRIPTOR_BINDING_VARIABLE_DESCRIPTOR_COUNT_BIT) {
+            /* From the Vulkan 1.3.256 spec:
+             *
+             *    "For the purposes of this command, a variable-sized
+             *    descriptor binding with a descriptorCount of zero is treated
+             *    as if the descriptorCount is one"
+             */
+            variable_count = MAX2(1, binding->descriptorCount);
+            variable_stride = stride;
+         } else {
+            /* Since we're aligning to the maximum and since this is just a
+             * check for whether or not the max buffer size is big enough, we
+             * keep non_variable_size aligned to max_align.
+             */
+            non_variable_size += stride * binding->descriptorCount;
+            non_variable_size = ALIGN_POT(non_variable_size, max_align);
+         }
       }
    }
 
-   pSupport->supported = dynamic_buffer_count <= NVK_MAX_DYNAMIC_BUFFERS;
+   uint64_t buffer_size = non_variable_size;
+   if (variable_stride > 0) {
+      buffer_size += variable_stride * variable_count;
+      buffer_size = ALIGN_POT(buffer_size, max_align);
+   }
+
+   uint32_t max_buffer_size;
    if (pCreateInfo->flags &
-       VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR) {
-      if (buffer_size > NVK_PUSH_DESCRIPTOR_SET_SIZE)
-         pSupport->supported = false;
-   } else {
-      if (buffer_size > NVK_MAX_DESCRIPTOR_SET_SIZE)
-         pSupport->supported = false;
+       VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR)
+      max_buffer_size = NVK_PUSH_DESCRIPTOR_SET_SIZE;
+   else
+      max_buffer_size = NVK_MAX_DESCRIPTOR_SET_SIZE;
+
+   pSupport->supported = dynamic_buffer_count <= NVK_MAX_DYNAMIC_BUFFERS &&
+                         buffer_size <= max_buffer_size;
+
+   vk_foreach_struct(ext, pSupport->pNext) {
+      switch (ext->sType) {
+      case VK_STRUCTURE_TYPE_DESCRIPTOR_SET_VARIABLE_DESCRIPTOR_COUNT_LAYOUT_SUPPORT: {
+         VkDescriptorSetVariableDescriptorCountLayoutSupport *vs = (void *)ext;
+         if (variable_stride > 0) {
+            vs->maxVariableDescriptorCount =
+               (max_buffer_size - non_variable_size) / variable_stride;
+         } else {
+            vs->maxVariableDescriptorCount = 0;
+         }
+         break;
+      }
+
+      default:
+         nvk_debug_ignored_stype(ext->sType);
+         break;
+      }
    }
 }
 

bminor/mesa-mesa

commit ff7f785f09ce72bbc56a8fd1f879d09ab814c08e

nvk: Fix maxVariableDescriptorCount with iub
Otherwise, we would report very high values for inline uniform block
since NVK_MAX_DESCRIPTOR_SET_SIZE is a lot larger than
NVK_MAX_INLINE_UNIFORM_BLOCK_SIZE.

Fixes
dEQP-VK.api.maintenance3_check.support_count_inline_uniform_block_nonzero_binding_offset
on vulkan-cts-1.4.4.0

Fixes: 6a74b3e3118 ("nvk: Support VkDescriptorSetVariableDescriptorCountLayoutSupport")
Reviewed-by: Mohamed Ahmed <mohamedahmedegypt2001@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37878>

src/nouveau/vulkan/nvk_descriptor_set_layout.c
@@ -448,6 +448,7 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice device,
    uint64_t non_variable_size = 0;
    uint32_t variable_stride = 0;
    uint32_t variable_count = 0;
+   bool variable_is_inline_uniform_block = false;
    uint8_t dynamic_buffer_count = 0;
 
    for (uint32_t i = 0; i < pCreateInfo->bindingCount; i++) {
@@ -472,6 +473,9 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice device,
       if (stride > 0) {
          assert(stride <= UINT8_MAX);
          assert(util_is_power_of_two_nonzero(alignment));
+
+         variable_is_inline_uniform_block =
+            binding->descriptorType == VK_DESCRIPTOR_TYPE_INLINE_UNIFORM_BLOCK;
 
          if (flags & VK_DESCRIPTOR_BINDING_VARIABLE_DESCRIPTOR_COUNT_BIT) {
             /* From the Vulkan 1.3.256 spec:
@@ -503,6 +507,8 @@ nvk_GetDescriptorSetLayoutSupport(VkDevice device,
    if (pCreateInfo->flags &
        VK_DESCRIPTOR_SET_LAYOUT_CREATE_PUSH_DESCRIPTOR_BIT_KHR)
       max_buffer_size = NVK_MAX_PUSH_DESCRIPTORS * nvk_max_descriptor_size(&pdev->info);
+   else if (variable_is_inline_uniform_block)
+      max_buffer_size = NVK_MAX_INLINE_UNIFORM_BLOCK_SIZE;
    else
       max_buffer_size = NVK_MAX_DESCRIPTOR_SET_SIZE;
 

src/nouveau/vulkan/nvk_physical_device.c
@@ -970,7 +970,7 @@ nvk_get_device_properties(const struct nvk_instance *instance,
       .maxSubgroupSize = 32,
       .maxComputeWorkgroupSubgroups = 1024 / 32,
       .requiredSubgroupSizeStages = 0,
-      .maxInlineUniformBlockSize = 1 << 16,
+      .maxInlineUniformBlockSize = NVK_MAX_INLINE_UNIFORM_BLOCK_SIZE,
       .maxPerStageDescriptorInlineUniformBlocks = 32,
       .maxPerStageDescriptorUpdateAfterBindInlineUniformBlocks = 32,
       .maxDescriptorSetInlineUniformBlocks = 6 * 32,

src/nouveau/vulkan/nvk_private.h
@@ -24,6 +24,7 @@
 #define NVK_MAX_DESCRIPTOR_SIZE 32
 #define NVK_MAX_PUSH_DESCRIPTORS 32
 #define NVK_MAX_DESCRIPTOR_SET_SIZE (1u << 30)
+#define NVK_MAX_INLINE_UNIFORM_BLOCK_SIZE (1u << 16)
 #define NVK_MAX_DESCRIPTORS (1 << 20)
 #define NVK_PUSH_DESCRIPTOR_SET_SIZE \
    (NVK_MAX_PUSH_DESCRIPTORS * NVK_MAX_DESCRIPTOR_SIZE)