1271850b9bb42dfa0a83b9fa3f96614d61a4502f
Marek Olšák <marek.olsak@amd.com>
2025-10-15 03:49:37 -0400

    radv: don't check VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT for surf_index
    already checked by use_tile_swizzle
    
    Reviewed-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38093>

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -1079,8 +1079,7 @@ radv_get_ac_surf_info(struct radv_device *device, const struct radv_image *image
        !(image->vk.create_flags & (VK_IMAGE_CREATE_SPARSE_ALIASED_BIT | VK_IMAGE_CREATE_ALIAS_BIT |
                                    VK_IMAGE_CREATE_DESCRIPTOR_BUFFER_CAPTURE_REPLAY_BIT_EXT)) &&
        !(image->vk.usage & (VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR | VK_IMAGE_USAGE_VIDEO_ENCODE_SRC_BIT_KHR |
-                            VK_IMAGE_USAGE_HOST_TRANSFER_BIT)) &&
-       image->vk.tiling != VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
+                            VK_IMAGE_USAGE_HOST_TRANSFER_BIT))) {
       info.surf_index = &device->image_mrt_offset_counter;
       info.fmask_surf_index = &device->fmask_mrt_offset_counter;
    }
