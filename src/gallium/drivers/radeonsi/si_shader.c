3d01529316e88c872b4acf4127df5ebecbeb4a84
Qiang Yu <yuq825@gmail.com>
2025-10-17 17:16:34 +0800

    radeonsi: si_calculate_max_simd_waves support task and mesh shader
    Reviewed-by: Marek Olšák <marek.olsak@amd.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38044>

--- src/gallium/drivers/radeonsi/si_shader.c
+++ src/gallium/drivers/radeonsi/si_shader.c
@@ -243,6 +243,8 @@ static void si_calculate_max_simd_waves(struct si_shader *shader)
       lds_per_wave = align(conf->lds_size, lds_increment) +
                      align(shader->info.num_ps_inputs * 48, lds_increment);
       break;
+   case MESA_SHADER_TASK:
+   case MESA_SHADER_MESH:
    case MESA_SHADER_COMPUTE: {
          unsigned max_workgroup_size = si_get_max_workgroup_size(shader);
          lds_per_wave = align(conf->lds_size, lds_increment) / DIV_ROUND_UP(max_workgroup_size, shader->wave_size);
