bminor/mesa-mesa

commit a91e0e0d616f857144c8cadfaed734ac5be8e729

brw: add support for separate tessellation shader compilation
Tessellation factors have to be written dynamically (based on the next
shader primitive topology) and the builtins read using a dynamic
offset (based on the preceeding shader's VUE).

Anv is updated to use this new infrastructure for dynamic
patch_control_points.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Reviewed-by: Caio Oliveira <caio.oliveira@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34872>

src/intel/vulkan/genX_gfx_state.c
@@ -2440,10 +2440,19 @@ cmd_buffer_flush_gfx_runtime_state(struct anv_gfx_dynamic_state *hw_state,
     * reemit if needed.
     */
    const struct brw_tcs_prog_data *tcs_prog_data = get_gfx_tcs_prog_data(gfx);
-   if (tcs_prog_data && tcs_prog_data->input_vertices == 0 &&
+   const bool tcs_dynamic =
+      tcs_prog_data && tcs_prog_data->input_vertices == 0;
+   if (tcs_dynamic &&
        ((gfx->dirty & ANV_CMD_DIRTY_HS) ||
-        BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_TS_PATCH_CONTROL_POINTS)))
-      SET(TCS_INPUT_VERTICES, tcs_input_vertices, dyn->ts.patch_control_points);
+        BITSET_TEST(dyn->dirty, MESA_VK_DYNAMIC_TS_PATCH_CONTROL_POINTS))) {
+      SET(TESS_CONFIG, tess_config,
+          intel_tess_config(dyn->ts.patch_control_points,
+                            tcs_prog_data->instances,
+                            0,
+                            tcs_prog_data->base.vue_map.num_per_patch_slots,
+                            tcs_prog_data->base.vue_map.num_per_vertex_slots,
+                            tcs_prog_data->base.vue_map.builtins_slot_offset));
+   }
 
 #if INTEL_WA_18019110168_GFX_VER
    const struct brw_mesh_prog_data *mesh_prog_data = get_gfx_mesh_prog_data(gfx);
@@ -3388,7 +3397,7 @@ genX(emit_urb_setup)(struct anv_batch *batch,
    }
 
 #if GFX_VERx10 >= 125
-   if (device->vk.enabled_extensions.EXT_mesh_shader) {
+   if (device->vk.enabled_features.meshShader) {
       anv_batch_emit(batch, GENX(3DSTATE_URB_ALLOC_TASK), urb) {
          if (urb_cfg->size[MESA_SHADER_TASK] > 0)
             urb.TASKURBEntryAllocationSize = urb_cfg->size[MESA_SHADER_TASK] - 1;
@@ -3463,8 +3472,8 @@ cmd_buffer_gfx_state_emission(struct anv_cmd_buffer *cmd_buffer)
     * Values provided by push constants
     */
 
-   if (IS_DIRTY(TCS_INPUT_VERTICES)) {
-      push_consts->gfx.tcs_input_vertices = dyn->ts.patch_control_points;
+   if (IS_DIRTY(TESS_CONFIG)) {
+      push_consts->gfx.tess_config = hw_state->tess_config;
       cmd_buffer->state.push_constants_dirty |= VK_SHADER_STAGE_TESSELLATION_CONTROL_BIT;
       gfx->base.push_constants_data_dirty = true;
    }

src/intel/compiler/brw_compile_tcs.cpp
@@ -204,14 +204,13 @@ brw_compile_tcs(const struct brw_compiler *compiler,
    brw_compute_tess_vue_map(&vue_prog_data->vue_map,
                             nir->info.outputs_written,
                             nir->info.patch_outputs_written,
-                            nir->info.separate_shader);
+                            key->separate_tess_vue_layout);
 
    brw_nir_apply_key(nir, compiler, &key->base, dispatch_width);
    brw_nir_lower_vue_inputs(nir, &input_vue_map);
-   brw_nir_lower_tcs_outputs(nir, &vue_prog_data->vue_map,
+   brw_nir_lower_tcs_outputs(nir, devinfo, &vue_prog_data->vue_map,
                              key->_tes_primitive_mode);
-   if (key->input_vertices > 0)
-      intel_nir_lower_patch_vertices_in(nir, key->input_vertices, NULL, NULL);
+   intel_nir_lower_patch_vertices_in(nir, key->input_vertices);
 
    brw_postprocess_nir(nir, compiler, debug_enabled,
                        key->base.robust_flags);

src/intel/compiler/brw_compiler.h
@@ -319,7 +319,10 @@ struct brw_tcs_prog_key
    /** A bitfield of per-patch outputs written. */
    uint32_t patch_outputs_written;
 
-   uint32_t padding;
+   /** Tesselation VUE layout */
+   bool separate_tess_vue_layout:1;
+
+   uint32_t padding:31;
 };
 
 #define BRW_MAX_TCS_INPUT_VERTICES (32)
@@ -342,7 +345,10 @@ struct brw_tes_prog_key
    /** A bitfield of per-patch inputs read. */
    uint32_t patch_inputs_read;
 
-   uint32_t padding;
+   /** Tesselation VUE layout */
+   bool separate_tess_vue_layout:1;
+
+   uint32_t padding:31;
 };
 
 /** The program key for Geometry Shaders. */
@@ -1199,11 +1205,23 @@ struct brw_tcs_prog_data
    /** Should the non-SINGLE_PATCH payload provide primitive ID? */
    bool include_primitive_id;
 
+   /** Whether the tessellation domain is unknown at compile time
+    *
+    * Used with VK_EXT_shader_object
+    */
+   bool dynamic_domain;
+
    /** Number vertices in output patch */
    int instances;
 
    /** Track patch count threshold */
    int patch_count_threshold;
+
+   /**
+    * Push constant location of intel_tess_config (dynamic configuration of
+    * the tessellation shaders).
+    */
+   unsigned tess_config_param;
 };
 
 
@@ -1215,6 +1233,12 @@ struct brw_tes_prog_data
    enum intel_tess_output_topology output_topology;
    enum intel_tess_domain domain;
    bool include_primitive_id;
+
+   /**
+    * Push constant location of intel_tess_config (dynamic configuration of
+    * the tessellation shaders).
+    */
+   unsigned tess_config_param;
 };
 
 struct brw_gs_prog_data

src/intel/compiler/brw_compile_tes.cpp
@@ -9,6 +9,7 @@
 #include "brw_generator.h"
 #include "brw_nir.h"
 #include "brw_private.h"
+#include "intel_nir.h"
 #include "dev/intel_debug.h"
 #include "util/macros.h"
 
@@ -65,7 +66,7 @@ brw_compile_tes(const struct brw_compiler *compiler,
    const struct intel_device_info *devinfo = compiler->devinfo;
    nir_shader *nir = params->base.nir;
    const struct brw_tes_prog_key *key = params->key;
-   const struct intel_vue_map *input_vue_map = params->input_vue_map;
+   struct intel_vue_map input_vue_map;
    struct brw_tes_prog_data *prog_data = params->prog_data;
    const unsigned dispatch_width = brw_geometry_stage_dispatch_width(compiler->devinfo);
 
@@ -73,12 +74,23 @@ brw_compile_tes(const struct brw_compiler *compiler,
 
    brw_prog_data_init(&prog_data->base.base, &params->base);
 
-   nir->info.inputs_read = key->inputs_read;
-   nir->info.patch_inputs_read = key->patch_inputs_read;
+   if (params->input_vue_map != NULL) {
+      assert(!key->separate_tess_vue_layout);
+      nir->info.inputs_read = key->inputs_read;
+      nir->info.patch_inputs_read = key->patch_inputs_read;
+      memcpy(&input_vue_map, params->input_vue_map,
+             sizeof(input_vue_map));
+   } else {
+      brw_compute_tess_vue_map(&input_vue_map,
+                               nir->info.inputs_read,
+                               nir->info.patch_inputs_read,
+                               key->separate_tess_vue_layout);
+   }
 
    brw_nir_apply_key(nir, compiler, &key->base, dispatch_width);
-   brw_nir_lower_tes_inputs(nir, input_vue_map);
+   brw_nir_lower_tes_inputs(nir, &input_vue_map);
    brw_nir_lower_vue_outputs(nir);
+   NIR_PASS(_, nir, intel_nir_lower_patch_vertices_tes);
    brw_postprocess_nir(nir, compiler, debug_enabled,
                        key->base.robust_flags);
 
@@ -155,7 +167,7 @@ brw_compile_tes(const struct brw_compiler *compiler,
 
    if (unlikely(debug_enabled)) {
       fprintf(stderr, "TES Input ");
-      brw_print_vue_map(stderr, input_vue_map, MESA_SHADER_TESS_EVAL);
+      brw_print_vue_map(stderr, &input_vue_map, MESA_SHADER_TESS_EVAL);
       fprintf(stderr, "TES Output ");
       brw_print_vue_map(stderr, &prog_data->base.vue_map,
                         MESA_SHADER_TESS_EVAL);

src/intel/compiler/brw_from_nir.cpp
@@ -3321,6 +3321,11 @@ brw_from_nir_emit_tcs_intrinsic(nir_to_brw_state &ntb,
       break;
    }
 
+   case nir_intrinsic_load_tess_config_intel:
+      bld.MOV(retype(dst, BRW_TYPE_UD),
+              brw_uniform_reg(tcs_prog_data->tess_config_param, BRW_TYPE_UD));
+      break;
+
    default:
       brw_from_nir_emit_intrinsic(ntb, bld, instr);
       break;
@@ -3429,6 +3434,12 @@ brw_from_nir_emit_tes_intrinsic(nir_to_brw_state &ntb,
       }
       break;
    }
+
+   case nir_intrinsic_load_tess_config_intel:
+      bld.MOV(retype(dest, BRW_TYPE_UD),
+              brw_uniform_reg(tes_prog_data->tess_config_param, BRW_TYPE_UD));
+      break;
+
    default:
       brw_from_nir_emit_intrinsic(ntb, bld, instr);
       break;

src/intel/compiler/brw_nir.c
@@ -142,15 +142,45 @@ type_size_dvec4(const struct glsl_type *type, bool bindless)
 }
 
 static bool
-remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr,
-                  enum tess_primitive_mode _primitive_mode)
+is_input(nir_intrinsic_instr *intrin)
 {
-   const int location = nir_intrinsic_base(intr);
+   return intrin->intrinsic == nir_intrinsic_load_input ||
+          intrin->intrinsic == nir_intrinsic_load_per_primitive_input ||
+          intrin->intrinsic == nir_intrinsic_load_per_vertex_input ||
+          intrin->intrinsic == nir_intrinsic_load_interpolated_input;
+}
+
+static bool
+is_output(nir_intrinsic_instr *intrin)
+{
+   return intrin->intrinsic == nir_intrinsic_load_output ||
+          intrin->intrinsic == nir_intrinsic_load_per_vertex_output ||
+          intrin->intrinsic == nir_intrinsic_load_per_view_output ||
+          intrin->intrinsic == nir_intrinsic_store_output ||
+          intrin->intrinsic == nir_intrinsic_store_per_vertex_output ||
+          intrin->intrinsic == nir_intrinsic_store_per_view_output;
+}
+
+static bool
+remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr, void *data)
+{
+   if (!(b->shader->info.stage == MESA_SHADER_TESS_CTRL && is_output(intr)) &&
+       !(b->shader->info.stage == MESA_SHADER_TESS_EVAL && is_input(intr)))
+      return false;
+
+   /* Handled in a different pass */
+   nir_io_semantics io_sem = nir_intrinsic_io_semantics(intr);
+   if (io_sem.location != VARYING_SLOT_TESS_LEVEL_INNER &&
+       io_sem.location != VARYING_SLOT_TESS_LEVEL_OUTER)
+      return false;
+
    const unsigned component = nir_intrinsic_component(intr);
    bool out_of_bounds = false;
    bool write = !nir_intrinsic_infos[intr->intrinsic].has_dest;
    unsigned mask = write ? nir_intrinsic_write_mask(intr) : 0;
    nir_def *src = NULL, *dest = NULL;
+
+   enum tess_primitive_mode _primitive_mode = (uintptr_t)data;
 
    if (write) {
       assert(intr->num_components == intr->src[0].ssa->num_components);
@@ -158,7 +188,7 @@ remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr,
       assert(intr->num_components == intr->def.num_components);
    }
 
-   if (location == VARYING_SLOT_TESS_LEVEL_INNER) {
+   if (io_sem.location == VARYING_SLOT_TESS_LEVEL_INNER) {
       b->cursor = write ? nir_before_instr(&intr->instr)
                         : nir_after_instr(&intr->instr);
 
@@ -201,7 +231,7 @@ remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr,
       default:
          UNREACHABLE("Bogus tessellation domain");
       }
-   } else if (location == VARYING_SLOT_TESS_LEVEL_OUTER) {
+   } else {
       b->cursor = write ? nir_before_instr(&intr->instr)
                         : nir_after_instr(&intr->instr);
 
@@ -253,8 +283,6 @@ remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr,
       default:
          UNREACHABLE("Bogus tessellation domain");
       }
-   } else {
-      return false;
    }
 
    if (out_of_bounds) {
@@ -275,72 +303,190 @@ remap_tess_levels(nir_builder *b, nir_intrinsic_instr *intr,
 }
 
 static bool
-is_input(nir_intrinsic_instr *intrin)
+remap_tess_header_values(nir_shader *nir, enum tess_primitive_mode _primitive_mode)
 {
-   return intrin->intrinsic == nir_intrinsic_load_input ||
-          intrin->intrinsic == nir_intrinsic_load_per_primitive_input ||
-          intrin->intrinsic == nir_intrinsic_load_per_vertex_input ||
-          intrin->intrinsic == nir_intrinsic_load_interpolated_input;
+   return nir_shader_intrinsics_pass(nir, remap_tess_levels,
+                                     nir_metadata_control_flow,
+                                     (void*)(uintptr_t)_primitive_mode);
 }
 
-static bool
-is_output(nir_intrinsic_instr *intrin)
-{
-   return intrin->intrinsic == nir_intrinsic_load_output ||
-          intrin->intrinsic == nir_intrinsic_load_per_vertex_output ||
-          intrin->intrinsic == nir_intrinsic_load_per_view_output ||
-          intrin->intrinsic == nir_intrinsic_store_output ||
-          intrin->intrinsic == nir_intrinsic_store_per_vertex_output ||
-          intrin->intrinsic == nir_intrinsic_store_per_view_output;
-}
-
-struct remap_patch_urb_offset_state {
-   const struct intel_vue_map *vue_map;
-   enum tess_primitive_mode tes_primitive_mode;
+struct tess_levels_temporary_state {
+   nir_variable *inner_factors_var;
+   nir_variable *outer_factors_var;
 };
 
 static bool
-remap_patch_urb_offsets(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
+remap_tess_levels_to_temporary(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
 {
-   struct remap_patch_urb_offset_state *state = data;
+   if (!(b->shader->info.stage == MESA_SHADER_TESS_CTRL && is_output(intrin)))
+      return false;
 
+   /* Handled in a different pass */
+   nir_io_semantics io_sem = nir_intrinsic_io_semantics(intrin);
+   if (io_sem.location != VARYING_SLOT_TESS_LEVEL_INNER &&
+       io_sem.location != VARYING_SLOT_TESS_LEVEL_OUTER)
+      return false;
+
+   struct tess_levels_temporary_state *state = data;
+
+   nir_variable *var = io_sem.location == VARYING_SLOT_TESS_LEVEL_INNER ?
+      state->inner_factors_var : state->outer_factors_var;
+   if (nir_intrinsic_infos[intrin->intrinsic].has_dest) {
+      b->cursor = nir_after_instr(&intrin->instr);
+      nir_def *new_val =
+         nir_load_array_var(b, var,
+                            nir_iadd_imm(b, nir_get_io_offset_src(intrin)->ssa,
+                                            nir_intrinsic_component(intrin)));
+      nir_def_replace(&intrin->def, new_val);
+   } else {
+      b->cursor = nir_instr_remove(&intrin->instr);
+      nir_store_array_var(b, var,
+                          nir_iadd_imm(b, nir_get_io_offset_src(intrin)->ssa,
+                                          nir_intrinsic_component(intrin)),
+                          intrin->src[0].ssa,
+                          nir_intrinsic_write_mask(intrin));
+   }
+
+   return true;
+}
+
+static bool
+remap_tess_header_values_dynamic(nir_shader *nir, const struct intel_device_info *devinfo)
+{
+   nir_function_impl *impl = nir_shader_get_entrypoint(nir);
+
+   struct tess_levels_temporary_state state = {
+      .inner_factors_var = nir_local_variable_create(
+         impl, glsl_array_type(glsl_uint_type(), 2, 0),
+         "__temp_inner_factors"),
+      .outer_factors_var = nir_local_variable_create(
+         impl, glsl_array_type(glsl_uint_type(), 4, 0),
+         "__temp_outer_factors"),
+   };
+
+   nir_shader_intrinsics_pass(nir, remap_tess_levels_to_temporary,
+                              nir_metadata_control_flow, &state);
+
+   nir_builder _b = nir_builder_at(nir_after_impl(impl)), *b = &_b;
+
+   nir_def *tess_config = nir_load_tess_config_intel(b);
+   nir_def *is_quad =
+      nir_test_mask(b, tess_config, INTEL_TESS_CONFIG_QUADS);
+   nir_def *is_tri =
+      nir_test_mask(b, tess_config, INTEL_TESS_CONFIG_TRIANGLES);
+   nir_def *is_quad_tri =
+      nir_test_mask(b, tess_config, (INTEL_TESS_CONFIG_QUADS |
+                                     INTEL_TESS_CONFIG_TRIANGLES));
+   nir_def *zero = nir_imm_int(b, 0);
+
+   /* Format below is described in the SKL PRMs, Volume 7: 3D-Media-GPGPU,
+    * Patch URB Entry (Patch Record) Output, Patch Header DW0-7
+    *
+    * Based on topology we use one of those :
+    *    - Patch Header: QUAD Domain / LEGACY Patch Header Layout
+    *    - Patch Header: TRI Domain / LEGACY Patch Header Layout
+    *    - Patch Header: ISOLINE Domain / LEGACY Patch Header Layout
+    *
+    * There are more convenient layouts in more recent generations but they're
+    * not available on all platforms.
+    */
+   nir_def *values[8] = {
+      zero,
+      zero,
+      nir_bcsel(b, is_quad_tri, nir_load_array_var_imm(b, state.inner_factors_var, 1), zero),
+      nir_bcsel(b, is_quad_tri, nir_load_array_var_imm(b, state.inner_factors_var, 0), zero),
+
+      nir_bcsel(b, is_quad, nir_load_array_var_imm(b, state.outer_factors_var, 3),
+                            nir_bcsel(b, is_tri, nir_load_array_var_imm(b, state.inner_factors_var, 0),
+                                                 zero)),
+      nir_bcsel(b, is_quad_tri, nir_load_array_var_imm(b, state.outer_factors_var, 2), zero),
+      nir_bcsel(b, is_quad_tri, nir_load_array_var_imm(b, state.outer_factors_var, 1),
+                                nir_load_array_var_imm(b, state.outer_factors_var, 0)),
+      nir_bcsel(b, is_quad_tri, nir_load_array_var_imm(b, state.outer_factors_var, 0),
+                                nir_load_array_var_imm(b, state.outer_factors_var, 1)),
+   };
+
+   nir_store_output(b, nir_vec(b, &values[0], 4), zero, .base = 0,
+                    .io_semantics.location = VARYING_SLOT_TESS_LEVEL_INNER);
+   nir_store_output(b, nir_vec(b, &values[4], 4), zero, .base = 1,
+                    .io_semantics.location = VARYING_SLOT_TESS_LEVEL_OUTER);
+
+   nir_progress(true, impl, nir_metadata_none);
+
+   return true;
+}
+
+static bool
+remap_patch_urb_offsets_instr(nir_builder *b, nir_intrinsic_instr *intrin, void *data)
+{
    if (!(b->shader->info.stage == MESA_SHADER_TESS_CTRL && is_output(intrin)) &&
        !(b->shader->info.stage == MESA_SHADER_TESS_EVAL && is_input(intrin)))
       return false;
 
-   if (remap_tess_levels(b, intrin, state->tes_primitive_mode))
+   /* Handled in a different pass */
+   nir_io_semantics io_sem = nir_intrinsic_io_semantics(intrin);
+   if (io_sem.location == VARYING_SLOT_TESS_LEVEL_INNER ||
+       io_sem.location == VARYING_SLOT_TESS_LEVEL_OUTER)
       return false;
 
-   int vue_slot = state->vue_map->varying_to_slot[intrin->const_index[0]];
+   gl_varying_slot varying = intrin->const_index[0];
+
+   const struct intel_vue_map *vue_map = data;
+   int vue_slot = vue_map->varying_to_slot[varying];
    assert(vue_slot != -1);
    intrin->const_index[0] = vue_slot;
 
    nir_src *vertex = nir_get_io_arrayed_index_src(intrin);
    if (vertex) {
-      if (nir_src_is_const(*vertex)) {
-         intrin->const_index[0] += nir_src_as_uint(*vertex) *
-            state->vue_map->num_per_vertex_slots;
-      } else {
-         b->cursor = nir_before_instr(&intrin->instr);
+      b->cursor = nir_before_instr(&intrin->instr);
 
-         /* Multiply by the number of per-vertex slots. */
-         nir_def *vertex_offset =
-            nir_imul(b,
-                     vertex->ssa,
-                     nir_imm_int(b,
-                                 state->vue_map->num_per_vertex_slots));
+      bool dyn_tess_config =
+         b->shader->info.stage == MESA_SHADER_TESS_EVAL &&
+         vue_map->layout != INTEL_VUE_LAYOUT_FIXED;
+      nir_def *num_per_vertex_slots =
+         dyn_tess_config ? intel_nir_tess_field(b, PER_VERTEX_SLOTS) :
+         nir_imm_int(b, vue_map->num_per_vertex_slots);
 
-         /* Add it to the existing offset */
-         nir_src *offset = nir_get_io_offset_src(intrin);
-         nir_def *total_offset =
-            nir_iadd(b, vertex_offset,
-                     offset->ssa);
+      /* Multiply by the number of per-vertex slots. */
+      nir_def *vertex_offset = nir_imul(b, vertex->ssa, num_per_vertex_slots);
 
-         nir_src_rewrite(offset, total_offset);
+      /* Add it to the existing offset */
+      nir_src *offset = nir_get_io_offset_src(intrin);
+      nir_def *total_offset = nir_iadd(b, vertex_offset, offset->ssa);
+
+      /* In the Tessellation evaluation shader, reposition the offset of
+       * builtins when using separate layout.
+       */
+      if (dyn_tess_config) {
+         if (varying < VARYING_SLOT_VAR0) {
+            nir_def *builtins_offset = intel_nir_tess_field(b, BUILTINS);
+            nir_def *builtins_base_offset = nir_iadd_imm(
+               b, builtins_offset,
+               vue_map->varying_to_slot[varying] - vue_map->builtins_slot_offset);
+
+            total_offset = nir_iadd(b, total_offset, builtins_base_offset);
+         } else {
+            nir_def *vertices_offset = intel_nir_tess_field(b, PER_PATCH_SLOTS);
+            nir_def *vertices_base_offset = nir_iadd_imm(
+               b, vertices_offset,
+               vue_map->varying_to_slot[varying] - vue_map->num_per_patch_slots);
+
+            total_offset = nir_iadd(b, total_offset, vertices_base_offset);
+         }
+         nir_intrinsic_set_base(intrin, 0);
       }
+
+      nir_src_rewrite(offset, total_offset);
    }
 
    return true;
+}
+
+static bool
+remap_non_header_patch_urb_offsets(nir_shader *nir, const struct intel_vue_map *vue_map)
+{
+   return nir_shader_intrinsics_pass(nir, remap_patch_urb_offsets_instr,
+                                     nir_metadata_control_flow, (void *)vue_map);
 }
 
 /* Replace store_per_view_output to plain store_output, mapping the view index
@@ -596,17 +742,22 @@ brw_nir_lower_tes_inputs(nir_shader *nir, const struct intel_vue_map *vue_map)
    NIR_PASS(_, nir, nir_lower_io, nir_var_shader_in, type_size_vec4,
             nir_lower_io_lower_64bit_to_32);
 
-   /* This pass needs actual constants */
+   /* Run add_const_offset_to_base to allow update base/io_semantic::location
+    * for the remapping pass to look into the VUE mapping.
+    */
+   NIR_PASS(_, nir, nir_opt_constant_folding);
+   NIR_PASS(_, nir, nir_io_add_const_offset_to_base, nir_var_shader_in);
+
+   NIR_PASS(_, nir, remap_non_header_patch_urb_offsets, vue_map);
+   NIR_PASS(_, nir, remap_tess_header_values, nir->info.tess._primitive_mode);
+
+   /* remap_non_header_patch_urb_offsets can add constant math into the
+    * shader, just fold it for the backend.
+    */
+   NIR_PASS(_, nir, nir_opt_algebraic);
    NIR_PASS(_, nir, nir_opt_constant_folding);
 
    NIR_PASS(_, nir, nir_io_add_const_offset_to_base, nir_var_shader_in);
-
-   NIR_PASS(_, nir, nir_shader_intrinsics_pass, remap_patch_urb_offsets,
-            nir_metadata_control_flow,
-            &(struct remap_patch_urb_offset_state) {
-               .vue_map = vue_map,
-               .tes_primitive_mode = nir->info.tess._primitive_mode,
-            });
 }
 
 static bool
@@ -874,7 +1025,9 @@ brw_nir_lower_vue_outputs(nir_shader *nir)
 }
 
 void
-brw_nir_lower_tcs_outputs(nir_shader *nir, const struct intel_vue_map *vue_map,
+brw_nir_lower_tcs_outputs(nir_shader *nir,
+                          const struct intel_device_info *devinfo,
+                          const struct intel_vue_map *vue_map,
                           enum tess_primitive_mode tes_primitive_mode)
 {
    nir_foreach_shader_out_variable(var, nir) {
@@ -884,17 +1037,23 @@ brw_nir_lower_tcs_outputs(nir_shader *nir, const struct intel_vue_map *vue_map,
    NIR_PASS(_, nir, nir_lower_io, nir_var_shader_out, type_size_vec4,
             nir_lower_io_lower_64bit_to_32);
 
-   /* This pass needs actual constants */
+   /* Run add_const_offset_to_base to allow update base/io_semantic::location
+    * for the remapping pass to look into the VUE mapping.
+    */
    NIR_PASS(_, nir, nir_opt_constant_folding);
-
    NIR_PASS(_, nir, nir_io_add_const_offset_to_base, nir_var_shader_out);
 
-   NIR_PASS(_, nir, nir_shader_intrinsics_pass, remap_patch_urb_offsets,
-            nir_metadata_control_flow,
-            &(struct remap_patch_urb_offset_state) {
-               .vue_map = vue_map,
-               .tes_primitive_mode = tes_primitive_mode,
-            });
+   NIR_PASS(_, nir, remap_non_header_patch_urb_offsets, vue_map);
+   if (tes_primitive_mode != TESS_PRIMITIVE_UNSPECIFIED)
+      NIR_PASS(_, nir, remap_tess_header_values, tes_primitive_mode);
+   else
+      NIR_PASS(_, nir, remap_tess_header_values_dynamic, devinfo);
+
+   /* remap_non_header_patch_urb_offsets can add constant math into the
+    * shader, just fold it for the backend.
+    */
+   NIR_PASS(_, nir, nir_opt_constant_folding);
+   NIR_PASS(_, nir, nir_io_add_const_offset_to_base, nir_var_shader_out);
 }
 
 void

src/intel/compiler/brw_nir.h
@@ -187,7 +187,9 @@ void brw_nir_lower_fs_inputs(nir_shader *nir,
                              const struct intel_device_info *devinfo,
                              const struct brw_wm_prog_key *key);
 void brw_nir_lower_vue_outputs(nir_shader *nir);
-void brw_nir_lower_tcs_outputs(nir_shader *nir, const struct intel_vue_map *vue,
+void brw_nir_lower_tcs_outputs(nir_shader *nir,
+                               const struct intel_device_info *devinfo,
+                               const struct intel_vue_map *vue,
                                enum tess_primitive_mode tes_primitive_mode);
 void brw_nir_lower_fs_outputs(nir_shader *nir);
 

src/intel/compiler/brw_vue_map.c
@@ -263,6 +263,7 @@ brw_compute_vue_map(const struct intel_device_info *devinfo,
     * recompute state when TF changes, so we just always include it.
     */
    if (layout != INTEL_VUE_LAYOUT_SEPARATE_MESH) {
+      vue_map->builtins_slot_offset = slot;
       const uint64_t builtins = slots_valid & BITFIELD64_MASK(VARYING_SLOT_VAR0);
       u_foreach_bit64(varying, builtins) {
          /* Already assigned above? */
@@ -273,7 +274,11 @@ brw_compute_vue_map(const struct intel_device_info *devinfo,
    }
 
    const int first_generic_slot = slot;
-   const uint64_t generics = slots_valid & ~BITFIELD64_MASK(VARYING_SLOT_VAR0);
+   const uint64_t generics =
+      (layout != INTEL_VUE_LAYOUT_FIXED ?
+       BITFIELD64_MASK(util_last_bit64(slots_valid)) :
+       slots_valid) &
+      ~BITFIELD64_MASK(VARYING_SLOT_VAR0);
    u_foreach_bit64(varying, generics) {
       if (layout != INTEL_VUE_LAYOUT_FIXED) {
          slot = first_generic_slot + varying - VARYING_SLOT_VAR0;
@@ -282,6 +287,7 @@ brw_compute_vue_map(const struct intel_device_info *devinfo,
    }
 
    if (layout == INTEL_VUE_LAYOUT_SEPARATE_MESH) {
+      vue_map->builtins_slot_offset = slot;
       const uint64_t builtins = slots_valid & BITFIELD64_MASK(VARYING_SLOT_VAR0);
       u_foreach_bit64(varying, builtins) {
          /* Already assigned above? */
@@ -314,8 +320,11 @@ brw_compute_tess_vue_map(struct intel_vue_map *vue_map,
    vertex_slots &= ~(VARYING_BIT_TESS_LEVEL_OUTER |
                      VARYING_BIT_TESS_LEVEL_INNER);
 
-   if (separate)
+   if (separate) {
       vertex_slots |= VARYING_BIT_POS;
+      vertex_slots |= VARYING_BIT_CLIP_DIST0;
+      vertex_slots |= VARYING_BIT_CLIP_DIST1;
+   }
 
    /* Make sure that the values we store in vue_map->varying_to_slot and
     * vue_map->slot_to_varying won't overflow the signed chars that are used
@@ -381,9 +390,11 @@ brw_compute_tess_vue_map(struct intel_vue_map *vue_map,
          slot = first_generics_slot + varying - VARYING_SLOT_VAR0;
          assign_vue_slot(vue_map, varying, slot++);
       }
+      vue_map->builtins_slot_offset = slot;
       u_foreach_bit64(varying, builtins)
          assign_vue_slot(vue_map, varying, slot++);
    } else {
+      vue_map->builtins_slot_offset = slot;
       u_foreach_bit64(varying, builtins) {
          assign_vue_slot(vue_map, varying, slot++);
       }

commit e450297ea9d5b715e6e34a6a3bb7a8b3ec5f4890

anv/brw: fix output tcs vertices
brw_prog_tcs_data::instances can be divided by vertices per threads on
earlier generations.

Signed-off-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Fixes: a91e0e0d61 ("brw: add support for separate tessellation shader compilation")
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@intel.com>
Reviewed-by: Ivan Briano <ivan.briano@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38036>

src/intel/compiler/brw/brw_compile_tcs.cpp
@@ -224,6 +224,7 @@ brw_compile_tcs(const struct brw_compiler *compiler,
       BITSET_TEST(nir->info.system_values_read, SYSTEM_VALUE_PRIMITIVE_ID);
 
    prog_data->input_vertices = key->input_vertices;
+   prog_data->output_vertices = nir->info.tess.tcs_vertices_out;
    prog_data->patch_count_threshold = get_patch_count_threshold(key->input_vertices);
 
    if (compiler->use_tcs_multi_patch) {

src/intel/compiler/brw/brw_compiler.h
@@ -1175,6 +1175,9 @@ struct brw_tcs_prog_data
    /** Number of input vertices, 0 means dynamic */
    unsigned input_vertices;
 
+   /** Number of output vertices */
+   unsigned output_vertices;
+
    /** Should the non-SINGLE_PATCH payload provide primitive ID? */
    bool include_primitive_id;
 

src/intel/vulkan/genX_gfx_state.c
@@ -2550,7 +2550,7 @@ cmd_buffer_flush_gfx_runtime_state(struct anv_gfx_dynamic_state *hw_state,
 
       SET(TESS_CONFIG, tess_config,
           intel_tess_config(dyn->ts.patch_control_points,
-                            tcs_prog_data->instances,
+                            tcs_prog_data->output_vertices,
                             brw_tess_info_domain(tess_info),
                             tcs_prog_data->base.vue_map.num_per_patch_slots,
                             tcs_prog_data->base.vue_map.num_per_vertex_slots,