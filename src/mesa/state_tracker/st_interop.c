aerisarn/mesa-uwp

commit 1396dc1c387a9f9a8b16ad358ee12a025159cc98

mesa/st, dri2, wgl, glx: Modify flush_objects interop func to export a fence_fd
Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Acked-by: Jesse Natalie <jenatali@microsoft.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/21305>

src/mesa/state_tracker/st_interop.c
@@ -25,6 +25,7 @@
 
 #include "st_interop.h"
 #include "st_cb_texture.h"
+#include "st_cb_flush.h"
 #include "st_texture.h"
 
 #include "bufferobj.h"
@@ -371,7 +372,7 @@ flush_object(struct gl_context *ctx,
 int
 st_interop_flush_objects(struct st_context *st,
                          unsigned count, struct mesa_glinterop_export_in *objects,
-                         GLsync *sync)
+                         GLsync *sync, int *fence_fd)
 {
    struct gl_context *ctx = st->ctx;
 
@@ -393,6 +394,10 @@ st_interop_flush_objects(struct st_context *st,
 
    if (count > 0 && sync) {
       *sync = _mesa_fence_sync(ctx, GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
+   } else if (count > 0 && fence_fd) {
+      struct pipe_fence_handle *fence = NULL;
+      ctx->pipe->flush(ctx->pipe, &fence, PIPE_FLUSH_FENCE_FD | PIPE_FLUSH_ASYNC);
+      *fence_fd = ctx->screen->fence_get_fd(ctx->screen, fence);
    }
 
    return MESA_GLINTEROP_SUCCESS;

include/GL/internal/dri_interface.h
@@ -31,7 +31,7 @@
  * between a DRI driver and driver loader.  Currently, the most common driver
  * loader is the XFree86 libGL.so.  However, other loaders do exist, and in
  * the future the server-side libglx.a will also be a loader.
- * 
+ *
  * \author Kevin E. Martin <kevin@precisioninsight.com>
  * \author Ian Romanick <idr@us.ibm.com>
  * \author Kristian Høgsberg <krh@redhat.com>
@@ -190,7 +190,7 @@ struct __DRItexBufferExtensionRec {
 
     /**
      * Method to override base texture image with the contents of a
-     * __DRIdrawable. 
+     * __DRIdrawable.
      *
      * For GLX_EXT_texture_from_pixmap with AIGLX.  Deprecated in favor of
      * setTexBuffer2 in version 2 of this interface.  Not used by post-2011 X.
@@ -457,12 +457,12 @@ struct __DRI2interopExtensionRec {
 
    /**
     * Same as MesaGLInterop*FlushObjects.
-    * 
+    *
     * \since 2
     */
    int (*flush_objects)(__DRIcontext *ctx,
                         unsigned count, struct mesa_glinterop_export_in *objects,
-                        GLsync *sync);
+                        GLsync *sync, int *fence_fd);
 };
 
 
@@ -544,7 +544,7 @@ struct __DRIsystemTimeExtensionRec {
 
     /**
      * Get the media stream counter (MSC) rate.
-     * 
+     *
      * Matching the definition in GLX_OML_sync_control, this function returns
      * the rate of the "media stream counter".  In practical terms, this is
      * the frame refresh rate of the display.
@@ -759,7 +759,7 @@ struct __DRIuseInvalidateExtensionRec {
 #define __DRI_ATTRIB_MAX			54
 
 /* __DRI_ATTRIB_RENDER_TYPE */
-#define __DRI_ATTRIB_RGBA_BIT			0x01	
+#define __DRI_ATTRIB_RGBA_BIT			0x01
 #define __DRI_ATTRIB_COLOR_INDEX_BIT		0x02
 #define __DRI_ATTRIB_LUMINANCE_BIT		0x04
 #define __DRI_ATTRIB_FLOAT_BIT			0x08
@@ -862,7 +862,7 @@ struct __DRIcoreExtensionRec {
 /**
  * Stored version of some component (i.e., server-side DRI module, kernel-side
  * DRM, etc.).
- * 
+ *
  * \todo
  * There are several data structures that explicitly store a major version,
  * minor version, and patch level.  These structures should be modified to
@@ -879,7 +879,7 @@ struct __DRIversionRec {
 /**
  * Framebuffer information record.  Used by libGL to communicate information
  * about the framebuffer to the driver's \c __driCreateNewScreen function.
- * 
+ *
  * In XFree86, most of this information is derrived from data returned by
  * calling \c XF86DRIGetDeviceInfo.
  *

include/GL/mesa_glinterop.h
@@ -363,13 +363,14 @@ wglMesaGLInteropExportObject(HDC dpy, HGLRC context,
  * \param count      number of resources
  * \param resources  resources to flush
  * \param sync       optional GLsync to map to CL event
+ * \param fence_fd   optional fence_fd to use in CL
  *
  * \return MESA_GLINTEROP_SUCCESS or MESA_GLINTEROP_* != 0 on error
  */
 int
 MesaGLInteropGLXFlushObjects(struct _XDisplay *dpy, struct __GLXcontextRec *context,
                              unsigned count, struct mesa_glinterop_export_in *resources,
-                             GLsync *sync);
+                             GLsync *sync, int *fence_fd);
 
 /**
 * Same as MesaGLInteropGLXFlushObjects except that it accepts
@@ -378,11 +379,11 @@ MesaGLInteropGLXFlushObjects(struct _XDisplay *dpy, struct __GLXcontextRec *cont
 int
 MesaGLInteropEGLFlushObjects(EGLDisplay dpy, EGLContext context,
                              unsigned count, struct mesa_glinterop_export_in *resources,
-                             GLsync *sync);
+                             GLsync *sync, int *fence_fd);
 
 /**
 * Same as MesaGLInteropGLXFlushObjects except that it accepts
-* HDC and HGLRC.
+* HDC and HGLRC, and not a fence_fd.
 */
 int
 wglMesaGLInteropFlushObjects(HDC dpy, HGLRC context,
@@ -407,10 +408,10 @@ typedef int (*PFNWGLMESAGLINTEROPEXPORTOBJECTPROC)(HDC dpy, HGLRC context,
                                                    struct mesa_glinterop_export_out *out);
 typedef int (*PFNMESAGLINTEROPGLXFLUSHOBJECTSPROC)(struct _XDisplay *dpy, struct __GLXcontextRec *context,
                                                    unsigned count, struct mesa_glinterop_export_in *resources,
-                                                   GLsync *sync);
+                                                   GLsync *sync, int *fence_fd);
 typedef int (*PFNMESAGLINTEROPEGLFLUSHOBJECTSPROC)(EGLDisplay dpy, EGLContext context,
                                                    unsigned count, struct mesa_glinterop_export_in *resources,
-                                                   GLsync *sync);
+                                                   GLsync *sync, int *fence_fd);
 typedef int (*PFNWGLMESAGLINTEROPFLUSHOBJECTSPROC)(HDC dpy, HGLRC context,
                                                    unsigned count, struct mesa_glinterop_export_in *resources,
                                                    GLsync *sync);

src/egl/drivers/dri2/egl_dri2.c
@@ -3664,7 +3664,7 @@ dri2_interop_export_object(_EGLDisplay *disp, _EGLContext *ctx,
 static int
 dri2_interop_flush_objects(_EGLDisplay *disp, _EGLContext *ctx, unsigned count,
                            struct mesa_glinterop_export_in *objects,
-                           GLsync *sync)
+                           GLsync *sync, int *fence_fd)
 {
    struct dri2_egl_display *dri2_dpy = dri2_egl_display(disp);
    struct dri2_egl_context *dri2_ctx = dri2_egl_context(ctx);
@@ -3673,7 +3673,7 @@ dri2_interop_flush_objects(_EGLDisplay *disp, _EGLContext *ctx, unsigned count,
       return MESA_GLINTEROP_UNSUPPORTED;
 
    return dri2_dpy->interop->flush_objects(dri2_ctx->dri_context, count,
-                                           objects, sync);
+                                           objects, sync, fence_fd);
 }
 
 const _EGLDriver _eglDriver = {

src/egl/drivers/wgl/egl_wgl.c
@@ -1164,7 +1164,7 @@ wgl_interop_export_object(_EGLDisplay *disp, _EGLContext *ctx,
 static int
 wgl_interop_flush_objects(_EGLDisplay *disp, _EGLContext *ctx, unsigned count,
                           struct mesa_glinterop_export_in *objects,
-                          GLsync *sync)
+                          GLsync *sync, int *fence_fd)
 {
    struct wgl_egl_context *wgl_ctx = wgl_egl_context(ctx);
    return stw_interop_flush_objects(wgl_ctx->ctx, count, objects, sync);

src/egl/main/eglapi.c
@@ -2899,7 +2899,7 @@ MesaGLInteropEGLExportObject(EGLDisplay dpy, EGLContext context,
 PUBLIC int
 MesaGLInteropEGLFlushObjects(EGLDisplay dpy, EGLContext context, unsigned count,
                              struct mesa_glinterop_export_in *objects,
-                             GLsync *sync)
+                             GLsync *sync, int *fence_fd)
 {
    _EGLDisplay *disp;
    _EGLContext *ctx;
@@ -2910,8 +2910,8 @@ MesaGLInteropEGLFlushObjects(EGLDisplay dpy, EGLContext context, unsigned count,
       return ret;
 
    if (disp->Driver->GLInteropFlushObjects)
-      ret =
-         disp->Driver->GLInteropFlushObjects(disp, ctx, count, objects, sync);
+      ret = disp->Driver->GLInteropFlushObjects(disp, ctx, count, objects, sync,
+                                                fence_fd);
    else
       ret = MESA_GLINTEROP_UNSUPPORTED;
 

src/egl/main/egldriver.h
@@ -208,7 +208,7 @@ struct _egl_driver {
    int (*GLInteropFlushObjects)(_EGLDisplay *disp, _EGLContext *ctx,
                                 unsigned count,
                                 struct mesa_glinterop_export_in *in,
-                                GLsync *sync);
+                                GLsync *sync, int *fence_fd);
 
    /* for EGL_EXT_image_dma_buf_import_modifiers */
    EGLBoolean (*QueryDmaBufFormatsEXT)(_EGLDisplay *disp, EGLint max_formats,

src/gallium/frontends/dri/dri2.c
@@ -2098,9 +2098,9 @@ dri2_interop_export_object(__DRIcontext *_ctx,
 static int
 dri2_interop_flush_objects(__DRIcontext *_ctx,
                            unsigned count, struct mesa_glinterop_export_in *objects,
-                           GLsync *sync)
+                           GLsync *sync, int *fence_fd)
 {
-   return st_interop_flush_objects(dri_context(_ctx)->st, count, objects, sync);
+   return st_interop_flush_objects(dri_context(_ctx)->st, count, objects, sync, fence_fd);
 }
 
 static const __DRI2interopExtension dri2InteropExtension = {

bminor/mesa-mesa

commit 87550fc6571968c61afbb23f0bc0b079b6cd8527

st/interop: fix fence leak
Cc: mesa-stable
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14050
Fixes: 1396dc1c387 ("mesa/st, dri2, wgl, glx: Modify flush_objects interop func to export a fence_fd")
Tested-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38310>

src/mesa/state_tracker/st_interop.c
@@ -437,6 +437,7 @@ st_interop_flush_objects(struct st_context *st,
             struct pipe_fence_handle *fence = NULL;
             ctx->pipe->flush(ctx->pipe, &fence, PIPE_FLUSH_FENCE_FD | PIPE_FLUSH_ASYNC);
             *out->fence_fd = ctx->screen->fence_get_fd(ctx->screen, fence);
+            ctx->screen->fence_reference(ctx->screen, &fence, NULL);
          }
          out->version = MIN2(out->version, 1);
       } else {