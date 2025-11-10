bminor/mesa-mesa

commit 0c4e1c9efcf4c1730e73d6aade2232d596ad8dcf

intel/common: Add helper for compute thread group dispatch size
The recommended settings is just a guidance and not a programming
requirement as per the Bspec.

Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35904>

src/intel/common/intel_common.c
@@ -217,3 +217,19 @@ intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo
    *ret_z_pass_async_compute_thread_limit = z_pass_async_compute_thread_limit;
    *ret_np_z_async_throttle_settings = np_z_async_throttle_settings;
 }
+
+int
+intel_compute_threads_group_dispatch_size(uint32_t hw_threads_in_wg)
+{
+   /* Following value calculated based on overdispatch is disabled. In case if
+    * compute overdispatch disabled set to 1, then we need to use TG Size 1.
+    */
+   switch (hw_threads_in_wg) {
+   case 1 ... 16:
+      return 0;
+   case 17 ... 32:
+      return 1;
+   default:
+      return 2;
+   }
+}

src/intel/common/intel_common.h
@@ -15,3 +15,6 @@ intel_compute_engine_async_threads_limit(const struct intel_device_info *devinfo
                                          uint8_t *ret_pixel_async_compute_thread_limit,
                                          uint8_t *ret_z_pass_async_compute_thread_limit,
                                          uint8_t *ret_np_z_async_throttle_settings);
+
+int
+intel_compute_threads_group_dispatch_size(uint32_t hw_threads_in_wg);

commit 16f66ffe55d7264b4b401dc31dcd91fdb615143b

intel/common: Consider 0 threads while setting TG
In ray tracing dispatch, we have dispatch.threads set to 0 since we
calculate the local_size_x/y/z based on the launch sizes.

This change takes 0 threads into an account and returh the TG size 8 in
such scenarios. Before this change, we were setting TG size to 2.

Fixes: 0c4e1c9efcf4 ("intel/common: Add helper for compute thread group dispatch size")
Signed-off-by: Sagar Ghuge <sagar.ghuge@intel.com>
Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38229>

src/intel/common/intel_common.c
@@ -227,7 +227,7 @@ intel_compute_threads_group_dispatch_size(uint32_t hw_threads_in_wg)
     * compute overdispatch disabled set to 1, then we need to use TG Size 1.
     */
    switch (hw_threads_in_wg) {
-   case 1 ... 16:
+   case 0 ... 16:
       return 0;
    case 17 ... 32:
       return 1;