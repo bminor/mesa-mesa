296839f489f84fb13702245756708f69f7fa963e
Marek Olšák <marek.olsak@amd.com>
2025-11-12 15:25:21 -0500

    nir/opt_copy_propagate: refactor for readability, describe missing stuff
    No functional change.
    
    Reviewed-by: Daniel Schürmann <daniel@schuermann.dev>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38411>

--- src/compiler/nir/nir_opt_copy_propagate.c
+++ src/compiler/nir/nir_opt_copy_propagate.c
@@ -56,12 +56,9 @@ is_swizzleless_move(nir_alu_instr *instr)
    return true;
 }
 
-static bool
-rewrite_to_vec(nir_alu_instr *mov, nir_alu_instr *vec)
+static void
+merge_vec_and_mov(nir_alu_instr *mov, nir_alu_instr *vec)
 {
-   if (mov->op != nir_op_mov)
-      return false;
-
    nir_builder b = nir_builder_at(nir_after_instr(&mov->instr));
 
    unsigned num_comp = mov->def.num_components;
@@ -74,48 +71,71 @@ rewrite_to_vec(nir_alu_instr *mov, nir_alu_instr *vec)
 
    /* If we remove "mov" and it's the next instruction in the
     * nir_foreach_instr_safe() loop, then we would end copy-propagation early. */
-
-   return true;
 }
 
 static bool
-copy_propagate_alu(nir_alu_src *src, nir_alu_instr *copy)
+copy_propagate_alu(nir_alu_src *use_of_copy, nir_alu_instr *copy)
 {
-   nir_def *def = NULL;
-   nir_alu_instr *user = nir_instr_as_alu(nir_src_parent_instr(&src->src));
-   unsigned src_idx = src - user->src;
+   nir_def *new_use_src = NULL;
+   nir_alu_instr *user = nir_instr_as_alu(nir_src_parent_instr(&use_of_copy->src));
+   unsigned src_idx = use_of_copy - user->src;
    assert(src_idx < nir_op_infos[user->op].num_inputs);
-   unsigned num_comp = nir_ssa_alu_instr_src_components(user, src_idx);
+   unsigned num_use_components = nir_ssa_alu_instr_src_components(user, src_idx);
 
    if (copy->op == nir_op_mov) {
-      def = copy->src[0].src.ssa;
+      new_use_src = copy->src[0].src.ssa;
 
-      for (unsigned i = 0; i < num_comp; i++)
-         src->swizzle[i] = copy->src[0].swizzle[src->swizzle[i]];
+      /* Propagate the mov swizzle to the use. */
+      for (unsigned i = 0; i < num_use_components; i++)
+         use_of_copy->swizzle[i] = copy->src[0].swizzle[use_of_copy->swizzle[i]];
    } else {
-      def = copy->src[src->swizzle[0]].src.ssa;
+      /* "copy" is vecN. */
+      new_use_src = copy->src[use_of_copy->swizzle[0]].src.ssa;
 
-      for (unsigned i = 1; i < num_comp; i++) {
-         if (copy->src[src->swizzle[i]].src.ssa != def)
-            return rewrite_to_vec(user, copy);
+      for (unsigned i = 1; i < num_use_components; i++) {
+         if (copy->src[use_of_copy->swizzle[i]].src.ssa != new_use_src) {
+            if (user->op == nir_op_mov) {
+               /* When vecN using different defs is followed by mov, they are
+                * merged, which becomes a new vecN instruction.
+                *
+                * Ideally, mov would be merged into the preceding vecN, but
+                * since the old vecN can have other uses, we create a new vecN.
+                *
+                * TODO: This ends up creating duplicated instructions if
+                * the mov isn't CSE'd before this pass. It also leaves the mov
+                * dead, and the original vecN can end up dead too.
+                */
+               merge_vec_and_mov(user, copy);
+               return true;
+            } else {
+               /* If the use uses at least 2 components of vecN and those
+                * components source at least 2 different defs, it's not
+                * possible to propagate that vecN.
+                */
+               return false;
+            }
+         }
       }
 
-      for (unsigned i = 0; i < num_comp; i++)
-         src->swizzle[i] = copy->src[src->swizzle[i]].swizzle[0];
+      /* Propagate vecN (using the same def in all srcs, so it's equivalent to
+       * a swizzled mov) to the use.
+       */
+      for (unsigned i = 0; i < num_use_components; i++)
+         use_of_copy->swizzle[i] = copy->src[use_of_copy->swizzle[i]].swizzle[0];
    }
 
-   nir_src_rewrite(&src->src, def);
+   nir_src_rewrite(&use_of_copy->src, new_use_src);
 
    return true;
 }
 
 static bool
-copy_propagate(nir_src *src, nir_alu_instr *copy)
+copy_propagate(nir_src *use_of_copy, nir_alu_instr *copy)
 {
    if (!is_swizzleless_move(copy))
       return false;
 
-   nir_src_rewrite(src, copy->src[0].src.ssa);
+   nir_src_rewrite(use_of_copy, copy->src[0].src.ssa);
 
    return true;
 }
@@ -126,22 +146,22 @@ copy_prop_instr(nir_instr *instr)
    if (instr->type != nir_instr_type_alu)
       return false;
 
-   nir_alu_instr *mov = nir_instr_as_alu(instr);
+   nir_alu_instr *copy = nir_instr_as_alu(instr);
 
-   if (!nir_op_is_vec_or_mov(mov->op))
+   if (!nir_op_is_vec_or_mov(copy->op))
       return false;
 
    bool progress = false;
 
-   nir_foreach_use_including_if_safe(src, &mov->def) {
+   nir_foreach_use_including_if_safe(src, &copy->def) {
       if (!nir_src_is_if(src) && nir_src_parent_instr(src)->type == nir_instr_type_alu)
-         progress |= copy_propagate_alu(container_of(src, nir_alu_src, src), mov);
+         progress |= copy_propagate_alu(container_of(src, nir_alu_src, src), copy);
       else
-         progress |= copy_propagate(src, mov);
+         progress |= copy_propagate(src, copy);
    }
 
-   if (progress && nir_def_is_unused(&mov->def))
-      nir_instr_remove(&mov->instr);
+   if (progress && nir_def_is_unused(&copy->def))
+      nir_instr_remove(&copy->instr);
 
    return progress;
 }
