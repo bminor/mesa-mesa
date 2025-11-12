bminor/mesa-mesa

commit 562bb8b62b3ec1dbb8b390864ce7c17027161bfe

v3dv: align width to 256 when using simulator
Signed-off-by: Juan A. Suarez Romero <jasuarez@igalia.com>
Signed-off-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
Reviewed-by: Jose Maria Casanova Crespo <jmcasanova@igalia.com>
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/37887>

src/broadcom/vulkan/v3dv_image.c
@@ -93,8 +93,8 @@ v3d_get_dimension_mpad(uint32_t dimension, uint32_t level, uint32_t block_dimens
 }
 
 static bool
-v3d_setup_plane_slices(struct v3dv_image *image, uint8_t plane,
-                       uint32_t plane_offset,
+v3d_setup_plane_slices(struct v3dv_device *device, struct v3dv_image *image,
+                       uint8_t plane, uint32_t plane_offset,
                        const VkSubresourceLayout *plane_layouts)
 {
    assert(image->planes[plane].cpp > 0);
@@ -242,6 +242,18 @@ v3d_setup_plane_slices(struct v3dv_image *image, uint8_t plane,
                (2 * v3d_utile_height(image->planes[plane].cpp));
       }
 
+#if USE_V3D_SIMULATOR
+      /* Ensure stride alignment matches the one required by the GPU that
+       * drives the display.
+       */
+      if (!image->tiled &&
+          image->vk.image_type == VK_IMAGE_TYPE_2D) {
+         slice->stride =
+            align(slice->stride,
+                  v3d_simulator_get_raster_stride_align(device->pdevice->render_fd));
+      }
+#endif
+
       slice->size = level_height * slice->stride;
       uint32_t slice_total_size = slice->size * level_depth;
 
@@ -327,8 +339,8 @@ v3d_setup_plane_slices(struct v3dv_image *image, uint8_t plane,
 }
 
 static VkResult
-v3d_setup_slices(struct v3dv_image *image, bool disjoint,
-                 const VkSubresourceLayout *plane_layouts)
+v3d_setup_slices(struct v3dv_device *device, struct v3dv_image *image,
+                 bool disjoint, const VkSubresourceLayout *plane_layouts)
 {
    if (disjoint && image->plane_count == 1)
       disjoint = false;
@@ -336,7 +348,7 @@ v3d_setup_slices(struct v3dv_image *image, bool disjoint,
    uint64_t offset = 0;
    for (uint8_t plane = 0; plane < image->plane_count; plane++) {
       offset = disjoint ? 0 : offset;
-      if (!v3d_setup_plane_slices(image, plane, offset, plane_layouts)) {
+      if (!v3d_setup_plane_slices(device, image, plane, offset, plane_layouts)) {
          assert(plane_layouts);
          return VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT;
       }
@@ -387,7 +399,7 @@ v3dv_update_image_layout(struct v3dv_device *device,
 
    image->vk.drm_format_mod = modifier;
 
-   return v3d_setup_slices(image, disjoint,
+   return v3d_setup_slices(device, image, disjoint,
                            explicit_mod_info ? explicit_mod_info->pPlaneLayouts :
                                                NULL);
 }

commit a0b8ee614df13c2dab1cbf6e7d8376acb8e339d9

v3dv: only apply simulator stride alignment for from_wsi images
This adds from_wsi field to v3dv_image, so we can apply simulator stride
 alignment only to WSI images.

Handling VK_STRUCTURE_TYPE_WSI_IMAGE_CREATE_INFO_MESA at
v3dv_GetPhysicalDeviceFormatProperties2 also removes debug warnings like:

MESA: debug: v3dv_GetPhysicalDeviceImageFormatProperties2: ignored VkStructureType Unknown VkStructureType value.(1000001002)

Fixes: 562bb8b62b3 ("v3dv: align width to 256 when using simulator")
Reviewed-by: Iago Toral Quiroga <itoral@igalia.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38374>

src/broadcom/vulkan/v3dv_formats.c
@@ -678,7 +678,7 @@ v3dv_GetPhysicalDeviceImageFormatProperties2(VkPhysicalDevice physicalDevice,
 
    /* Extract input structs */
    vk_foreach_struct_const(s, base_info->pNext) {
-      switch (s->sType) {
+      switch ((unsigned)s->sType) {
       case VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_EXTERNAL_IMAGE_FORMAT_INFO:
          external_info = (const void *) s;
          break;
@@ -697,6 +697,9 @@ v3dv_GetPhysicalDeviceImageFormatProperties2(VkPhysicalDevice physicalDevice,
          default:
             assert("Unknown DRM format modifier");
          }
+         break;
+      case VK_STRUCTURE_TYPE_WSI_IMAGE_CREATE_INFO_MESA:
+         /* Do nothing, v3dv_image_init will handle it */;
          break;
       default:
          vk_debug_ignored_stype(s->sType);

src/broadcom/vulkan/v3dv_image.c
@@ -246,8 +246,7 @@ v3d_setup_plane_slices(struct v3dv_device *device, struct v3dv_image *image,
       /* Ensure stride alignment matches the one required by the GPU that
        * drives the display.
        */
-      if (!image->tiled &&
-          image->vk.image_type == VK_IMAGE_TYPE_2D) {
+      if (image->from_wsi) {
          slice->stride =
             align(slice->stride,
                   v3d_simulator_get_raster_stride_align(device->pdevice->render_fd));
@@ -436,6 +435,10 @@ v3dv_image_init(struct v3dv_device *device,
       explicit_mod_info = &eci;
       modifier = eci.drmFormatModifier;
    }
+
+   const struct wsi_image_create_info *wsi_info =
+      vk_find_struct_const(pCreateInfo->pNext, WSI_IMAGE_CREATE_INFO_MESA);
+   image->from_wsi = wsi_info != NULL;
 
    if (tiling == VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
       mod_info =

src/broadcom/vulkan/v3dv_private.h
@@ -746,6 +746,10 @@ struct v3dv_image {
     * This holds a tiled copy of the image we can use for that purpose.
     */
    struct v3dv_image *shadow;
+
+   /* Image is a WSI image.
+    */
+   bool from_wsi;
 };
 
 VkResult