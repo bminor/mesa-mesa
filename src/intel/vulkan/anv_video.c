26aba9dc9faadc8eb9822e417611e26f9f5dfff1
Dylan Baker <dylan.c.baker@intel.com>
2025-11-13 13:28:44 -0800

    anv/video: void cast array we intentionally read off the end of
    Coverity notices we're reading off the end of the array here, which is
    true. We also intend to do that because we want to read the next field as
    well. Cast to a `void *` to help Coverity out.
    
    CID: 1649593
    Reviewed-by: Hyunjun Ko <zzoon@igalia.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38437>

--- src/intel/vulkan/anv_video.c
+++ src/intel/vulkan/anv_video.c
@@ -1271,7 +1271,7 @@ vp9_prob_buf_update(struct anv_video_session *vid,
              sizeof(ctx.seg_tree_probs));
       memcpy(ctx.seg_pred_probs, seg->segmentation_pred_prob,
              sizeof(ctx.seg_pred_probs));
-      memcpy(ptr + SEG_PROBS_OFFSET, &ctx.seg_tree_probs,
+      memcpy(ptr + SEG_PROBS_OFFSET, (void *)&ctx.seg_tree_probs,
              SEG_TREE_PROBS + PREDICTION_PROBS);
    } else if (BITSET_TEST(vid->prob_tbl_set, 3)) {
       VP9_CTX_DEFAULT(seg_tree_probs);
