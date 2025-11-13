bminor/mesa-mesa

commit cb7726bb2cf7e25661c1401dbef2a6a597748ecd

vulkan/wsi: validate HDR metadata to not cause protocol errors
If it would trigger a protocol error, we must not use it.

Signed-off-by: Xaver Hugl <xaver.hugl@kde.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/34000>

src/vulkan/wsi/wsi_common_wayland.c
@@ -1172,6 +1172,36 @@ wsi_wl_surface_remove_color_refcount(struct wsi_wl_surface *wsi_surface)
    }
 }
 
+struct wayland_hdr_metadata {
+   uint32_t min_luminance;
+   uint32_t max_luminance;
+   uint32_t max_fall;
+   uint32_t max_cll;
+};
+
+#define MIN_LUM_FACTOR 10000
+
+static bool
+is_hdr_metadata_legal(struct wayland_hdr_metadata *l)
+{
+   if (l->max_cll != 0) {
+      if (l->max_cll * MIN_LUM_FACTOR < l->min_luminance)
+         return false;
+      if (l->max_cll > l->max_luminance)
+         return false;
+   }
+   if (l->max_fall != 0) {
+      if (l->max_fall * MIN_LUM_FACTOR < l->min_luminance)
+         return false;
+      if (l->max_fall > l->max_luminance)
+         return false;
+      if (l->max_cll != 0 && l->max_fall > l->max_cll) {
+         return false;
+      }
+   }
+   return l->max_luminance * MIN_LUM_FACTOR > l->min_luminance;
+}
+
 static bool
 compare_hdr_metadata(struct VkHdrMetadataEXT *l, struct VkHdrMetadataEXT *r)
 {
@@ -1215,7 +1245,14 @@ wsi_wl_swapchain_update_colorspace(struct wsi_wl_swapchain *chain)
       wsi_wl_surface_remove_color_refcount(surface);
    }
 
-   bool should_use_hdr_metadata = chain->color.has_hdr_metadata;
+   struct wayland_hdr_metadata wayland_hdr_metadata = {
+      .min_luminance = round(MIN_LUM_FACTOR * chain->color.hdr_metadata.minLuminance),
+      .max_luminance = round(chain->color.hdr_metadata.maxLuminance),
+      .max_fall = round(chain->color.hdr_metadata.maxFrameAverageLightLevel),
+      .max_cll = round(chain->color.hdr_metadata.maxContentLightLevel),
+   };
+   bool should_use_hdr_metadata = chain->color.has_hdr_metadata
+                               && is_hdr_metadata_legal(&wayland_hdr_metadata);
    for (int i = 0; i < ARRAY_SIZE(colorspace_mapping); i++) {
       if (colorspace_mapping[i].colorspace == chain->color.colorspace) {
          should_use_hdr_metadata &= colorspace_mapping[i].should_use_hdr_metadata;
@@ -1259,10 +1296,8 @@ wsi_wl_swapchain_update_colorspace(struct wsi_wl_swapchain *chain)
    wp_image_description_creator_params_v1_set_primaries_named(creator, primaries);
    wp_image_description_creator_params_v1_set_tf_named(creator, tf);
    if (should_use_hdr_metadata) {
-      uint32_t max_cll = round(chain->color.hdr_metadata.maxContentLightLevel);
-      uint32_t max_fall = round(chain->color.hdr_metadata.maxFrameAverageLightLevel);
-      wp_image_description_creator_params_v1_set_max_cll(creator, max_cll);
-      wp_image_description_creator_params_v1_set_max_fall(creator, max_fall);
+      wp_image_description_creator_params_v1_set_max_cll(creator, wayland_hdr_metadata.max_cll);
+      wp_image_description_creator_params_v1_set_max_fall(creator, wayland_hdr_metadata.max_fall);
       if (display->color_features.mastering_display_primaries) {
          uint32_t red_x = round(chain->color.hdr_metadata.displayPrimaryRed.x * 1000000);
          uint32_t red_y = round(chain->color.hdr_metadata.displayPrimaryRed.y * 1000000);
@@ -1276,9 +1311,9 @@ wsi_wl_swapchain_update_colorspace(struct wsi_wl_swapchain *chain)
                                                                                 green_x, green_y,
                                                                                 blue_x, blue_y,
                                                                                 white_x, white_y);
-         uint32_t min_lum = round(chain->color.hdr_metadata.minLuminance * 10000);
-         uint32_t max_lum = round(chain->color.hdr_metadata.maxLuminance);
-         wp_image_description_creator_params_v1_set_mastering_luminance(creator, min_lum, max_lum);
+         wp_image_description_creator_params_v1_set_mastering_luminance(creator,
+                                                                        wayland_hdr_metadata.min_luminance,
+                                                                        wayland_hdr_metadata.max_luminance);
       }
    }
 

commit 490f05f82ced94d0a2893946aad96083447cac38

wsi/wayland: Zero min_luminance, max_luminance HDR light levels are valid.
CTA-861-G section 6.9.1 Static Metadata Type 1 declares that zero values
for different groups of HDR Metadata properties are allowed, including
zero nits values for max display mastering luminance, max content light
level, max frame-average light level and min display mastering luminance.

A zero value is meant to be treated by the video sink as "undefined" /
"unknown", and handled accordingly. This is common for dynamically
generated visual content.

The is_hdr_metadata_legal() function in the Vulkan/WSI/Wayland HDR backend
currently declares HDR light level metadata as invalid if the mastering
display min_luminance and max_luminance light levels are set to the legal
level of zero nits. This causes valid HDR metadata as set by the client
via vkSetHdrMetadata() to be not sent to the compositor.

Fix this by skipping checks that don't apply if min_luminance or
max_luminance are zero. If max_luminance is zero then we skip sending
of mastering display min/max luminance to Wayland, as sending a a
max_luminance <= min_luminance would trigger a protocol error. All
other valid data is still send, ie. color primaries, white-point,
content light levels.

Fixes: cb7726bb2cf ("vulkan/wsi: validate HDR metadata to not cause protocol errors")
Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Co-authored-by: Michel Dänzer <michel@daenzer.net>
Reviewed-by: Xaver Hugl <xaver.hugl@kde.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38326>

src/vulkan/wsi/wsi_common_wayland.c
@@ -1192,18 +1192,26 @@ is_hdr_metadata_legal(struct wayland_hdr_metadata *l)
    if (l->max_cll != 0) {
       if (l->max_cll * MIN_LUM_FACTOR < l->min_luminance)
          return false;
-      if (l->max_cll > l->max_luminance)
+      if (l->max_luminance != 0 && l->max_cll > l->max_luminance)
          return false;
    }
    if (l->max_fall != 0) {
       if (l->max_fall * MIN_LUM_FACTOR < l->min_luminance)
          return false;
-      if (l->max_fall > l->max_luminance)
+      if (l->max_luminance != 0 && l->max_fall > l->max_luminance)
          return false;
       if (l->max_cll != 0 && l->max_fall > l->max_cll) {
          return false;
       }
    }
+
+   /* Be lenient here for a zero (=undefined) max_luminance and handle
+    * this in the calling code instead, by not sending min/max mastering
+    * luminance data to Wayland, thereby avoiding protocol errors.
+    */
+   if (l->max_luminance == 0)
+      return true;
+
    return l->max_luminance * MIN_LUM_FACTOR > l->min_luminance;
 }
 
@@ -1321,9 +1329,15 @@ wsi_wl_swapchain_update_colorspace(struct wsi_wl_swapchain *chain)
                                                                                 green_x, green_y,
                                                                                 blue_x, blue_y,
                                                                                 white_x, white_y);
-         wp_image_description_creator_params_v1_set_mastering_luminance(creator,
-                                                                        wayland_hdr_metadata.min_luminance,
-                                                                        wayland_hdr_metadata.max_luminance);
+
+         /* A max_luminance of 0 is legal by spec and means "undefined", but would cause a
+          * Wayland protocol error, so skip setting mastering luminance for zero value.
+          */
+         if (wayland_hdr_metadata.max_luminance != 0) {
+            wp_image_description_creator_params_v1_set_mastering_luminance(creator,
+                                                                           wayland_hdr_metadata.min_luminance,
+                                                                           wayland_hdr_metadata.max_luminance);
+         }
       }
    }
 