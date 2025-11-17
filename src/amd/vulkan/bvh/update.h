660af9a83034907ffc82eba63e9b7f7427945c0c
Natalie Vock <natalie.vock@gmx.de>
2025-11-17 16:41:19 +0100

    radv/rt: Keep updated nodes always active
    In updateable AS, we keep all nodes active even if they're
    degenerate/NaN, because too many games ignore API rules about not
    making inactive nodes active (and some vendor tips outright advise this
    behavior). We also need to match this by keeping everything active in
    the update side. The ALWAYS_ACTIVE macro has been long removed and
    replaced by VK_BVH_BUILD_FLAG, too. Since updating only happens to
    updateable AS, don't even check for the flag, just implement the
    always-active handling.
    
    Cc: mesa-stable
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38488>
    (cherry picked from commit bc1eea90b961c8a3bb4dcf8124e850ef777c35a3)

--- src/amd/vulkan/bvh/update.h
+++ src/amd/vulkan/bvh/update.h
@@ -10,25 +10,13 @@
 
 #include "encode.h"
 
-bool
+void
 radv_build_triangle(inout vk_aabb bounds, VOID_REF dst_ptr, vk_bvh_geometry_data geom_data, uint32_t global_id,
                     bool gfx12)
 {
-   bool is_valid = true;
    triangle_indices indices = load_indices(geom_data.indices, geom_data.index_format, global_id);
 
    triangle_vertices vertices = load_vertices(geom_data.data, indices, geom_data.vertex_format, geom_data.stride);
-
-   /* An inactive triangle is one for which the first (X) component of any vertex is NaN. If any
-    * other vertex component is NaN, and the first is not, the behavior is undefined. If the vertex
-    * format does not have a NaN representation, then all triangles are considered active.
-    */
-   if (isnan(vertices.vertex[0].x) || isnan(vertices.vertex[1].x) || isnan(vertices.vertex[2].x))
-#if ALWAYS_ACTIVE
-      is_valid = false;
-#else
-      return false;
-#endif
 
    if (geom_data.transform != NULL) {
       mat4 transform = mat4(1.0);
@@ -61,16 +49,12 @@ radv_build_triangle(inout vk_aabb bounds, VOID_REF dst_ptr, vk_bvh_geometry_data
       radv_encode_triangle_gfx12(dst_ptr, node);
    else
       radv_encode_triangle_gfx10_3(dst_ptr, node);
-
-   return is_valid;
 }
 
-bool
+void
 radv_build_aabb(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32_t geometry_id, uint32_t global_id,
                 bool gfx12)
 {
-   bool is_valid = true;
-
    for (uint32_t vec = 0; vec < 2; vec++)
       for (uint32_t comp = 0; comp < 3; comp++) {
          float coord = DEREF(INDEX(float, src_ptr, comp + vec * 3));
@@ -81,16 +65,6 @@ radv_build_aabb(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32
             bounds.max[comp] = coord;
       }
 
-   /* An inactive AABB is one for which the minimum X coordinate is NaN. If any other component is
-    * NaN, and the first is not, the behavior is undefined.
-    */
-   if (isnan(bounds.min.x))
-#if ALWAYS_ACTIVE
-      is_valid = false;
-#else
-      return false;
-#endif
-
    vk_ir_aabb_node node;
    node.base.aabb = bounds;
    node.primitive_id = global_id;
@@ -100,8 +74,6 @@ radv_build_aabb(inout vk_aabb bounds, VOID_REF src_ptr, VOID_REF dst_ptr, uint32
       radv_encode_aabb_gfx12(dst_ptr, node);
    else
       radv_encode_aabb_gfx10_3(dst_ptr, node);
-
-   return is_valid;
 }
 
 #endif
