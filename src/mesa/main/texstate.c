Grima04/mesa-turnip-kgsl

commit ec978e002f59c8ed829e354f8ca8d9440df487f1

mesa: only update fixed-func programs on texture matrix enablement changes
This should reduce fixed-func program key recomputations.

I also update the fixed-func fragment program, which was incorrectly
ignored because it's clearly used there.

Reviewed-by: Zoltán Böszörményi <zboszor@gmail.com>
Acked-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8850>

src/mesa/main/texstate.c
@@ -387,10 +387,11 @@ _mesa_ClientActiveTexture(GLenum texture)
  *
  * \param ctx GL context.
  */
-void
+GLbitfield
 _mesa_update_texture_matrices(struct gl_context *ctx)
 {
    GLuint u;
+   GLbitfield old_texmat_enabled = ctx->Texture._TexMatEnabled;
 
    ctx->Texture._TexMatEnabled = 0x0;
 
@@ -404,6 +405,11 @@ _mesa_update_texture_matrices(struct gl_context *ctx)
 	    ctx->Texture._TexMatEnabled |= ENABLE_TEXMAT(u);
       }
    }
+
+   if (old_texmat_enabled != ctx->Texture._TexMatEnabled)
+      return _NEW_FF_VERT_PROGRAM | _NEW_FF_FRAG_PROGRAM;
+
+   return 0;
 }
 
 

src/mesa/main/state.c
@@ -457,7 +457,7 @@ _mesa_update_state_locked( struct gl_context *ctx )
          _mesa_update_modelview_project( ctx, new_state );
 
       if (new_state & _NEW_TEXTURE_MATRIX)
-         _mesa_update_texture_matrices(ctx);
+         new_state |= _mesa_update_texture_matrices(ctx);
 
       if (new_state & (_NEW_TEXTURE_OBJECT | _NEW_TEXTURE_STATE | _NEW_PROGRAM))
          new_state |= _mesa_update_texture_state(ctx);
@@ -493,8 +493,8 @@ _mesa_update_state_locked( struct gl_context *ctx )
       }
 
       if (ctx->VertexProgram._UsesTnlProgram) {
-         prog_flags |= _NEW_FF_VERT_PROGRAM | _NEW_TEXTURE_MATRIX |
-                       _NEW_TRANSFORM | _NEW_POINT | _NEW_FOG;
+         prog_flags |= _NEW_FF_VERT_PROGRAM | _NEW_TRANSFORM | _NEW_POINT |
+                       _NEW_FOG;
       }
 
       if (new_state & prog_flags) {

src/mesa/main/texstate.h
@@ -109,7 +109,7 @@ _mesa_ClientActiveTexture( GLenum target );
  */
 /*@{*/
 
-extern void
+extern GLbitfield
 _mesa_update_texture_matrices(struct gl_context *ctx);
 
 extern GLbitfield

bminor/mesa-mesa

commit b0047be0c253d1fdaf8ba015704deb5380ce7fc3

mesa: fix _mesa_update_texture_matrices()
_math_matrix_is_dirty() should only be used to decide if we need to
run _math_matrix_analyse(). We already decided that we had a new
texture matrix when we called _mesa_update_texture_matrices() so
we need to set _TexMatEnabled correctly otherwise we might
incorrectly return _NEW_FF_VERT_PROGRAM | _NEW_FF_FRAG_PROGRAM in
the following if-statement.

Fixes: ec978e002f59 ("mesa: only update fixed-func programs on texture matrix enablement changes")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14286
Reviewed-by: Emma Anholt <emma@anholt.net>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38473>

src/mesa/main/texstate.c
@@ -401,12 +401,12 @@ _mesa_update_texture_matrices(struct gl_context *ctx)
    for (u = 0; u < ctx->Const.MaxTextureCoordUnits; u++) {
       assert(u < ARRAY_SIZE(ctx->TextureMatrixStack));
       if (_math_matrix_is_dirty(ctx->TextureMatrixStack[u].Top)) {
-	 _math_matrix_analyse( ctx->TextureMatrixStack[u].Top );
-
-	 if (ctx->Texture.Unit[u]._Current &&
-	     ctx->TextureMatrixStack[u].Top->type != MATRIX_IDENTITY)
-	    ctx->Texture._TexMatEnabled |= ENABLE_TEXMAT(u);
+         _math_matrix_analyse(ctx->TextureMatrixStack[u].Top);
       }
+
+      if (ctx->Texture.Unit[u]._Current &&
+          ctx->TextureMatrixStack[u].Top->type != MATRIX_IDENTITY)
+         ctx->Texture._TexMatEnabled |= ENABLE_TEXMAT(u);
    }
 
    if (old_texmat_enabled != ctx->Texture._TexMatEnabled)