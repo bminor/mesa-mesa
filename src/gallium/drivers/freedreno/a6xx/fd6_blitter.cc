alexvorxx/Mesa-VirGL

commit 76e350671f3ba2d8ccbe6851de868d8897a8bb98

freedreno/a6xx: Sysmem clear fixes
Handled signed/unsigned channel clamping.

Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/11676
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/30794>

src/gallium/drivers/freedreno/a6xx/fd6_blitter.cc
@@ -850,7 +850,25 @@ FD_GENX(fd6_clear_lrz);
 static union pipe_color_union
 convert_color(enum pipe_format format, union pipe_color_union *pcolor)
 {
+   const struct util_format_description *desc = util_format_description(format);
    union pipe_color_union color = *pcolor;
+
+   for (unsigned i = 0; i < 4; i++) {
+      unsigned channel = desc->swizzle[i];
+
+      if (desc->channel[channel].normalized)
+         continue;
+
+      switch (desc->channel[channel].type) {
+      case UTIL_FORMAT_TYPE_SIGNED:
+         color.i[i] = MAX2(color.i[i], -(1<<(desc->channel[channel].size - 1)));
+         color.i[i] = MIN2(color.i[i], (1 << (desc->channel[channel].size - 1)) - 1);
+         break;
+      case UTIL_FORMAT_TYPE_UNSIGNED:
+         color.ui[i] = MIN2(color.ui[i], BITFIELD_MASK(desc->channel[channel].size));
+         break;
+      }
+   }
 
    /* For solid-fill blits, the hw isn't going to convert from
     * linear to srgb for us:
@@ -864,8 +882,6 @@ convert_color(enum pipe_format format, union pipe_color_union *pcolor)
       for (int i = 0; i < 3; i++)
          color.f[i] = CLAMP(color.f[i], -1.0f, 1.0f);
    }
-
-   /* Note that float_to_ubyte() already clamps, for the unorm case */
 
    return color;
 }

bminor/mesa-mesa

commit f0465ced7f9924d0be8e219ed0afe907994bd516

freedreno/a6xx: Fix UB in convert_color()
Swizzle can include PIPE_SWIZZLE_0/_1 (4 and 5) which result in indexing
beyond the channel array.

Reported-by: Danylo Piliaiev <dpiliaiev@igalia.com>
Fixes: 76e350671f3b ("freedreno/a6xx: Sysmem clear fixes")
Signed-off-by: Rob Clark <rob.clark@oss.qualcomm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38593>

src/gallium/drivers/freedreno/a6xx/fd6_blitter.cc
@@ -867,6 +867,9 @@ convert_color(enum pipe_format format, union pipe_color_union *pcolor)
    for (unsigned i = 0; i < 4; i++) {
       unsigned channel = desc->swizzle[i];
 
+      if (channel >= 4) /* PIPE_SWIZZLE_0/_1 */
+         continue;
+
       if (desc->channel[channel].normalized)
          continue;
 