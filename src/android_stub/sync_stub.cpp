5af6af9644a43ffd736cc348435a0df49e60b89c
Gurchetan Singh <gurchetansingh@google.com>
2025-11-24 15:14:07 -0800

    android_stub: add missing definition
    Fixes:
    
    android_stub/sync_stub.cpp:18:11: error: use of undeclared identifier 'NULL'
       18 |    return NULL;
          |           ^~~~
    
    Reviewed-by: Yonggang Luo <luoyonggang@gmail.com>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38644>

--- src/android_stub/sync_stub.cpp
+++ src/android_stub/sync_stub.cpp
@@ -15,7 +15,7 @@ int sync_merge(const char *name, int fd, int fd2)
 
 struct sync_file_info* sync_file_info(int32_t fd)
 {
-   return NULL;
+   return nullptr;
 }
 
 void sync_file_info_free(struct sync_file_info* info)
