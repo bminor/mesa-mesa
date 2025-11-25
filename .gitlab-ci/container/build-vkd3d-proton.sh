92a468f8f23a990a2f4c5fbdef9de74a169731b1
Samuel Pitoiset <samuel.pitoiset@gmail.com>
2025-11-25 10:20:59 +0100

    ci: uprev vkd3d
    vkd3d-proton had an issue with its runner and few tests were excluded
    by accident.
    
    Signed-off-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38652>

--- .gitlab-ci/container/build-vkd3d-proton.sh
+++ .gitlab-ci/container/build-vkd3d-proton.sh
@@ -11,7 +11,7 @@ section_start vkd3d-proton "Building vkd3d-proton"
 # setting up the environment variables locally
 ci_tag_build_time_check "VKD3D_PROTON_TAG"
 
-VKD3D_PROTON_COMMIT="4acd2271319df0ca06b7c0429d371cc8a0504037"
+VKD3D_PROTON_COMMIT="33a41f9d14460f998c5ce8a4aab42ca1cce8dcc6"
 
 VKD3D_PROTON_DST_DIR="/vkd3d-proton-tests"
 VKD3D_PROTON_SRC_DIR="/vkd3d-proton-src"
