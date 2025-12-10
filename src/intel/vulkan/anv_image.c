bminor/mesa-mesa

commit 01c4ea771cd6c091320c918553fd7de3f2b9940e

anv: Enable storage accesses with modifiers on gfx12+
I tested this patch with an ACM card. It enables "Halo: The Master Chief
Collection" to use the clear color modifier instead falling back to the
uncompressed Tile4 modifier.

Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32192>

src/intel/vulkan/anv_image.c
@@ -539,7 +539,8 @@ anv_formats_ccs_e_compatible(const struct intel_device_info *devinfo,
                                     format, vk_tiling, fmt_list))
          return false;
 
-      if (vk_usage & VK_IMAGE_USAGE_STORAGE_BIT) {
+      if ((vk_usage & VK_IMAGE_USAGE_STORAGE_BIT) &&
+          vk_tiling != VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
          if (devinfo->verx10 < 125)
             return false;
 

src/intel/vulkan/anv_formats.c
@@ -836,12 +836,20 @@ anv_get_image_format_features2(const struct anv_physical_device *physical_device
           */
          flags &= ~VK_FORMAT_FEATURE_2_DISJOINT_BIT;
 
-         /* When the hardware accesses a storage image, it bypasses the aux
-          * surface. We could support storage access on images with aux
-          * modifiers by resolving the aux surface prior to the storage access.
+         /* Gfx11 and prior bypass the aux surface when accessing storage
+          * images. We could support storage access on images with aux
+          * modifiers by resolving the aux surface prior to the storage
+          * access.
           */
-         flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_BIT;
-         flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
+         if (devinfo->ver <= 11)
+            flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_BIT;
+
+         /* Starting with gfx12.5, atomics are supported with compression.
+          * However, the performance of this combination is slow on gfx12.5.
+          * So, only allow atomic support on Xe2+.
+          */
+         if (devinfo->verx10 <= 125)
+            flags &= ~VK_FORMAT_FEATURE_2_STORAGE_IMAGE_ATOMIC_BIT;
 
          /* Host transfer don't touch the AUX data, so if that is required by
           * the modifier, just drop support on the format.

commit fe372f3b1bfaa2908fbfc18ad702fe8cd19490a6

anv: Don't allow STORAGE + CCS for Y_TILED mod
This can happen as a result of us adding on CCS to modifiers which don't
support it on gfx9-11.

Fixes image corruption seen with the following test:

   $ mpv av://lavfi:testsrc --config=no --vo=gpu-next --scale=ewa_lanczossharp --fs

Fixes: 01c4ea771cd ("anv: Enable storage accesses with modifiers on gfx12+")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/12910
Reviewed-by: Lionel Landwerlin <lionel.g.landwerlin@intel.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38855>

src/intel/vulkan/anv_image.c
@@ -505,8 +505,7 @@ anv_formats_ccs_e_compatible(const struct anv_physical_device *physical_device,
          return false;
    }
 
-   if ((vk_usage & VK_IMAGE_USAGE_STORAGE_BIT) &&
-       vk_tiling != VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT) {
+   if (vk_usage & VK_IMAGE_USAGE_STORAGE_BIT) {
       /* Only color */
       assert((vk_format_aspects(vk_format) & ~VK_IMAGE_ASPECT_ANY_COLOR_BIT_ANV) == 0);
       if (devinfo->ver == 12) {
@@ -522,8 +521,13 @@ anv_formats_ccs_e_compatible(const struct anv_physical_device *physical_device,
           * On gfx12.0, compression is not supported with atomic
           * operations. On gfx12.5, the support is there, but it's slow
           * (see HSD 1406337848).
+          *
+          * We only care about the non-modifier case. Modifier capabilities
+          * are exposed via the standard interfaces and unlike prior
+          * platforms, we don't enable compression for uncompressed modifiers.
           */
-         if (image_may_use_r32_view(create_flags, vk_format, fmt_list))
+         if (vk_tiling != VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT &&
+             image_may_use_r32_view(create_flags, vk_format, fmt_list))
             return false;
 
       } else if (devinfo->ver <= 11) {