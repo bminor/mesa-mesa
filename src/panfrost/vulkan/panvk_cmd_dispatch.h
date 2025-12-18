bminor/mesa-mesa

commit ae76a6a04596bfdbd37bab165bc5f2a5ff60d389

panvk: Pack push constants
We're about to add more sysvals, and the more we add, the bigger the
sysvals region gets, which increases the amount of memory we have to
allocate when push_uniforms are dirty.

Instead of allocating FAUs for all sysvals/push_constants, track FAU
usage per-shader, and pack those. This implies emitting an FAU buffer
per stage instead of trying to share it, but that's an acceptable
trade-off.

While at it, automate the sysval dirty tracking a bit.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Lars-Ivar Hesselberg Simonsen <lars-ivar.simonsen@arm.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/32415>

src/panfrost/vulkan/panvk_cmd_dispatch.h
@@ -43,11 +43,24 @@ struct panvk_cmd_compute_state {
       compute_state_clear_all_dirty(__cmdbuf);                                 \
    } while (0)
 
+#define set_compute_sysval(__cmdbuf, __dirty, __name, __val)                   \
+   do {                                                                        \
+      struct panvk_compute_sysvals __new_sysval;                               \
+      __new_sysval.__name = (__val);                                           \
+      if (memcmp(&(__cmdbuf)->state.compute.sysvals.__name,                    \
+                 &__new_sysval.__name, sizeof(__new_sysval.__name))) {         \
+         (__cmdbuf)->state.compute.sysvals.__name = __new_sysval.__name;       \
+         BITSET_SET_RANGE(__dirty, sysval_fau_start(compute, __name),          \
+                          sysval_fau_start(compute, __name));                  \
+      }                                                                        \
+   } while (0)
+
 struct panvk_dispatch_info {
    struct {
-      struct {
-         uint32_t x, y, z;
-      } wg_base;
+      uint32_t x, y, z;
+   } wg_base;
+
+   struct {
       struct {
          uint32_t x, y, z;
       } wg_count;

src/panfrost/vulkan/csf/panvk_vX_cmd_dispatch.c
@@ -64,15 +64,6 @@ prepare_driver_set(struct panvk_cmd_buffer *cmdbuf)
    return VK_SUCCESS;
 }
 
-static VkResult
-prepare_push_uniforms(struct panvk_cmd_buffer *cmdbuf)
-{
-   cmdbuf->state.compute.push_uniforms = panvk_per_arch(
-      cmd_prepare_push_uniforms)(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE);
-   return cmdbuf->state.compute.push_uniforms ? VK_SUCCESS
-                                              : VK_ERROR_OUT_OF_DEVICE_MEMORY;
-}
-
 static void
 calculate_task_axis_and_increment(const struct panvk_shader *shader,
                                   struct panvk_physical_device *phys_dev,
@@ -238,7 +229,8 @@ cmd_dispatch(struct panvk_cmd_buffer *cmdbuf, struct panvk_dispatch_info *info)
    if (result != VK_SUCCESS)
       return;
 
-   result = prepare_push_uniforms(cmdbuf);
+   result = panvk_per_arch(cmd_prepare_push_uniforms)(
+      cmdbuf, cmdbuf->state.compute.shader);
    if (result != VK_SUCCESS)
       return;
 
@@ -268,11 +260,8 @@ cmd_dispatch(struct panvk_cmd_buffer *cmdbuf, struct panvk_dispatch_info *info)
          cs_move64_to(b, cs_sr_reg64(b, 0), cs_desc_state->res_table);
 
       if (compute_state_dirty(cmdbuf, PUSH_UNIFORMS)) {
-         uint32_t push_size =
-            SYSVALS_PUSH_CONST_BASE + sizeof(struct panvk_compute_sysvals);
-         uint64_t fau_count = DIV_ROUND_UP(push_size, 8);
-         mali_ptr fau_ptr =
-            cmdbuf->state.compute.push_uniforms | (fau_count << 56);
+         mali_ptr fau_ptr = cmdbuf->state.compute.push_uniforms |
+                            ((uint64_t)shader->fau.total_count << 56);
          cs_move64_to(b, cs_sr_reg64(b, 8), fau_ptr);
       }
 
@@ -294,11 +283,11 @@ cmd_dispatch(struct panvk_cmd_buffer *cmdbuf, struct panvk_dispatch_info *info)
       }
       cs_move32_to(b, cs_sr_reg32(b, 33), wg_size.opaque[0]);
       cs_move32_to(b, cs_sr_reg32(b, 34),
-                   info->direct.wg_base.x * shader->local_size.x);
+                   info->wg_base.x * shader->local_size.x);
       cs_move32_to(b, cs_sr_reg32(b, 35),
-                   info->direct.wg_base.y * shader->local_size.y);
+                   info->wg_base.y * shader->local_size.y);
       cs_move32_to(b, cs_sr_reg32(b, 36),
-                   info->direct.wg_base.z * shader->local_size.z);
+                   info->wg_base.z * shader->local_size.z);
       if (indirect) {
          /* Load parameters from indirect buffer and update workgroup count
           * registers and sysvals */
@@ -309,10 +298,25 @@ cmd_dispatch(struct panvk_cmd_buffer *cmdbuf, struct panvk_dispatch_info *info)
          cs_move64_to(b, cs_scratch_reg64(b, 0),
                       cmdbuf->state.compute.push_uniforms);
          cs_wait_slot(b, SB_ID(LS), false);
-         cs_store(b, cs_sr_reg_tuple(b, 37, 3), cs_scratch_reg64(b, 0),
-                  BITFIELD_MASK(3),
-                  SYSVALS_PUSH_CONST_BASE +
-                     offsetof(struct panvk_compute_sysvals, num_work_groups));
+
+         if (shader_uses_sysval(shader, compute, num_work_groups.x)) {
+            cs_store32(b, cs_sr_reg32(b, 37), cs_scratch_reg64(b, 0),
+                       shader_remapped_sysval_offset(
+                          shader, sysval_offset(compute, num_work_groups.x)));
+         }
+
+         if (shader_uses_sysval(shader, compute, num_work_groups.y)) {
+            cs_store32(b, cs_sr_reg32(b, 38), cs_scratch_reg64(b, 0),
+                       shader_remapped_sysval_offset(
+                          shader, sysval_offset(compute, num_work_groups.y)));
+         }
+
+         if (shader_uses_sysval(shader, compute, num_work_groups.z)) {
+            cs_store32(b, cs_sr_reg32(b, 39), cs_scratch_reg64(b, 0),
+                       shader_remapped_sysval_offset(
+                          shader, sysval_offset(compute, num_work_groups.z)));
+         }
+
          cs_wait_slot(b, SB_ID(LS), false);
       } else {
          cs_move32_to(b, cs_sr_reg32(b, 37), info->direct.wg_count.x);
@@ -385,10 +389,8 @@ panvk_per_arch(CmdDispatchBase)(VkCommandBuffer commandBuffer,
 {
    VK_FROM_HANDLE(panvk_cmd_buffer, cmdbuf, commandBuffer);
    struct panvk_dispatch_info info = {
-      .direct = {
-         .wg_base = {baseGroupX, baseGroupY, baseGroupZ},
-         .wg_count = {groupCountX, groupCountY, groupCountZ},
-      }
+      .wg_base = {baseGroupX, baseGroupY, baseGroupZ},
+      .direct.wg_count = {groupCountX, groupCountY, groupCountZ},
    };
    cmd_dispatch(cmdbuf, &info);
 }

src/panfrost/vulkan/csf/panvk_vX_cmd_draw.c
@@ -404,7 +404,9 @@ prepare_blend(struct panvk_cmd_buffer *cmdbuf)
 
    panvk_per_arch(blend_emit_descs)(cmdbuf, bds);
 
-   cs_move64_to(b, cs_sr_reg64(b, 50), ptr.gpu | bd_count);
+   cs_update_vt_ctx(b)
+      cs_move64_to(b, cs_sr_reg64(b, 50), ptr.gpu | bd_count);
+
    return VK_SUCCESS;
 }
 
@@ -1200,22 +1202,36 @@ prepare_push_uniforms(struct panvk_cmd_buffer *cmdbuf)
 {
    struct cs_builder *b =
       panvk_get_cs_builder(cmdbuf, PANVK_SUBQUEUE_VERTEX_TILER);
+   const struct panvk_shader *vs = cmdbuf->state.gfx.vs.shader;
+   const struct panvk_shader *fs = get_fs(cmdbuf);
+   VkResult result;
 
-   if (gfx_state_dirty(cmdbuf, PUSH_UNIFORMS)) {
-      cmdbuf->state.gfx.push_uniforms = panvk_per_arch(
-         cmd_prepare_push_uniforms)(cmdbuf, VK_PIPELINE_BIND_POINT_GRAPHICS);
-      if (!cmdbuf->state.gfx.push_uniforms)
-         return VK_ERROR_OUT_OF_DEVICE_MEMORY;
-
-      uint32_t push_size =
-         SYSVALS_PUSH_CONST_BASE + sizeof(struct panvk_graphics_sysvals);
-      uint64_t fau_count = DIV_ROUND_UP(push_size, 8);
-      mali_ptr fau_ptr = cmdbuf->state.gfx.push_uniforms | (fau_count << 56);
+   if (gfx_state_dirty(cmdbuf, VS_PUSH_UNIFORMS)) {
+      result = panvk_per_arch(cmd_prepare_push_uniforms)(cmdbuf, vs);
+      if (result != VK_SUCCESS)
+         return result;
 
       cs_update_vt_ctx(b) {
-         cs_move64_to(b, cs_sr_reg64(b, 8), fau_ptr);
-         cs_move64_to(b, cs_sr_reg64(b, 12), fau_ptr);
+         cs_move64_to(b, cs_sr_reg64(b, 8),
+                      cmdbuf->state.gfx.vs.push_uniforms |
+                         ((uint64_t)vs->fau.total_count << 56));
       }
+   }
+
+   if (fs_user_dirty(cmdbuf) || gfx_state_dirty(cmdbuf, FS_PUSH_UNIFORMS)) {
+      mali_ptr fau_ptr = 0;
+
+      if (fs) {
+         result = panvk_per_arch(cmd_prepare_push_uniforms)(cmdbuf, fs);
+         if (result != VK_SUCCESS)
+            return result;
+
+         fau_ptr = cmdbuf->state.gfx.fs.push_uniforms |
+                   ((uint64_t)fs->fau.total_count << 56);
+      }
+
+      cs_update_vt_ctx(b)
+         cs_move64_to(b, cs_sr_reg64(b, 12), fau_ptr);
    }
 
    return VK_SUCCESS;
@@ -1535,6 +1551,10 @@ prepare_draw(struct panvk_cmd_buffer *cmdbuf, struct panvk_draw_info *draw)
          return result;
    }
 
+   result = prepare_blend(cmdbuf);
+   if (result != VK_SUCCESS)
+      return result;
+
    panvk_per_arch(cmd_prepare_draw_sysvals)(cmdbuf, draw);
 
    result = prepare_push_uniforms(cmdbuf);
@@ -1569,10 +1589,6 @@ prepare_draw(struct panvk_cmd_buffer *cmdbuf, struct panvk_draw_info *draw)
       set_tiler_idvs_flags(b, cmdbuf, draw);
 
       cs_move32_to(b, cs_sr_reg32(b, 48), varying_size);
-
-      result = prepare_blend(cmdbuf);
-      if (result != VK_SUCCESS)
-         return result;
 
       result = prepare_ds(cmdbuf);
       if (result != VK_SUCCESS)
@@ -1789,7 +1805,7 @@ panvk_cmd_draw_indirect(struct panvk_cmd_buffer *cmdbuf,
    assert(draw->indirect.draw_count == 1);
 
    /* Force a new push uniform block to be allocated */
-   gfx_state_set_dirty(cmdbuf, PUSH_UNIFORMS);
+   gfx_state_set_dirty(cmdbuf, VS_PUSH_UNIFORMS);
 
    result = prepare_draw(cmdbuf, draw);
    if (result != VK_SUCCESS)
@@ -1808,17 +1824,27 @@ panvk_cmd_draw_indirect(struct panvk_cmd_buffer *cmdbuf,
    /* Wait for the SR33-37 indirect buffer load. */
    cs_wait_slot(b, SB_ID(LS), false);
 
-   struct cs_index fau_block_addr = cs_scratch_reg64(b, 2);
-   cs_move64_to(b, fau_block_addr, cmdbuf->state.gfx.push_uniforms);
-   cs_store32(b, cs_sr_reg32(b, 36), fau_block_addr,
-              SYSVALS_PUSH_CONST_BASE +
-                 offsetof(struct panvk_graphics_sysvals, vs.first_vertex));
-   cs_store32(b, cs_sr_reg32(b, 37), fau_block_addr,
-              SYSVALS_PUSH_CONST_BASE +
-                 offsetof(struct panvk_graphics_sysvals, vs.base_instance));
+   if (shader_uses_sysval(vs, graphics, vs.first_vertex) ||
+       shader_uses_sysval(vs, graphics, vs.base_instance)) {
+      struct cs_index fau_block_addr = cs_scratch_reg64(b, 2);
+      cs_move64_to(b, fau_block_addr, cmdbuf->state.gfx.vs.push_uniforms);
 
-   /* Wait for the store using SR-37 as src to finish, so we can overwrite it. */
-   cs_wait_slot(b, SB_ID(LS), false);
+      if (shader_uses_sysval(vs, graphics, vs.first_vertex)) {
+         cs_store32(b, cs_sr_reg32(b, 36), fau_block_addr,
+                    shader_remapped_sysval_offset(
+                       vs, sysval_offset(graphics, vs.first_vertex)));
+      }
+
+      if (shader_uses_sysval(vs, graphics, vs.base_instance)) {
+         cs_store32(b, cs_sr_reg32(b, 37), fau_block_addr,
+                    shader_remapped_sysval_offset(
+                       vs, sysval_offset(graphics, vs.base_instance)));
+      }
+
+      /* Wait for the store using SR-37 as src to finish, so we can overwrite
+       * it. */
+      cs_wait_slot(b, SB_ID(LS), false);
+   }
 
    /* NIR expects zero-based instance ID, but even if it did have an intrinsic to
     * load the absolute instance ID, we'd want to keep it zero-based to work around

src/panfrost/vulkan/jm/panvk_vX_cmd_dispatch.c
@@ -44,10 +44,8 @@ panvk_per_arch(CmdDispatchBase)(VkCommandBuffer commandBuffer,
       return;
 
    struct panvk_dispatch_info info = {
-      .direct = {
-         .wg_base = {baseGroupX, baseGroupY, baseGroupZ},
-         .wg_count = {groupCountX, groupCountY, groupCountZ},
-      },
+      .wg_base = {baseGroupX, baseGroupY, baseGroupZ},
+      .direct.wg_count = {groupCountX, groupCountY, groupCountZ},
    };
    struct panvk_device *dev = to_panvk_device(cmdbuf->vk.base.device);
    struct panvk_physical_device *phys_dev =
@@ -80,14 +78,10 @@ panvk_per_arch(CmdDispatchBase)(VkCommandBuffer commandBuffer,
 
    panvk_per_arch(cmd_prepare_dispatch_sysvals)(cmdbuf, &info);
 
-   if (compute_state_dirty(cmdbuf, PUSH_UNIFORMS)) {
-      cmdbuf->state.compute.push_uniforms = panvk_per_arch(
-         cmd_prepare_push_uniforms)(cmdbuf, VK_PIPELINE_BIND_POINT_COMPUTE);
-      if (!cmdbuf->state.compute.push_uniforms)
-         return;
-   }
-
-   mali_ptr push_uniforms = cmdbuf->state.compute.push_uniforms;
+   result = panvk_per_arch(cmd_prepare_push_uniforms)(
+      cmdbuf, cmdbuf->state.compute.shader);
+   if (result != VK_SUCCESS)
+      return;
 
    struct panfrost_ptr copy_desc_job = {0};
 
@@ -130,7 +124,7 @@ panvk_per_arch(CmdDispatchBase)(VkCommandBuffer commandBuffer,
          cs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_IMG];
       cfg.thread_storage = tsd;
       cfg.uniform_buffers = cs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_UBO];
-      cfg.push_uniforms = push_uniforms;
+      cfg.push_uniforms = cmdbuf->state.compute.push_uniforms;
       cfg.textures = cs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_TEXTURE];
       cfg.samplers = cs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_SAMPLER];
    }

src/panfrost/vulkan/jm/panvk_vX_cmd_draw.c
@@ -50,7 +50,6 @@ struct panvk_draw_data {
       mali_ptr rsd;
       mali_ptr varyings;
    } fs;
-   mali_ptr push_uniforms;
    mali_ptr varying_bufs;
    mali_ptr position;
    mali_ptr indices;
@@ -722,7 +721,7 @@ panvk_emit_vertex_dcd(struct panvk_cmd_buffer *cmdbuf,
       cfg.instance_size =
          draw->info.instance.count > 1 ? draw->padded_vertex_count : 1;
       cfg.uniform_buffers = vs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_UBO];
-      cfg.push_uniforms = draw->push_uniforms;
+      cfg.push_uniforms = cmdbuf->state.gfx.vs.push_uniforms;
       cfg.textures = vs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_TEXTURE];
       cfg.samplers = vs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_SAMPLER];
    }
@@ -900,7 +899,7 @@ panvk_emit_tiler_dcd(struct panvk_cmd_buffer *cmdbuf,
       cfg.instance_size =
          draw->info.instance.count > 1 ? draw->padded_vertex_count : 1;
       cfg.uniform_buffers = fs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_UBO];
-      cfg.push_uniforms = draw->push_uniforms;
+      cfg.push_uniforms = cmdbuf->state.gfx.fs.push_uniforms;
       cfg.textures = fs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_TEXTURE];
       cfg.samplers = fs_desc_state->tables[PANVK_BIFROST_DESC_TABLE_SAMPLER];
 
@@ -1261,12 +1260,18 @@ panvk_cmd_draw(struct panvk_cmd_buffer *cmdbuf, struct panvk_draw_data *draw)
 
       panvk_per_arch(cmd_prepare_draw_sysvals)(cmdbuf, &draw->info);
 
-      cmdbuf->state.gfx.push_uniforms = panvk_per_arch(
-         cmd_prepare_push_uniforms)(cmdbuf, VK_PIPELINE_BIND_POINT_GRAPHICS);
-      if (!cmdbuf->state.gfx.push_uniforms)
+      result = panvk_per_arch(cmd_prepare_push_uniforms)(
+         cmdbuf, cmdbuf->state.gfx.vs.shader);
+      if (result != VK_SUCCESS)
          return;
 
-      draw->push_uniforms = cmdbuf->state.gfx.push_uniforms;
+      if (fs) {
+         result = panvk_per_arch(cmd_prepare_push_uniforms)(
+            cmdbuf, cmdbuf->state.gfx.fs.shader);
+         if (result != VK_SUCCESS)
+            return;
+      }
+
       result = panvk_draw_prepare_tiler_context(cmdbuf, draw);
       if (result != VK_SUCCESS)
          return;

src/panfrost/vulkan/panvk_cmd_draw.h
@@ -87,7 +87,8 @@ enum panvk_cmd_graphics_dirty_state {
    PANVK_CMD_GRAPHICS_DIRTY_OQ,
    PANVK_CMD_GRAPHICS_DIRTY_DESC_STATE,
    PANVK_CMD_GRAPHICS_DIRTY_RENDER_STATE,
-   PANVK_CMD_GRAPHICS_DIRTY_PUSH_UNIFORMS,
+   PANVK_CMD_GRAPHICS_DIRTY_VS_PUSH_UNIFORMS,
+   PANVK_CMD_GRAPHICS_DIRTY_FS_PUSH_UNIFORMS,
    PANVK_CMD_GRAPHICS_DIRTY_STATE_COUNT,
 };
 
@@ -109,6 +110,7 @@ struct panvk_cmd_graphics_state {
    struct {
       const struct panvk_shader *shader;
       struct panvk_shader_desc_state desc;
+      mali_ptr push_uniforms;
       bool required;
 #if PAN_ARCH <= 7
       mali_ptr rsd;
@@ -118,6 +120,7 @@ struct panvk_cmd_graphics_state {
    struct {
       const struct panvk_shader *shader;
       struct panvk_shader_desc_state desc;
+      mali_ptr push_uniforms;
 #if PAN_ARCH <= 7
       mali_ptr attribs;
       mali_ptr attrib_bufs;
@@ -141,8 +144,6 @@ struct panvk_cmd_graphics_state {
    } cb;
 
    struct panvk_rendering_state render;
-
-   mali_ptr push_uniforms;
 
 #if PAN_ARCH <= 7
    mali_ptr vpd;
@@ -170,6 +171,18 @@ struct panvk_cmd_graphics_state {
 
 #define gfx_state_set_all_dirty(__cmdbuf)                                      \
    BITSET_ONES((__cmdbuf)->state.gfx.dirty)
+
+#define set_gfx_sysval(__cmdbuf, __dirty, __name, __val)                       \
+   do {                                                                        \
+      struct panvk_graphics_sysvals __new_sysval;                              \
+      __new_sysval.__name = __val;                                             \
+      if (memcmp(&(__cmdbuf)->state.gfx.sysvals.__name, &__new_sysval.__name,  \
+                 sizeof(__new_sysval.__name))) {                               \
+         (__cmdbuf)->state.gfx.sysvals.__name = __new_sysval.__name;           \
+         BITSET_SET_RANGE(__dirty, sysval_fau_start(graphics, __name),         \
+                          sysval_fau_end(graphics, __name));                   \
+      }                                                                        \
+   } while (0)
 
 static inline uint32_t
 panvk_select_tiler_hierarchy_mask(const struct panvk_physical_device *phys_dev,
@@ -278,11 +291,15 @@ cached_fs_required(ASSERTED const struct panvk_cmd_graphics_state *state,
    do {                                                                        \
       bool __set_fs_dirty =                                                    \
          (__cmdbuf)->state.gfx.fs.shader != get_fs(__cmdbuf);                  \
+      bool __set_fs_push_dirty =                                               \
+         __set_fs_dirty && gfx_state_dirty(__cmdbuf, FS_PUSH_UNIFORMS);        \
       vk_dynamic_graphics_state_clear_dirty(                                   \
          &(__cmdbuf)->vk.dynamic_graphics_state);                              \
       gfx_state_clear_all_dirty(__cmdbuf);                                     \
       if (__set_fs_dirty)                                                      \
          gfx_state_set_dirty(__cmdbuf, FS);                                    \
+      if (__set_fs_push_dirty)                                                 \
+         gfx_state_set_dirty(__cmdbuf, FS_PUSH_UNIFORMS);                      \
    } while (0)
 
 void

src/panfrost/vulkan/panvk_cmd_push_constant.h
@@ -11,15 +11,16 @@
 #include "genxml/gen_macros.h"
 
 struct panvk_cmd_buffer;
+struct panvk_shader;
 
 #define MAX_PUSH_CONSTANTS_SIZE 128
 
 struct panvk_push_constant_state {
-   uint8_t data[MAX_PUSH_CONSTANTS_SIZE];
+   uint64_t data[MAX_PUSH_CONSTANTS_SIZE / sizeof(uint64_t)];
 };
 
-mali_ptr
+VkResult
 panvk_per_arch(cmd_prepare_push_uniforms)(struct panvk_cmd_buffer *cmdbuf,
-                                          VkPipelineBindPoint ptype);
+                                          const struct panvk_shader *shader);
 
 #endif

src/panfrost/vulkan/panvk_shader.h
@@ -53,14 +53,14 @@ enum panvk_desc_table_id {
 
 struct panvk_graphics_sysvals {
    struct {
+      float constants[4];
+   } blend;
+
+   struct {
       struct {
          float x, y, z;
       } scale, offset;
    } viewport;
-
-   struct {
-      float constants[4];
-   } blend;
 
    struct {
 #if PAN_ARCH <= 7
@@ -105,32 +105,94 @@ struct panvk_compute_sysvals {
 #endif
 };
 
+/* This is not the final offset in the push constant buffer (AKA FAU), but
+ * just a magic offset we use before packing push constants so we can easily
+ * identify the type of push constant (driver sysvals vs user push constants).
+ */
 #define SYSVALS_PUSH_CONST_BASE MAX_PUSH_CONSTANTS_SIZE
+#define FAU_WORD_SIZE           sizeof(uint64_t)
+
+static_assert((sizeof(struct panvk_compute_sysvals) % FAU_WORD_SIZE) == 0,
+              "struct panvk_compute_sysvals must be 8-byte aligned");
+static_assert((sizeof(struct panvk_graphics_sysvals) % FAU_WORD_SIZE) == 0,
+              "struct panvk_graphics_sysvals must be 8-byte aligned");
+
+#define sysval_size(__ptype, __name)                                           \
+   sizeof(((struct panvk_##__ptype##_sysvals *)NULL)->__name)
+
+#define sysval_offset(__ptype, __name)                                         \
+   offsetof(struct panvk_##__ptype##_sysvals, __name)
+
+#define sysval_entry_size(__ptype, __name)                                     \
+   sizeof(((struct panvk_##__ptype##_sysvals *)NULL)->__name[0])
+
+#define sysval_entry_offset(__ptype, __name, __idx)                            \
+   (sysval_offset(__ptype, __name) +                                           \
+    (sysval_entry_size(__ptype, __name) * __idx))
+
+#define sysval_fau_start(__ptype, __name)                                      \
+   (sysval_offset(__ptype, __name) / FAU_WORD_SIZE)
+
+#define sysval_fau_end(__ptype, __name)                                        \
+   ((sysval_offset(__ptype, __name) + sysval_size(__ptype, __name) - 1) /      \
+    FAU_WORD_SIZE)
+
+#define sysval_fau_entry_start(__ptype, __name, __idx)                         \
+   (sysval_entry_offset(__ptype, __name, __idx) / FAU_WORD_SIZE)
+
+#define sysval_fau_entry_end(__ptype, __name, __idx)                           \
+   ((sysval_entry_offset(__ptype, __name, __idx + 1) - 1) / FAU_WORD_SIZE)
+
+#define shader_remapped_fau_offset(__shader, __kind, __offset)                 \
+   ((FAU_WORD_SIZE * BITSET_PREFIX_SUM((__shader)->fau.used_##__kind,          \
+                                       (__offset) / FAU_WORD_SIZE)) +          \
+    ((__offset) % FAU_WORD_SIZE))
+
+#define shader_remapped_sysval_offset(__shader, __offset)                      \
+   shader_remapped_fau_offset(__shader, sysvals, __offset)
+
+#define shader_remapped_push_const_offset(__shader, __offset)                  \
+   (((__shader)->fau.sysval_count * FAU_WORD_SIZE) +                     \
+    shader_remapped_fau_offset(__shader, push_consts, __offset))
+
+#define shader_use_sysval(__shader, __ptype, __name)                           \
+   BITSET_SET_RANGE((__shader)->fau.used_sysvals,                              \
+                    sysval_fau_start(__ptype, __name),                         \
+                    sysval_fau_end(__ptype, __name))
+
+#define shader_uses_sysval(__shader, __ptype, __name)                          \
+   BITSET_TEST_RANGE((__shader)->fau.used_sysvals,                             \
+                     sysval_fau_start(__ptype, __name),                        \
+                     sysval_fau_end(__ptype, __name))
+
+#define shader_uses_sysval_entry(__shader, __ptype, __name, __idx)             \
+   BITSET_TEST_RANGE((__shader)->fau.used_sysvals,                             \
+                     sysval_fau_entry_start(__ptype, __name, __idx),           \
+                     sysval_fau_entry_end(__ptype, __name, __idx))
+
+#define shader_use_sysval_range(__shader, __base, __range)                     \
+   BITSET_SET_RANGE((__shader)->fau.used_sysvals, (__base) / FAU_WORD_SIZE,    \
+                    ((__base) + (__range) - 1) / FAU_WORD_SIZE)
+
+#define shader_use_push_const_range(__shader, __base, __range)                 \
+   BITSET_SET_RANGE((__shader)->fau.used_push_consts,                          \
+                    (__base) / FAU_WORD_SIZE,                                  \
+                    ((__base) + (__range) - 1) / FAU_WORD_SIZE)
 
 #define load_sysval(__b, __ptype, __bitsz, __name)                             \
    nir_load_push_constant(                                                     \
-      __b,                                                                     \
-      sizeof(((struct panvk_##__ptype##_sysvals *)NULL)->__name) /             \
-         ((__bitsz) / 8),                                                      \
-      __bitsz,                                                                 \
-      nir_imm_int(__b, offsetof(struct panvk_##__ptype##_sysvals, __name)),    \
-      .base = SYSVALS_PUSH_CONST_BASE,                                         \
-      .range = sizeof(struct panvk_##__ptype##_sysvals))
+      __b, sysval_size(__ptype, __name) / ((__bitsz) / 8), __bitsz,            \
+      nir_imm_int(__b, sysval_offset(__ptype, __name)),                        \
+      .base = SYSVALS_PUSH_CONST_BASE)
 
 #define load_sysval_entry(__b, __ptype, __bitsz, __name, __dyn_idx)            \
    nir_load_push_constant(                                                     \
-      __b,                                                                     \
-      sizeof(((struct panvk_##__ptype##_sysvals *)NULL)->__name[0]) /          \
-         ((__bitsz) / 8),                                                      \
-      __bitsz,                                                                 \
+      __b, sysval_entry_size(__ptype, __name) / ((__bitsz) / 8), __bitsz,      \
       nir_iadd_imm(                                                            \
          __b,                                                                  \
-         nir_imul_imm(                                                         \
-            __b, __dyn_idx,                                                    \
-            sizeof(((struct panvk_##__ptype##_sysvals *)NULL)->__name[0])),    \
-         offsetof(struct panvk_##__ptype##_sysvals, __name)),                  \
-      .base = SYSVALS_PUSH_CONST_BASE,                                         \
-      .range = sizeof(struct panvk_##__ptype##_sysvals))
+         nir_imul_imm(__b, __dyn_idx, sysval_entry_size(__ptype, __name)),     \
+         sysval_offset(__ptype, __name)),                                      \
+      .base = SYSVALS_PUSH_CONST_BASE)
 
 #if PAN_ARCH <= 7
 enum panvk_bifrost_desc_table_type {
@@ -154,6 +216,20 @@ enum panvk_bifrost_desc_table_type {
 #define COPY_DESC_HANDLE(table, idx)           ((table << 28) | (idx))
 #define COPY_DESC_HANDLE_EXTRACT_INDEX(handle) ((handle) & BITFIELD_MASK(28))
 #define COPY_DESC_HANDLE_EXTRACT_TABLE(handle) ((handle) >> 28)
+
+#define MAX_COMPUTE_SYSVAL_FAUS                                                \
+   (sizeof(struct panvk_compute_sysvals) / FAU_WORD_SIZE)
+#define MAX_GFX_SYSVAL_FAUS                                                    \
+   (sizeof(struct panvk_graphics_sysvals) / FAU_WORD_SIZE)
+#define MAX_SYSVAL_FAUS     MAX2(MAX_COMPUTE_SYSVAL_FAUS, MAX_GFX_SYSVAL_FAUS)
+#define MAX_PUSH_CONST_FAUS (MAX_PUSH_CONSTANTS_SIZE / FAU_WORD_SIZE)
+
+struct panvk_shader_fau_info {
+   BITSET_DECLARE(used_sysvals, MAX_SYSVAL_FAUS);
+   BITSET_DECLARE(used_push_consts, MAX_PUSH_CONST_FAUS);
+   uint32_t sysval_count;
+   uint32_t total_count;
+};
 
 struct panvk_shader {
    struct vk_shader vk;
@@ -183,6 +259,8 @@ struct panvk_shader {
       } dyn_bufs;
 #endif
    } desc_info;
+
+   struct panvk_shader_fau_info fau;
 
    const void *bin_ptr;
    uint32_t bin_size;

commit d7d690b47fb2a3d9e511b094244c9c4e5b3c104f

panvk: Fix set_compute_sysval()
BITSET_SET_RANGE() was passed sysval_fau_start() instead of
sysval_fau_end() as a 3rd argument.

Fixes: ae76a6a04596 ("panvk: Pack push constants")
Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/14489
Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38980>

src/panfrost/vulkan/panvk_cmd_dispatch.h
@@ -53,7 +53,7 @@ struct panvk_cmd_compute_state {
                  &__new_sysval.__name, sizeof(__new_sysval.__name))) {         \
          (__cmdbuf)->state.compute.sysvals.__name = __new_sysval.__name;       \
          BITSET_SET_RANGE(__dirty, sysval_fau_start(compute, __name),          \
-                          sysval_fau_start(compute, __name));                  \
+                          sysval_fau_end(compute, __name));                    \
       }                                                                        \
    } while (0)
 