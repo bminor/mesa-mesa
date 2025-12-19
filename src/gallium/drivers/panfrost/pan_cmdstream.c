Grima04/mesa-turnip-kgsl

commit 0efe7a6eb909b47fb24a7e15edcf4ee0d01cfd95

panfrost: implement image_size sysval
Since there's no hardware support for it, we use a sysval to implement
nir_intrinsic_image_size.

Signed-off-by: Italo Nicola <italonicola@collabora.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Reviewed-by: Alyssa Rosenzweig <alyssa.rosenzweig@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/8066>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -789,6 +789,41 @@ static void panfrost_upload_txs_sysval(struct panfrost_batch *batch,
                 uniform->i[dim] = tex->texture->array_size;
 }
 
+static void panfrost_upload_image_size_sysval(struct panfrost_batch *batch,
+                                              enum pipe_shader_type st,
+                                              unsigned int sysvalid,
+                                              struct sysval_uniform *uniform)
+{
+        struct panfrost_context *ctx = batch->ctx;
+        unsigned idx = PAN_SYSVAL_ID_TO_TXS_TEX_IDX(sysvalid);
+        unsigned dim = PAN_SYSVAL_ID_TO_TXS_DIM(sysvalid);
+        unsigned is_array = PAN_SYSVAL_ID_TO_TXS_IS_ARRAY(sysvalid);
+
+        assert(dim && dim < 4);
+
+        struct pipe_image_view *image = &ctx->images[st][idx];
+
+        if (image->resource->target == PIPE_BUFFER) {
+                unsigned blocksize = util_format_get_blocksize(image->format);
+                uniform->i[0] = image->resource->width0 / blocksize;
+                return;
+        }
+
+        uniform->i[0] = u_minify(image->resource->width0,
+                                 image->u.tex.level);
+
+        if (dim > 1)
+                uniform->i[1] = u_minify(image->resource->height0,
+                                         image->u.tex.level);
+
+        if (dim > 2)
+                uniform->i[2] = u_minify(image->resource->depth0,
+                                         image->u.tex.level);
+
+        if (is_array)
+                uniform->i[dim] = image->resource->array_size;
+}
+
 static void
 panfrost_upload_ssbo_sysval(struct panfrost_batch *batch,
                             enum pipe_shader_type st,
@@ -910,6 +945,11 @@ panfrost_upload_sysvals(struct panfrost_batch *batch, void *buf,
                         panfrost_upload_sampler_sysval(batch, st,
                                                        PAN_SYSVAL_ID(sysval),
                                                        &uniforms[i]);
+                        break;
+                case PAN_SYSVAL_IMAGE_SIZE:
+                        panfrost_upload_image_size_sysval(batch, st,
+                                                          PAN_SYSVAL_ID(sysval),
+                                                          &uniforms[i]);
                         break;
                 default:
                         assert(0);

src/panfrost/util/pan_ir.h
@@ -52,6 +52,7 @@ enum {
         PAN_SYSVAL_SAMPLER = 7,
         PAN_SYSVAL_LOCAL_GROUP_SIZE = 8,
         PAN_SYSVAL_WORK_DIM = 9,
+        PAN_SYSVAL_IMAGE_SIZE = 10,
 };
 
 #define PAN_TXS_SYSVAL_ID(texidx, dim, is_array)          \

bminor/mesa-mesa

commit 95ed8d6638d6a6fedf51a346bb9a6ba6a7a512c9

panfrost: fix get_image_width for 1D buffer images
Image size queries for buffer images were incorrectly using the
underlying buffer's width instead of the image view's size.

This affected `get_image_width` in OpenCL C for 1Dbuffer images, in
cases where the buffer is larger than the image to account for
padding, breaking the conformance test `test_kernel_image_methods
1Dbuffer`.

Fixes: 0efe7a6eb90 ("panfrost: implement image_size sysval")

Signed-off-by: Ahmed Hesham <ahmed.hesham@arm.com>
Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/38949>

src/gallium/drivers/panfrost/pan_cmdstream.c
@@ -1135,7 +1135,7 @@ panfrost_upload_image_size_sysval(struct panfrost_batch *batch,
 
    if (image->resource->target == PIPE_BUFFER) {
       unsigned blocksize = util_format_get_blocksize(image->format);
-      uniform->i[0] = image->resource->width0 / blocksize;
+      uniform->i[0] = image->u.buf.size / blocksize;
       return;
    }
 