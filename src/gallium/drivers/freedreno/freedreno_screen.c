bminor/mesa-mesa

commit 203ac73374a734ecd0157a21cd750fef7a589a67

gallium: set prefer_real_buffer_in_constbuf0 for all drivers using tc
this isn't really a functional change since tc was always doing
cbuf uploads anyway

Reviewed-by: Marek Olšák <marek.olsak@amd.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/36715>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -365,6 +365,7 @@ fd_init_screen_caps(struct fd_screen *screen)
    /* this is probably not totally correct.. but it's a start: */
 
    /* Supported features (boolean caps). */
+   caps->prefer_real_buffer_in_constbuf0 = true;
    caps->npot_textures = true;
    caps->mixed_framebuffer_sizes = true;
    caps->anisotropic_filter = true;

src/gallium/auxiliary/util/u_threaded_context.c
@@ -5452,6 +5452,9 @@ threaded_context_create(struct pipe_context *pipe,
       return NULL;
    }
 
+   /* drivers must set this */
+   assert(pipe->screen->caps.prefer_real_buffer_in_constbuf0);
+
    if (options) {
       /* this is unimplementable */
       assert(!(options->parse_renderpass_info && options->driver_calls_flush_notify));

src/gallium/drivers/crocus/crocus_screen.c
@@ -235,6 +235,7 @@ crocus_init_screen_caps(struct crocus_screen *screen)
 
    u_init_pipe_screen_caps(&screen->base, 1);
 
+   caps->prefer_real_buffer_in_constbuf0 = true;
    caps->npot_textures = true;
    caps->anisotropic_filter = true;
    caps->occlusion_query = true;

src/gallium/drivers/d3d12/d3d12_screen.cpp
@@ -232,6 +232,7 @@ d3d12_init_screen_caps(struct d3d12_screen *screen)
 
    u_init_pipe_screen_caps(&screen->base, caps->accelerated);
 
+   caps->prefer_real_buffer_in_constbuf0 = true;
    caps->npot_textures = true;
 
    /* D3D12 only supports dual-source blending for a single

src/gallium/drivers/iris/iris_screen.c
@@ -285,6 +285,7 @@ iris_init_screen_caps(struct iris_screen *screen)
 
    const struct intel_device_info *devinfo = screen->devinfo;
 
+   caps->prefer_real_buffer_in_constbuf0 = true;
    caps->npot_textures = true;
    caps->anisotropic_filter = true;
    caps->occlusion_query = true;

src/gallium/drivers/r600/r600_pipe.c
@@ -404,6 +404,7 @@ static void r600_init_screen_caps(struct r600_screen *rscreen)
 	enum radeon_family family = rscreen->b.family;
 
 	/* Supported features (boolean caps). */
+	caps->prefer_real_buffer_in_constbuf0 = true;
 	caps->npot_textures = true;
 	caps->mixed_framebuffer_sizes = true;
 	caps->mixed_color_depth_bits = true;

commit 974ca3264367833cf62273018484f9b9ea0034ca

freedreno/a3xx-a5xx: restore cbuf0 direct upload.
This broke a number of dEQP tests, for reasons that are unclear -- in
looking at cffdumps, the const setup looks appropriate, just changing from
e.g:

VS_CTRL_REG1(CONSTLEN=2)
FS_CTRL_REG1(CONSTLEN=1)
LOAD_STATE6(VERT, DIRECT, DST_OFF=2, NUM_UNIT=2)
LOAD_STATE6(FRAG, DIRECT, DST_OFF=0, NUM_UNIT=2)

to:

VS_CTRL_REG1(CONSTLEN=2)
FS_CTRL_REG1(CONSTLEN=1)
LOAD_STATE6(VERT, DIRECT, DST_OFF=2, NUM_UNIT=2)
LOAD_STATE6(FRAG, INDIRECT, DST_OFF=0, NUM_UNIT=2)

Fixes: 203ac73374a7 ("gallium: set prefer_real_buffer_in_constbuf0 for all drivers using tc")
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/39070>

src/gallium/drivers/freedreno/freedreno_screen.c
@@ -365,7 +365,12 @@ fd_init_screen_caps(struct fd_screen *screen)
    /* this is probably not totally correct.. but it's a start: */
 
    /* Supported features (boolean caps). */
-   caps->prefer_real_buffer_in_constbuf0 = true;
+   if (is_a6xx(screen)) {
+      /* Direct upload is slightly faster without TC, and indirect causes
+       * regression on at least a3xx.
+       */
+      caps->prefer_real_buffer_in_constbuf0 = true;
+   }
    caps->npot_textures = true;
    caps->mixed_framebuffer_sizes = true;
    caps->anisotropic_filter = true;